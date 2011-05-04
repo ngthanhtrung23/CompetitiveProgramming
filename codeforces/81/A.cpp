0bb4d8ef32b6de938f32eb153b3f7717429bd76d
Trung Nguyen <ngthanhtrung23@gmail.com>
2011-05-04 13:08:00 +0800

    Add solution for problem `A - Plug-in` Link: https://codeforces.com/contest/81/problem/A Tags: implementation, *1400 Ref: https://codeforces.com/contest/81/submission/423743

--- codeforces/81/A.cpp
+++ codeforces/81/A.cpp
@@ -1,29 +1,52 @@
-#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:16777216")
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+#include <sstream>
+#include <iomanip>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 using namespace std;
 
-char tmp[200111], st[200111];
+const double PI = acos(-1.0);
+
+char s[200111], st[200111];
+int top;
 
 int main() {
-    while (gets(tmp)) {
-        int top = 0;
-        REP(i,strlen(tmp)) {
-            if (top && st[top] == tmp[i]) --top;
-            else st[++top] = tmp[i];
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    gets(s);
+    top = 0;
+    int n = strlen(s);
+    int i = 0;
+    while (i < n) {
+        if (top && s[i] == st[top]) {
+            top--;
         }
-        FOR(i,1,top) putchar(st[i]);
-        puts("");
+        else {
+            st[++top] = s[i];
+        }
+        i++;
     }
+    FOR(i,1,top) cout << st[i];
     return 0;
 }
-
