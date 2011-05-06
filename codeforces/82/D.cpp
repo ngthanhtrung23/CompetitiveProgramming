de185f4c5bc71f1cd3bb4c61292bf9532f91370e
Trung Nguyen <ngthanhtrung23@gmail.com>
2011-05-07 00:13:00 +0800

    Add solution for problem `D - Two out of Three` Link: https://codeforces.com/contest/82/problem/D Tags: dp, *2000 Ref: https://codeforces.com/contest/82/submission/438456

--- codeforces/82/D.cpp
+++ codeforces/82/D.cpp
@@ -1,89 +1,102 @@
-
-#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:16777216")
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+#include <sstream>
+#include <iomanip>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 using namespace std;
 
-const int MN = 1011;
-int n, a[MN], f[MN][MN], tr[MN][MN];
+const double PI = acos(-1.0);
 
-void update(int i, int rem, int val, int prev) {
-    if (f[i][rem] > val) {
-        f[i][rem] = val;
-        tr[i][rem] = prev;
+int f[1011][1011], a[1011], n, tr[1011][1011], saven;
+
+void u(int ii, int jj, int i, int j, int a, int b) {
+    int now = f[i][j] + max(a, b);
+    if (f[ii][jj] == -1 || f[ii][jj] > now) {
+        f[ii][jj] = now;
+        tr[ii][jj] = j;
     }
 }
 
-void trace(int n, int rem) {
-    if (n == 1) return ;
-
-    int prev = tr[n][rem];
-    int x = n, y = n-1, z = prev;
-
-    trace(n-2, prev);
-
-    if (rem == x) cout << y << ' ' << z << endl;
-    else if (rem == y) cout << x << ' ' << z << endl;
-    else if (rem == z) cout << x << ' ' << y << endl;
+void trace(int N, int u) {
+    if (N == 3) {
+        if (u == 1) cout << "2 3\n";
+        else if (u == 2) cout << "1 3\n";
+        else if (u == 3) cout << "1 2\n";
+        return ;
+    }
+    trace(N-2, tr[N][u]);
+    if (u == tr[N][u]) cout << N-1 << ' ' << N << endl;
+    else if (u == N-1) cout << tr[N][u] << ' ' << N << endl;
+    else cout << tr[N][u] << ' ' << N-1 << endl;
 }
 
 int main() {
-    ios :: sync_with_stdio(false);
-    while (cin >> n) {
-        FOR(i,1,n) cin >> a[i];
-        if (n == 1) {
-            cout << a[1] << endl;
-            cout << 1 << endl;
-            continue;
-        }
-        else if (n == 2) {
-            cout << max(a[1], a[2]) << endl;
-            cout << 1 << ' ' << 2 << endl;
-            continue;
-        }
-
-        FOR(i,1,n) FOR(j,1,i) f[i][j] = 1000111000;
-
-        f[3][1] = max(a[2], a[3]); tr[3][1] = 1;
-        f[3][2] = max(a[1], a[3]); tr[3][2] = 1;
-        f[3][3] = max(a[1], a[2]); tr[3][3] = 1;
-        for(int i = 3; i < n; i += 2) FOR(rem,1,i) {
-            int x = rem, y = i+1, z = i+2;
-            update(i+2, x, f[i][rem] + max(a[y], a[z]), rem);
-            update(i+2, y, f[i][rem] + max(a[z], a[x]), rem);
-            update(i+2, z, f[i][rem] + max(a[x], a[y]), rem);
-        }
-        if (n % 2 == 0) {
-            int best = 1000111000;
-            FOR(rem,1,n-1) best = min(best, f[n-1][rem] + max(a[rem], a[n]));
-
-            cout << best << endl;
-            FOR(rem,1,n) if (f[n-1][rem] + max(a[rem], a[n]) == best) {
-                trace(n-1, rem);
-                cout << rem << ' ' << n << endl;
-                break;
-            }
-        }
-        else {
-            int best = 1000111000;
-            FOR(rem,1,n) best = min(best, f[n][rem] + a[rem]);
-            cout << best << endl;
-            FOR(rem,1,n) if (f[n][rem] + a[rem] == best) {
-                trace(n, rem);
-                cout << rem << endl;
-                break;
-            }
-        }
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    scanf("%d", &n); saven = n;
+    FOR(i,1,n) scanf("%d", &a[i]);
+    if (n == 1) {
+        cout << a[1] << endl;
+        cout << 1 << endl;
+        return 0;
     }
+    if (n == 2) {
+        cout << max(a[1], a[2]) << endl;
+        cout << "1 2\n";
+        return 0;
+    }
+    
+    if (n % 2 == 1) {
+        n++;
+        a[n] = 0;
+    }
+    
+    memset(f, -1, sizeof f);
+    f[3][3] = max(a[1], a[2]);
+    f[3][2] = max(a[1], a[3]);
+    f[3][1] = max(a[2], a[3]);
+    
+    for(int i = 3; i < n-1; i += 2)
+    FOR(j,1,n)
+    if (f[i][j] >= 0) {
+        u(i+2,j, i, j, a[i+1], a[i+2]);
+        u(i+2,i+1, i, j, a[j], a[i+2]);
+        u(i+2,i+2, i, j, a[j], a[i+1]);
+    }
+    
+    int res = 1000111000;
+    FOR(i,1,n)
+    if (f[n-1][i] >= 0) {
+        res = min(res, f[n-1][i] + max(a[i], a[n]));
+    }
+    cout << res << endl;
+    FOR(i,1,n)
+        if (res == f[n-1][i] + max(a[i], a[n])) {
+            trace(n-1, i);
+            if (n == saven) cout << i << ' ' << n << endl;
+            else cout << i << endl;
+            return 0;
+        }
     return 0;
 }
-
