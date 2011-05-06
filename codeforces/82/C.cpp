8993ee183c4484b892767a27099d117c6b902ef2
Trung Nguyen <ngthanhtrung23@gmail.com>
2011-05-07 00:51:00 +0800

    Add solution for problem `C - General Mobilization` Link: https://codeforces.com/contest/82/problem/C Tags: data structures, dfs and similar, sortings, *2000 Ref: https://codeforces.com/contest/82/submission/440168

--- codeforces/82/C.cpp
+++ codeforces/82/C.cpp
@@ -1,73 +1,97 @@
-
-#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:16777216")
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+#include <sstream>
+#include <iomanip>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 using namespace std;
 
-const int MN = 5011;
+const double PI = acos(-1.0);
 
-int n;
-pair<int,int> a[MN];
-vector< pair<int,int> > ke[MN];
-pair<int,int> father[MN];
+int n, a[5011], fat[5011], up[5011], space[5011];
+vector< pair<int,int> > x[2];
+vector< pair<int,int> > ke[5011];
+map<int,int> m;
 
 void dfs(int u, int fu) {
-    for(auto e : ke[u]) {
-        if (e.first == fu) continue;
-
-        father[e.first] = make_pair(u, e.second);
-        dfs(e.first, u);
+    REP(i,ke[u].size()) {
+        int v = ke[u][i].F, c = ke[u][i].S;
+        if (v == fu) continue;
+        
+        fat[v] = u;
+        up[v] = c;
+        dfs(v, u);
     }
 }
 
-int res[MN], pos[MN], used[MN];
+int t[5011];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    while (cin >> n) {
-        FOR(i,1,n) {
-            cin >> a[i].first;
-            a[i].second = i;
-        }
-        sort(a+1, a+n+1);
-        FOR(i,1,n) ke[i].clear();
-
-        FOR(i,2,n) {
-            int u, v, c; cin >> u >> v >> c;
-            ke[u].push_back(make_pair(v, c));
-            ke[v].push_back(make_pair(u, c));
-        }
-        dfs(1, -1);
-
-        FOR(i,1,n) pos[i] = i;
-
-        memset(res, 0, sizeof res);
-        FOR(turn,1,n) {
-            memset(used, 0, sizeof used);
-
-            FOR(i,1,n) {
-                int u = a[i].second;
-                if (pos[u] != 1) {
-                    res[u] = turn;
-
-                    if (used[pos[u]] == father[pos[u]].second) continue;
-
-                    used[pos[u]]++;
-                    pos[u] = father[pos[u]].first;
-                }
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+    FOR(i,1,n) {
+        scanf("%d", &a[i]);
+        m[a[i]] = i;
+    }
+    FOR(i,2,n) {
+        int u, v, c;
+        scanf("%d %d %d", &u, &v, &c);
+        ke[u].PB(MP(v,c));
+        ke[v].PB(MP(u,c));
+    }
+    dfs(1, -1);
+    
+    t[1] = 0;
+    FOR(i,2,n) x[0].PB(MP(a[i], i));
+    int now = 0, time = 0;
+    
+    while (x[now].size()) {
+        time++;
+//        if (time % 100 == 0) cerr << time << " " << x[now].size() << endl;
+        sort(x[now].begin(), x[now].end());
+        
+//        REP(i, x[now].size()) cout << x[now][i].F << ' ' << x[now][i].S << "    "; cout << endl;
+        
+        x[1-now].clear();
+        
+        FOR(i,1,n) space[i] = up[i];
+        
+        REP(i,x[now].size()) {
+            int ai = x[now][i].F, pos = x[now][i].S;
+//            cout << ai << ' ' << pos << ' ' << space[pos] << endl;
+            if (space[pos]) {
+                space[pos]--;
+                if (fat[pos] == 1) t[m[ai]] = time;
+                else x[1-now].PB(MP(ai, fat[pos]));
+            }
+            else {
+                x[1-now].PB(MP(ai, pos));
             }
         }
-        FOR(i,1,n) cout << res[i] << " \n"[i == n];
+        
+        now = 1 - now;
     }
+    FOR(i,1,n) cout << t[i] << ' ';
     return 0;
 }
-
