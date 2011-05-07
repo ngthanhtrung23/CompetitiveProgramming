655b347b08ff5f926c89b41a572d0408c5f8d256
Trung Nguyen <ngthanhtrung23@gmail.com>
2011-05-07 10:26:00 +0800

    Add solution for problem `B - Sets` Link: https://codeforces.com/contest/82/problem/B Tags: constructive algorithms, hashing, implementation, *1700 Ref: https://codeforces.com/contest/82/submission/441854

--- codeforces/82/B.cpp
+++ codeforces/82/B.cpp
@@ -1,67 +1,106 @@
-
-#include <bits/stdc++.h>
+#pragma comment(linker, "/STACK:16777216")
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+#include <sstream>
+#include <iomanip>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 using namespace std;
 
-int n, nRes;
-const int NBIT = 211;
-bitset<NBIT + 1> a[211*211], all, res[211], contains[211];
+const double PI = acos(-1.0);
 
-int main() {
-    ios :: sync_with_stdio(false);
-    while (scanf("%d", &n) == 1) {
-        if (n == 2) {
-            int k; scanf("%d", &k);
-            int u; scanf("%d", &u);
-            printf("%d %d\n", 1, u);
-            printf("%d", k-1);
+vector<int> s[222*222];
+bool mark[222], c[222][222], exist[222];
+int n, m;
 
-            FOR(i,2,k) {
-                scanf("%d", &u);
-                printf(" %d", u);
-            }
-            puts("");
-            continue;
-        }
+int a[222], na, b[222], nb;
 
-        int k = n * (n-1) / 2;
-        all.reset();
-        REP(val,NBIT) {
-            contains[val].reset();
-            REP(j,NBIT) contains[val].set(j);
-        }
-
-        FOR(i,1,k) {
-            a[i].reset();
-            int cnt; scanf("%d", &cnt);
-            while (cnt--) {
-                int u; scanf("%d", &u);
-                a[i].set(u);
-            }
-            all |= a[i];
-
-            REP(val,NBIT) if (a[i].test(val)) {
-                contains[val] &= a[i];
-            }
-        }
-
-        REP(val,NBIT) if (all.test(val)) {
-            printf("%d", contains[val].count());
-            REP(j,NBIT) if (contains[val].test(j)) printf(" %d", j);
-            puts("");
-            all &= ~contains[val];
-        }
+void init() {
+    FOR(i,1,m) FOR(j,1,m) if (exist[i] && exist[j]) c[i][j] = 1;
+    
+    FOR(i,1,n) {
+        memset(mark, 0, sizeof mark);
+        REP(x,s[i].size()) mark[s[i][x]] = 1;
+        
+        na = 0; nb = 0;
+        FOR(j,1,m) if (exist[j] && mark[j]) a[++na] = j;
+        FOR(k,1,m) if (exist[k] && !mark[k]) b[++nb] = k;
+        
+        FOR(j,1,na)
+        FOR(k,1,nb)
+            c[a[j]][b[k]] = 0;
     }
-    return 0;
 }
 
+int qu[222], dd[222];
+
+void bfs(int start) {
+    int first = 1, last = 1; qu[1] = start;
+    dd[start] = 1;
+    while (first <= last) {
+        int u = qu[first++];
+        FOR(v,1,m)
+        if (c[u][v] && !dd[v]) {
+            qu[++last] = v;
+            dd[v] = 1;
+        }
+    }
+    
+    cout << last;
+    FOR(i,1,last) cout << ' ' << qu[i];
+    cout << endl;
+}
+
+void solve() {
+    FOR(i,1,m)
+    if (!dd[i] && exist[i]) {
+        bfs(i);
+    }
+}
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+    n = n*(n-1)/2;
+    FOR(i,1,n) {
+        int k; scanf("%d", &k);
+        while (k--) {
+            int u; scanf("%d", &u);
+            s[i].PB(u);
+            m = max(m, u);
+            exist[u] = 1;
+        }
+    }
+    if (n == 1) {
+        cout << "1 " << s[1][0] << endl;
+        cout << s[1].size()-1;
+        FOR(i,1,s[1].size()-1) {
+            cout << ' ' << s[1][i];
+        }
+        cout << endl;
+        return 0;
+    }
+    init();
+    solve();
+    return 0;
+}
