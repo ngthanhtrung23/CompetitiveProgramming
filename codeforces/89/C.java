1d9c79afc19e496622da11b6e5cdf51392658f84
Trung Nguyen <ngthanhtrung23@gmail.com>
2011-06-17 15:52:00 +0800

    Add solution for problem `C - Chip Play` Link: https://codeforces.com/contest/89/problem/C Tags: brute force, data structures, implementation, *2300 Ref: https://codeforces.com/contest/89/submission/495560

--- codeforces/89/C.java
+++ codeforces/89/C.java
@@ -1,93 +1,106 @@
 import java.io.*;
 import java.util.*;
-import java.math.*;
 import java.util.regex.*;
+import java.math.*;
 
 public class Timus
 {
     public static Scanner sc;
-    public static String[] a;
-    public static int m, n, sl;
-    public static int[][] next, save, id;
+    public static int m, n;
+    public static String a[];
+    public static int[][][] nextu, nextv, saveu, savev;
+    
     public static final int[] di = {-1,1,0,0};
     public static final int[] dj = {0,0,-1,1};
-    public static char[] x;
     
     public static void inp() {
-        m = sc.nextInt(); n = sc.nextInt(); sc.nextLine();
+        m = sc.nextInt();
+        n = sc.nextInt(); sc.nextLine();
         a = new String[m];
-        for(int i = 0; i < m; i++)
+        for(int i = 0; i < m; i++) {
             a[i] = sc.nextLine();
-        
-        sl = 0;
-        id = new int[m][n];
-        x = new char[m*n + 1];
-        for(int i = 0; i < m; i++)
-            for(int j = 0; j < n; j++)
-                if (a[i].charAt(j) != '.') {
-                    id[i][j] = ++sl;
-                    x[sl] = a[i].charAt(j);
-                }
-        
-        next = new int[4][sl+1];
-        save = new int[4][sl+1];
+        }
     }
     
-    public static boolean inside(int u, int v) {
-        if (u < 0 || v < 0) return false;
-        if (u >= m || v >= n) return false;
+    public static boolean inside(int i, int j) {
+        if (i < 0 || j < 0) return false;
+        if (i >= m || j >= n) return false;
         return true;
     }
     
     public static void init() {
-        for(int dir = 0; dir < 4; dir++)
-            for(int i = 0; i < m; i++)
-                for(int j = 0; j < n; j++)
-                if (a[i].charAt(j) != '.') {
-                    int u = i + di[dir], v = j + dj[dir];
-                    while (inside(u,v) && a[u].charAt(v) == '.') {
-                        u = u + di[dir];
-                        v = v + dj[dir];
-                    }
-                    if (inside(u,v)) next[dir][id[i][j]] = id[u][v];
-                    else next[dir][id[i][j]] = -1;
-                }
-                else next[dir][id[i][j]] = -1;
+        nextu = new int[4][m][n];
+        nextv = new int[4][m][n];
+        saveu = new int[4][m][n];
+        savev = new int[4][m][n];
         
         for(int dir = 0; dir < 4; dir++)
-            for(int i = 1; i <= sl; i++) save[dir][i] = next[dir][i];
+            for(int i = 0; i < m; i++) for(int j = 0; j < n; j++)
+            if (a[i].charAt(j) != '.') {
+                int u = i + di[dir], v = j + dj[dir];
+                while (inside(u,v) && a[u].charAt(v) == '.') {
+                    u = u + di[dir];
+                    v = v + dj[dir];
+                }
+                if (inside(u,v)) {
+                    nextu[dir][i][j] = u;
+                    nextv[dir][i][j] = v;
+                }
+                else {
+                    nextu[dir][i][j] = nextv[dir][i][j] = -1;
+                }
+            }
+            else {
+                nextu[dir][i][j] = nextv[dir][i][j] = -1;
+            }
+        
+        for(int dir = 0; dir < 4; dir++)
+            for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) {
+                saveu[dir][i][j] = nextu[dir][i][j];
+                savev[dir][i][j] = nextv[dir][i][j];
+            }
     }
     
     public static void solve() {
         int res = 0, cnt = 0;
-        for(int i = 1; i <= sl; i++) {
-                for(int dir = 0; dir < 4; dir++)
-                    for(int u = 1; u <= sl; u++)
-                        next[dir][u] = save[dir][u];
+        for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) if (a[i].charAt(j) != '.') {
+            for(int dir = 0; dir < 4; dir++)
+                for(int u = 0; u < m; u++) for(int v = 0; v < n; v++) {
+                    nextu[dir][u][v] = saveu[dir][u][v];
+                    nextv[dir][u][v] = savev[dir][u][v];
+                }
+            
+            int u = i, v = j, now = 0;
+            while (inside(u,v)) {
+                now++;
+                int dir = -1;
+                switch (a[u].charAt(v)) {
+                    case 'U': dir = 0; break;
+                    case 'D': dir = 1; break;
+                    case 'L': dir = 2; break;
+                    case 'R': dir = 3; break;
+                }
+                int uu = nextu[dir][u][v], vv = nextv[dir][u][v];
                 
-                int u = i, now = 0;
-                while (u != -1) {
-                    now++;
-                    if (next[0][u] > 0) next[1][next[0][u]] = next[1][u];
-                    if (next[1][u] > 0) next[0][next[1][u]] = next[0][u];
-                    if (next[3][u] > 0) next[2][next[3][u]] = next[2][u];
-                    if (next[2][u] > 0) next[3][next[2][u]] = next[3][u];
-                    
-                    int dir = -1;
-                    switch (x[u]) {
-                        case 'U': dir = 0; break;
-                        case 'D': dir = 1; break;
-                        case 'L': dir = 2; break;
-                        case 'R': dir = 3; break;
-                    }
-                    u = next[dir][u];
-                }
-                if (now > res) {
-                    res = now;
-                    cnt = 1;
-                }
-                else if (now == res) cnt++;
+                int up_u = nextu[0][u][v], up_v = nextv[0][u][v];
+                int down_u = nextu[1][u][v], down_v = nextv[1][u][v];
+                int left_u = nextu[2][u][v], left_v = nextv[2][u][v];
+                int right_u = nextu[3][u][v], right_v = nextv[3][u][v];
+                
+                if (inside(up_u, up_v)) { nextu[1][up_u][up_v] = down_u; nextv[1][up_u][up_v] = down_v; }
+                if (inside(down_u, down_v)) { nextu[0][down_u][down_v] = up_u; nextv[0][down_u][down_v] = up_v; }
+                if (inside(left_u, left_v)) { nextu[3][left_u][left_v] = right_u; nextv[3][left_u][left_v] = right_v; }
+                if (inside(right_u, right_v)) { nextu[2][right_u][right_v] = left_u; nextv[2][right_u][right_v] = left_v; }
+                
+                u = uu; v = vv;
             }
+            if (now > res) {
+                res = now;
+                cnt = 1;
+            }
+            else if (now == res) cnt++;
+        }
+        
         System.out.println(res + " " + cnt);
     }
     
