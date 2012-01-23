24ce21f5462175645600f834b1f0956fbdb442a5
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-01-23 21:42:00 +0800

    Add solution for problem `A - Lucky Conversion` Link: https://codeforces.com/contest/145/problem/A Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/145/submission/1112512

---
+++ codeforces/145/A.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+#include <sstream>
+#include <iomanip>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+
+int INP,AM;
+#define BUFSIZE (1<<10)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (feof(stdin)) memset(BUF,0,sizeof BUF); else fread(BUF,1,BUFSIZE,stdin); \
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+
+const double PI = acos(-1.0);
+
+char a[100111], b[100111];
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    gets(a); gets(b);
+    int s4 = 0, s7 = 0;
+    int n = strlen(a);
+    REP(i,n) if (a[i] != b[i]) {
+        if (a[i] == '4') s4++;
+        else s7++;
+    }
+    printf("%d", max(s4,s7));
+    return 0;
+}
