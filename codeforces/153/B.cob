498943b0f547605cc0091d6bec71e047aae99683
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-02-23 00:04:00 +0800

    Add solution for problem `B - Binary notation` Link: https://codeforces.com/contest/153/problem/B Tags: *special problem, *1800 Ref: https://codeforces.com/contest/153/submission/1217181

--- codeforces/153/B.cob
+++ codeforces/153/B.cob
@@ -1,38 +1,26 @@
-        IDENTIFICATION DIVISION.
-        PROGRAM-ID. SOLUTION.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SOLUTION.
 
-        DATA DIVISION.
-        WORKING-STORAGE SECTION.
-        01 A         PIC 9(9)    VALUE ZEROES.
-        01 I         PIC 9(9)    VALUE ZEROES.
-        01 NEED      PIC 9(2)    VALUE ZEROES.
-        01 STR       PIC X(10).
-        01 RES       PIC X(30).
- 
-        PROCEDURE DIVISION.
-          ACCEPT STR
-          MOVE STR TO A
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 A        PIC 9(10)   VALUE ZEROES.
+       01 B        PIC 9(10)   VALUE ZEROES.
+       01 STR      PIC X(100).
+       01 RES      PIC X(100).
+	   01 F        PIC 9(1).
+       01 T        PIC 9(10).
 
-          MOVE 524288 TO I
-          MOVE 0 to NEED
-
-          PERFORM until I equal 0
-            IF A >= I
-              MOVE '1' TO STR
-              string RES DELIMITED BY SPACE
-                     STR DELIMITED BY SIZE into RES
-              MOVE 1 TO NEED
-
-              COMPUTE A = A - I
-            ELSE
-              IF NEED EQUAL 1
-                MOVE '0' TO STR
-                string RES DELIMITED BY SPACE
-                       STR DELIMITED BY SIZE into RES
-              END-IF
-            END-IF
-
-            COMPUTE I = I / 2
-          END-PERFORM.
-          DISPLAY RES
-          stop run.
+       PROCEDURE DIVISION.
+         ACCEPT STR
+         MOVE STR TO A
+         MOVE '' TO STR
+         perform UNTIL A IS EQUAL TO ZERO
+            DIVIDE 2 INTO A GIVING B REMAINDER F
+            MOVE B TO A
+            string F   DELIMITED BY SPACE
+                   STR DELIMITED BY SIZE into STR
+         end-perform.
+         MOVE LENGTH OF STR TO T
+         SUBTRACT 1 FROM T
+         display STR(2:T)
+         STOP RUN.
