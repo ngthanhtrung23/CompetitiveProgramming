78c353cb5767248973231c8e9cdf9f0474512e57
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-02-22 23:44:00 +0800

    Add solution for problem `A - A + B` Link: https://codeforces.com/contest/153/problem/A Tags: *special problem, *1600 Ref: https://codeforces.com/contest/153/submission/1216926

--- codeforces/153/A.cob
+++ codeforces/153/A.cob
@@ -1,48 +1,24 @@
-        IDENTIFICATION DIVISION.
-        PROGRAM-ID. SOLUTION.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SOLUTION.
 
-        DATA DIVISION.
-        WORKING-STORAGE SECTION.
-        01 A         PIC 9(6)    VALUE ZEROES.
-        01 B         PIC 9(6)    VALUE ZEROES.
-        01 X1        PIC 9(1)    VALUE ZEROES.
-        01 X2        PIC 9(2)    VALUE ZEROES.
-        01 X3        PIC 9(3)    VALUE ZEROES.
-        01 X4        PIC 9(4)    VALUE ZEROES.
-        01 X5        PIC 9(5)    VALUE ZEROES.
-        01 STR       PIC X(10).
- 
-        PROCEDURE DIVISION.
-          ACCEPT STR
-          MOVE STR TO A
-          ACCEPT STR
-          MOVE STR TO B
-          ADD A TO B
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 A        PIC 9(10)   VALUE ZEROES.
+       01 B        PIC 9(10)   VALUE ZEROES.
+       01 STR      PIC X(10).
+	   01 F        PIC 9(10).
 
-          IF B < 10
-            MOVE B TO X1
-            DISPLAY X1
-          ELSE
-            IF B < 100
-              MOVE B TO X2
-              DISPLAY X2
-            ELSE
-              IF B < 1000
-                MOVE B TO X3
-                DISPLAY X3
-              ELSE
-                IF B < 10000
-                  MOVE B TO X4
-                  DISPLAY X4
-                ELSE
-                  IF B < 100000
-                    MOVE B TO X5
-                    DISPLAY X5
-                  ELSE
-                    DISPLAY B
-                  END-IF
-                END-IF
-              END-IF
-            END-IF
-          END-IF
-          STOP RUN.
+       PROCEDURE DIVISION.
+         ACCEPT STR
+         MOVE STR TO A
+         ACCEPT STR
+         MOVE STR TO B
+         ADD A TO B
+		 MOVE B TO STR
+		 perform until STR(1:1) IS NOT EQUAL TO '0'
+		   move length OF STR TO F
+		   SUBTRACT 1 FROM F
+		   move STR(2:F) TO STR
+		 end-perform.
+         DISPLAY STR
+         STOP RUN.
