2530fe3716b65b1cf467344a0686334bdde7c2bc
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-02-25 16:06:00 +0800

    Add solution for problem `C - Double Profiles` Link: https://codeforces.com/contest/154/problem/C Tags: graphs, hashing, sortings, *2300 Ref: https://codeforces.com/contest/154/submission/1233285

--- codeforces/154/C.cpp
+++ codeforces/154/C.cpp
@@ -1,65 +1,68 @@
-
-#include <bits/stdc++.h>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
 #define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 using namespace std;
 
-const int MN = 1000111;
+const double PI = acos(-1.0);
 
-long long p[MN], h[MN], h2[MN];
-
-int n, m;
-pair<int,int> e[MN];
+long long p[1000111];
+long long h[1000111], h2[1000111];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    cout << (fixed) << setprecision(9);
-    p[0] = 1;
-    FOR(i,1,MN-1) p[i] = p[i-1] * 5;
-
-    while (cin >> n >> m) {
-        memset(h, 0, sizeof h);
-
-        FOR(i,1,m) {
-            int u, v; cin >> u >> v;
-            e[i] = make_pair(u, v);
-
-            h[u] = h[u] + p[v];
-            h[v] = h[v] + p[u];
-        }
-
-        FOR(i,1,n) h2[i] = h[i];
-
-        sort(h+1, h+n+1);
-        ll res = 0;
-
-        int i = 1;
-        while (i <= n) {
-            int j = i;
-            while (j+1 <= n && h[j] == h[j+1]) ++j;
-
-            ll len = j - i + 1;
-            res += len * (len-1) / 2;
-
-            i = j + 1;
-        }
-
-        FOR(i,1,m) {
-            int u = e[i].first, v = e[i].second;
-            if (h2[u] + p[u] == h2[v] + p[v]) ++res;
-        }
-        cout << res << endl;
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int n, m; scanf("%d%d", &n, &m);
+    p[0] = 1; FOR(i,1,n) p[i] = p[i-1] * 1000003;
+    FOR(i,1,n) h2[i] = p[i];
+    int u, v;
+    while (m--) {
+        scanf("%d%d", &u, &v);
+        h[u] += p[v];
+        h[v] += p[u];
+        
+        h2[u] += p[v];
+        h2[v] += p[u];
     }
+    sort(h + 1, h + n + 1);
+    sort(h2 + 1, h2 + n + 1);
+    
+    int i = 1, j = 1;
+    long long res = 0;
+    while (i <= n) {
+        j = i;
+        while (j < n && h[j+1] == h[i]) j++;
+        res += (j - i) * (j - i + 1LL);
+        i = j + 1;
+    }
+    i = 1; j = 1;
+    while (i <= n) {
+        j = i;
+        while (j < n && h2[j+1] == h2[i]) j++;
+        res += (j - i) * (j - i + 1LL);
+        i = j + 1;
+    }
+    cout << res / 2;
     return 0;
 }
-
