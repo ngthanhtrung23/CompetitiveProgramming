508dcc593e4e5cfe396ffc856263562adbc9e934
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-03-16 23:50:00 +0800

    Add solution for problem `B - Burning Midnight Oil` Link: https://codeforces.com/contest/165/problem/B Tags: binary search, implementation, *1500 Ref: https://codeforces.com/contest/165/submission/1363362

--- codeforces/165/B.cpp
+++ codeforces/165/B.cpp
@@ -1,42 +1,61 @@
-#include <bits/stdc++.h>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
 #define ll long long
-#define SZ(X) ((int) ((X).size()))
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
-ll get(ll mid, int k) {
-    ll res = 0;
-    while (mid) {
-        res += mid;
-        mid /= k;
+const double PI = acos(-1.0);
+
+ll n;
+int k;
+
+bool check(ll v) {
+    ll cur = 1, can = 0;
+    while (cur <= v) {
+        can += v / cur;
+        if (can >= n) return true;
+        cur *= k;
     }
-    return res;
+    if (can >= n) return true;
+    return false;
 }
 
 int main() {
-    ios :: sync_with_stdio(false);
-    ll n, k;
-    while (cin >> n >> k) {
-        ll l = 0, r = 1000111000111LL, res = r;
-        while (l <= r) {
-            ll mid = (l + r) >> 1;
-            if (get(mid, k) >= n) {
-                res = mid;
-                r = mid - 1;
-            }
-            else l = mid + 1;
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    cin >> n >> k;
+    ll l = 0, r = n * k, res = r;
+    while (l <= r) {
+        ll mid = (l + r) >> 1;
+        if (check(mid)) {
+            res = mid;
+            r = mid - 1;
         }
-        cout << res << endl;
+        else l = mid + 1;
     }
+    cout << res;
+    return 0;
 }
-
