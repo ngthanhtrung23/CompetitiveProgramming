b93f4a8ae4c7d1e20dbafa0dbc90419600e1cd38
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-03-16 23:53:00 +0800

    Add solution for problem `C - Another Problem on Strings` Link: https://codeforces.com/contest/165/problem/C Tags: binary search, brute force, dp, math, strings, two pointers, *1600 Ref: https://codeforces.com/contest/165/submission/1363559

--- codeforces/165/C.cpp
+++ codeforces/165/C.cpp
@@ -1,41 +1,63 @@
-#include <bits/stdc++.h>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
 #define ll long long
-#define SZ(X) ((int) ((X).size()))
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
-int sum[1000111];
+const double PI = acos(-1.0);
+
+char s[1000111];
+int a[1000111];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    int k; string s;
-    while (cin >> k >> s) {
-        int n = s.length();
-        s = " " + s;
-        FOR(i,1,n) sum[i] = sum[i-1] + s[i] - '0';
-        ll res = 0;
-        int j1 = 0, j2 = 0;
-        FOR(i,1,n) {
-            while (j1 <= n && sum[j1] - sum[i-1] < k) ++j1;
-            while (j2 < n && sum[j2+1] - sum[i-1] <= k) ++j2;
-
-            if (j2 > n) break;
-            if (j1 < i) j1 = i;
-            if (j2 < i) j2 = i;
-
-            if (sum[j1] - sum[i-1] == k) 
-                res += j2 - j1 + 1;
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int n1 = 0; ll res = 0;
+    int k; scanf("%d\n", &k);
+    gets(s); int n = strlen(s);
+    int cur = 0;
+    REP(i,n) {
+        if (s[i] == '1') {
+            n1++;
+            a[n1] = cur;
+            cur = 0;
         }
-        cout << res << endl;
+        else cur++;
     }
+    n1++; a[n1] = cur;
+    if (k == 0) {
+        FOR(i,1,n1) res += a[i] * (ll) (a[i] + 1) / 2;
+        cout << res;
+        return 0;
+    }
+    
+    FOR(i,1,n1) a[i]++;
+    FOR(start,1,n1-k) {
+        res += a[start] * (ll) a[start+k];
+    }
+    cout << res;
+    return 0;
 }
