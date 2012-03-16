1a5fbac9b073852403903d7b0fcaedd8eb0430b8
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-03-17 00:25:00 +0800

    Add solution for problem `D - Beard Graph` Link: https://codeforces.com/contest/165/problem/D Tags: data structures, dsu, trees, *2100 Ref: https://codeforces.com/contest/165/submission/1365747

--- codeforces/165/D.cpp
+++ codeforces/165/D.cpp
@@ -1,177 +1,129 @@
-
-#include <bits/stdc++.h>
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
 #define ll long long
-#define SZ(X) ((int) ((X).size()))
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
-const int MN = 100111;
-
-int n, deg[MN], nChain;
-vector<int> ke[MN];
-pair<int,int> chain[MN];
-int chainSize[MN];
-pair<int,int> edge[MN];
+const double PI = acos(-1.0);
 
 struct Node {
-    Node* left;
-    Node* right;
-    int sum;
-};
+    int path, pos;
+    Node(int path = 0, int pos = 0) : path(path), pos(pos) {}
+} a[100111], b[100111];
 
-vector<Node*> it;
+vector< pair<int,int> > ke[100111];
+vector< vector<int> > cnt;
 
-Node* build(int l, int r) {
-    if (l > r) return NULL;
-    if (l == r) {
-        Node* res = new Node();
-        res->left = res->right = NULL;
-        res->sum = 1;
-        return res;
+int n, root, npath, sz[100111];
+
+void dfs(int u, int lu, int path, int pos) {
+    a[u] = Node(path, pos);
+    b[lu] = Node(path, pos);
+    sz[path] = pos;
+    
+    REP(i, ke[u].size()) {
+        int v = ke[u][i].F, id = ke[u][i].S;
+        if (id == lu) continue;
+        dfs(v, id, path, pos + 1);
     }
-    int mid = (l + r) >> 1;
-    Node* res = new Node();
-    res->sum = r - l + 1;
+}
 
-    res->left = build(l, mid);
-    res->right = build(mid+1, r);
+#define _(u) (u&(-u))
 
+void update(int path, int u, int k) {
+    while (u <= sz[path]) {
+        cnt[path][u] += k;
+        u += _(u);
+    }
+}
+
+int get(int path, int u) {
+    int res = 0;
+    while (u > 0) {
+        res += cnt[path][u];
+        u -= _(u);
+    }
     return res;
 }
 
-void setVal(Node* cur, int l, int r, int u, int val) {
-    if (u < l || r < u) return ;
-    if (l == r) {
-        cur->sum = val;
-        return ;
-    }
-    int mid = (l + r) >> 1;
-    setVal(cur->left, l, mid, u, val);
-    setVal(cur->right, mid+1, r, u, val);
-
-    cur->sum = cur->left->sum + cur->right->sum;
-}
-
-int getSum(Node* cur, int l, int r, int u, int v) {
-    if (v < l || r < u) return 0;
-    if (u <= l && r <= v) return cur->sum;
-
-    int mid = (l + r) >> 1;
-    return getSum(cur->left, l, mid, u, v) + getSum(cur->right, mid+1, r, u, v);
-}
-
-void init() {
-    FOR(i,1,n) ke[i].clear();
-    memset(deg, 0, sizeof deg);
-    memset(chain, 0, sizeof chain);
-    memset(chainSize, 0, sizeof chainSize);
-}
-
-void dfs(int u, int fu, int chainId, int chainPos) {
-    chain[u] = make_pair(chainId, chainPos);
-    chainSize[chainId]++;
-
-    REP(i,SZ(ke[u])) {
-        int v = ke[u][i];
-        if (v == fu) continue;
-
-        dfs(v, u, chainId, chainPos + 1);
-    }
-}
-
 int main() {
-    ios :: sync_with_stdio(false);
-    while (scanf("%d", &n) == 1) {
-        init();
-        FOR(i,2,n) {
-            int u, v; scanf("%d%d", &u, &v);
-            ke[u].push_back(v);
-            ke[v].push_back(u);
-
-            deg[u]++;
-            deg[v]++;
-
-            edge[i-1] = make_pair(u, v);
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+    int u, v;
+    FOR(i,1,n-1) {
+        scanf("%d%d", &u, &v);
+        ke[u].PB(MP(v, i)); ke[v].PB(MP(u, i));
+    }
+    root = 1;
+    FOR(i,2,n) if (ke[i].size() > 2) root = i;
+    cnt.resize(ke[root].size() + 2);
+    
+    REP(i,ke[root].size()) {
+        npath++;
+        dfs(ke[root][i].F, ke[root][i].S, npath, 1);
+        cnt[npath].resize(sz[npath] + 2);
+    }
+/*    PR(sz, npath);
+    
+    FOR(i,1,n) cout << a[i].path << ' ' << a[i].pos << endl;
+    FOR(i,1,n-1) cout << b[i].path << ' ' << b[i].pos << endl;
+*/    
+    int q; scanf("%d", &q);
+    int x, cur, res;
+    while (q--) {
+        scanf("%d", &x);
+        if (x == 1) {
+            scanf("%d", &u);
+            update(b[u].path, b[u].pos, -1);
         }
-        int root = 1;
-        FOR(i,2,n) if (deg[i] > deg[root]) root = i;
-
-        REP(i,SZ(ke[root])) {
-            int u = ke[root][i];
-            ++nChain;
-            dfs(u, root, nChain, 1);
+        else if (x == 2) {
+            scanf("%d", &u);
+            update(b[u].path, b[u].pos, +1);
         }
-
-        it.resize(nChain + 5);
-        FOR(i,1,nChain) it[i] = build(1, chainSize[i]);
-
-        int q; scanf("%d", &q);
-        while (q--) {
-            int typ; scanf("%d", &typ);
-            int u, v;
-            if (typ == 1 || typ == 2) {
-                int id; scanf("%d", &id);
-                u = edge[id].first;
-                v = edge[id].second;
-
-                if (chain[u].second < chain[v].second) {
-                    swap(u, v);
-                }
-
-                int chainId = chain[u].first;
-                if (typ == 1) {
-                    // paint black
-                    setVal(it[chainId], 1, chainSize[chainId], chain[u].second, 1);
-                }
-                else {
-                    // paint white
-                    setVal(it[chainId], 1, chainSize[chainId], chain[u].second, 0);
-                }
+        else {
+            scanf("%d%d", &u, &v);
+            if (v == root) swap(u, v);
+            if (u == root) {
+                cur = get(a[v].path, a[v].pos);
+                res = a[v].pos;
+            }
+            else if (a[u].path == a[v].path) {
+                if (a[v].pos > a[u].pos) swap(u, v);
+                cur = get(a[u].path, a[u].pos) - get(a[v].path, a[v].pos);
+                res = a[u].pos - a[v].pos;
             }
             else {
-                // query u --> v
-                scanf("%d%d", &u, &v);
-                if (chain[u].second < chain[v].second) swap(u, v);
-                int chainU = chain[u].first;
-                int chainV = chain[v].first;
-
-                if (v == root) {
-                    if (getSum(it[chainU], 1, chainSize[chainU], 1, chain[u].second) == chain[u].second) {
-                        printf("%d\n", chain[u].second);
-                    }
-                    else {
-                        puts("-1");
-                    }
-                }
-                else if (chainU == chainV) {
-                    if (getSum(it[chainU], 1, chainSize[chainU], chain[v].second + 1, chain[u].second)
-                            == chain[u].second - chain[v].second) {
-                        printf("%d\n", chain[u].second - chain[v].second);
-                    }
-                    else {
-                        puts("-1");
-                    }
-                }
-                else {
-                    if (getSum(it[chainU], 1, chainSize[chainU], 1, chain[u].second) == chain[u].second
-                            && getSum(it[chainV], 1, chainSize[chainV], 1, chain[v].second) == chain[v].second) {
-                        printf("%d\n", chain[u].second + chain[v].second);
-                    }
-                    else {
-                        puts("-1");
-                    }
-                }
+                cur = get(a[u].path, a[u].pos) + get(a[v].path, a[v].pos);
+                res = a[u].pos + a[v].pos;
             }
+            if (cur > 0) puts("-1");
+            else printf("%d\n", res);
         }
     }
+    
+    return 0;
 }
