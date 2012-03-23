a96d3f1b11a4d6a85536205d3251ed64d27f2502
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-03-23 23:41:00 +0800

    Add solution for problem `E - Tetrahedron` Link: https://codeforces.com/contest/166/problem/E Tags: dp, math, matrices, *1500 Ref: https://codeforces.com/contest/166/submission/1390403

--- codeforces/166/E.cpp
+++ codeforces/166/E.cpp
@@ -1,29 +1,68 @@
-
-#include <bits/stdc++.h>
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
-int f[2][10111000];
+const double PI = acos(-1.0);
 
-int main() {
-    ios :: sync_with_stdio(false);
-    f[0][0] = 1;
-    FOR(i,1,10000 * 1000) {
-        f[0][i] = f[1][i-1];
-        f[1][i] = (f[0][i-1] * 3LL + f[1][i-1] * 2) % 1000000007;
+struct Matrix {
+    int x[4][4];
+} a, I, Id;
+
+Matrix operator * (Matrix a, Matrix b) {
+    Matrix res;
+    memset(res.x, 0, sizeof res.x);
+    REP(i,4) REP(j,4) {
+        REP(k,4) {
+            res.x[i][j] = (res.x[i][j] + a.x[i][k] * (ll) b.x[k][j]) % 1000000007;
+        }
     }
-    int n;
-    while (cin >> n) cout << f[0][n] << endl;
-    return 0;
+    return res;
 }
 
+Matrix power(int n) {
+    if (n == 0) return Id;
+    if (n == 1) return I;
+    Matrix p = power(n >> 1);
+    p = p * p;
+    if (n & 1) return p * I;
+    else return p;
+}
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int n; scanf("%d", &n);
+    REP(i,4) REP(j,4) {
+        if (i == j) { I.x[i][j] = 0; Id.x[i][j] = 1; }
+        else { I.x[i][j] = 1; Id.x[i][j] = 0; }
+    }
+    a = power(n);
+    cout << a.x[3][3] << endl;
+    return 0;
+}
