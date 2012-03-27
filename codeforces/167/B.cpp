f37621faba66578c09ded108d63422116d78fa92
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-03-27 23:25:00 +0800

    Add solution for problem `B - Wizards and Huge Prize` Link: https://codeforces.com/contest/167/problem/B Tags: dp, math, probabilities, *1800 Ref: https://codeforces.com/contest/167/submission/1425899

--- codeforces/167/B.cpp
+++ codeforces/167/B.cpp
@@ -1,48 +1,62 @@
-
-#include <bits/stdc++.h>
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
-long double f[2][411][211], p[211];
-int n, a[211], l, k;
+const double PI = acos(-1.0);
+
+int n, l, k, p[222], a[222];
+long double f[222][422][222];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    while (cin >> n >> l >> k) {
-        FOR(i,1,n) {
-            int t; cin >> t;
-            p[i] = t;
-        }
-        FOR(i,1,n) cin >> a[i];
-
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    while (scanf("%d%d%d", &n, &l, &k) == 3) {
+        FOR(i,1,n) scanf("%d", &p[i]);
+        FOR(i,1,n) scanf("%d", &a[i]);
+        
+        if (k > n) k = n;
         memset(f, 0, sizeof f);
-        f[0][200 + k][0] = 1.0;
-        FOR(i,0,n-1) {
-            int t = i % 2;
-            memset(f[1-t], 0, sizeof f[1-t]);
-
-            FOR(cap,0,400) FOR(won,0,i)
-                if (f[t][cap][won] >= 1e-100) {
-                    f[1-t][min(400, cap + a[i+1])][won+1] += f[t][cap][won] / 100.0 * p[i+1];
-                    f[1-t][cap][won] += f[t][cap][won] / 100.0 * (100 - p[i+1]);
-                }
+        f[0][k+200][0] = 1.0;
+        int space2;
+        FOR(i,0,n-1) FOR(space,0,n+200) FOR(nWin,0,n) if (f[i][space][nWin] > 0) {
+            // Win
+            space2 = space + a[i+1];
+            if (space2 > n + 200) space2 = n + 200;
+            f[i+1][space2][nWin+1] += f[i][space][nWin] / 100.0 * p[i+1];
+            
+            // Lose
+            f[i+1][space][nWin] += f[i][space][nWin] / 100.0 * (100 - p[i+1]);
         }
-
         long double res = 0.0;
-        FOR(cap,200,400) FOR(won,l,n)
-            res += f[n%2][cap][won];
-
-        cout << (fixed) << setprecision(10) << res << endl;
+        FOR(space,200,n+200) FOR(nWin,l,n)
+            res += f[n][space][nWin];
+        printf("%.8lf\n", (double) res);
     }
     return 0;
 }
