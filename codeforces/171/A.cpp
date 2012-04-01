0c3dddb3b6edf3e0c3f3c987fe92502698e3f709
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-04-01 23:02:00 +0800

    Add solution for problem `A - Mysterious numbers - 1` Link: https://codeforces.com/contest/171/problem/A Tags: *special problem, constructive algorithms, *1200 Ref: https://codeforces.com/contest/171/submission/1449773

---
+++ codeforces/171/A.cpp
@@ -0,0 +1,48 @@
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
+using namespace std;
+
+const double PI = acos(-1.0);
+
+ll rev(ll x) {
+    ll res = 0;
+    while (x) {
+        res = (res * 10) + x % 10;
+        x /= 10;
+    }
+    return res;
+}
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    ll a, b;
+    while (cin >> a >> b) cout << rev(b) + a << endl;
+    return 0;
+}
