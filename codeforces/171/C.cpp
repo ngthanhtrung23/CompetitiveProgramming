64b3a075fceb376178358b5586d5b9db9262c6e9
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-04-01 23:54:00 +0800

    Add solution for problem `C - A Piece of Cake` Link: https://codeforces.com/contest/171/problem/C Tags: *special problem, implementation, *2000 Ref: https://codeforces.com/contest/171/submission/1457245

--- codeforces/171/C.cpp
+++ codeforces/171/C.cpp
@@ -1,32 +1,52 @@
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
-#include <bits/stdc++.h>
-#define int long long
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
-#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
-
-#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
-#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
-#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
 #define ll long long
-#define __builtin_popcount __builtin_popcountll
-#define SZ(x) ((int) (x).size())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
-#undef int
+const double PI = acos(-1.0);
+
+ll st[100111], top;
+
 int main() {
-#define int long long
-    ios :: sync_with_stdio(0); cin.tie(0);
-    cout << (fixed) << setprecision(9);
-	int n;
-	while (cin >> n) {
-		int res = 0;
-		FOR(i,1,n) {
-			int a; cin >> a;
-			res += i * a;
-		}
-		cout << res << endl;
-	}
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int n; cin >> n;
+    st[++top] = 0;
+    ll flour = 1;
+    FOR(i,1,n) {
+        ll choc; cin >> choc;
+        st[++top] = choc;
+        st[top] *= flour;
+        choc = st[top--];
+        st[top] += choc;
+        
+        flour++;
+    }
+    cout << st[top] << endl;
+    return 0;
 }
