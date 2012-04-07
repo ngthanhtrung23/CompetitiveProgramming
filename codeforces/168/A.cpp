f5b19db5fef96c632d72e592014a6568893a3ca8
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-04-07 17:38:00 +0800

    Add solution for problem `A - Wizards and Demonstration` Link: https://codeforces.com/contest/168/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/168/submission/1486920

---
+++ codeforces/168/A.cpp
@@ -0,0 +1,42 @@
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
+using namespace std;
+
+const double PI = acos(-1.0);
+
+int main() {
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+    int n, x, y;
+    while (cin >> n >> x >> y) {
+        int need = (int) ceil(n * y / 100.0 - 1e-6);
+        cout << max(need - x, 0) << endl;
+    }
+    return 0;
+}
