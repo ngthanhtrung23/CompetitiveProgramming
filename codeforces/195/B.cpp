7a3207921399e5317a0b1ae92b082634c795464b
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-06-11 16:02:00 +0800

    Add solution for problem `B - After Training` Link: https://codeforces.com/contest/195/problem/B Tags: data structures, implementation, math, *1300 Ref: https://codeforces.com/contest/195/submission/1783051

--- codeforces/195/B.cpp
+++ codeforces/195/B.cpp
@@ -1,50 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <iomanip>
+#include <bitset>
+#include <complex>
 
-#include <bits/stdc++.h>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define REP(i,a) for(int i = 0; i < a; ++i)
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(x,n) cout << #x << " = "; FOR(__,1,n) cout << x[__] << ' '; puts("");
+#define PR0(x,n) cout << #x << " = "; REP(__,n) cout << x[__] << ' '; puts("");
 using namespace std;
 
-struct Index {
-    int cnt;
-    int i;
-};
-
-int n, m;
-bool operator < (const Index& a, const Index& b) {
-    if (a.cnt != b.cnt) return a.cnt < b.cnt;
-    int d1 = abs((m + 1) - 2*a.i);
-    int d2 = abs((m + 1) - 2*b.i);
-    if (d1 != d2) return d1 < d2;
-    return a.i < b.i;
-}
+int n, m, res[100111];
 
 int main() {
-    ios :: sync_with_stdio(false);
     while (cin >> n >> m) {
-        set<Index> s;
-        FOR(i,1,m) {
-            Index cur;
-            cur.cnt = 0;
-            cur.i = i;
-            s.insert(cur);
-        }
-        FOR(i,1,n) {
-            Index cur = *s.begin();
-            s.erase(s.begin());
+        int mid = (m + 1) >> 1;
+        int cnt = 0;
+        while (cnt < n) {
+            FOR(diff,0,mid+10) {
+                int now = mid - diff;
+                if (now > 0 && now <= m) {
+                    res[++cnt] = now;
+                    if (cnt == n) break;
+                }
+                if (m % 2 == 1 && !diff) continue;
 
-            cout << cur.i << '\n';
-            cur.cnt++;
-            s.insert(cur);
+                now = mid + diff + (m+1)%2;
+                if (now > 0 && now <= m) {
+                    res[++cnt] = now;
+                    if (cnt == n) break;
+                }
+            }
         }
+
+        FOR(i,1,n) printf("%d\n", res[i]);
     }
     return 0;
 }
