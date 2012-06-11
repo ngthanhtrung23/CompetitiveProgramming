9284990de75496cd091fe396dcaced3c3e3d297f
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-06-11 15:51:00 +0800

    Add solution for problem `A - Let's Watch Football` Link: https://codeforces.com/contest/195/problem/A Tags: binary search, brute force, math, *1000 Ref: https://codeforces.com/contest/195/submission/1783005

--- codeforces/195/A.cpp
+++ codeforces/195/A.cpp
@@ -1,33 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <iomanip>
+#include <bitset>
+#include <complex>
 
-#include <bits/stdc++.h>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define REP(i,a) for(int i = 0; i < a; ++i)
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(x,n) cout << #x << " = "; FOR(__,1,n) cout << x[__] << ' '; puts("");
+#define PR0(x,n) cout << #x << " = "; REP(__,n) cout << x[__] << ' '; puts("");
 using namespace std;
 
+long long a, b, c;
+
+long long check(long long t0) {
+    for(long long t = t0; t <= t0+c; ++t) {
+        long long downloaded = t * b;
+        long long need = (t - t0) * a;
+        if (need > downloaded) return false;
+    }
+    return true;
+}
+
 int main() {
-    ios :: sync_with_stdio(false);
-    int a, b, c;
     while (cin >> a >> b >> c) {
-        int l = 0, r = 1000111, res = r;
+        long long l = 0, r = 1000111000, res = r;
         while (l <= r) {
-            int mid = (l + r) / 2;
-            bool ok = true;
-
-            FOR(i,1,c) if ((mid + i) * b < i*a) {
-                ok = false;
-                break;
+            long long mid = (l + r) >> 1;
+            if (check(mid)) {
+                r = mid - 1;
+                res = mid;
             }
-
-            if (ok) res = mid, r = mid - 1;
             else l = mid + 1;
         }
         cout << res << endl;
