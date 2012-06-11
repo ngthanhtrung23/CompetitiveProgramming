2440b8759299697e13c3265e9e93884f601c5a72
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-06-11 16:54:00 +0800

    Add solution for problem `D - Analyzing Polyline` Link: https://codeforces.com/contest/195/problem/D Tags: geometry, math, sortings, *1900 Ref: https://codeforces.com/contest/195/submission/1783237

--- codeforces/195/D.cpp
+++ codeforces/195/D.cpp
@@ -1,35 +1,123 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <iomanip>
+#include <bitset>
+#include <complex>
 
-#include <bits/stdc++.h>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define REP(i,a) for(int i = 0; i < a; ++i)
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(x,n) cout << #x << " = "; FOR(__,1,n) cout << x[__] << ' '; puts("");
+#define PR0(x,n) cout << #x << " = "; REP(__,n) cout << x[__] << ' '; puts("");
 using namespace std;
 
-int main() {
-    ios :: sync_with_stdio(false);
-    int n;
-    while (cin >> n) {
-        set< pair<int,int> > s;
-        FOR(i,1,n) {
-            int x, y; cin >> x >> y;
-            if (x == 0) continue;
+long long gcd(long long a, long long b) {
+    if (!b) return a;
+    return gcd(b, a % b);
+}
 
-            if (x < 0) x = -x, y = -y;
+long long ab(long long x) {
+    if (x < 0) return -x; else return x;
+}
 
-            int g = __gcd(x, y);
-            x /= g;
-            y /= g;
-            s.insert(make_pair(x, y));
+struct Fraction {
+    long long x, y;
+    Fraction() { x = 0; y = 1; }
+    Fraction(long long X, long long Y) {
+        if (Y < 0) {
+            Y = -Y;
+            X = -X;
         }
-        cout << s.size() << endl;
+        long long g = gcd(ab(X), ab(Y));
+        x = X / g;
+        y = Y / g;
+    }
+} c[100111];
+
+bool operator == (Fraction a, Fraction b) {
+    return a.x * b.y == a.y * b.x;
+}
+
+bool operator < (Fraction a, Fraction b) {
+    if (a.x < 0 && b.x > 0) return true;
+    if (a.x > 0 && b.x < 0) return false;
+    return a.x * b.y < a.y * b.x;
+}
+
+Fraction operator * (long long x, Fraction a) {
+    return Fraction(x*a.x, a.y);
+}
+
+Fraction operator + (Fraction a, Fraction b) {
+    long long lcm = a.y * b.y / gcd(a.y, b.y);
+    a = (lcm / a.y) * a;
+    b = (lcm / b.y) * b;
+    return Fraction(a.x + b.x, lcm);
+}
+
+pair<long long, long long> a[100111];
+vector<int> ls[100111];
+int n;
+
+int main() {
+    while (scanf("%d", &n) == 1) {
+        int s = 0;
+        FOR(i,1,n) {
+            scanf("%I64d%I64d", &a[i].F, &a[i].S);
+            if (a[i].F) c[++s] = Fraction(-a[i].S, a[i].F);
+        }
+        sort(c+1, c+s+1);
+        s = unique(c+1, c+s+1) - c - 1;
+
+        FOR(i,1,s) ls[i].clear();
+        FOR(i,1,n) if (a[i].F) {
+            int u = lower_bound(c+1, c+s+1, Fraction(-a[i].S, a[i].F)) - c;
+            ls[u].PB(i);
+        }
+
+        long long k = 0, b = 0, cnt = 0;
+        FOR(i,1,n) if (a[i].F <= 0) {
+            k += a[i].F;
+            b += a[i].S;
+        }
+        // cout << k << ' ' << b << endl;
+        FOR(i,1,s) {
+            long long kk = k, bb = b;
+            REP(x,ls[i].size()) {
+                int u = ls[i][x];
+                if (a[u].F > 0) {
+                    kk += a[u].F;
+                    bb += a[u].S;
+                }
+                else if (a[u].F < 0) {
+                    kk -= a[u].F;
+                    bb -= a[u].S;
+                }
+            }
+            if (kk != k || bb != b) {
+                if (kk*c[i]+Fraction(bb,1) == k*c[i]+Fraction(b,1)) cnt += 1;
+                else cnt += 1;
+            }
+            k = kk; b = bb;
+            // cout << "ls = "; REP(x,ls[i].size()) cout << ls[i][x] << ' '; cout << endl;
+            // cout << c[i].x << ' ' << c[i].y << "  -->  ";
+            // cout << k << ' ' << b << ' ' << cnt << endl;
+        }
+        cout << cnt << endl;
     }
     return 0;
 }
