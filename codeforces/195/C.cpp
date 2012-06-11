7597a3bf75706ec56cfa94047674482b25cae130
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-06-11 16:16:00 +0800

    Add solution for problem `C - Try and Catch` Link: https://codeforces.com/contest/195/problem/C Tags: expression parsing, implementation, *1800 Ref: https://codeforces.com/contest/195/submission/1783099

--- codeforces/195/C.cpp
+++ codeforces/195/C.cpp
@@ -1,62 +1,81 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <iomanip>
+#include <bitset>
+#include <complex>
 
-#include <bits/stdc++.h>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+#define FORD(i,a,b) for(int i = a; i >= b; --i)
+#define REP(i,a) for(int i = 0; i < a; ++i)
+#define MP make_pair
+#define PB push_back
+#define F first
+#define S second
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(x,n) cout << #x << " = "; FOR(__,1,n) cout << x[__] << ' '; puts("");
+#define PR0(x,n) cout << #x << " = "; REP(__,n) cout << x[__] << ' '; puts("");
 using namespace std;
 
-string s[100111];
-char tmp[100111];
+struct Statement {
+    string type;
+    string arg1, arg2;
+} a[100111];
+
+int n, match[100111], st[100111];
+char c[1000111];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    int n;
     while (scanf("%d\n", &n) == 1) {
         FOR(i,1,n) {
-            gets(tmp);
-            s[i] = string(tmp);
-        }
-        stack< pair<string,string> > st;
-        FORD(i,n,1) {
-            REP(t,s[i].length()) {
-                if (s[i][t] == '(' || s[i][t] == ')' || s[i][t] == ',') s[i][t] = ' ';
+            gets(c);
+            bool conv = false;
+            REP(x,strlen(c)) {
+                if (c[x] == ' ' && conv) c[x] = '_';
+                if (c[x] == '"') conv = !conv;
             }
-            istringstream ss(s[i]);
+            REP(x,strlen(c))
+                if (c[x] == '(' || c[x] == ')' || c[x] == '"' || c[x] == ',') c[x] = ' ';
+            istringstream sin(c);
+            sin >> a[i].type;
+            if (a[i].type == "throw") sin >> a[i].arg1;
+            else {
+                if (a[i].type == "catch") sin >> a[i].arg1 >> a[i].arg2;
+                REP(x,a[i].arg2.length())
+                if (a[i].arg2[x] == '_') a[i].arg2[x] = ' ';
+            }
+        }
+        int top = 0;
+        FOR(i,1,n) {
+            if (a[i].type == "try") {
+                st[++top] = i;
+            }
+            else if (a[i].type == "catch") {
+                match[i] = st[top];
+                match[st[top]] = i;
+                --top;
+            }
+        }
 
-            string command; ss >> command;
-            if (command == "try") {
-                st.pop();
-            }
-            else if (command == "throw") {
-                string typ; ss >> typ;
-                while (!st.empty() && st.top().first != typ) {
-                    st.pop();
-                }
-                if (st.empty()) puts("Unhandled Exception");
-                else puts(st.top().second.c_str());
-                break;
-            }
-            else if (command == "catch") {
-                string typ; ss >> typ;
-                string msg = "";
-                bool need = false;
-                REP(t,s[i].length()) {
-                    if (s[i][t] == '"') need = !need;
-                    else {
-                        if (need) msg += s[i][t];
-                    }
-                }
-                st.push(make_pair(typ, msg));
-            }
+        int u = 0;
+        bool ok = false;
+        FOR(i,1,n) if (a[i].type == "throw") u = i;
+
+        FORD(i,u-1,1)
+        if (a[i].type == "try" && match[i] > u && a[match[i]].arg1 == a[u].arg1) {
+            puts(a[match[i]].arg2.c_str());
+            ok = true;
+            break;
         }
+        if (!ok) puts("Unhandled Exception");
     }
     return 0;
 }
