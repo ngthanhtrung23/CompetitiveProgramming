6ddd0bbc3e4cd0094df337966ee00aec9495d316
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-07-31 15:33:00 +0800

    Add solution for problem `A - Vasya and the Bus` Link: https://codeforces.com/contest/190/problem/A Tags: greedy, math, *1100 Ref: https://codeforces.com/contest/190/submission/1963858

--- codeforces/190/A.cpp
+++ codeforces/190/A.cpp
@@ -1,29 +1,54 @@
-
-#include <bits/stdc++.h>
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
 
+const double PI = acos(-1.0);
+
+int getMin(int a, int b) {
+	b -= min(a, b);
+	return a + b;
+}
+
+int getMax(int a, int b) {
+	int res = a;
+	if (b) res += b-1;
+	return res;
+}
+
 int main() {
-    ios :: sync_with_stdio(false);
-    int grown, kid;
-    while (cin >> grown >> kid) {
-        if (grown == 0 && kid) cout << "Impossible" << endl;
-        else {
-            // min
-            cout << grown + max(0, kid - grown) << ' ';
-            // max
-            cout << grown + max(0, kid - 1) << endl;
-        }
-    }
+	int grow, child;
+	while (cin >> grow >> child) {
+		if (child && !grow) {
+			puts("Impossible");
+			continue;
+		}
+		cout << getMin(grow, child) << ' ' << getMax(grow, child) << endl;
+	}
     return 0;
 }
