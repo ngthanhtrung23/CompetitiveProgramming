e1148f98b7afa972b1ca0f02efe3724ba48a68e7
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-07-31 15:50:00 +0800

    Add solution for problem `C - STL` Link: https://codeforces.com/contest/190/problem/C Tags: dfs and similar, *1500 Ref: https://codeforces.com/contest/190/submission/1963927

--- codeforces/190/C.cpp
+++ codeforces/190/C.cpp
@@ -1,58 +1,83 @@
-
-#include <bits/stdc++.h>
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
 using namespace std;
+const double PI = acos(-1.0);
 
-const int MN = 100111;
-string s[MN];
-int n;
+struct Node {
+	string type;
+	Node *left, *right;
+} *root;
 
-int go(int start, bool trace) {
-    if (start > n) return 0;
+int n, cnt = 0;
 
-    if (s[start] == "int") {
-        if (trace) cout << "int";
-        return start + 1;
-    }
-    else {
-        if (trace) cout << "pair<";
-        int part2 = go(start+1, trace);
-        if (!part2) return 0;
+Node* get() {
+	string s;
+	if (!(cin >> s)) return NULL;
+	Node *p = new Node;
+	p->type = s;
+	p->left = p->right = NULL;
 
-        if (trace) cout << ',';
-        int res = go(part2, trace);
-        if (!res) return 0;
-        if (trace) cout << '>';
-
-        return res;
-    }
+	if (s == "int") {
+		++cnt;
+		return p;
+	}
+	else {
+		p->left = get();
+		if (p->left == NULL) return NULL;
+		p->right = get();
+		if (p->right == NULL) return NULL;
+		return p;
+	}
 }
 
-char tmp[1000111];
+void print(Node *root) {
+	if (root->type == "int") {
+		cout << root->type;
+		return ;
+	}
+	cout << "pair<";
+	print(root->left);
+	cout << ",";
+	print(root->right);
+	cout << ">";
+}
 
 int main() {
-    int need;
-    while (scanf("%d\n", &need) == 1) {
-        gets(tmp);
-        istringstream ss(tmp);
-        n = 0;
-        while (ss >> s[n+1]) ++n;
-
-        if (go(1, false) == n+1) {
-            go(1, true);
-            cout << endl;
-        }
-        else cout << "Error occurred" << endl;
-    }
+	ios :: sync_with_stdio();
+	cin >> n;
+	root = get();
+	string s;
+	if (root == NULL || cnt != n || (cin >> s)) {
+		puts("Error occurred");
+		return 0;
+	}
+	print(root);
+	cout << endl;
     return 0;
 }
