e4eb0a1c2d17416a162d618db3b8e1256d21b2fe
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-04 17:01:00 +0800

    Add solution for problem `B - Vasya's Calendar` Link: https://codeforces.com/contest/182/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/182/submission/1975451

---
+++ codeforces/182/B.cpp
@@ -0,0 +1,44 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+using namespace std;
+
+const double PI = acos(-1.0);
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int d, n;
+    while (cin >> d) {
+        long long res = 0;
+        cin >> n;
+        int a;
+        FOR(i,1,n) {
+            cin >> a;
+            if (i < n) res += d - a;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
