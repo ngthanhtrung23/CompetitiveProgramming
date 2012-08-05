57c983f28c33b9c7a9f997e612dfb31cef076c14
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-05 22:02:00 +0800

    Add solution for problem `A - Funky Numbers` Link: https://codeforces.com/contest/192/problem/A Tags: binary search, brute force, implementation, *1300 Ref: https://codeforces.com/contest/192/submission/1978285

---
+++ codeforces/192/A.cpp
@@ -0,0 +1,45 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+set<long long> s;
+
+int main() {
+    FOR(i,1,100000)
+        s.insert(i*(i+1LL) / 2);
+    long long n; cin >> n;
+    for(set<long long> :: iterator it = s.begin(); it != s.end(); it++)
+        if (s.find(n-*it) != s.end()) {
+            puts("YES");
+            return 0;
+        }
+    puts("NO");
+    return 0;
+}
