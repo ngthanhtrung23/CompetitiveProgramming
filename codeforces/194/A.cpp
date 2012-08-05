f92dedfb907b6a722f16d45b0414ed8a9ed01bfd
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-05 21:41:00 +0800

    Add solution for problem `A - Exams` Link: https://codeforces.com/contest/194/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/194/submission/1978238

---
+++ codeforces/194/A.cpp
@@ -0,0 +1,45 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+bool can(int n, int k) {
+    return k >= 3*n && k <= 5*n;
+}
+
+int main() {
+    int n, k; cin >> n >> k;
+    FOR(res,0,n) {
+        if (can(n-res, k-2*res)) {
+            cout << res << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
