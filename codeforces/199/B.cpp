4206255fac9db1d31dcd70536ce36ffeaeb1b77d
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-05 18:58:00 +0800

    Add solution for problem `B - Special Olympics` Link: https://codeforces.com/contest/199/problem/B Tags: geometry, *1900 Ref: https://codeforces.com/contest/199/submission/1978011

--- codeforces/199/B.cpp
+++ codeforces/199/B.cpp
@@ -1,96 +1,79 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
-#include <bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
-#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
-
-#define DEBUG(X) { cout << #X << " = " << X << endl; }
-#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
-#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
 #define ll long long
-#define SZ(x) ((int) (x).size())
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-#define double long double
-const double EPS = 1e-6;
-
-int cmp(double a, double b) {
-    if (fabs(a - b) < EPS) return 0;
-    if (a < b) return -1;
-    return 1;
-}
-
 struct Point {
-    double x, y;
-    Point() {}
-    Point(double x, double y) : x(x), y(y) {}
+    long long x, y;
+    Point() { x = y = 0; }
+    Point(long long x, long long y) : x(x), y(y) {}
 
+    Point operator + (Point a) { return Point(x+a.x, y+a.y); }
     Point operator - (Point a) { return Point(x-a.x, y-a.y); }
 
-    double len() { return sqrt(x*x + y*y); }
+    long long sqlen() { return x*x + y*y; }
+    long double len() { return sqrt(sqlen()); }
 
-    friend istream& operator >> (istream& cin, Point& p) {
-        cin >> p.x >> p.y;
-        return cin;
+    void read() { cin >> x >> y; }
+} A, B;
+
+long long r1, r2, R1, R2;
+
+bool inside(Point A, long long r1, Point B, long long r2) {
+    return r2 + 1e-9 > (B-A).len() + r1;
+}
+
+bool cut(Point A, long long r1, Point B, long long r2) {
+    if (inside(A, r1, B, r2)) return false;
+    if (inside(B, r2, A, r1)) return false;
+    return (A-B).len() < r1 + r2 - 1e-9;
+}
+
+bool check(Point A, long long r, Point B, long long r2, long long R2) {
+    if (cut(A, r, B, r2)) return false;
+    if (cut(A, r, B, R2)) return false;
+    if (inside(A, r, B, R2)) {
+        if (inside(B, r2, A, r)) return false;
+        if (!cut(A, r, B, r2) && !inside(A, r, B, r2) && !inside(B, r2, A, r)) return false;
     }
-};
-
-struct Circle : Point {
-    double r;
-} A1, A2, B1, B2;
-
-bool inside(Circle a, Circle b) {  // b in a
-    return cmp((a - b).len() + b.r, a.r) <= 0;
-}
-
-bool outside(Circle a, Circle b) {
-    return cmp((a - b).len(), a.r + b.r) >= 0;
-}
-
-bool intersect(Circle a, Circle b) {
-    if (a.r <= b.r) swap(a, b);  // now a is bigger circle
-
-    // b inside a
-    if (inside(a, b)) return false;
-
-    // b outside a
-    if (outside(a, b)) return false;
     return true;
 }
 
-bool in_middle(Circle a, Circle x, Circle y) {
-    if (outside(a, x) && inside(y, a)) return true;
-    if (inside(a, x) && inside(y, a)) return true;
-    return false;
-}
-
-bool check(Circle a, Circle b1, Circle b2) {
-    return !intersect(a, b1)
-        && !intersect(a, b2)
-        && !in_middle(a, b1, b2);
-}
-
 int main() {
-    ios :: sync_with_stdio(0); cin.tie(0);
-    double ra, RA, rb, RB;
-    while (cin >> A1 >> ra >> RA >> B1 >> rb >> RB) {
-        A2 = A1;
-        B2 = B1;
+    A.read(); cin >> r1 >> R1;
+    B.read(); cin >> r2 >> R2;
 
-        A1.r = ra;
-        A2.r = RA;
-
-        B1.r = rb;
-        B2.r = RB;
-
-        int res = 0;
-        if (check(A1, B1, B2)) ++res;
-        if (check(A2, B1, B2)) ++res;
-
-        if (check(B1, A1, A2)) ++res;
-        if (check(B2, A1, A2)) ++res;
-        cout << res << endl;
-    }
+    int res = 0;
+    if (check(A, r1, B, r2, R2)) ++res;
+    if (check(A, R1, B, r2, R2)) ++res;
+    if (check(B, r2, A, r1, R1)) ++res;
+    if (check(B, R2, A, r1, R1)) ++res;
+    cout << res << endl;
+    return 0;
 }
