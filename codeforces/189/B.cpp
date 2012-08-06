893df768f9de42f8259e23cc760f0d1d96c74a08
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-06 22:33:00 +0800

    Add solution for problem `B - Counting Rhombi` Link: https://codeforces.com/contest/189/problem/B Tags: brute force, math, *1300 Ref: https://codeforces.com/contest/189/submission/1980749

---
+++ codeforces/189/B.cpp
@@ -0,0 +1,42 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+const double PI = acos(-1.0);
+
+int main() {
+    long long res = 0;
+    int a, b; cin >> a >> b;
+    for(int x = 2; x <= a; x += 2)
+        for(int y = 2; y <= b; y += 2)
+            res += (a - x + 1) * (ll) (b - y + 1);
+    cout << res << endl;
+    return 0;
+}
