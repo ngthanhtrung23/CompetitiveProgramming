4c24547d4c2ef7d3dd5261e9dd58ea08fdc791c8
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-06 23:12:00 +0800

    Add solution for problem `A - Comparing Strings` Link: https://codeforces.com/contest/186/problem/A Tags: implementation, strings, *1100 Ref: https://codeforces.com/contest/186/submission/1980823

--- codeforces/186/A.cpp
+++ codeforces/186/A.cpp
@@ -1,39 +1,61 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
+
+const double PI = acos(-1.0);
+
+string a, b;
 
 int main() {
     ios :: sync_with_stdio(false);
-    string a, b;
-    while (cin >> a >> b) {
-        int cnt = 0;
-        int u = -1, v = -1;
-        int n = a.length();
-
-        if (a.length() != b.length()) {
-            puts("NO");
-            continue;
-        }
-
-        REP(i,n) if (a[i] != b[i]) {
-            if (cnt == 0) u = i;
-            else if (cnt == 1) v = i;
-            ++cnt;
-        }
-
-        if (cnt == 1 || cnt > 2 || a[u] != b[v] || a[v] != b[u]) puts("NO");
-        else puts("YES");
+    cin >> a >> b;
+    if (a.length() != b.length()) {
+        puts("NO");
+        return 0;
     }
+    int n = a.length();
+    int first, second, cnt = 0;
+
+    REP(i,n) if (a[i] != b[i]) {
+        ++cnt;
+        if (cnt == 1) first = i;
+        else if (cnt == 2) second = i;
+    }
+    if (cnt != 2) {
+        puts("NO");
+        return 0;
+    }
+    if (a[first] != b[second] || a[second] != b[first]) {
+        puts("NO");
+        return 0;
+    }
+    puts("YES");
     return 0;
 }
