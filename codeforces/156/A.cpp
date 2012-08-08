ffd9605c440b1185133d90859c648ea216c0a2a0
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-08 18:11:00 +0800

    Add solution for problem `A - Message` Link: https://codeforces.com/contest/156/problem/A Tags: brute force, *1700 Ref: https://codeforces.com/contest/156/submission/1992679

---
+++ codeforces/156/A.cpp
@@ -0,0 +1,50 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+const double PI = acos(-1.0);
+
+char a[4011], b[4011];
+
+int main() {
+    while (gets(a) && gets(b)) {
+        int n = strlen(a), m = strlen(b);
+
+        int res = m;
+        FOR(i,-m,n) {
+            int cur = 0;
+            REP(j,m)
+                if (i+j < 0 || i+j >= n || b[j] != a[i+j]) ++cur;
+            res = min(res, cur);
+        }
+        printf("%d\n", res);
+    }
+    return 0;
+}
