7f126abca234d893c6126e562fa4bb4d333e50f3
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-13 10:56:00 +0800

    Add solution for problem `C - Registration System` Link: https://codeforces.com/contest/4/problem/C Tags: data structures, hashing, implementation, *1300 Ref: https://codeforces.com/contest/4/submission/2003104

---
+++ codeforces/4/C.cpp
@@ -0,0 +1,56 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+const double PI = acos(-1.0);
+
+map<string,int> m;
+int n;
+char tmp[111];
+
+int main() {
+    while (scanf("%d\n", &n) == 1) {
+        m.clear();
+        while (n--) {
+            gets(tmp);
+            string s(tmp);
+            if (m.find(s) == m.end()) {
+                puts("OK");
+                m[s] = 1;
+            }
+            else {
+                cout << s << m[s];
+                puts("");
+                m[s]++;
+            }
+        }
+    }
+    return 0;
+}
