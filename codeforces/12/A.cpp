e43851c953a8c0d546adbef99247f3cd1809a1ad
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-13 16:47:00 +0800

    Add solution for problem `A - Super Agent` Link: https://codeforces.com/contest/12/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/12/submission/2003695

---
+++ codeforces/12/A.cpp
@@ -0,0 +1,46 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+const double PI = acos(-1.0);
+
+char a[11][11];
+
+int main() {
+    while (gets(a[0]) && gets(a[1]) && gets(a[2])) {
+        bool ok = true;
+        REP(i,3) REP(j,3) {
+            if (a[i][j] != a[2-i][2-j]) ok = false;
+        }
+        if (ok) puts("YES");
+        else puts("NO");
+    }
+    return 0;
+}
