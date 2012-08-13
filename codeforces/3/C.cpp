a9d209e8aff4b7eed33f7e7d99f9622931f1132c
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-13 14:25:00 +0800

    Add solution for problem `C - Tic-tac-toe` Link: https://codeforces.com/contest/3/problem/C Tags: brute force, games, implementation, *1800 Ref: https://codeforces.com/contest/3/submission/2003376

--- codeforces/3/C.cpp
+++ codeforces/3/C.cpp
@@ -1,80 +1,91 @@
-#include <set>
-#include <map>
-#include <list>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <cstring>
 #include <sstream>
 #include <iomanip>
-#include <complex>
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
 #include <algorithm>
-
-#include <ctime>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
 #include <deque>
-#include <bitset>
-#include <cctype>
-#include <utility>
-#include <cassert>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-#define F0R(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define F0RD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-#define SZ(S) ((int) ((S).size()))
+const double PI = acos(-1.0);
 
-#define DEBUG(x) { cout << #x << " = " << x << endl; }
-#define PR(a,n) { cout << #a << " = "; F0R(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-char a[5][5];
+char a[11][11];
 
 int count(char c) {
     int res = 0;
-    F0R(i,1,3) F0R(j,1,3) if (a[i][j] == c) ++res;
+    REP(i,3) REP(j,3)
+        if (a[i][j] == c) ++res;
     return res;
 }
 
-bool win(char c) {
-    F0R(i,1,3) {
-        if (a[i][1] == c && a[i][2] == c && a[i][3] == c) return true;
-        if (a[1][i] == c && a[2][i] == c && a[3][i] == c) return true;
+bool check(char c) {
+    REP(i,3) {
+        if (a[i][0] == c && a[i][1] == c && a[i][2] == c) return true;
+        if (a[0][i] == c && a[1][i] == c && a[2][i] == c) return true;
     }
-    if (a[1][1] == c && a[2][2] == c && a[3][3] == c) return true;
-    if (a[1][3] == c && a[2][2] == c && a[3][1] == c) return true;
+    if (a[0][0] == c && a[1][1] == c && a[2][2] == c) return true;
+    if (a[2][0] == c && a[1][1] == c && a[0][2] == c) return true;
     return false;
 }
 
 int main() {
-    ios :: sync_with_stdio(false); cin.tie(NULL);
-    cout << (fixed) << setprecision(6);
-    while (cin >> a[1][1]) {
-        F0R(i,1,3) F0R(j,1,3) if (i > 1 || j > 1) cin >> a[i][j];
+    while (gets(a[0])) {
+        gets(a[1]); gets(a[2]);
 
         int nx = count('X');
         int no = count('0');
+        int turn;
 
-        if (!(nx == no || nx == no + 1)) cout << "illegal" << endl;
+        if (nx == no+1) turn = 0;
+        else if (nx == no) turn = 1;
         else {
-            bool winx = win('X');
-            bool wino = win('0');
-
-            if (winx && wino) cout << "illegal" << endl;
-            else if (winx && nx != no + 1) cout << "illegal" << endl;
-            else if (wino && nx != no) cout << "illegal" << endl;
-            else if (winx) cout << "the first player won" << endl;
-            else if (wino) cout << "the second player won" << endl;
-            else if (nx + no == 9) cout << "draw" << endl;
-            else if (nx == no) cout << "first" << endl;
-            else cout << "second" << endl;
+            puts("illegal");
+            continue;
         }
+
+        if (check('X') && check('0')) {
+            puts("illegal");
+            continue;
+        }
+
+        if (check('X')) {
+            if (turn == 1) puts("illegal");
+            else puts("the first player won");
+            continue;
+        }
+        if (check('0')) {
+            if (turn == 0) puts("illegal");
+            else puts("the second player won");
+            continue;
+        }
+        if (count('.') == 0) {
+            puts("draw");
+            continue;
+        }
+        if (turn == 0) puts("second");
+        else puts("first");
     }
     return 0;
 }
-
