c55687867422a2106c2fb0070204874c68b7f535
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-13 19:33:00 +0800

    Add solution for problem `C - Longest Regular Bracket Sequence` Link: https://codeforces.com/contest/5/problem/C Tags: constructive algorithms, data structures, dp, greedy, sortings, strings, *1900 Ref: https://codeforces.com/contest/5/submission/2004081

--- codeforces/5/C.cpp
+++ codeforces/5/C.cpp
@@ -1,83 +1,85 @@
-#include <set>
-#include <map>
-#include <list>
-#include <cmath>
-#include <queue>
-#include <stack>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <cstring>
 #include <sstream>
 #include <iomanip>
-#include <complex>
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
 #include <algorithm>
-
-#include <ctime>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
 #include <deque>
-#include <bitset>
-#include <cctype>
-#include <utility>
-#include <cassert>
-using namespace std;
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-#define SZ(S) ((int) ((S).size()))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = " << x << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
 
+const double PI = acos(-1.0);
 const int MN = 1000111;
-string s;
-stack<int> st;
-int match[MN], start[MN];
+
+int sum[MN], r[MN], st[MN], top;
+char tmp[MN];
+vector<int> ls[MN*2];
 
 int main() {
-    ios :: sync_with_stdio(false); cin.tie(NULL);
-    cout << (fixed) << setprecision(6);
-    while (cin >> s) {
-        while (!st.empty()) st.pop();
-        REP(i,s.length()) {
-            if (s[i] == '(') st.push(i);
-            else {
-                if (st.empty()) match[i] = -1;
-                else {
-                    int u = st.top(); st.pop();
-                    match[i] = u;
-                    match[u] = i;
-                }
-            }
+    while (gets(tmp)) {
+        int n = strlen(tmp);
+        string s = ' ' + string(tmp);
+
+        FOR(i,0,2000000) ls[i].clear();
+
+        sum[0] = 1000000; ls[sum[0]].PB(0);
+        FOR(i,1,n) {
+            if (s[i] == '(') sum[i] = sum[i-1] + 1;
+            else sum[i] = sum[i-1] - 1;
+            ls[sum[i]].PB(i);
         }
 
-        memset(start, -1, sizeof start);
-        REP(i,s.length()) {
-            if (s[i] == ')' && match[i] >= 0) {
-                int j = match[i];
-                start[i] = j;
-
-                if (j > 0 && start[j-1] >= 0) start[i] = start[j-1];
-            }
+        top = 0; st[0] = n+1;
+        FORD(i,n,0) {
+            while (top && sum[st[top]] >= sum[i]) --top;
+            r[i] = st[top];
+            st[++top] = i;
         }
 
         int res = 0, cnt = 1;
-        REP(i,s.length()) {
-            if (s[i] == ')' && match[i] >= 0) {
-                int j = start[i];
-                int cur = i - j + 1;
-                if (cur > res) {
-                    res = cur;
-                    cnt = 1;
-                }
-                else if (cur == res) ++cnt;
+        FOR(i,0,n-1) {
+            int j = r[i] - 1;
+            if (j <= i) continue;
+            int u, sz = ls[sum[i]].size();
+            if (ls[sum[i]][sz-1] <= j) {
+                u = ls[sum[i]][sz-1];
+            }
+            else {
+                u = lower_bound(ls[sum[i]].begin(), ls[sum[i]].end(), j+1) - ls[sum[i]].begin();
+                if (u == 0) continue;
+                u = ls[sum[i]][u-1];
+            }
+            if (u - i > res) {
+                res = u - i;
+                cnt = 1;
+            }
+            else if (u - i == res) {
+                ++cnt;
             }
         }
-        cout << res << ' ' << cnt << endl;
+        if (res == 0) cnt = 1;
+        printf("%d %d\n", res, cnt);
     }
     return 0;
 }
