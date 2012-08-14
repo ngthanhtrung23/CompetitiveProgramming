43fb5375b454064c0e60c755ad0f7c9c9bef608e
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-08-14 23:34:00 +0800

    Add solution for problem `A - Tiling with Hexagons` Link: https://codeforces.com/contest/216/problem/A Tags: implementation, math, *1200 Ref: https://codeforces.com/contest/216/submission/2006490

---
+++ codeforces/216/A.cpp
@@ -0,0 +1,44 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+const double PI = acos(-1.0);
+
+int main() {
+    int a, b, c;
+    while (cin >> a >> b >> c) {
+        int res = b*c;
+        FOR(i,2,a) {
+            res += b+c-1;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
