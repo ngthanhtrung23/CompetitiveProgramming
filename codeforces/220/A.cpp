a397a1389ca15f10ca5e6d55fb556a0ca16389a0
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-01 01:18:00 +0800

    Add solution for problem `A - Little Elephant and Problem` Link: https://codeforces.com/contest/220/problem/A Tags: implementation, sortings, *1300 Ref: https://codeforces.com/contest/220/submission/2081671

--- codeforces/220/A.cpp
+++ codeforces/220/A.cpp
@@ -1,51 +1,48 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+const long double PI = acos((long double) -1.0);
 const int MN = 100111;
 
-int n, a[MN];
-
-bool isIncreasing() {
-    FOR(i,1,n-1) if (a[i] > a[i+1]) return false;
-    return true;
-}
+int n, a[MN], b[MN];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    cout << (fixed) << setprecision(9);
-    while (cin >> n) {
-        FOR(i,1,n) cin >> a[i];
-        if (isIncreasing()) cout << "YES";
-        else {
-            int x = -1;
-            FOR(i,1,n-1) if (a[i] > a[i+1]) {
-                x = i;
-                break;
-            }
-            while (x > 1 && a[x-1] == a[x]) --x;
-            int y = x + 1;
-
-            FOR(i,x+1,n) if (a[i] < a[x]) y = i;
-
-            swap(a[x], a[y]);
-
-            if (isIncreasing()) cout << "YES";
-            else cout << "NO";
-        }
-        cout << endl;
+    scanf("%d", &n);
+    FOR(i,1,n) {
+        scanf("%d", &a[i]);
+        b[i] = a[i];
     }
+    sort(b+1, b+n+1);
+    int cnt = 0;
+    FOR(i,1,n) if (b[i] != a[i]) ++cnt;
+    if (cnt <= 2) puts("YES"); else puts("NO");
     return 0;
 }
-
