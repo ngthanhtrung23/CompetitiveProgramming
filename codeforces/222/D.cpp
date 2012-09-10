25e1d3934190f18a5c4bdd496722f110ad6af358
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-11 00:07:00 +0800

    Add solution for problem `D - Olympiad` Link: https://codeforces.com/contest/222/problem/D Tags: binary search, greedy, sortings, two pointers, *1900 Ref: https://codeforces.com/contest/222/submission/2111256

--- codeforces/222/D.cpp
+++ codeforces/222/D.cpp
@@ -1,40 +1,78 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (feof(stdin)) memset(BUF, 0, sizeof BUF); else fread(BUF,1,BUFSIZE,stdin); \
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
 const int MN = 100111;
-int n, a[MN], b[MN], x;
+
+int n, x, a[MN], b[MN];
+multiset<int> s;
 
 int main() {
-    ios :: sync_with_stdio(false);
-    while (scanf("%d%d", &n, &x) == 2) {
-        FOR(i,1,n) scanf("%d", &a[i]);
-        FOR(i,1,n) scanf("%d", &b[i]);
-        sort(a+1, a+n+1);
-        multiset<int> s;
-        FOR(i,1,n) s.insert(b[i]);
-
-        int res = 0;
-        FOR(i,1,n) {
-            int need = x - a[i];
-            auto it = s.lower_bound(need);
-            if (it == s.end()) continue;
-
-            ++res;
-            s.erase(it);
-        }
-        cout << 1 << ' ' << res << endl;
+    GN(n); GN(x);
+    FOR(i,1,n) GN(a[i]);
+    sort(a+1, a+n+1);
+    FOR(i,1,n) {
+        GN(b[i]);
+        s.insert(b[i]);
     }
+    printf("1 ");
+    int cnt = 0;
+    FORD(i,n,1) {
+        multiset<int> :: iterator it = s.lower_bound(x - a[i]);
+        if (it == s.end()) continue;
+        s.erase(it);
+        ++cnt;
+    }
+    printf("%d\n", cnt);
     return 0;
 }
