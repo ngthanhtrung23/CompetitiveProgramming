005c3ae7f4856fc8ef926b3593cce0202dd292f9
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-11 00:18:00 +0800

    Add solution for problem `E - Decoding Genome` Link: https://codeforces.com/contest/222/problem/E Tags: dp, matrices, *1900 Ref: https://codeforces.com/contest/222/submission/2112472

--- codeforces/222/E.cpp
+++ codeforces/222/E.cpp
@@ -1,93 +1,120 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-bool can[55][55];
+//Buffer reading
+int INP,AM;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (feof(stdin)) memset(BUF, 0, sizeof BUF); else fread(BUF,1,BUFSIZE,stdin); \
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-int getId(char c) {
+char getAlpha() {
+    char c = ' '; while (c < 'A' || c > 'z') GETCHAR(c);
+    return c;
+}
+
+int value(char c) {
     if (c >= 'a' && c <= 'z') return c - 'a';
     else return c - 'A' + 26;
 }
 
+const long double PI = acos((long double) -1.0);
+const long long MOD = 1000000007;
+
+struct Matrix {
+    long long x[55][55];
+} I, A, p;
 long long n;
 int m, k;
 
-const long long MOD = 1e9 + 7;
-struct Matrix {
-    int m, n;
-    long long x[55][55];
-
-    void print() {
-        cout << "m = " << m << ", n = " << n << endl;
-        REP(i,m) {
-            REP(j,n) cout << x[i][j] << ' ';
-            cout << endl;
-        }
-    }
-} trans[66], start, target, T;
-
-Matrix operator * (const Matrix& a, const Matrix& b) {
-    Matrix c;
-    c.m = a.m; c.n = b.n;
-
-    REP(i,c.m) REP(j,c.n) {
+Matrix operator * (Matrix a, Matrix b) {
+    static Matrix c;
+    REP(i,m) REP(j,m) {
         c.x[i][j] = 0;
-        REP(k,a.n)
+        REP(k,m)
             c.x[i][j] = (c.x[i][j] + a.x[i][k] * b.x[k][j]) % MOD;
     }
     return c;
 }
 
-int main() {
-    ios :: sync_with_stdio(false);
-    while (cin >> n >> m >> k) {
-        memset(can, true, sizeof can);
-        FOR(i,1,k) {
-            string s; cin >> s;
-            int u = getId(s[0]);
-            int v = getId(s[1]);
-
-            can[u][v] = false;
-        }
-        memset(trans[0].x, 0, sizeof trans[0].x);
-        trans[0].m = trans[0].n = m;
-
-        REP(to,m) REP(from,m)
-            if (can[from][to])
-                trans[0].x[to][from] = 1;
-        FOR(i,1,62) trans[i] = trans[i-1] * trans[i-1];
-
-//        trans[0].print();
-
-        start.m = m; start.n = 1;
-        REP(i,m) start.x[i][0] = 1;
-
-        Matrix T; T.m = T.n = m;
-        REP(i,m) REP(j,m) T.x[i][j] = (i == j) ? 1 : 0;
-
-        --n;
-        REP(t,63)
-            if ((n >> t) & 1) {
-                T = T * trans[t];
-            }
-        target = T * start;
-        long long res = 0;
-        REP(i,m)
-            res = (res + target.x[i][0]) % MOD;
-
-        cout << res << endl;
-    }
-    return 0;
+Matrix power(long long k) {
+    if (k == 0) return I;
+    if (k == 1) return A;
+    Matrix mid = power(k >> 1);
+    mid = mid * mid;
+    if (k & 1) return mid * A;
+    else return mid;
 }
 
+bool can[55][55];
+long long f[55], g[55];
+
+int main() {
+    GN(n); GN(m); GN(k);
+    REP(i,m) I.x[i][i] = 1;
+    memset(can, true, sizeof can);
+    while (k--) {
+        char c = getAlpha();
+        int i = value(c);
+        c = getAlpha();
+        int j = value(c);
+        can[i][j] = false;
+    }
+    REP(i,m) REP(j,m)
+        if (can[i][j]) A.x[i][j] = 1;
+        else A.x[i][j] = 0;
+
+    p = power(n-1);
+    REP(i,m) f[i] = 1;
+    long long res = 0;
+    REP(i,m) {
+        g[i] = 0;
+        REP(j,m) g[i] = (g[i] + p.x[i][j] * f[j]) % MOD;
+        res = (res + g[i]) % MOD;
+    }
+    cout << res << endl;
+    return 0;
+}
