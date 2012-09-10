e5bd7e1b6b2c01aea1c3680115520d94efb13997
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-10 23:54:00 +0800

    Add solution for problem `A - Shooshuns and Sequence ` Link: https://codeforces.com/contest/222/problem/A Tags: brute force, implementation, *1200 Ref: https://codeforces.com/contest/222/submission/2109612

---
+++ codeforces/222/A.cpp
@@ -0,0 +1,82 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (feof(stdin)) memset(BUF, 0, sizeof BUF); else fread(BUF,1,BUFSIZE,stdin); \
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int n, k, a[200111];
+int cnt[100111];
+
+int main() {
+    GN(n); GN(k);
+    FOR(i,1,n) {
+        GN(a[i]);
+        ++cnt[a[i]];
+    }
+    if (cnt[a[1]] == n) {
+        puts("0");
+        return 0;
+    }
+    int now = k;
+    FOR(i,1,n) {
+        a[n+i] = a[now];
+        ++cnt[a[now]];
+        --cnt[a[i]];
+        if (cnt[a[now]] == n) {
+            printf("%d\n", i);
+            return 0;
+        }
+        ++now;
+    }
+    puts("-1");
+    return 0;
+}
