20d33aee26d326bc7492a28edc7716bb793bae3d
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-17 01:56:00 +0800

    Add solution for problem `C - Partial Sums` Link: https://codeforces.com/contest/223/problem/C Tags: combinatorics, math, number theory, *1900 Ref: https://codeforces.com/contest/223/submission/2198565

---
+++ codeforces/223/C.cpp
@@ -0,0 +1,99 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (feof(stdin)) memset(BUF, 0, sizeof BUF); else fread(BUF,1,BUFSIZE,stdin); \
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const long long MOD = 1000000007;
+
+int k, n, f[2011][2011], rev[2011];
+
+long long power(long long x, int k) {
+    if (k == 0) return 1;
+    if (k == 1) return x % MOD;
+    long long mid = power(x, k >> 1);
+    mid = (mid * mid) % MOD;
+    if (k & 1) return (mid * x) % MOD;
+    else return mid;
+}
+
+int main() {
+    FOR(i,1,2000) {
+        rev[i] = power(i, MOD - 2);
+    }
+
+    while (scanf("%d%d", &n, &k) == 2) {
+        FOR(i,1,n) scanf("%d", &f[0][i]);
+        FOR(i,1,n-1) {
+            FOR(j,1,n) f[i][j] = (f[i-1][j] + f[i][j-1]) % MOD;
+        }
+
+        if (k <= n-1) {
+            FOR(j,1,n) printf("%d ", f[k][j]);
+            puts("");
+            continue;
+        }
+
+        FOR(j,1,n) {
+            int ii = 0;
+            long long x = 0, c = 1;
+            long long res = 0;
+            FORD(jj,j,1) {
+                res = (res + c * f[ii][jj]) % MOD;
+                c = c * rev[x+1] % MOD * (k-x) % MOD;
+                ++x; ++ii;
+            }
+            printf("%d ", (int) res);
+        }
+        puts("");
+    }
+    return 0;
+}
