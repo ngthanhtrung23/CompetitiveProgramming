288d65b3dac87b77cb69970df0e49ba10da9db52
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-19 17:15:00 +0800

    Add solution for problem `D - D` Link: https://codeforces.com/gym/100070/problem/D Tags: Ref: https://codeforces.com/gym/100070/submission/2211574

---
+++ codeforces/100070/D.cpp
@@ -0,0 +1,77 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (feof(stdin)) memset(BUF, 0, sizeof BUF); else fread(BUF,1,BUFSIZE,stdin); \
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+const int MOD = 1743;
+int n, k;
+int cnt[MOD + 10];
+
+int main() {
+    freopen("kth.in", "r", stdin);
+    freopen("kth.out", "w", stdout);
+    GN(n); GN(k);
+    FOR(i,1,n) {
+        long long t = i % MOD;
+        int a;
+        a = (132 * t * t * t + 77 * t * t + 1345 * t + 1577) % MOD;
+        cnt[a]++;
+    }
+    REP(i,MOD)
+    if (cnt[i] >= k) {
+        printf("%d\n", i);
+        return 0;
+    }
+    else k -= cnt[i];
+    return 0;
+}
