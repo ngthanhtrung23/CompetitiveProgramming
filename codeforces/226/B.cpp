0088de5f463b3d60676d3f1da44ce4d5120764e3
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-09-25 00:20:00 +0800

    Add solution for problem `B - Naughty Stone Piles` Link: https://codeforces.com/contest/226/problem/B Tags: greedy, *1900 Ref: https://codeforces.com/contest/226/submission/2241104

---
+++ codeforces/226/B.cpp
@@ -0,0 +1,102 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll unsigned long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const int MN = 100111;
+
+int n;
+unsigned long long a[MN], s[MN];
+
+int main() {
+    GN(n);
+    FOR(i,1,n) {
+        GN(a[i]);
+    }
+    sort(a+1, a+n+1);
+    FOR(i,1,n) s[i] = s[i-1] + a[i];
+    int q; GN(q);
+    long long rem = 0, now = 0;
+    FORD(i,n,1) {
+        rem += now * a[i];
+        ++now;
+    }
+    while (q--) {
+        int k; GN(k);
+        unsigned long long res = 0;
+        if (k == 1) {
+            res = rem;
+        }
+        else {
+            long long l = n, r = n;
+            unsigned long long now = 0, p = 1;
+            while (l >= 1 && r >= 1) {
+                res += (s[r] - s[l-1]) * now;
+                p *= k;
+                r = l - 1;
+                l -= p;
+                if (r >= 1 && l < 1) l = 1;
+                ++now;
+            }
+        }
+        cout << res << ' ';
+    }
+    cout << endl;
+    return 0;
+}
