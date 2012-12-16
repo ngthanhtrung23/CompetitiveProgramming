262824a7613c7357a2f26a20f1fb2bb401092662
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-12-17 03:21:00 +0800

    Add solution for problem `B - Mr. Bender and Square` Link: https://codeforces.com/contest/256/problem/B Tags: binary search, brute force, math, *1800 Ref: https://codeforces.com/contest/256/submission/2782793

---
+++ codeforces/256/B.cpp
@@ -0,0 +1,107 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+long long n, x, y, need;
+
+long long tri(long long n) {
+    if (n <= 0) return 0;
+    return n*(n+1LL) / 2;
+}
+
+long long get(long long t, long long x, long long y) {
+    return tri(t - (t - x) - (t - y));
+}
+
+bool check(long long t) {
+    ll low = x - t;
+    ll high = x + t;
+    ll left = y - t;
+    ll right = y + t;
+
+    if (low < 1) low = 1;
+    if (high > n) high = n;
+    if (left < 1) left = 1;
+    if (right > n) right = n;
+
+    long long can = (right - left + 1LL) * (high - low + 1LL);
+    can -= get(t, x-low, y-left);
+    can -= get(t, x-low, right-y);
+    can -= get(t, high-x, y-left);
+    can -= get(t, high-x, right-y);
+    return can >= need;
+}
+
+int main() {
+    while (cin >> n >> x >> y >> need) {
+        long long l = 0, r = n+n, res = r;
+        while (l <= r) {
+            long long mid = (l + r) >> 1;
+            if (check(mid)) {
+                res = mid;
+                r = mid - 1;
+            }
+            else l = mid + 1;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
