ef93046ed7ff01dddf470a92d6187333cc16d422
Trung Nguyen <ngthanhtrung23@gmail.com>
2012-12-30 15:09:00 +0800

    Add solution for problem `A - Adding Digits` Link: https://codeforces.com/contest/260/problem/A Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/260/submission/2855878

--- codeforces/260/A.cpp
+++ codeforces/260/A.cpp
@@ -1,31 +1,96 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int res[100111];
+
 int main() {
-    ios :: sync_with_stdio(false);
     int a, b, n;
     while (cin >> a >> b >> n) {
-        bool ok = false;
-        REP(add,10) if ((a * 10 + add) % b == 0) {
-            cout << a << add;
-            FOR(turn,2,n) cout << 0;
-            ok = true;
-            break;
+        int rem = a % b;
+        bool ok = true;
+
+        REP(turn,n) {
+            bool found = false;
+            FOR(add,0,9) {
+                if ((rem * 10 + add) % b == 0) {
+                    found = true;
+                    res[turn] = add;
+                    rem = (rem * 10 + add) % b;
+                    break;
+                }
+            }
+
+            if (!found) {
+                ok = false;
+                break;
+            }
         }
-        if (ok) cout << endl;
-        else cout << -1 << endl;
+
+        if (!ok) puts("-1");
+        else {
+            cout << a;
+            REP(turn,n) printf("%d", res[turn]);
+            puts("");
+        }
     }
     return 0;
 }
