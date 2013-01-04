5bc9d61e4b8c75d557261c5648cfc27ece4a1819
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-01-04 21:34:00 +0800

    Add solution for problem `C - Number Transformation` Link: https://codeforces.com/contest/251/problem/C Tags: dp, greedy, number theory, *2000 Ref: https://codeforces.com/contest/251/submission/2873383

--- codeforces/251/C.cpp
+++ codeforces/251/C.cpp
@@ -1,77 +1,127 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
 #define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-const int MN = 500111;
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-ll a, b;
-int k;
-ll dist[MN];
+const long double PI = acos((long double) -1.0);
 
-ll lcm(ll a, ll b) {
-    return a * b / __gcd(a, b);
+long long gcd(long long a, long long b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
 }
 
-void dijkstra(int lcm, int start) {
-    set< pair<ll, int> > s;
-    REP(i,lcm) dist[i] = 1000111000111000111LL;
-    dist[start] = 0;
-    s.insert(make_pair(0, start));
+long long lcm(long long a, long long b) {
+    return a * b / gcd(a, b);
+}
 
-    while (!s.empty()) {
-        ll l = s.begin()->first;
-        int u = s.begin()->second;
-        s.erase(s.begin());
+const int MN = 1000111;
 
-        if (dist[u] != l) continue;
+long long a, b, k, l;
+int d[MN], qu[MN], first, last;
 
-        FOR(i,1,k) {
-            int v = (i == 1) ? (u-1) : u - u % i;
-            if (dist[v] > dist[u] + 1) {
-                dist[v] = dist[u] + 1;
-                s.insert(make_pair(dist[v], v));
+void bfs(int start) {
+    qu[first = last = 1] = start;
+    memset(d, -1, sizeof d);
+    d[start] = 0;
+
+    while (first <= last) {
+        int u = qu[first++];
+        FOR(x,1,k) {
+            int v;
+            if (x == 1) v = u - 1;
+            else v = u - u % x;
+            if (d[v] < 0) {
+                d[v] = d[u] + 1;
+                qu[++last] = v;
             }
         }
     }
+    // PR0(d, l+1);
 }
 
 int main() {
-    ios :: sync_with_stdio(false);
-    cout << (fixed) << setprecision(9);
     while (cin >> a >> b >> k) {
-        ll x = 2;
-        FOR(i,2,k) x = lcm(x, i);
+        l = 1;
+        FOR(i,1,k) l = lcm(i, l);
+        // DEBUG(l);
 
-        ll ka = a / x, ua = a % x;
-        ll kb = b / x, ub = b % x;
+        bfs(l);
 
-        if (ka == kb) {
-            dijkstra(x, ua);
-            cout << dist[ub] << endl;
+        if (a % l == 0 && b % l == 0) {
+            long long need = (a - b) / l;
+            cout << need * d[0] << endl;
+        }
+        else if (a / l == b / l) {
+            a = a % l;
+            b = b % l;
+            bfs(a);
+            cout << d[b] << endl;
         }
         else {
-            dijkstra(x, ua);
-            ll res = dist[0];
+            long long aa = a - a % l;
+            long long bb = b - b % l + l;
 
-            ll turn = ka - kb - 1;
-            dijkstra(x, x-1);
-            ll each = dist[0] + 1;
+            long long need = (aa - bb) / l;
+            long long res = need * d[0];
+            res += d[b % l];
 
-            res += each * turn;
-            
-            res += 1 + dist[ub];
+            bfs(a % l);
+            res += d[0];
 
             cout << res << endl;
         }
