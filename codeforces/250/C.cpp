42c45229b72e261e8d7eb268ebc045cc8bfa7fa8
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-01-07 11:07:00 +0800

    Add solution for problem `C - Movie Critics` Link: https://codeforces.com/contest/250/problem/C Tags: greedy, *1600 Ref: https://codeforces.com/contest/250/submission/2879614

--- codeforces/250/C.cpp
+++ codeforces/250/C.cpp
@@ -1,32 +1,89 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-#define SZ(S) ((int) ((S).size()))
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = " << x << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
 
-int n, k, a[100111], cnt[100111];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const int MN = 100111;
+
+int n, a[MN], k, red[MN];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    cout << (fixed) << setprecision(6);
-    while (cin >> n >> k) {
-        memset(cnt, 0, sizeof cnt);
-        FOR(i,1,n) cin >> a[i];
+    while (scanf("%d%d", &n, &k) == 2) {
+        FOR(i,1,n) scanf("%d", &a[i]);
         n = unique(a+1, a+n+1) - a - 1;
+
+        // PR(a, n);
+
+        memset(red, 0, sizeof red);
         FOR(i,1,n) {
-            cnt[a[i]] += (i == 1 || i == n) ? 1 : ((a[i-1] == a[i+1]) ? 2 : 1);
+            if (i == 1 || i == n) red[a[i]]++;
+            else {
+                if (a[i-1] == a[i+1]) red[a[i]] += 2;
+                else red[a[i]]++;
+            }
         }
-        int res = 1;
-        FOR(i,2,k)
-            if (cnt[i] > cnt[res]) res = i;
-        cout << res << endl;
+
+        // PR(red, k);
+
+        int best = 1;
+        FOR(i,2,k) if (red[i] > red[best]) best = i;
+        cout << best << endl;
     }
     return 0;
 }
