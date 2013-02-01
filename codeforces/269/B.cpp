e1a908debc06d9b73dfee5fa7b97e31ea679c4ff
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-02-01 23:51:00 +0800

    Add solution for problem `B - Greenhouse Effect` Link: https://codeforces.com/contest/269/problem/B Tags: dp, *1700 Ref: https://codeforces.com/contest/269/submission/3048339

---
+++ codeforces/269/B.cpp
@@ -0,0 +1,90 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int n, m, a[5011], f[5011][5011];
+
+void update(int &a, int b) {
+    if (a < 0) a = b;
+    else a = min(a, b);
+}
+
+int main() {
+    while (scanf("%d%d", &n, &m) == 2) {
+        float x;
+        FOR(i,1,n) scanf("%d%f", &a[i], &x);
+        memset(f, -1, sizeof f);
+        f[0][0] = 0;
+        FOR(i,0,n-1) FOR(j,0,m)
+        if (f[i][j] >= 0) {
+            update(f[i+1][j], f[i][j] + (a[i+1] != j));
+            if (a[i+1] > j)
+                update(f[i+1][a[i+1]], f[i][j]);
+        }
+
+        int res = -1;
+        FOR(j,0,m)
+            update(res, f[n][j]);
+
+        cout << res << endl;
+    }
+    return 0;
+}
