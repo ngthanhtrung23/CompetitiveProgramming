d4d4201163413d970c49bbf88d995c865a6c1f0b
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-02-11 23:36:00 +0800

    Add solution for problem `B - Prime Matrix` Link: https://codeforces.com/contest/271/problem/B Tags: binary search, brute force, math, number theory, *1300 Ref: https://codeforces.com/contest/271/submission/3094792

--- codeforces/271/B.cpp
+++ codeforces/271/B.cpp
@@ -1,55 +1,102 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int a[511][511];
-int sieve[1000111], primes[1000111];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int n, m, a[511][511], sieve[200111], next[200111];
 
 void init() {
-    FOR(i,2,1000) if (sieve[i] == 0) {
-        for(int j=i*i; j <= 1000*1000; j += i) {
+    FOR(i,2,1000) if (!sieve[i]) {
+        int j = i*i;
+        while (j <= 200000) {
             sieve[j] = i;
+            j += i;
         }
     }
     sieve[1] = 1;
 
-    FORD(i,1000*1000,1)
-        if (sieve[i] == 0) primes[i] = i;
-        else primes[i] = primes[i+1];
+    FORD(i,200000,1)
+        if (!sieve[i]) next[i] = i;
+        else next[i] = next[i+1];
 }
 
 int main() {
-    ios :: sync_with_stdio(false);
     init();
-    int m, n;
-    while (cin >> m >> n) {
-        FOR(i,1,m) FOR(j,1,n) {
-            int x; cin >> x;
-            a[i][j] = primes[x] - x;
-        }
-        int best = 1000111000;
+    while (scanf("%d%d", &m, &n) == 2) {
+        FOR(i,1,m) FOR(j,1,n) scanf("%d", &a[i][j]);
+
+        int res = 1000111000;
+
         FOR(i,1,m) {
-            int cur = 0;
-            FOR(j,1,n) cur += a[i][j];
-            best = min(best, cur);
+            int now = 0;
+            FOR(j,1,n) now += next[a[i][j]] - a[i][j];
+            res = min(res, now);
         }
+
         FOR(j,1,n) {
-            int cur = 0;
-            FOR(i,1,m) cur += a[i][j];
-            best = min(best, cur);
+            int now = 0;
+            FOR(i,1,m) now += next[a[i][j]] - a[i][j];
+            res = min(res, now);
         }
-        cout << best << endl;
+
+        cout << res << endl;
     }
     return 0;
 }
