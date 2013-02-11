f1cf5fbed9625f6e6cbd00fc182f17f7cd8cf773
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-02-12 00:42:00 +0800

    Add solution for problem `E - Three Horses` Link: https://codeforces.com/contest/271/problem/E Tags: constructive algorithms, math, number theory, *2200 Ref: https://codeforces.com/contest/271/submission/3100269

---
+++ codeforces/271/E.cpp
@@ -0,0 +1,93 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int n, m, a[100111], uoc[100111], k;
+
+int gcd(int x, int y) {
+    if (y == 0) return x;
+    return gcd(y, x % y);
+}
+
+int main() {
+    while (scanf("%d%d", &n, &m) == 2) {
+        FOR(i,1,n) scanf("%d", &a[i]);
+        int d = a[1] - 1;
+        FOR(i,2,n) d = gcd(d, a[i] - 1);
+
+        while (d % 2 == 0) d /= 2;
+        k = 0;
+        for(int i = 1; i*i <= d; ++i) if (d % i == 0) {
+            uoc[++k] = i;
+            if (i * i != d) uoc[++k] = d / i;
+        }
+
+        long long res = 0;
+        for(int p2 = 1; p2 <= m; p2 *= 2) {
+            FOR(i,1,k) {
+                long long now = uoc[i] * (ll) p2;
+                if (now < m) res += m - now;
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
