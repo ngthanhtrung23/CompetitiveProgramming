994946ed7dc3d1120f475dad9de7672eee47e235
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-02-21 01:13:00 +0800

    Add solution for problem `A - k-Multiple Free Set` Link: https://codeforces.com/contest/274/problem/A Tags: binary search, greedy, sortings, *1500 Ref: https://codeforces.com/contest/274/submission/3160325

--- codeforces/274/A.cpp
+++ codeforces/274/A.cpp
@@ -1,33 +1,98 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int a[100111];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int n, k, a[100111];
+set<int> s, has;
 
 int main() {
-    int n, k;
     while (scanf("%d%d", &n, &k) == 2) {
-        set<int> s;
-        FOR(i,1,n) scanf("%d", &a[i]);
-        sort(a+1, a+n+1);
+        s.clear(); has.clear();
 
         FOR(i,1,n) {
-            if (a[i] % k) s.insert(a[i]);
-            else if (!s.count(a[i] / k)) s.insert(a[i]);
+            scanf("%d", &a[i]);
+            has.insert(a[i]);
         }
-        cout << s.size() << endl;
+        sort(a+1, a+n+1);
+
+        if (k == 1) {
+            printf("%d\n", n);
+            continue;
+        }
+
+        int res = 0;
+        FOR(i,1,n)
+        if (s.find(a[i]) == s.end()) {
+            int now = 0;
+            long long u = a[i];
+            while (u <= 1000111000 && has.find(u) != has.end()) {
+                s.insert(u);
+                u = u * k;
+                ++now;
+            }
+
+            res = res + now / 2 + now % 2;
+        }
+
+        cout << res << endl;
     }
     return 0;
 }
-
