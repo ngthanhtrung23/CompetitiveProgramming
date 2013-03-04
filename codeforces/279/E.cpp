ea143f21fbccc2633481edf12fe3bf8b3036a38d
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-03-05 00:11:00 +0800

    Add solution for problem `E - Beautiful Decomposition` Link: https://codeforces.com/contest/279/problem/E Tags: dp, games, greedy, number theory, *1900 Ref: https://codeforces.com/contest/279/submission/3242547

---
+++ codeforces/279/E.cpp
@@ -0,0 +1,96 @@
+#define _GLIBCXX_DEBUG
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const int MN = 1000111;
+
+char s[MN];
+int a[MN], b[MN], f[MN], g[MN];
+
+int main() {
+    while (gets(s)) {
+        int n = strlen(s);
+        FOR(i,1,n) {
+            if (s[i-1] == '0') a[i] = 0;
+            else a[i] = 1;
+        }
+
+        while (n && a[n] == 0) --n;
+        if (n == 0) {
+            puts("0");
+            continue;
+        }
+
+        b[n] = a[n];
+        FORD(i,n-1,1) b[i] = 1 - a[i];
+
+        // PR(a, n);
+        // PR(b, n);
+
+        f[n] = g[n] = 1;
+        FORD(i,n-1,1) {
+            f[i] = min(f[i+1] + a[i], g[i+1] + b[i] + 1);
+            g[i] = min(g[i+1] + b[i], f[i+1] + a[i] + 1);
+        }
+        cout << f[1] << endl;
+    }
+    return 0;
+}
