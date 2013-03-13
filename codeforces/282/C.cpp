1cb21117ac1bdf3ebb39be38ba5aae77b8ad9647
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-03-14 00:00:00 +0800

    Add solution for problem `C - XOR and OR` Link: https://codeforces.com/contest/282/problem/C Tags: constructive algorithms, implementation, math, *1500 Ref: https://codeforces.com/contest/282/submission/3303078

--- codeforces/282/C.cpp
+++ codeforces/282/C.cpp
@@ -1,41 +1,88 @@
-#include<iostream>
-#include<vector>
-#include<cstring>
-#include<algorithm>
-#include<cmath>
-#define MP make_pair
-#define PB push_back
-#include<cstdio>
+#define _GLIBCXX_DEBUG
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-char a[1000111], b[1000111];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const int MN = 1000111;
+
+char a[MN], b[MN];
+int n;
+
+bool all0(char a[]) {
+    for(int i = 0; i < n; ++i)
+        if (a[i] == '1') return false;
+    return true;
+}
 
 int main() {
-    gets(a);
-    gets(b);
-    if (strlen(a) != strlen(b)) {
-        cout << "NO"; return 0;
-    }
-    if (strcmp(a, b) == 0) {
-        cout << "YES"; return 0;
-    }
-    if (strlen(a) == 1) {
-        cout << "NO"; return 0;
-    }
-    bool check = false;
-    for(int i=0; i<strlen(a); i++)
-        if (a[i] == '1') {
-            check = true; break;
+    while (gets(a)) {
+        gets(b);
+        if (strlen(a) != strlen(b)) {
+            puts("NO");
+            continue;
         }
-    if (!check) { cout << "NO"; return 0;}
-    check = false;
-    for(int i=0; i<strlen(a); i++)
-        if (b[i] == '1') {
-            check = true; break;
-        }
-    if (!check) { cout << "NO"; return 0;}
-    
-    cout << "YES";
+        n = strlen(a);
+
+        if (all0(a) && all0(b)) puts("YES");
+        else if (all0(a) || all0(b)) puts("NO");
+        else puts("YES");
+    }
     return 0;
 }
