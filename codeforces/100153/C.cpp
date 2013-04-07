5fb3edafca57a88eb9b7546595f3067837b2a27a
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-07 19:07:00 +0800

    Add solution for problem `C - Dowry` Link: https://codeforces.com/gym/100153/problem/C Tags: Ref: https://codeforces.com/gym/100153/submission/3481506

--- codeforces/100153/C.cpp
+++ codeforces/100153/C.cpp
@@ -1,102 +1,78 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <utility>
+#include <queue>
+#define plli pair < pair<long long,long long>, int >
 using namespace std;
 
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-#define SZ(S) ((int) ((S).size()))
+int n;
+long long w[33], v[33], L, R;
 
-#define DEBUG(x) { cout << #x << " = " << x << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define TWO(X) (1LL<<((long long)X))
-#define CONTAIN(S,X) (S & TWO(X))
-pair< pair<long long, long long>, long long> ls[TWO(16) + 11];
-long long w[44], v[44];
-long long best, save;
-pair<long long, long long> it[TWO(20)];
-
-#define CT(X) ((X) << 1)
-#define CP(X) (CT(X) + 1)
-void build(int i, int l, int r) {
-    if (l == r) {
-        it[i] = make_pair(ls[l].first.second, ls[l].second);
-        return ;
-    }
-    int mid = (l + r) >> 1;
-    build(CT(i), l, mid);
-    build(CP(i), mid+1, r);
-    it[i] = max(it[CT(i)], it[CP(i)]);
+vector < plli > get(int n, vector <long long> w, vector <long long> v)
+{
+	vector < plli > res;
+	for (int mask = 0; mask < 1 << n; mask++)
+	{
+		long long V = 0, W = 0;
+		for (int i = 0; i < n; i++)
+			if (mask >> i & 1)
+			{
+				V += v[i];
+				W += w[i];
+			}
+		res.push_back(make_pair(make_pair(W, V), mask));
+	}
+	sort(res.begin(), res.end());
+	return res;
 }
 
-const long long oo = 1000111000111000111LL;
-pair<long long, long long> get(int i, int l, int r, long long u, long long v) {
-    if (ls[r].first.first < u || v < ls[l].first.first) return make_pair(-oo, 0);
-    if (u <= ls[l].first.first && ls[r].first.first <= v) return it[i];
-    int mid = (l + r) >> 1;
-    return max(get(CT(i), l, mid, u, v), get(CP(i), mid+1, r, u, v));
-}
-
-int main() {
-    freopen("dowry.in", "r", stdin);
-    freopen("dowry.out", "w", stdout);
-    ios :: sync_with_stdio(false); cin.tie(NULL);
-    cout << (fixed) << setprecision(6);
-    int n;
-    long long L, R;
-    while (cin >> n >> L >> R) {
-        REP(i,n) cin >> w[i] >> v[i];
-        
-        int l_from = 0, l_to = n/2 - 1;
-        int r_from = l_to + 1, r_to = n-1;
-        best = -oo; save = 0;
-
-        int x = 0;
-        REP(mask,TWO(l_to+1)) {
-            long long cur_w = 0, cur_v = 0;
-            FOR(i,l_from,l_to)
-                if (CONTAIN(mask,i))
-                    cur_w += w[i], cur_v += v[i];
-            ls[++x] = make_pair(make_pair(cur_w, cur_v), mask);
-        }
-        sort(ls+1, ls+x+1);
-//        DEBUG(l_from);
-//        DEBUG(l_to);
-//        FOR(i,1,x) {
-//            auto t = ls[i];
-//            cout << t.first.first << ' ' << t.first.second << "   " << t.second << endl;
-//        }
-//        DEBUG("---");
-        build(1, 1, x);
-        for(long long mask = 0; mask < TWO(n); mask += TWO(n/2)) {
-            long long cur_w = 0, cur_v = 0;
-            FOR(i,r_from,r_to)
-                if (CONTAIN(mask,i))
-                    cur_w += w[i], cur_v += v[i];
-
-//            cout << cur_w << ' ' << cur_v << ' ' << mask << endl;
-            pair<long long, long long> left = get(1, 1, x, L - cur_w, R - cur_w);
-//            DEBUG(left.first);
-
-            if (left.first + cur_v > best) {
-                best = left.first + cur_v;
-                save = left.second + mask;
-            }
-        }
-        if (best < 0) cout << 0 << endl;
-        else {
-            int cnt = 0;
-            vector<int> res;
-            REP(i,n)
-                if (CONTAIN(save, i)) {
-                    ++cnt;
-                    res.push_back(i);
-                }
-            cout << cnt << endl;
-            for(int x : res) cout << x+1 << ' '; cout << endl;
-        }
-    }
-    return 0;
+int main()
+{
+	freopen("dowry.in", "r", stdin);
+	freopen("dowry.out", "w", stdout);
+	cin >> n >> L >> R;
+	for (int i = 0; i < n; i++) cin >> w[i] >> v[i];
+	
+	int n1 = n / 2, n2 = n - n1;
+	vector <long long> w1, w2, v1, v2;
+	for (int i = 0; i < n; i++)
+		if (i < n1) w1.push_back(w[i]), v1.push_back(v[i]);
+		else w2.push_back(w[i]), v2.push_back(v[i]);
+	
+	vector < plli > a = get(n1, w1, v1);
+	vector < plli > b = get(n2, w2, v2);
+	
+	priority_queue < plli > q;
+	long long ans = 0, maskRes = 0;
+	for (int i = int(a.size()) - 1, j = 0; i >= 0; i--)
+	{
+		long long low = L - a[i].first.first, high = R - a[i].first.first;
+		while (j < int(b.size()) && b[j].first.first <= high) 
+			q.push(make_pair(make_pair(b[j].first.second, b[j].first.first), b[j].second)), j++;
+		
+		while (!q.empty())
+		{
+			plli u = q.top();
+			if (u.first.second < low) q.pop();
+			else
+			{
+				if (ans < u.first.first + a[i].first.second)
+				{
+					ans = u.first.first + a[i].first.second;
+					maskRes = (1LL * u.second << n1) | a[i].second;
+				}
+				break;
+			}
+		}
+	}
+	
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+		if (maskRes >> i & 1) cnt++;
+	cout << cnt << endl;
+	for (int i = 0; i < n; i++)
+		if (maskRes >> i & 1) cout << i + 1 << endl;
 }
