7da2d42605169ac88de5d01836de599a5cc50cb9
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-12 13:25:00 +0800

    Add solution for problem `B - Polo the Penguin and Matrix` Link: https://codeforces.com/contest/289/problem/B Tags: brute force, dp, implementation, sortings, ternary search, *1400 Ref: https://codeforces.com/contest/289/submission/3516002

--- codeforces/289/B.cpp
+++ codeforces/289/B.cpp
@@ -1,43 +1,92 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int m, n, d, a[111*111];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int a[111][111], x[111*111];
 
 int main() {
-    ios :: sync_with_stdio(false);
-    while (cin >> m >> n >> d) {
-        int t = 0;
-        FOR(i,1,m) FOR(j,1,n) {
-            ++t; cin >> a[t];
-        }
-        n = m * n;
-
+    int m, n, d;
+    while (scanf("%d%d%d", &m, &n, &d) == 3) {
         bool ok = true;
-        FOR(i,2,n) if (a[i] % d != a[1] % d) ok = false;
+        int k = 0;
+        FOR(i,1,m) FOR(j,1,n) {
+            scanf("%d", &a[i][j]);
+            x[++k] = a[i][j];
+            if (a[i][j] % d != a[1][1] % d) {
+                ok = false;
+            }
+        }
+
         if (!ok) {
-            cout << -1 << endl;
+            puts("-1");
             continue;
         }
-        sort(a+1, a+n+1);
-        FOR(i,1,n) a[i] = a[i] / d;
-        int to = a[n / 2 + n % 2];
 
-        long long res = 0;
-        FOR(i,1,n) res += abs(to - a[i]);
+        sort(x+1, x+k+1);
+        int u = x[k/2 + k%2], res = 0;
+
+        FOR(i,1,m) FOR(j,1,n) {
+            res += abs(a[i][j] - u) / d;
+        }
         cout << res << endl;
     }
     return 0;
 }
-
