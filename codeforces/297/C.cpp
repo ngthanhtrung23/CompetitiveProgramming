80515ca0aecffc3cb13f334b7abffcc651ebec06
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-20 00:46:00 +0800

    Add solution for problem `C - Splitting the Uniqueness` Link: https://codeforces.com/contest/297/problem/C Tags: constructive algorithms, *2400 Ref: https://codeforces.com/contest/297/submission/3572378

--- codeforces/297/C.cpp
+++ codeforces/297/C.cpp
@@ -1,51 +1,94 @@
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <vector>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <utility>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
 #include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int n, a[100100], b[100100], ansA[100100], ansB[100100];
-pair <int,int> s[100100];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-int main()
-{
-	//freopen("c.in", "r", stdin);
-	cin >> n;
-	for (int i = 0; i < n; i++) scanf("%d", &s[i].first), s[i].second = i;
-	
-	sort(s, s + n);
-	int k = (n + 2) / 3;
-	int common = (n - k) * 2 - n;
-	
-	for (int i = 0; i < common; i++)
-	{
-		a[n - 1 - i] = i + 1;
-		b[n - 1 - i] = s[n - 1 - i].first - i - 1;
-	}
-	
-	int each = n - k - common;
-	for (int i = 0; i < each; i++) 
-	{
-		b[i] = s[i].first;
-		a[i + each] = s[i + each].first;
-	}
-	
-	for (int i = 0; i < n; i++) 
-	{
-		ansA[s[i].second] = a[i]; 
-		ansB[s[i].second] = b[i];
-	}
-	
-	puts("YES");
-	for (int i = 0; i < n; i++) printf("%d%c", ansA[i], (i == n - 1 ? '\n' :' '));
-	for (int i = 0; i < n; i++) printf("%d%c", ansB[i], (i == n - 1 ? '\n' :' '));
+const long double PI = acos((long double) -1.0);
+const int MN = 100111;
+
+int n, a[MN], b[MN];
+pair<int,int> s[MN];
+
+int main() {
+    while (scanf("%d", &n) == 1) {
+        puts("YES");
+        FOR(i,1,n) {
+            scanf("%d", &s[i].first);
+            s[i].second = i;
+        }
+        sort(s+1, s+n+1);
+
+        int start = n / 3; if (n % 3) ++start;
+        ++start;
+        for(int i = 0, id = start; id <= n; id += 2, ++i) {
+            a[s[id].second] = i;
+            b[s[id].second] = s[id].first - i;
+        }
+        for(int i = 0, id = start + 1; id <= n; id += 2, i += 1) {
+            b[s[id].second] = i;
+            a[s[id].second] = s[id].first - i;
+        }
+        FOR(i,1,start-1) {
+            a[s[i].second] = s[i].first;
+            b[s[i].second] = 0;
+        }
+        FOR(i,1,n) printf("%d ", a[i]); puts("");
+        FOR(i,1,n) printf("%d ", b[i]); puts("");
+    }
+    return 0;
 }
