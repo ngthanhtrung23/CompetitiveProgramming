fd7138af575feb918e99f232d0e7f62dd04de66e
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-20 14:14:00 +0800

    Add solution for problem `A - Star triangles` Link: https://codeforces.com/gym/100147/problem/A Tags: Ref: https://codeforces.com/gym/100147/submission/3578864

---
+++ codeforces/100147/A.cpp
@@ -0,0 +1,84 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const int MN = 300111;
+
+int n, x[MN], y[MN];
+map<int,int> cx, cy;
+
+int main() {
+    freopen("triangles.in", "r", stdin);
+    freopen("triangles.out", "w", stdout);
+    while (scanf("%d", &n) == 1) {
+        cx.clear(); cy.clear();
+        FOR(i,1,n) {
+            scanf("%d%d", &x[i], &y[i]);
+            cx[x[i]]++;
+            cy[y[i]]++;
+        }
+        long long res = 0;
+        FOR(i,1,n) {
+            res += (cx[x[i]] - 1) * (long long) (cy[y[i]] - 1);
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
