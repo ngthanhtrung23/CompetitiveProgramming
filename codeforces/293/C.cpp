b8453ce21d0b0f658b7206ac64fef86cb613cdcf
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-23 00:50:00 +0800

    Add solution for problem `C - Cube Problem` Link: https://codeforces.com/contest/293/problem/C Tags: brute force, math, number theory, *2400 Ref: https://codeforces.com/contest/293/submission/3605770

--- codeforces/293/C.cpp
+++ codeforces/293/C.cpp
@@ -1,88 +1,130 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-long long n;
-long long p[1011], k[1011], used[1011];
-int np;
-set< tuple<long long, long long, long long> > res;
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-void attempt2(int i, long long bc, long long ab) {
-    if (i > np) {
-        long long ca = n / ab / bc;
-        long long abc = (ab + bc + ca) / 2;
-        long long a = abc - bc;
-        long long b = abc - ca;
-        long long c = abc - ab;
-        if (a > 0 && b > 0 && c > 0
-                && (a + b) * (b + c) * (c + a) == n)
-            res.insert(make_tuple(a, b, c));
+const long double PI = acos((long double) -1.0);
+
+long long p[100111], k[100111], has[100111];
+long long n, res;
+int cnt;
+
+void check(long long x, long long y, long long z) {
+    if ((x + y - z) & 1) return ;
+
+    if (x+y > z && y+z > x && z+x > y) ++res;
+}
+
+void attempt2(int i, long long y, long long x) {
+    if (i > cnt) {
+        long long z = n / x / y;
+        check(x, y, z);
         return ;
     }
-    for(int power = 0, pp = 1; power <= k[i] - used[i]; power += 1, pp *= p[i]) {
-        attempt2(i+1, bc * pp, ab);
+    long long cur = 1;
+    FOR(now,0,has[i]) {
+        attempt2(i+1, y*cur, x);
+        cur *= p[i];
     }
 }
 
-void attempt1(int i, long long ab) {
-    if (i > np) {
-        attempt2(1, 1LL, ab);
+void attempt1(int i, long long x) {
+    if (i > cnt) {
+        attempt2(1, 1, x);
         return ;
     }
-
-    for(int power=0, pp = 1; power <= k[i]; power += 1, pp *= p[i]) {
-        used[i] = power;
-        attempt1(i+1, ab * pp);
+    long long cur = 1;
+    FOR(now,0,k[i]) {
+        has[i] = k[i] - now;
+        attempt1(i+1, x * cur);
+        cur = cur * p[i];
     }
 }
 
 int main() {
-    ios :: sync_with_stdio(false);
     while (cin >> n) {
-//        cout << "-----------------\n";
+        memset(k, 0, sizeof k);
+        memset(p, 0, sizeof p);
+
         if (n % 3) {
-            cout << 0 << endl;
+            puts("0");
             continue;
         }
+        res = 0;
         n /= 3;
-//        DEBUG(n);
         long long saven = n;
-        np = 0;
-        for(long long i = 2; i <= 10111000; ++i)
-            if (n % i == 0) {
-                ++np;
-                p[np] = i;
-                k[np] = 0;
-                while (n % i == 0) {
-                    n /= i;
-                    ++k[np];
-                }
+        cnt = 0;
+        for(long long x = 2; x*x <= n; ++x) if (n % x == 0) {
+            ++cnt;
+            p[cnt] = x;
+            while (n % x == 0) {
+                ++k[cnt];
+                n /= x;
             }
-        if (n > 1) {
-            ++np;
-            p[np] = n;
-            k[np] = 1;
         }
-//        PR(p, np);
-//        PR(k, np);
-
+        if (n > 1) {
+            ++cnt;
+            p[cnt] = n;
+            k[cnt] = 1;
+        }
         n = saven;
-
-        res.clear();
-        attempt1(1, 1LL);
-        cout << res.size() << endl;
+        attempt1(1, 1);
+        cout << res << endl;
     }
     return 0;
 }
-
