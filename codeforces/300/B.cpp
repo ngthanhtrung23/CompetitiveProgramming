8075a5d8d823d0b9dd05adc848a3bc15f0cb272b
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-25 23:51:00 +0800

    Add solution for problem `B - Coach` Link: https://codeforces.com/contest/300/problem/B Tags: brute force, dfs and similar, graphs, *1500 Ref: https://codeforces.com/contest/300/submission/3623141

--- codeforces/300/B.cpp
+++ codeforces/300/B.cpp
@@ -1,66 +1,139 @@
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <vector>
-#include <cstring>
-#include <string>
-#include <cmath>
-#include <utility>
-#include <map>
-#include <set>
-#include <queue>
-#include <stack>
 #include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int d[100];
-vector <int> a[100];
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-int get(int x)
-{
-	return x == d[x] ? x : d[x] = get(d[x]);
+const long double PI = acos((long double) -1.0);
+
+int n, m, c[55][55], deg[55], team[55], sz[55];
+int first, last, qu[55];
+
+void bfs(int u) {
+    first = last = 1;
+    qu[1] = u;
+    while (first <= last) {
+        u = qu[first++];
+        FOR(v,1,n)
+        if (!team[v] && c[u][v]) {
+            team[v] = team[u];
+            qu[++last] = v;
+        }
+    }
 }
 
-int main()
-{
-	int n, m, x, y;
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) d[i] = i;
-	while (m--) 
-	{
-		cin >> x >> y;
-		d[get(x)] = get(y);
-	}
-	
-	for (int i = 1; i <= n; i++) a[get(i)].push_back(i);
-	
-	vector <int> p1;
-	vector < pair<int,int> > p2;
-	
-	for (int i = 1; i <= n; i++)
-		if (a[i].size() > 3)
-		{
-			puts("-1");
-			return 0;
-		}
-		else
-			if (a[i].size() == 1) p1.push_back(i);
-			else 
-				if (a[i].size() == 2) p2.push_back(make_pair(a[i][0], a[i][1]));
-		
-	if (p2.size() > p1.size()) 
-	{
-		puts("-1"); return 0;
-	}
-	
-	for (int i = 1; i <= n; i++)
-		if (a[i].size() == 3)
-			for (int j = 0; j < 3; j++)	
-				cout << a[i][j] << (j == 2 ? '\n' : ' ');
-	
-	for (int i = 0; i < int(p2.size()); i++)
-		cout << p2[i].first << ' ' << p2[i].second << ' ' << p1[i] << endl;
-		
-	for (int i = p2.size(); i < int(p1.size()); i += 3)
-		cout << p1[i] << ' ' << p1[i + 1] << ' ' << p1[i + 2] << endl;
+void solve() {
+    FOR(i,1,n) if (deg[i] > 2) {
+        puts("-1");
+        return ;
+    }
+    int nTeam = 0;
+    FOR(i,1,n) if (deg[i] > 0 && !team[i]) {
+        ++nTeam; team[i] = nTeam;
+        bfs(i);
+        if (last > 3) {
+            puts("-1");
+            return ;
+        }
+        sz[nTeam] = last;
+    }
+
+    FOR(i,1,n) if (!team[i]) {
+        FOR(t,1,nTeam)
+        if (sz[t] < 3) {
+            ++sz[t];
+            team[i] = t;
+            break;
+        }
+
+        if (!team[i]) {
+            ++nTeam;
+            team[i] = nTeam;
+            sz[nTeam] = 1;
+        }
+    }
+
+    if (nTeam > n / 3) {
+        puts("-1");
+        return ;
+    }
+
+    FOR(t,1,nTeam) {
+        FOR(i,1,n) if (team[i] == t) {
+            printf("%d ", i);
+        }
+        puts("");
+    }
+}
+
+int main() {
+    while (cin >> n >> m) {
+        memset(c, 0, sizeof c);
+        memset(deg, 0, sizeof deg);
+        memset(team, 0, sizeof team);
+        memset(sz, 0, sizeof sz);
+
+        while (m--) {
+            int u, v; cin >> u >> v;
+            c[u][v] = c[v][u] = 1;
+            deg[u]++; deg[v]++;
+        }
+
+        solve();
+    }
+    return 0;
 }
