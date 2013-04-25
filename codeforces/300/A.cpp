36dab9751dcfaa7c0f7287926772840bf87cc50b
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-25 23:38:00 +0800

    Add solution for problem `A - Array` Link: https://codeforces.com/contest/300/problem/A Tags: brute force, constructive algorithms, implementation, *1100 Ref: https://codeforces.com/contest/300/submission/3621585

--- codeforces/300/A.cpp
+++ codeforces/300/A.cpp
@@ -1,63 +1,132 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int n, a[111], used[111];
-
-void print(const vector<int>& a) {
-    cout << a.size();
-    for(int x : a) cout << ' ' << x;
-    cout << endl;
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
 }
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int n, a[111];
 
 int main() {
     while (cin >> n) {
-        FOR(i,1,n) cin >> a[i];
-        vector<int> neg, pos, zero;
-
-        memset(used, 0, sizeof used);
-        FOR(i,1,n) if (a[i] == 0) {
-            zero.push_back(a[i]);
-            used[i] = true;
+        int am = 0, duong = 0, zero = 0;
+        FOR(i,1,n) {
+            cin >> a[i];
+            if (a[i] < 0) ++am;
+            else if (a[i] > 0) ++duong; 
+            else ++zero;
         }
 
-        FOR(i,1,n) if (a[i] < 0) {
-            used[i] = true;
-            neg.push_back(a[i]);
-            break;
-        }
-
-        FOR(i,1,n) if (a[i] > 0) {
-            used[i] = true;
-            pos.push_back(a[i]);
-        }
-
-        if (pos.empty()) {
-            FOR(i,1,n) if (!used[i] && a[i] < 0) {
-                used[i] = true;
-                pos.push_back(a[i]);
-                if (pos.size() == 2) break;
+        if (am && duong) {
+            printf("1 "); FOR(i,1,n) if (a[i] < 0) {
+                printf("%d\n", a[i]);
+                break;
             }
-        }
+            printf("1 "); FOR(i,1,n) if (a[i] > 0) {
+                printf("%d\n", a[i]);
+                break;
+            }
 
-        FOR(i,1,n) if (!used[i]) {
-            zero.push_back(a[i]);
+            bool c1 = false, c2 = false;
+            printf("%d ", n-2);
+            FOR(i,1,n) {
+                if (a[i] < 0 && c1) {
+                    printf("%d ", a[i]);
+                }
+                if (a[i] > 0 && c2) {
+                    printf("%d ", a[i]);
+                }
+                if (a[i] == 0) {
+                    printf("%d ", a[i]);
+                }
+                if (a[i] < 0) c1 = true;
+                if (a[i] > 0) c2 = true;
+            }
+            puts("");
         }
+        else {
+            printf("1 ");
+            int save = -1;
+            FOR(i,1,n) if (a[i] < 0) {
+                printf("%d\n", a[i]);
+                save = i+1;
+                break;
+            }
+            printf("2 ");
+            int cur = 0;
+            FOR(i,save,n) if (a[i] < 0) {
+                printf("%d ", a[i]);
+                ++cur;
+                if (cur == 2) break;
+            }
+            puts("");
 
-        print(neg);
-        print(pos);
-        print(zero);
+            printf("%d ", n-3);
+            cur = 0;
+            FOR(i,1,n) {
+                if (a[i] < 0) {
+                    ++cur;
+                    if (cur > 3) printf("%d ", a[i]);
+                }
+                else printf("%d ", a[i]);
+            }
+            puts("");
+        }
     }
     return 0;
 }
