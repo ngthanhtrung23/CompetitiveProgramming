c39de6756ae060ce48dd64d465c14e9f4b54e130
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-04-27 18:26:00 +0800

    Add solution for problem `D - Holidays` Link: https://codeforces.com/gym/100187/problem/D Tags: Ref: https://codeforces.com/gym/100187/submission/3641806

---
+++ codeforces/100187/D.cpp
@@ -0,0 +1,93 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const long long MOD = 1000000009;
+
+const int MN = 200111;
+long long f[MN], p2[MN];
+
+long long power(long long x, int k) {
+    if (k == 0) return 1;
+    if (k == 1) return x % MOD;
+    long long mid = power(x, k >> 1);
+    mid = (mid * mid) % MOD;
+    if (k & 1) return (mid * x) % MOD;
+    else return mid;
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        f[0] = 1;
+        p2[0] = 1;
+        FOR(i,1,n) {
+            f[i] = f[i-1] * (n - i + 1) % MOD * power(i, MOD - 2) % MOD;
+            p2[i] = (p2[i-1] * 2) % MOD;
+        }
+
+        long long res = 0;
+        FOR(i,1,n-1) {
+            res = (res + f[i] * (p2[n - i] - 1 + MOD)) % MOD;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
