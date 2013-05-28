e1ba9f22920941d722587ca54f59d40c1d546041
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-05-29 02:03:00 +0800

    Add solution for problem `A - Two Bags of Potatoes` Link: https://codeforces.com/contest/239/problem/A Tags: greedy, implementation, math, *1200 Ref: https://codeforces.com/contest/239/submission/3788869

--- codeforces/239/A.cpp
+++ codeforces/239/A.cpp
@@ -1,31 +1,77 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int main() {
-    long long y, k, n;
-    while (cin >> y >> k >> n) {
-        long long lb = y - y % k;
-        while (lb <= y) lb += k;
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
+const long double PI = acos((long double) -1.0);
+
+int main() {
+    int y, k, n;
+    while (cin >> y >> k >> n) {
+        n -= n % k;
         bool ok = false;
-        for(long long sum = lb; sum <= n; sum += k) {
+        vector<int> res;
+        while (n > y) {
             ok = true;
-            printf("%d ", (int) sum - y);
+            res.push_back(n-y);
+            n -= k;
         }
-        if (!ok) puts("-1");
-        else puts("");
+        if (ok) {
+            FORD(i,res.size()-1,0) printf("%d ", res[i]);
+            puts("");
+        }
+        else puts("-1");
     }
     return 0;
 }
