a49478b6bbdd5bd441bb5f2b5285fbab315a0a0e
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-06-02 18:43:00 +0800

    Add solution for problem `A - Dragons` Link: https://codeforces.com/contest/230/problem/A Tags: greedy, sortings, *1000 Ref: https://codeforces.com/contest/230/submission/3814068

---
+++ codeforces/230/A.cpp
@@ -0,0 +1,81 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int s, n;
+pair<int,int> a[1011];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> s >> n) {
+        FOR(i,1,n) cin >> a[i].first >> a[i].second;
+        sort(a+1, a+n+1);
+
+        bool ok = true;
+        FOR(i,1,n) {
+            if (s <= a[i].first) {
+                ok = false;
+                break;
+            }
+            s += a[i].second;
+        }
+
+        if (ok) puts("YES");
+        else puts("NO");
+    }
+    return 0;
+}
