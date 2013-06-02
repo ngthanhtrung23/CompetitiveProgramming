a89d6ae5201b11445f42a42aa824c49c44de90fa
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-06-02 17:29:00 +0800

    Add solution for problem `B - Planets` Link: https://codeforces.com/contest/229/problem/B Tags: binary search, data structures, graphs, shortest paths, *1700 Ref: https://codeforces.com/contest/229/submission/3813892

--- codeforces/229/B.cpp
+++ codeforces/229/B.cpp
@@ -1,76 +1,117 @@
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+const int oo = 1000111000;
 const int MN = 100111;
+
 int n, m;
 vector< pair<int,int> > ke[MN];
-vector<int> busy[MN];
-int d[2][MN];
+set<int> arrive[MN];
+
+set< pair<int,int> > s;
+int f[MN];
 
 int main() {
     while (scanf("%d%d", &n, &m) == 2) {
         FOR(i,1,n) {
             ke[i].clear();
-            busy[i].clear();
+            arrive[i].clear();
+            f[i] = oo;
         }
-        FOR(i,1,m) {
+
+        while (m--) {
             int u, v, c; scanf("%d%d%d", &u, &v, &c);
             ke[u].push_back(make_pair(v, c));
             ke[v].push_back(make_pair(u, c));
         }
+
         FOR(i,1,n) {
             int k; scanf("%d", &k);
             while (k--) {
-                int u; scanf("%d", &u);
-                busy[i].push_back(u);
+                int t; scanf("%d", &t);
+                arrive[i].insert(t);
             }
-            busy[i].push_back(1000111000);
-            sort(busy[i].begin(), busy[i].end());
         }
-        REP(t,2) FOR(i,1,n) d[t][i] = 1000111000;
-        d[0][1] = 0;
-        set< pair<int, pair<int,int> > > s;
-        s.insert(make_pair(0, make_pair(0, 1)));
+
+        f[1] = 0; s.insert(make_pair(0, 1));
+        bool ok = false;
         while (!s.empty()) {
-            int l = s.begin()->first;
-            int t = s.begin()->second.first;
-            int u = s.begin()->second.second;
-
+            int l = s.begin()->first, u = s.begin()->second;
             s.erase(s.begin());
-            if (l != d[t][u]) continue;
+            if (l != f[u]) continue;
 
-            if (t == 0) {
-                int l2 = l;
-                while (*lower_bound(busy[u].begin(), busy[u].end(), l2) == l2) ++l2;
-                if (d[1][u] > l2) {
-                    d[1][u] = l2;
-                    s.insert(make_pair(l2, make_pair(1, u)));
-                }
+            if (u == n) {
+                printf("%d\n", f[u]);
+                ok = true;
+                break;
             }
-            else {
-                for(auto e : ke[u]) {
-                    int v = e.first, c = e.second;
-                    if (d[0][v] > d[1][u] + c) {
-                        d[0][v] = d[1][u] + c;
-                        s.insert(make_pair(d[0][v], make_pair(0, v)));
-                    }
+
+            while (arrive[u].find(l) != arrive[u].end()) ++l;
+
+            REP(i,ke[u].size()) {
+                int v = ke[u][i].first, c = ke[u][i].second;
+                if (f[v] > l + c) {
+                    f[v] = l + c;
+                    s.insert(make_pair(f[v], v));
                 }
             }
         }
-        if (d[0][n] == 1000111000) d[0][n] = -1;
-        cout << d[0][n] << endl;
+        // PR(f, n);
+        if (!ok) puts("-1");
     }
     return 0;
 }
-
