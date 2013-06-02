19b0efcdf63d41d70e76e311e4de487f0e416134
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-06-02 18:12:00 +0800

    Add solution for problem `C - Triangles` Link: https://codeforces.com/contest/229/problem/C Tags: combinatorics, graphs, math, *1900 Ref: https://codeforces.com/contest/229/submission/3813995

--- codeforces/229/C.cpp
+++ codeforces/229/C.cpp
@@ -1,36 +1,81 @@
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
 const int MN = 1000111;
-int n, m;
-vector<int> ke[MN];
+
+int n, m, eu[MN], ev[MN], deg[MN];
 
 int main() {
     while (scanf("%d%d", &n, &m) == 2) {
-        FOR(i,1,n) ke[i].clear();
+        memset(deg, 0, sizeof deg);
         FOR(i,1,m) {
             int u, v; scanf("%d%d", &u, &v);
-            ke[u].push_back(v);
-            ke[v].push_back(u);
+            eu[i] = u; ev[i] = v;
+            deg[u]++;
+            deg[v]++;
         }
 
-        long long all = n * (n - 1LL) * (n - 2LL) / 3;
-        FOR(i,1,n) {
-            all -= ke[i].size() * (n - 1LL - ke[i].size());
+        long long all = n * (n-1LL) * (n-2LL) / 6, bad = 0;
+        FOR(i,1,m) {
+            int u = eu[i], v = ev[i];
+            bad += n - deg[u] - 1;
+            bad += n - deg[v] - 1;
         }
-        cout << all / 2 << endl;
+        cout << all - bad / 2 << endl;
     }
     return 0;
 }
-
