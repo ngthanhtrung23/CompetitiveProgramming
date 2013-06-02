14b6abba1f8eb46231ca99eb4e6b89a17ab03a31
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-06-02 16:53:00 +0800

    Add solution for problem `A - Where do I Turn?` Link: https://codeforces.com/contest/227/problem/A Tags: geometry, *1300 Ref: https://codeforces.com/contest/227/submission/3813744

---
+++ codeforces/227/A.cpp
@@ -0,0 +1,80 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+struct Point {
+    long long x, y;
+    Point() { x = y = 0; }
+    Point(long long x, long long y) : x(x), y(y) {}
+
+    Point operator - (Point a) { return Point(x-a.x, y-a.y); }
+    long long operator % (Point a) {
+        return x * a.y - y * a.x;
+    }
+
+    bool read() { return (cin >> x >> y); }
+} A, B, C;
+
+int main() {
+    while (A.read() && B.read() && C.read()) {
+        long long t = (B - A) % (C - A);
+        if (t == 0) puts("TOWARDS");
+        else if (t > 0) puts("LEFT");
+        else puts("RIGHT");
+    }
+    return 0;
+}
