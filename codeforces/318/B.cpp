1a4d10b83f0961311fb53da7843cad07ed3bea85
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-06-15 04:17:00 +0800

    Add solution for problem `B - Strings of Power` Link: https://codeforces.com/contest/318/problem/B Tags: implementation, strings, two pointers, *1300 Ref: https://codeforces.com/contest/318/submission/3897220

--- codeforces/318/B.cpp
+++ codeforces/318/B.cpp
@@ -1,35 +1,78 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+char tmp[1000111];
+int cnt[1000111];
+
 int main() {
-    ios :: sync_with_stdio(false);
-    string s;
-    while (cin >> s) {
-        long long res = 0;
-        int cnt = 0;
-
-        REP(i,s.length()-4) {
-            if (s.substr(i, 5) == "heavy") {
-                ++cnt;
-            }
-            if (s.substr(i, 5) == "metal") {
-                res += cnt;
-            }
-        }
-
-        cout << res << endl;
-    }
+    // freopen("input.txt", "r", stdin);
+	while (gets(tmp)) {
+		memset(cnt, 0, sizeof cnt);
+		string s(tmp);
+		int l = s.length();
+		FORD(i,l-1,0) {
+			cnt[i] = cnt[i+1];
+			if (s.substr(i, 5) == "metal") cnt[i]++;
+		}
+		long long res = 0;
+		REP(i,l)
+			if (s.substr(i, 5) == "heavy") {
+				res += cnt[i];
+			}
+		cout << res << endl;
+	}
     return 0;
 }
