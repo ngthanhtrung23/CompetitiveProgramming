224f42c787d678aaee1ab025f8f01d9b3249412a
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-06-15 03:18:00 +0800

    Add solution for problem `A - Perfect Pair` Link: https://codeforces.com/contest/317/problem/A Tags: brute force, *1600 Ref: https://codeforces.com/contest/317/submission/3896783

--- codeforces/317/A.cpp
+++ codeforces/317/A.cpp
@@ -1,49 +1,77 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
 #define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int main() {
-    ll x, y, m;
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    long long x, y, m;
     while (cin >> x >> y >> m) {
-        if (x >= m || y >= m) cout << 0 << endl;
-        else if (x <= 0 && y <= 0) cout << -1 << endl;
-        else {
+        long long res = 0;
+        while (x < m && y < m) {
             if (x > y) swap(x, y);
-            ll add = 0;
-            if (x < 0) {
-                add = (-x) / y;
-                x = x + y * add;
+            if (x <= 0 && y <= 0) {
+                res = -1;
+                break;
             }
-
-            bool ok = false;
-            REP(turn,1000111) {
-                if (x > y) swap(x, y);
-                // now x <= y
-                ll s = x + y;
-                if (s < x) break;
-
-                x = s;
-                if (x >= m || y >= m) {
-                    cout << add+1+turn << endl;
-                    ok = true;
-                    break;
-                }
+            if (x < 0 && x + y < 0) {
+                res += -x / y;
+                x += res * y;
             }
-            if (!ok) cout << -1 << endl;
+            else x += y, ++res;
         }
+        cout << res << endl;
     }
     return 0;
 }
-
