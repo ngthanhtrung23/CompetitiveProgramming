48ed1f88a4fabd67a8f5d28330740d950ec49c85
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-07-04 20:41:00 +0800

    Add solution for problem `C - Magic Five` Link: https://codeforces.com/contest/327/problem/C Tags: combinatorics, math, *1700 Ref: https://codeforces.com/contest/327/submission/4012413

--- codeforces/327/C.cpp
+++ codeforces/327/C.cpp
@@ -1,48 +1,99 @@
-
-#include <bits/stdc++.h>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
-
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
 #define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-const int MN = 100111;
-const long long MOD = 1e9 + 7;
-char a[MN];
-int k;
+//Buffer reading
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp) { \
+        if (REACHEOF) return 0;\
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-ll power(ll x, ll k) {
+const long double PI = acos((long double) -1.0);
+const long long MOD = 1000000007LL;
+
+char s[100111];
+int r, p2[100111];
+
+long long power(long long x, int k) {
     if (k == 0) return 1;
     if (k == 1) return x % MOD;
-    ll mid = power(x * x % MOD, k >> 1);
+    long long mid = power(x, k >> 1);
+    mid = mid * mid % MOD;
+
     if (k & 1) return mid * x % MOD;
     else return mid;
 }
 
-ll inverse(ll x) {
+long long inverse(long long x) {
     return power(x, MOD - 2);
 }
 
 int main() {
-    while (scanf("%s", &a[1]) == 1) {
-        cin >> k;
-        int l = strlen(a+1);
+    p2[0] = 1; FOR(i,1,100000) p2[i] = (p2[i-1] * 2) % MOD;
 
-        ll res = 0;
-        ll x = power(2, l);
-        FOR(i,1,l) if (a[i] == '0' || a[i] == '5') {
-            res = (res + power(2, i-1) * (power(x, k) - 1 + MOD) % MOD * inverse(x-1)) % MOD;
+    while (gets(s)) {
+        scanf("%d\n", &r);
+        int l = strlen(s);
+
+        long long res = 0, x = p2[l];
+        REP(i,l) {
+            if (s[i] == '0' || s[i] == '5') {
+                long long t = (power(x, r) - 1 + MOD) % MOD;
+                t = t * inverse((x-1 + MOD) % MOD) % MOD;
+                res = (res + p2[i]*t) % MOD;
+            }
         }
-        cout << res << endl;
+        cout << res % MOD << endl;
     }
     return 0;
 }
-
