ed4cf10a14eec0dd652c0d1f712b736f42846c7e
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-07-13 14:26:00 +0800

    Add solution for problem `A - IQ Test` Link: https://codeforces.com/contest/328/problem/A Tags: implementation, *1800 Ref: https://codeforces.com/contest/328/submission/4058203

---
+++ codeforces/328/A.cpp
@@ -0,0 +1,75 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int main() {
+    int a, b, c, d;
+    while (cin >> a >> b >> c >> d) {
+        int d1 = b - a, d2 = c - b, d3 = d - c;
+        pair<int,int> r1 = MP(b/__gcd(a, b), a/__gcd(a, b)),
+                      r2 = MP(c/__gcd(b, c), b/__gcd(b, c)),
+                      r3 = MP(d/__gcd(c, d), c/__gcd(c, d));
+
+        if (d1 == d2 && d2 == d3) printf("%d\n", d + d3);
+        else if (r1 == r2 && r2 == r3 && r1.first * d % r1.second == 0) printf("%d\n", r1.first * d / r1.second);
+        else puts("42");
+    }
+    return 0;
+}
