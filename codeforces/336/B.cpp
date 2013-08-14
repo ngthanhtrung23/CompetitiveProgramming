e80f42e3986473a2f035e6a15893e9ee9c88a786
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-14 15:19:00 +0800

    Add solution for problem `B - Vasily the Bear and Fly` Link: https://codeforces.com/contest/336/problem/B Tags: math, *1900 Ref: https://codeforces.com/contest/336/submission/4276299

---
+++ codeforces/336/B.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+long double sqrt2 = sqrt((long double) 2.0);
+
+int m, R;
+long double f[100111], sum[100111];
+
+int main() {
+    while (cin >> m >> R) {
+        long double res = 0.0;
+        f[0] = R + R;
+        f[1] = R + R + sqrt2 * R;
+        f[2] = R + R + sqrt2 * 2 * R;
+
+        FOR(i,3,m) {
+            f[i] = f[i-1] + R + R;
+        }
+        sum[0] = f[0];
+        FOR(i,1,m) sum[i] = sum[i-1] + f[i];
+
+        FOR(i,1,m) {
+            long double now = sum[i-1] + sum[m-i] - f[0];
+            res += now / m;
+        }
+        res /= m;
+        cout << (fixed) << setprecision(9) << res << endl;
+    }
+    return 0;
+}
