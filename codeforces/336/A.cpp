38234c9b687895ae78b0b15079b8c83ca177f378
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-14 14:20:00 +0800

    Add solution for problem `A - Vasily the Bear and Triangle` Link: https://codeforces.com/contest/336/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/336/submission/4276058

---
+++ codeforces/336/A.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+int main() {
+    int x, y;
+    bool flipx, flipy;
+    while (cin >> x >> y) {
+        if (x < 0) flipx = true, x = -x;
+        else flipx = false;
+
+        if (y < 0) flipy = true, y = -y;
+        else flipy = false;
+
+        int x1 = 0, y1 = x+y, x2 = x+y, y2 = 0;
+        if (flipx) x1 = -x1, x2 = -x2;
+        if (flipy) y1 = -y1, y2 = -y2;
+
+        if (x1 > x2) {
+            swap(x1, x2);
+            swap(y1, y2);
+        }
+        cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << endl;
+    }
+    return 0;
+}
