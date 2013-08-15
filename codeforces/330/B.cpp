8eca72150d121c1fc0b80e9b18ce4290a0fd86a6
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-15 18:58:00 +0800

    Add solution for problem `B - Road Construction` Link: https://codeforces.com/contest/330/problem/B Tags: constructive algorithms, graphs, *1300 Ref: https://codeforces.com/contest/330/submission/4281695

--- codeforces/330/B.cpp
+++ codeforces/330/B.cpp
@@ -1,35 +1,69 @@
-
 #include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const int MN = 1011;
 int n, m;
-bool bad[1011][1011];
+bool good[MN][MN];
 
 int main() {
+    ios :: sync_with_stdio(false);
     while (cin >> n >> m) {
-        FOR(i,1,m) {
+        memset(good, true, sizeof good);
+        while (m--) {
             int u, v; cin >> u >> v;
-            bad[u][v] = bad[v][u] = true;
+            good[u][v] = good[v][u] = false;
         }
 
-        FOR(center,1,n) {
-            bool ok = true;
-            FOR(i,1,n) if (i != center && bad[i][center]) ok = false;
-
-            if (ok) {
+        FOR(i,1,n) {
+            bool can = true;
+            FOR(j,1,n) if (j != i && !good[i][j]) {
+                can = false;
+                break;
+            }
+            if (can) {
                 cout << n-1 << endl;
-                FOR(i,1,n) if (i != center) cout << center << ' ' << i << endl;
+                FOR(j,1,n) if (j != i) cout << i << ' ' << j << endl;
                 break;
             }
         }
