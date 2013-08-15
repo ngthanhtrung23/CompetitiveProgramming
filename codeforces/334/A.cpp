6caca4b7a5c6d3b10c62804bbbf65b50783ea577
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-15 18:34:00 +0800

    Add solution for problem `A - Candy Bags` Link: https://codeforces.com/contest/334/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/334/submission/4281593

---
+++ codeforces/334/A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int a[111][111];
+
+int main() {
+    int n;
+    while (cin >> n) {
+        int now = 0;
+        FOR(j,1,n) {
+            if (j % 2 == 1) {
+                FOR(i,1,n) a[i][j] = ++now;
+            }
+            else {
+                FORD(i,n,1) a[i][j] = ++now;
+            }
+        }
+        FOR(i,1,n) {
+            FOR(j,1,n) cout << a[i][j] << ' ';
+            cout << endl;
+        }
+    }
+    return 0;
+}
