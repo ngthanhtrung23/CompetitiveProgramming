d91d9bea3811cd51ff3d17488de42d649b7ccecf
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-15 14:41:00 +0800

    Add solution for problem `B - Chips` Link: https://codeforces.com/contest/333/problem/B Tags: greedy, *1800 Ref: https://codeforces.com/contest/333/submission/4280636

---
+++ codeforces/333/B.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const int MN = 1011;
+bool row[MN], col[MN];
+int n, m, cnt;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n >> m) {
+        memset(row, true, sizeof row);
+        memset(col, true, sizeof col);
+        while (m--) {
+            int u, v; cin >> u >> v;
+            row[u] = col[v] = false;
+        }
+
+        int res = 0;
+        for(int i1 = 2, i2 = n-1; i1 <= i2; ++i1, --i2) {
+            if (i1 == i2) {
+                if (row[i1] || col[i1]) ++res;
+            }
+            else {
+                int cnt = 0;
+                if (row[i1]) ++cnt;
+                if (row[i2]) ++cnt;
+                if (col[i1]) ++cnt;
+                if (col[i2]) ++cnt;
+
+                res += cnt;
+            }
+        }
+
+        cout << res << endl;
+    }
+    return 0;
+}
