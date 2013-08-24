d0947f6d4c68016788387c1a6f193e187d65fc82
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-24 14:57:00 +0800

    Add solution for problem `B - Routine Problem` Link: https://codeforces.com/contest/337/problem/B Tags: greedy, math, number theory, *1400 Ref: https://codeforces.com/contest/337/submission/4331267

---
+++ codeforces/337/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+long long gcd(long long a, long long b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main() {
+    long long a, b, c, d;
+    while (cin >> a >> b >> c >> d) {
+        long long bc = b * c;
+        long long ad = a * d;
+        long long g = gcd(bc, ad);
+
+        if (bc == ad) cout << "0/1" << endl;
+        else if (bc < ad) cout << (ad - bc) / g << '/' << ad / g << endl;
+        else cout << (bc - ad) / g << '/' << bc / g << endl;
+    }
+    return 0;
+}
