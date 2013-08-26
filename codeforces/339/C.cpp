c7baada15f5c37b39e18a35aa5d141a65f81d765
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-26 23:58:00 +0800

    Add solution for problem `C - Xenia and Weights` Link: https://codeforces.com/contest/339/problem/C Tags: constructive algorithms, dfs and similar, dp, graphs, greedy, shortest paths, *1700 Ref: https://codeforces.com/contest/339/submission/4344415

--- codeforces/339/C.cpp
+++ codeforces/339/C.cpp
@@ -1,38 +1,75 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
 
-char s[12309];
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
 
-int FF[2309][12][12];
-int F[2309][12][12];
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
 
-bool f(int rem, int just, int level, bool tracing=false){
-    int i;
-    bool r=false;
-    if (rem==0) return true;
-    if (!tracing && FF[rem][just][level]++) return F[rem][just][level];
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-    for (i=1; i<=10; i++)
-    if (s[i]=='1' && i!=just && i>level)
-    if (f(rem-1, i, i-level)) {
-        r=true;
-        if (tracing) {
-            printf("%d ", i);
-            f(rem-1, i, i-level, true);
-        }
-        break;
-    }
+string s;
+int m;
+bool f[1011][22][22];
+pair<int,int> tr[1011][22][22];
 
-    //printf("f(%d, %d, %d) = %d\n", rem, just, level, r);
-    return F[rem][just][level] = r;
+void trace(int m, int a, int b) {
+    if (m == 0) return ;
+    trace(m-1, tr[m][a][b].first, tr[m][a][b].second);
+    printf("%d ", b);
 }
 
-int n;
-main(){
-    scanf("%s%d", s+1, &n);
-    if (f(n, 0, 0)) {
-        printf("YES\n");
-        f(n, 0, 0, true);
-        printf("\n");
+int main() {
+    while (cin >> s) {
+        cin >> m;
+        memset(f, false, sizeof f);
+
+        f[0][0][0] = true;
+        FOR(move,0,m-1) FOR(diff,0,9) FOR(last,0,10)
+        if (f[move][diff][last])
+            FOR(cur,1,10)
+            if (s[cur-1] == '1' && cur != last && cur > diff) {
+                f[move+1][cur - diff][cur] = true;
+                tr[move+1][cur - diff][cur] = make_pair(diff, last);
+            }
+
+        bool found = false;
+        FOR(diff,0,10) FOR(last,0,10) {
+            if (f[m][diff][last]) {
+                puts("YES");
+                trace(m, diff, last);
+                puts("");
+                found = true;
+                break;
+            }
+            if (found) break;
+        }
+        if (!found) puts("NO");
     }
-    else printf("NO\n");
+    return 0;
 }
