a815817274d056f53a5eeb5e1694339385ead729
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-31 03:21:00 +0800

    Add solution for problem `A - Tourist Problem` Link: https://codeforces.com/contest/341/problem/A Tags: math, *1600 Ref: https://codeforces.com/contest/341/submission/4383777

--- codeforces/341/A.cpp
+++ codeforces/341/A.cpp
@@ -1,56 +1,62 @@
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <iostream>
-#include <cstdio>
-#include <set>
-#include <queue>
-#include <map>
-#include <string>
-#include <cstring>
-#include <ctime>
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
-#define For(i,a,b) for(int i=a;i<=b;i++)
-#define Ford(i,a,b) for(int i=a;i>=b;i--)
-#define Rep(i,c) for((type of c.begin()) i;i!=c.end();i++)
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
-#define sr(x) (int)x.size()
-#define modul 1000000007
-#define nmax 100100
-#define BUG(x) {cout << #x << " = " << x << endl;}
-#define PR(x,a,b) {cout << #x << " = "; For(_,a,b) cout << x[_] << ' '; cout << endl;}
-#define fillchar(x,a,b,delta) For(_,a,b) x[_]=delta;
-#define Bit(s,i) ((s&(1<<i))>0)
-#define Two(x) (1<<x)
-#define pii pair<int,int>
-#define ll long long
-#define e 1e-6
-#define PI acos(-1)
-int n;
-int a[nmax];
-ll sum;
-ll gcd(ll u,ll v) {
-    if (v==0) return u;
-    return gcd(v,u % v);
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
 }
-int main()
-{
-  //  freopen("codeforce.inp","r",stdin);
-    scanf("%d",&n);
-    For(i,1,n) scanf("%d",&a[i]);
-    ll res=0;
-    sort(a+1,a+n+1);
-    sum=0;
-    For(i,1,n) {
-        res+=(ll)a[i]*(i-1) - sum;
-        sum=sum+a[i];
-    } 
-    res*=2;
-    For(i,1,n) res+=a[i];
-    ll sol=gcd(res,n); 
-    cout << res/sol << " " << n/sol;
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const int MN = 100111;
+
+int n;
+long long a[MN], l[MN], r[MN];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        sort(a+1, a+n+1);
+        r[n+1] = 0;
+        FOR(i,1,n) l[i] = l[i-1] + a[i];
+        FORD(i,n,1) r[i] = r[i+1] + a[i];
+
+        long long sum = l[n];
+
+        FOR(i,1,n) {
+            sum += i * a[i] - l[i];
+            sum += r[i] - a[i] * (n-i+1);
+        }
+
+        long long g = __gcd(sum, (long long) n);
+
+        cout << sum / g << ' ' << n / g << endl;
+    }
     return 0;
 }
