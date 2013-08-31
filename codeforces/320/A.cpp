cfb25d8d859c5454fe84976eb6b4f458f64e1d57
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-31 17:13:00 +0800

    Add solution for problem `A - Magic Numbers` Link: https://codeforces.com/contest/320/problem/A Tags: brute force, greedy, *900 Ref: https://codeforces.com/contest/320/submission/4386633

---
+++ codeforces/320/A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+string s;
+
+int main() {
+    while (cin >> s) {
+        int n = s.length();
+
+        bool ok = true;
+        REP(i,n) if (s[i] != '1' && s[i] != '4') ok = false;
+
+        REP(i,n-2) if (s[i] == '4' && s[i+1] == '4' && s[i+2] == '4') ok = false;
+
+        if (s[0] == '4') ok = false;
+
+        if (ok) puts("YES"); else puts("NO");
+    }
+    return 0;
+}
