ea9d611037bd03d23de1cac79fd65dbaa106959c
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-08-31 17:51:00 +0800

    Add solution for problem `A - Black-and-White Cube` Link: https://codeforces.com/contest/323/problem/A Tags: combinatorics, constructive algorithms, *1600 Ref: https://codeforces.com/contest/323/submission/4386844

---
+++ codeforces/323/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int main() {
+    int n;
+    while (cin >> n) {
+        if (n & 1) puts("-1\n");
+        else {
+            FOR(layer,1,n) {
+                FOR(i,1,n) {
+                    FOR(j,1,n) {
+                        int d = min( min(i-1, n-i), min(j-1,n-j) );
+                        if ((d+layer) & 1) putchar('b');
+                        else putchar('w');
+                    }
+                    puts("");
+                }
+                puts("");
+            }
+        }
+    }
+    return 0;
+}
