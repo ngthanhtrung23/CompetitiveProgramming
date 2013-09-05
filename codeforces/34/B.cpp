cdf2dce623eb109902eef12ec754fb76c0ca321d
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-05 23:44:00 +0800

    Add solution for problem `B - Sale` Link: https://codeforces.com/contest/34/problem/B Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/34/submission/4411755

---
+++ codeforces/34/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int m, n, a[111];
+
+int main() {
+    while (cin >> n >> m) {
+        FOR(i,1,n) cin >> a[i];
+        int res = 0;
+        sort(a+1, a+n+1);
+        FOR(i,1,min(m,n)) {
+            if (a[i] > 0) break;
+            res -= a[i];
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
