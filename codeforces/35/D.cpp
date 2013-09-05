771a49cd4ddcc972d7c026064db4bdfac96f6333
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-05 23:14:00 +0800

    Add solution for problem `D - Animals` Link: https://codeforces.com/contest/35/problem/D Tags: dp, greedy, *1700 Ref: https://codeforces.com/contest/35/submission/4411620

---
+++ codeforces/35/D.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int a[111];
+
+int main() {
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    int n, x;
+    while (cin >> n >> x) {
+        FOR(i,1,n) {
+            cin >> a[i];
+            a[i] = a[i] * (n - i + 1);
+        }
+        sort(a+1, a+n+1);
+        int res = 0, cur = 0;
+        FOR(i,1,n) {
+            if (cur + a[i] <= x) {
+                res = i;
+                cur += a[i];
+            }
+            else break;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
