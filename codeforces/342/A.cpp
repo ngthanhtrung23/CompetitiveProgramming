93f22a6b4616f533d2ce9827567a6faf4a943754
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-07 16:05:00 +0800

    Add solution for problem `A - Xenia and Divisors` Link: https://codeforces.com/contest/342/problem/A Tags: greedy, implementation, *1200 Ref: https://codeforces.com/contest/342/submission/4417001

---
+++ codeforces/342/A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int a[100111], cnt[11];
+
+int main() {
+    int n;
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        memset(cnt, 0, sizeof cnt);
+        FOR(i,1,n) cin >> a[i], ++cnt[a[i]];
+
+        int x = cnt[4], z = cnt[3], y = cnt[6] - z;
+        if (x >= 0 && y >= 0 && z >= 0 && cnt[5] == 0 && cnt[7] == 0) {
+            if (cnt[1] == x + y + z && cnt[2] == x + y) {
+                while (x--) puts("1 2 4");
+                while (y--) puts("1 2 6");
+                while (z--) puts("1 3 6");
+                continue;
+            }
+        }
+        puts("-1");
+    }
+    return 0;
+}
