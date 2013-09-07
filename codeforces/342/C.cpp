d7fd7c3f2f26b86ddf614a2f7ea084a27f9f85ae
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-07 17:33:00 +0800

    Add solution for problem `C - Cupboard and Balloons` Link: https://codeforces.com/contest/342/problem/C Tags: geometry, *1900 Ref: https://codeforces.com/contest/342/submission/4423660

---
+++ codeforces/342/C.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int main() {
+    int h, r;
+    while (cin >> r >> h) {
+        int res = (h / r) * 2;
+        int t = h % r;
+        if (t >= sqrt((double)3.0) * r / 2.0) res += 3;
+        else if (2*t >= r) res += 2;
+        else res += 1;
+        cout << res << endl;
+    }
+    return 0;
+}
