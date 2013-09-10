4a8c837296be0072a1ec48441d30f0d7d8b50db1
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-10 23:53:00 +0800

    Add solution for problem `C - Ilya and Matrix` Link: https://codeforces.com/contest/313/problem/C Tags: constructive algorithms, greedy, implementation, sortings, *1400 Ref: https://codeforces.com/contest/313/submission/4439737

---
+++ codeforces/313/C.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int x[4000111];
+int n;
+
+inline bool good(int k) {
+    while ((k & 3) == 0) k >>= 2;
+    return k == 1;
+}
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        int t = 0; while ((1<<(t+t)) < n) ++t;
+        FOR(i,1,n) cin >> x[i];
+        sort(x+1, x+n+1, greater<int>());
+
+        long long res = 0;
+        ++t;
+        FOR(i,1,n) {
+            res += x[i] * (long long) t;
+            if (good(i)) {
+                --t;
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
