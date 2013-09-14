125d9ce6a09979cbd5abb9152f2d51f76f0fa62f
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-15 01:28:00 +0800

    Add solution for problem `A - Rational Resistance` Link: https://codeforces.com/contest/343/problem/A Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/343/submission/4469772

--- codeforces/343/A.cpp
+++ codeforces/343/A.cpp
@@ -1,27 +1,48 @@
-
 #include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
 long long get(long long a, long long b) {
+    if (a == 1 || b == 1) return max(a, b);
     if (a > b) swap(a, b);
-    if (a == 1) return b;
-    return get(b%a, a) + b / a;
+    return b / a + get(b%a, a);
 }
 
 int main() {
-    ios :: sync_with_stdio(false);
     long long a, b;
-    while (cin >> a >> b) cout << get(a, b) << endl;
+    while (cin >> a >> b) {
+        cout << get(a, b) << endl;
+    }
     return 0;
 }
