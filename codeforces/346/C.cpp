9e23db71609d05e209ef43d7d918b8eb303a2354
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-09-21 16:13:00 +0800

    Add solution for problem `C - Number Transformation II` Link: https://codeforces.com/contest/346/problem/C Tags: greedy, math, *2200 Ref: https://codeforces.com/contest/346/submission/4528381

--- codeforces/346/C.cpp
+++ codeforces/346/C.cpp
@@ -1,67 +1,91 @@
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-#include <complex>
-#include <cstring>
-#include <cstdlib>
-#include <string>
-#include <vector>
-#include <cstdio>
-#include <cmath>
-#include <map>
-#include <set>
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
-//#pragma comment(linker,"/STACK:102400000,102400000")
 
-int n;
-vector <int> xs;
-vector <int> nex_xs;
-int A, B;
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-map <int, int> occur;
+int n, x[100111], a, b, gh;
+int uoc[1000111];
+int bit[1000111];
 
-int MAIN()
-{
-	cin >> n;
-	for(int i = 1; i <= n; i++)
-	{
-		int t;
-		cin >> t;
-		if(!occur.count(t))
-			xs.push_back(t);
-		occur[t] = 1;
-	}
-	cin >> A >> B;
-	int ans = 0;
-	while(A != B)
-	{
-		nex_xs.clear();
-		int bestA = A - 1;
-		for(int i = 0; i < xs.size(); i++)
-		{
-			int t = xs[i];
-			int v = (A / t) * t;
-			if(v >= B)
-			{
-				nex_xs.push_back(t);
-				bestA = min(bestA, v);
-			}
-		}
-		A = bestA;
-		xs = nex_xs;
-		ans ++;
-	}
-	cout << ans << endl;
-	return 0;
+inline void update(int u, int val) {
+    for(int x = u; x <= gh; x += x & -x)
+        bit[x] = min(bit[x], val);
 }
 
-int main()
-{
-	#ifdef LOCAL_TEST
-		freopen("in.txt", "r", stdin);
-		freopen("out.txt", "w", stdout);
-	#endif
-	ios :: sync_with_stdio(false);
-	cout << fixed << setprecision(16);
-	return MAIN();
+inline int get(int u) {
+    int res = 1000111000;
+    for(int x = u; x > 0; x -= x & -x)
+        res = min(res, bit[x]);
+    return res;
+}
+
+int main() {
+    GN(n);
+    FOR(i,1,n) GN(x[i]);
+    GN(a); GN(b);
+    sort(x+1, x+n+1);
+    n = unique(x+1, x+n+1) - x - 1;
+
+    cin >> a >> b;
+    if (a == b) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    memset(uoc, 0, sizeof uoc);
+    FOR(i,1,n) {
+        int u = (b / x[i]) * x[i]; if (u < b) u += x[i];
+        while (u < a) {
+            uoc[u-b] = x[i];
+            u += x[i];
+        }
+    }
+
+    gh = a - b;
+    FOR(i,1,1000110) bit[i] = 1000111000;
+
+    update(gh, 0);
+
+    int ln, d;
+    FORD(u,gh-1,0) {
+        ln = u+1;
+        if (uoc[u]) ln = max(ln, u+uoc[u]-1);
+
+        if (ln > gh) ln = gh;
+        d = get(ln) + 1;
+
+        if (u != 0) update(u, d);
+        else cout << d << endl;
+    }
+    return 0;
 }
