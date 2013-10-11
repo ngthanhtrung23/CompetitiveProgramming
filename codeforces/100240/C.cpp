f4931c0cb5ee548b524a9e8eb9b3317feff71882
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-10-11 14:27:00 +0800

    Add solution for problem `C - Optimal Parking` Link: https://codeforces.com/gym/100240/problem/C Tags: Ref: https://codeforces.com/gym/100240/submission/4742216

---
+++ codeforces/100240/C.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int n, a[111];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int ntest; cin >> ntest;
+    while (ntest--) {
+        cin >> n; FOR(i,1,n) cin >> a[i];
+        cout << (*max_element(a+1, a+n+1) - *min_element(a+1, a+n+1)) * 2 << endl;
+    }
+    return 0;
+}
