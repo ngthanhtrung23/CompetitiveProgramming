76ac1374f04d5b2b85240709a6ea181808a47d63
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-10-14 03:54:00 +0800

    Add solution for problem `C - Vasya and Beautiful Arrays` Link: https://codeforces.com/contest/354/problem/C Tags: brute force, dp, number theory, *2100 Ref: https://codeforces.com/contest/354/submission/4779221

---
+++ codeforces/354/C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int n, k, a[300111];
+
+bool check(int &val) {
+    FOR(i,1,n) {
+        if (a[i] % val > k) {
+            if (a[i] / val == a[i] / (val - 1)) val = a[i] / (a[i] / val + 1);
+            else val--;
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n >> k) {
+        FOR(i,1,n) cin >> a[i];
+        sort(a+1, a+n+1);
+        n = unique(a+1, a+n+1) - a - 1;
+        int res = a[1];
+        while (!check(res));
+        cout << res << endl;
+    }
+    return 0;
+}
