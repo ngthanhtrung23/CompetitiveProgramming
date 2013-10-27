df25180a5ba09112397296d02f74b8528f72fa3a
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-10-27 15:49:00 +0800

    Add solution for problem `H - Password Service` Link: https://codeforces.com/gym/100253/problem/H Tags: Ref: https://codeforces.com/gym/100253/submission/4909971

---
+++ codeforces/100253/H.cpp
@@ -0,0 +1,163 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+
+#define Rep(i,n) for(int i = 0; i < (n); ++i)
+#define Repd(i,n) for(int i = (n)-1; i >= 0; --i)
+#define For(i,a,b) for(int i = (a); i <= (b); ++i)
+#define Ford(i,a,b) for(int i = (a); i >= (b); --i)
+#define Fit(i,v) for(__typeof((v).begin()) i = (v).begin(); i != (v).end(); ++i)
+#define Fitd(i,v) for(__typeof((v).rbegin()) i = (v).rbegin(); i != (v).rend(); ++i)
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(), (a).end()
+#define ms(a,x) memset(a, x, sizeof(a))
+
+template<class F, class T> T convert(F a, int p = -1) { stringstream ss; if (p >= 0) ss << fixed << setprecision(p); ss << a; T r; ss >> r; return r; }
+template<class T> T gcd(T a, T b) { T r; while (b != 0) { r = a % b; a = b; b = r; } return a; }
+template<class T> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+template<class T> T sqr(T x) { return x * x; }
+template<class T> T cube(T x) { return x * x * x; }
+template<class T> int getbit(T s, int i) { return (s >> i) & 1; }
+template<class T> T onbit(T s, int i) { return s | (T(1) << i); }
+template<class T> T offbit(T s, int i) { return s & (~(T(1) << i)); }
+template<class T> int cntbit(T s) { return s == 0 ? 0 : cntbit(s >> 1) + (s & 1); }
+const int bfsz = 1 << 16; char bf[bfsz + 5]; int rsz = 0;int ptr = 0;
+char gc() { if (rsz <= 0) { ptr = 0; rsz = (int) fread(bf, 1, bfsz, stdin); if (rsz <= 0) return EOF; } --rsz; return bf[ptr++]; }
+void ga(char &c) { c = EOF; while (!isalpha(c)) c = gc(); }
+int gs(char s[]) { int l = 0; char c = gc(); while (isspace(c)) c = gc(); while (c != EOF && !isspace(c)) { s[l++] = c; c = gc(); } s[l] = '\0'; return l; }
+template<class T> bool gi(T &v) {
+    v = 0; char c = gc(); while (c != EOF && c != '-' && !isdigit(c)) c = gc(); if (c == EOF) return false; bool neg = c == '-'; if (neg) c = gc();
+    while (isdigit(c)) { v = v * 10 + c - '0'; c = gc(); } if (neg) v = -v; return true;
+}
+
+typedef pair<int, int> II;
+
+const ld eps = 1e-9;
+const ld PI = acos(-1.0);
+
+const int inf = (int)1e9 + 5;
+const ll linf = (ll)1e17 + 5;
+int dr[4] = {-1, 0, +1, 0};
+int dc[4] = {0, -1, 0, +1};
+const ll mod = 1000000007;
+
+#define maxn 5005
+
+string s;
+int a[maxn][2], f[maxn][2], ch[maxn];
+int number;
+
+int main() {
+//#ifdef LOCAL
+//  freopen("in.txt", "r", stdin);
+//  freopen("output", "w", stdout);
+//#endif
+
+    cin >> number;
+    cin >> s;
+    int n = s.length();
+    ms(a, 0);
+    a[1][0] = -1;
+    a[n + 1][1] = -1;
+    Rep(i, n){
+        if(s[i] == '<'){
+            a[i + 1][1] = -1;
+            a[i + 2][0] = 1;
+        }
+        else if(s[i] == '>'){
+            a[i + 1][1] = 1;
+            a[i + 2][0] = -1;
+        }
+    }
+    n++;
+//  For(i, 1, n) cout << a[i][0] << " " << a[i][1] << endl;
+    f[1][0] = -1;
+    For(i, 2, n) {
+        if(a[i][0] == 0){
+            f[i][0] = f[i - 1][0];
+        }
+        else f[i][0] = a[i][0];
+    }
+
+    f[n][1] = -1;
+    Ford(i, n - 1, 1) {
+        if(a[i][1] == 0){
+            f[i][1] = f[i + 1][1];
+        }
+        else f[i][1] = a[i][1];
+    }
+
+    ms(ch, -1);
+
+    For(i, 1, n) if(f[i][0] == -1 && f[i][1] == -1){
+        ch[i] = 0;
+    }
+    int u;
+    Rep(run, n + 2){
+        For(i, 1, n){
+            u = i - 1;
+            if(u > 0){
+                if(a[i][0] == 0){
+                    ch[u] = max(ch[u], ch[i]);
+                }
+                if(a[i][0] == -1){
+                    ch[u] = max(ch[u], ch[i] + 1);
+                }
+            }
+            u = i + 1;
+            if(u < n + 1){
+                if(a[i][1] == 0){
+                    ch[u] = max(ch[u], ch[i]);
+                }
+                if(a[i][1] == -1){
+                    ch[u] = max(ch[u], ch[i] + 1);
+                }
+            }
+        }
+    }
+
+    int MAX = 0;
+    For(i, 1, n) MAX = max(MAX, ch[i]);
+    if(MAX >= number){
+        cout << -1;
+        return 0;
+    }
+
+    For(i, 1, n) {
+        cout << (char)(ch[i] + 'a');
+//      cout << ch[i] << endl;
+    }
+
+    return 0;
+}
+
