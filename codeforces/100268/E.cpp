d4e8fa643d55f7f11e8bc8a68819f1d0e83c7aaa
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-11-01 14:10:00 +0800

    Add solution for problem `E - Credit Card Payment` Link: https://codeforces.com/gym/100268/problem/E Tags: Ref: https://codeforces.com/gym/100268/submission/4948869

---
+++ codeforces/100268/E.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int r, b, m;
+
+int read() {
+    int x, y;
+    scanf("%d.%d", &x, &y);
+    return x * 100 + y;
+}
+
+int main() {
+    int ntest; scanf("%d", &ntest);
+    while (ntest--) {
+        int r = read(), b = read(), m = read();
+
+        int res = 1500;
+
+        FOR(t,1,1200) {
+            b = (int) (round(b * (100 + r / (double) 100.0) / (double) 100.0) + 1e-6);
+            b -= m;
+            if (b <= 0) {
+                res = t;
+                break;
+            }
+        }
+
+        if (res == 1500) puts("impossible");
+        else printf("%d\n", res);
+    }
+    return 0;
+}
