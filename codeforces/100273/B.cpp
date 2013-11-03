620918a16b700b3ba2a1437500e0ca7aed669347
Trung Nguyen <ngthanhtrung23@gmail.com>
2013-11-03 22:17:00 +0800

    Add solution for problem `B - Building for UN` Link: https://codeforces.com/gym/100273/problem/B Tags: Ref: https://codeforces.com/gym/100273/submission/4979864

---
+++ codeforces/100273/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+char get(int u) {
+    if (u <= 25) return 'a' + u - 1;
+    return 'A' + u - 26;
+}
+
+int main() {
+    ios :: sync_with_stdio(false);
+    freopen("building.in", "r", stdin);
+    freopen("building.out", "w", stdout);
+    int n;
+    while (cin >> n) {
+        cout << 2 << ' ' << n << ' ' << n << endl;
+        FOR(i,1,n) {
+            FOR(j,1,n) cout << get(i);
+            cout << endl;
+        }
+        cout << endl;
+        FOR(i,1,n) {
+            FOR(j,1,n) cout << get(j);
+            cout << endl;
+        }
+        cout << endl;
+    }
+    return 0;
+}
