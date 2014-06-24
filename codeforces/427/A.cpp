7d0740b2150b87497cd7c36672b464fa7065256c
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-06-25 00:20:00 +0800

    Add solution for problem `A - Police Recruits` Link: https://codeforces.com/contest/427/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/427/submission/6953874

---
+++ codeforces/427/A.cpp
@@ -0,0 +1,52 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int n;
+
+int main() {
+	ios :: sync_with_stdio(false);
+	while (cin >> n) {
+		int res = 0, cur = 0;
+		FOR(i,1,n) {
+			int a; cin >> a;
+			if (a < 0) {
+				if (cur == 0) ++res;
+				else --cur;
+			}
+			else {
+				cur += a;
+			}
+		}
+		cout << res << endl;
+	}
+	return 0;
+}
