28711b12b7ced0911f1b206f2001ead8b33d82bb
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-08-26 22:04:00 +0800

    Add solution for problem `A - Appleman and Toastman` Link: https://codeforces.com/contest/461/problem/A Tags: greedy, sortings, *1200 Ref: https://codeforces.com/contest/461/submission/7580500

---
+++ codeforces/461/A.cpp
@@ -0,0 +1,51 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int n, a[300111];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        sort(a+1, a+n+1);
+
+        long long cur = n+1;
+        long long res = 0;
+        FORD(i,n,1) {
+            res += a[i] * min(cur, (long long) n);
+            --cur;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
