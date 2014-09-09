af390dfe6fc7fc969120f384a7795d9431779a97
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-09-09 14:21:00 +0800

    Add solution for problem `A - inc ARG` Link: https://codeforces.com/contest/465/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/465/submission/7725893

---
+++ codeforces/465/A.cpp
@@ -0,0 +1,48 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+string s;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (cin >> n) {
+        cin >> s;
+        int last = 0;
+        while (last < n && s[last] == '1') ++last;
+        if (last == n) --last;
+
+        cout << last + 1 << endl;
+    }
+    return 0;
+}
