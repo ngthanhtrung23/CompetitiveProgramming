08bdb7368e719462d0d8279f5beddbb623df14ce
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-09-09 15:08:00 +0800

    Add solution for problem `B - Caisa and Pylons` Link: https://codeforces.com/contest/463/problem/B Tags: brute force, implementation, math, *1100 Ref: https://codeforces.com/contest/463/submission/7726117

---
+++ codeforces/463/B.cpp
@@ -0,0 +1,53 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+const int MN = 100111;
+int n, a[MN];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        long long res = a[1];
+        long long energy = 0; a[0] = a[1];
+        FOR(i,1,n) {
+            energy += a[i-1] - a[i];
+            if (energy < 0) {
+                res -= energy;
+                energy = 0;
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
