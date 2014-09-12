9a0661797b0f0d64cffff8a3d554805248459ebf
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-09-13 00:52:00 +0800

    Add solution for problem `C - Expression` Link: https://codeforces.com/gym/100484/problem/C Tags: Ref: https://codeforces.com/gym/100484/submission/7767454

---
+++ codeforces/100484/C.cpp
@@ -0,0 +1,48 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (cin >> n) {
+        string res = "((A0|B0)|(A0|B0))";
+        FOR(i,1,n-1) {
+            stringstream ss; ss << i;
+            string ii = ss.str();
+
+            res = "((A" + ii + "|B" + ii + ")|(" + res + "|" + "((A" + ii + "|A" + ii + ")|(B" + ii + "|B" + ii + "))))";
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
