80b25a63c75b1f66a4a35c3ab061f538e74a4e92
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-09-21 16:33:00 +0800

    Add solution for problem `A - I Wanna Be the Guy` Link: https://codeforces.com/contest/469/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/469/submission/7892831

---
+++ codeforces/469/A.cpp
@@ -0,0 +1,53 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int n, p, q;
+bool can[111];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        memset(can, false, sizeof can);
+        REP(turn,2) {
+            cin >> p; while (p--) { int u; cin >> u; can[u] = true; }
+        }
+
+        bool ok = true;
+        FOR(i,1,n) if (!can[i]) ok = false;
+        if (ok) cout << "I become the guy.";
+        else cout << "Oh, my keyboard!";
+        cout << endl;
+    }
+    return 0;
+}
