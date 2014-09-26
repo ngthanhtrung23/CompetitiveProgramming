f651f56b542c9dfd28c8f5d40bd6402c63937960
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-09-26 23:55:00 +0800

    Add solution for problem `C - MUH and House of Cards` Link: https://codeforces.com/contest/471/problem/C Tags: binary search, brute force, greedy, math, *1700 Ref: https://codeforces.com/contest/471/submission/7962571

---
+++ codeforces/471/C.cpp
@@ -0,0 +1,59 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+const int MN = 1000111;
+long long start[MN + 11];
+
+int main() {
+    ios :: sync_with_stdio(false);
+
+    long long cur = 2, step = 5;
+    FOR(i,1,MN) {
+        start[i] = cur;
+
+        cur += step;
+        step += 3;
+    }
+
+    long long n;
+    while (cin >> n) {
+        int res = 0;
+        FOR(i,1,MN) if (n >= start[i]) {
+            if ((n - start[i]) % 3 == 0) ++res;
+        } else break;
+
+        cout << res << endl;
+    }
+    return 0;
+}
