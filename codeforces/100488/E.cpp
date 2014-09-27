7e076e6516530f83885ffdd79a8f5b7f7ff5de35
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-09-27 17:17:00 +0800

    Add solution for problem `E - Just Change a Word` Link: https://codeforces.com/gym/100488/problem/E Tags: Ref: https://codeforces.com/gym/100488/submission/7982129

---
+++ codeforces/100488/E.cpp
@@ -0,0 +1,55 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int cnt[300];
+string s;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> s) {
+        memset(cnt, 0, sizeof cnt);
+        int ln = 0;
+        REP(i,s.length()) {
+            cnt[s[i]]++;
+            ln = max(ln, cnt[s[i]]);
+        }
+
+        if (s.length() % 2 == 1) cout << "NO";
+        else if (ln > s.length() / 2) cout << "NO";
+        else cout << "YES";
+        cout << endl;
+    }
+    return 0;
+}
+
