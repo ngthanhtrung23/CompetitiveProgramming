d395e4c234f810f1b7853292f6f065ee012d0b8b
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-02 21:28:00 +0800

    Add solution for problem `A - Dance Floor` Link: https://codeforces.com/gym/100497/problem/A Tags: Ref: https://codeforces.com/gym/100497/submission/8058203

---
+++ codeforces/100497/A.cpp
@@ -0,0 +1,51 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (cin >> n) {
+        int sum_m = 0, sum_w = 0, sum_s = 0;
+        int m, w, s;
+        FOR(i,1,n) {
+            cin >> m >> w >> s;
+            if (s > m + w) s = m + w;
+            sum_m += m;
+            sum_w += w;
+            sum_s += s;
+        }
+        cout << min(sum_s / 2, min(sum_m, sum_w)) << endl;
+    }
+    return 0;
+}
