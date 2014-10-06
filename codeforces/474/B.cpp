94da8e9052ffa8a882a292662ff8be9089ed6022
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-07 00:27:00 +0800

    Add solution for problem `B - Worms` Link: https://codeforces.com/contest/474/problem/B Tags: binary search, implementation, *1200 Ref: https://codeforces.com/contest/474/submission/8115267

---
+++ codeforces/474/B.cpp
@@ -0,0 +1,53 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+const int MN = 100111;
+
+int n, a[MN];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    while (cin >> n) {
+        FOR(i,1,n) {
+            cin >> a[i];
+            a[i] += a[i-1];
+        }
+        int q; cin >> q;
+        while (q--) {
+            int u; cin >> u;
+            cout << lower_bound(a+1, a+n+1, u) - a << "\n";
+        }
+    }
+    return 0;
+}
