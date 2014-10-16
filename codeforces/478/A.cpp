cfd6c8d89d8b0cbd04cb421e2f8f06e4f79b00e5
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-16 23:42:00 +0800

    Add solution for problem `A - Initial Bet` Link: https://codeforces.com/contest/478/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/478/submission/8249810

---
+++ codeforces/478/A.cpp
@@ -0,0 +1,44 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int a[7];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int sum = 0;
+    REP(i,5) cin >> a[i], sum += a[i];
+    if (!sum || sum % 5) cout << -1 << endl; else cout << sum / 5 << endl;
+    return 0;
+}
