62ffaaca47b92a96f2cd213ce644e8e76721583d
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-16 19:37:00 +0800

    Add solution for problem `D - k-Maximum Subsequence Sum` Link: https://codeforces.com/contest/280/problem/D Tags: data structures, flows, graphs, implementation, *2800 Ref: https://codeforces.com/contest/280/submission/8247170

--- codeforces/280/D.cpp
+++ codeforces/280/D.cpp
@@ -1,218 +1,214 @@
-#include <bits/stdc++.h>
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
-using namespace std;
-
-//Buffer reading
-int INP,AM,REACHEOF;
-const int BUFSIZE = (1<<12) + 17;
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp && !REACHEOF) { \
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
 
 const int MN = 100111;
 
-template<typename T> T MIN(T a, T b) { return (a < b) ? a : b; }
-template<typename T> T MAX(T a, T b) { return (a > b) ? a : b; }
-
 struct Val {
     int u, v, val;
-
     Val(int u = 0, int v = 0, int val = 0) : u(u), v(v), val(val) {}
 
     Val operator + (Val a) {
         return Val(min(u, a.u), max(v, a.v), val + a.val);
     }
-
-    bool operator < (const Val &a) { return val < a.val; }
-    bool operator > (const Val &a) { return val > a.val; }
+    bool operator < (const Val &a) const { return val < a.val; }
+    bool operator > (const Val &a) const { return val > a.val; }
 
     void neg() { val = -val; }
 };
 
-struct Node {
-    struct F {
-        Val left, right, gss;
-        void neg() { left.neg(); right.neg(); gss.neg(); }
-    } nn, ln;
+struct F {
+    Val left, right, gss;
+    void neg() { left.neg(); right.neg(); gss.neg(); }
+    F(int u = 0, int v = 0, int val = 0) : left(u, v, val), right(u, v, val), gss(u, v, val) {}
+};
 
-    bool neg;
+struct Node {
+    F nn, ln;
+    bool negated;
     Val sum;
 
-    void negate() {
-        neg = !neg;
-        nn.neg(); ln.neg();
+    void neg() {
+        negated = !negated;
+        nn.neg();
+        ln.neg();
         sum.neg();
 
         swap(nn, ln);
     }
-} it[MN * 3];
+} it[MN * 4];
+
+#define CT(X) ((X)<<1)
+#define CP(X) (CT(X) + 1)
+
 int a[MN], n;
 
-#define CT(x) ((x) << 1)
-#define CP(x) (CT(x) + 1)
+template<typename T> T MIN(T a, T b) { return (a < b) ? a : b; }
+template<typename T> T MAX(T a, T b) { return (a > b) ? a : b; }
 
-inline void update(int i) {
+void updateNode(int i) {
     it[i].sum = it[CT(i)].sum + it[CP(i)].sum;
-
-    it[i].nn.left  = MIN(it[CT(i)].nn.left, it[CT(i)].sum + it[CP(i)].nn.left);
+    it[i].nn.left = MIN(it[CT(i)].nn.left, it[CT(i)].sum + it[CP(i)].nn.left);
     it[i].nn.right = MIN(it[CP(i)].nn.right, it[CP(i)].sum + it[CT(i)].nn.right);
-    it[i].nn.gss   = MIN(
+    it[i].nn.gss = MIN(
             MIN(it[CT(i)].nn.gss, it[CP(i)].nn.gss),
             it[CT(i)].nn.right + it[CP(i)].nn.left
     );
 
-    it[i].ln.left  = MAX(it[CT(i)].ln.left, it[CT(i)].sum + it[CP(i)].ln.left);
+    it[i].ln.left = MAX(it[CT(i)].ln.left, it[CT(i)].sum + it[CP(i)].ln.left);
     it[i].ln.right = MAX(it[CP(i)].ln.right, it[CP(i)].sum + it[CT(i)].ln.right);
-    it[i].ln.gss   = MAX(
+    it[i].ln.gss = MAX(
             MAX(it[CT(i)].ln.gss, it[CP(i)].ln.gss),
             it[CT(i)].ln.right + it[CP(i)].ln.left
     );
 }
 
-inline void refine(int i) {
-    if (it[i].neg) {
-        it[CT(i)].negate();
-        it[CP(i)].negate();
-        it[i].neg = false;
+void refine(int i) {
+    if (it[i].negated) {
+        it[CT(i)].neg();
+        it[CP(i)].neg();
+
+        it[i].negated = false;
     }
 }
 
-void build(int i, int l, int r) {
-    it[i].neg = false;
+void init(int i, int l, int r) {
+    it[i].negated = false;
     if (l == r) {
-        it[i].nn.left = it[i].nn.right = it[i].nn.gss = Val(l, l, a[l]);
-        it[i].ln.left = it[i].ln.right = it[i].ln.gss = Val(l, l, a[l]);
+        it[i].nn = F(l, l, a[l]);
+        it[i].ln = F(l, l, a[l]);
         it[i].sum = Val(l, l, a[l]);
-        return ;
+        return;
     }
     int mid = (l + r) >> 1;
-    build(CT(i), l, mid);
-    build(CP(i), mid+1, r);
-
-    update(i);
+    init(CT(i), l, mid);
+    init(CP(i), mid+1, r);
+    updateNode(i);
 }
 
-void negateSeg(int i, int l, int r, int u, int v) {
+void update(int i, int l, int r, int u, int val) {
+    if (u < l || r < u) return ;
+    if (r == l) {
+        it[i].nn = F(u, u, val);
+        it[i].ln = F(u, u, val);
+        it[i].sum = Val(u, u, val);
+        return ;
+    }
+    refine(i);
+
+    int mid = (l + r) >> 1;
+    update(CT(i), l, mid, u, val);
+    update(CP(i), mid+1, r, u, val);
+    updateNode(i);
+}
+
+void reverse(int i, int l, int r, int u, int v) {
     if (v < l || r < u) return ;
     if (u <= l && r <= v) {
-        it[i].negate();
+        it[i].neg();
         return ;
     }
     refine(i);
     int mid = (l + r) >> 1;
-    negateSeg(CT(i), l, mid, u, v);
-    negateSeg(CP(i), mid+1, r, u, v);
-
-    update(i);
+    reverse(CT(i), l, mid, u, v);
+    reverse(CP(i), mid+1, r, u, v);
+    updateNode(i);
 }
 
-int top = 0;
-Node x[1011];
+vector<Node> nodes;
 
-void get(int i, int l, int r, int u, int v) {
+void visit(int i, int l, int r, int u, int v) {
     if (v < l || r < u) return ;
     if (u <= l && r <= v) {
-        x[++top] = it[i];
-        return ;
+        nodes.push_back(it[i]);
+        return;
     }
-    refine(i);
     int mid = (l + r) >> 1;
-    get(CT(i), l, mid, u, v);
-    get(CP(i), mid+1, r, u, v);
-    update(i);
+    refine(i);
+    visit(CT(i), l, mid, u, v);
+    visit(CP(i), mid+1, r, u, v);
+    updateNode(i);
 }
 
-Val getMax(int u, int v) {
-    top = 0;
-    get(1, 1, n, u, v);
+Val get(int u, int v) {
+    nodes.clear();
+    visit(1, 1, n, u, v);
+    Val res = Val(0, 0, 0), maxRight = Val(0, 0, 0);
 
-    Val res = Val(0, 0, 0), maxright = Val(0, 0, 0);
+    REP(i,nodes.size()) {
+        res = MAX(res, nodes[i].ln.gss);
+        res = MAX(res, maxRight + nodes[i].ln.left);
 
-    FOR(i,1,top) {
-        res = MAX(res, x[i].ln.gss);
-        res = MAX(res, maxright + x[i].ln.left);
-
-        maxright = MAX(x[i].ln.right, maxright + x[i].sum);
+        maxRight = MAX(nodes[i].ln.right, maxRight + nodes[i].sum);
     }
-
     return res;
 }
 
-void modify(int i, int l, int r, int u, int val) {
-    if (u < l || r < u) return ;
-    if (l == r) {
-        it[i].nn.left = it[i].nn.right = it[i].nn.gss = Val(l, l, val);
-        it[i].ln.left = it[i].ln.right = it[i].ln.gss = Val(l, l, val);
-        it[i].sum = Val(l, l, val);
-        return ;
-    }
-    refine(i);
-    int mid = (l + r) >> 1;
-    modify(CT(i), l, mid, u, val);
-    modify(CP(i), mid+1, r, u, val);
-    update(i);
-}
-
-pair<int,int> save[22];
-
 int main() {
-    ios :: sync_with_stdio(false);
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+//    freopen("input.txt", "r", stdin);
     while (cin >> n) {
         FOR(i,1,n) cin >> a[i];
-        build(1,1,n);
+
+        init(1, 1, n);
 
         int q; cin >> q;
         while (q--) {
-            int type; cin >> type;
-            if (type == 0) {
+            int typ; cin >> typ;
+            if (typ == 0) {
                 int u, val; cin >> u >> val;
-                modify(1, 1, n, u, val);
-            }
-            else {
-                int l, r, k; cin >> l >> r >> k;
-                int res = 0;
-                FOR(kk,1,k) {
-                    Val cur = getMax(l, r);
-                    if (cur.val > 0) {
-                        res += cur.val;
-                        save[kk] = make_pair(cur.u, cur.v);
-                        negateSeg(1, 1, n, cur.u, cur.v);
-                    }
-                    else save[kk] = make_pair(-1, -1);
+                update(1, 1, n, u, val);
+            } else {
+                int u, v, k; cin >> u >> v >> k;
+                vector< pair<int,int> > reversed;
+
+                int res = 0, all = 0;
+                while (k--) {
+                    Val cur = get(u, v);
+                    all += cur.val;
+                    res = max(res, all);
+
+                    reverse(1, 1, n, cur.u, cur.v);
+                    reversed.push_back(make_pair(cur.u, cur.v));
                 }
-                FOR(kk,1,k) {
-                    if (save[kk].first != -1) {
-                        negateSeg(1, 1, n, save[kk].first, save[kk].second);
-                    }
-                }
+
                 cout << res << endl;
+                REP(i,reversed.size()) {
+                    reverse(1, 1, n, reversed[i].first, reversed[i].second);
+                }
             }
         }
-        cout << endl;
     }
     return 0;
 }
