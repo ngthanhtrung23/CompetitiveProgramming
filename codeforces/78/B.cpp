4a5bc9a9b24dde4626503781b0b4331306209bd3
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-18 15:37:00 +0800

    Add solution for problem `B - Easter Eggs` Link: https://codeforces.com/contest/78/problem/B Tags: constructive algorithms, implementation, *1200 Ref: https://codeforces.com/contest/78/submission/8290648

---
+++ codeforces/78/B.cpp
@@ -0,0 +1,45 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n;
+    string s = "ROYBGIV";
+    while (cin >> n) {
+        REP(i,n-3) cout << s[i%4];
+        cout << s[4] << s[5] << s[6] << endl;
+    }
+    return 0;
+}
