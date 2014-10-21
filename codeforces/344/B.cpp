dad64327caeb2e7c6ccc550c0bb549d5eee552d9
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-22 04:40:00 +0800

    Add solution for problem `B - Simple Molecules` Link: https://codeforces.com/contest/344/problem/B Tags: brute force, graphs, math, *1200 Ref: https://codeforces.com/contest/344/submission/8346255

---
+++ codeforces/344/B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int main() {
+    int a, b, c;
+    while (cin >> a >> b >> c) {
+        int x, y, z;
+        // x + y = b, y + z = c, z + x = a
+        if ((a + b + c) & 1) puts("Impossible");
+        else {
+            int sum = (a + b + c) / 2;
+            x = sum - c;
+            y = sum - a;
+            z = sum - b;
+            if (x < 0 || y < 0 || z < 0) puts("Impossible");
+            else cout << x << ' ' << y << ' ' << z << endl;
+        }
+    }
+    return 0;
+}
