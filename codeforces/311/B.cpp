fc95e1fca80ed5a01c716c7cac26c33649e46e69
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-10-22 03:59:00 +0800

    Add solution for problem `B - Cats Transport` Link: https://codeforces.com/contest/311/problem/B Tags: data structures, dp, *2400 Ref: https://codeforces.com/contest/311/submission/8346017

--- codeforces/311/B.cpp
+++ codeforces/311/B.cpp
@@ -1,134 +1,101 @@
-#include <sstream>
-#include <iomanip>
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <vector>
 #include <set>
 #include <map>
-#include <stack>
+#include <list>
+#include <cmath>
 #include <queue>
+#include <stack>
+#include <cstdio>
 #include <string>
-#include <deque>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
 #include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
+const int MAXN = 100100;
 
-const long double PI = acos((long double) -1.0);
+struct Hull {
+    long long a[MAXN], b[MAXN];
+    double x[MAXN];
+    int head, tail;
+
+    Hull(): head(1), tail(0) {}
+
+    long long get(long long xQuery) {
+        if (head > tail) return 0;
+        while (head < tail && x[head + 1] <= xQuery) head++;
+        x[head] = xQuery;
+        return a[head] * xQuery + b[head];
+    }
+
+    void add(long long aNew, long long bNew) {
+        double xNew = -1e18;
+        while (head <= tail) {
+            if (aNew == a[tail]) return;
+            xNew = 1.0 * (b[tail] - bNew) / (aNew - a[tail]);
+            if (head == tail || xNew >= x[tail]) break;
+            tail--;
+        }
+        a[++tail] = aNew;
+        b[tail] = bNew;
+        x[tail] = xNew;
+    }
+};
+
+
 const int MN = 100111;
 
-int d[MN], pos[MN], h[MN], t[MN];
-long long val[MN], sumVal[MN];
+int n, m, nFeed;
+long long d[MN], p[MN], h[MN], a[MN], s[MN], t[MN];
 long long f[111][MN];
-int m, n, p;
-deque<int> q;
-
-long long g(int x, int j, int k) {
-    return f[x-1][j] - f[x-1][k] + sumVal[j] - sumVal[k];
-}
-
-long long fi(int x, int i, int j) {
-    return f[x-1][j] + val[i] * (i-j) - sumVal[i] + sumVal[j];
-}
-
-template <typename T> void write(T a) {
-    FOR(i,0,a.size()-1) cout<<a[i]<<" ";
-    cout<<endl;
-}
 
 int main() {
-    while (scanf("%d%d%d", &n, &m, &p) == 3) {
+    while (cin >> n >> m >> nFeed) {
         FOR(i,2,n) {
-            scanf("%d", &d[i]);
-            pos[i] = pos[i-1] + d[i];
+            cin >> d[i];
+            p[i] = p[i-1] + d[i];
         }
         FOR(i,1,m) {
-            scanf("%d%d", &h[i], &t[i]);
-            val[i] = t[i] - pos[h[i]];
+            cin >> h[i] >> t[i];
+            a[i] = t[i] - p[h[i]];
         }
-        sort(val+1, val+m+1);
-        FOR(i,1,m) sumVal[i] = sumVal[i-1] + val[i];
+        sort(a+1, a+m+1);
+        FOR(i,1,m) s[i] = s[i-1] + a[i];
 
-        if (p >= m) {
-            puts("0");
-            continue;
-        }
+        // PR(a, m);
 
-        f[0][0] = 0;
-        FOR(i,1,m) f[0][i] = 1000111000111000111LL;
-
-        // PR(val, m);
-
-        FOR(x,1,p) {
-            // DEBUG(x);
-            while (!q.empty()) q.pop_back();
-
-            f[x][x] = 0;
-            q.push_back(x-1);
-
-            FOR(i,x+1,m) {
-                // DEBUG(i);
-                // PR0(q, q.size());
-
-                // if (q.size() > 1) cout << (fixed) << setprecision(6) << g(x, q[1], q[0]) / (long double) (q[1] - q[0]) << ' ' << val[i] << endl;
-
-                while (q.size() > 1 && g(x, q[1], q[0]) <= val[i] * (q[1] - q[0])) q.pop_front();
-
-                // PR0(q, q.size());
-                f[x][i] = fi(x, i, q[0]);
-
-                q.PB(i-1);
-                while (q.size() > 1
-                        && g(x, i, q[q.size()-1]) * (q[q.size()-1] - q[q.size()-2])
-                            <= g(x, q[q.size()-1], q[q.size()-2]) * (i - q[q.size()-1]))
-                    q.pop_back();
-
-                f[x][i] = min(f[x][i], fi(x, i, i-1));
+        FOR(i,1,nFeed) {
+            Hull hull;
+            FOR(j,1,m) {
+                f[i][j] = hull.get(a[j]) + a[j] * j - s[j];
+                if (i > 1) hull.add(-j, f[i-1][j] + s[j]);
             }
-
-            // PR(f[x], m);
+            // PR(f[i], m);
         }
-        cout << f[p][m] << endl;
+        long long res = f[1][m];
+        FOR(i,1,nFeed) res = min(res, f[i][m]);
+        cout << res << endl;
     }
-    return 0;
 }
