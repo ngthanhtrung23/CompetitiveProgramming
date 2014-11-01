88ea96c5be686435c39f9aeb3fe46058733b3391
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-01 20:57:00 +0800

    Add solution for problem `K - Crap` Link: https://codeforces.com/gym/100514/problem/K Tags: Ref: https://codeforces.com/gym/100514/submission/8512144

---
+++ codeforces/100514/K.cpp
@@ -0,0 +1,52 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n;
+    while (cin >> n) {
+        long long cur = 0;
+        FOR(i,1,n) {
+            long long u; cin >> u;
+            cur *= 31;
+            cur %= 1000000007;
+            cout << (char) (u - cur);
+
+            cur = u;
+        }
+        cout << endl;
+    }
+    return 0;
+}
