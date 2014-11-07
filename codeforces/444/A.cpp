d78974ed77ffdee0f6627b5053e9e2cf65045814
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-07 19:08:00 +0800

    Add solution for problem `A - DZY Loves Physics` Link: https://codeforces.com/contest/444/problem/A Tags: greedy, math, *1600 Ref: https://codeforces.com/contest/444/submission/8601555

---
+++ codeforces/444/A.cpp
@@ -0,0 +1,51 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int x[511];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(12);
+    int n, m;
+    while (cin >> n >> m) {
+        FOR(i,1,n) cin >> x[i];
+        double res = 0.0;
+        while (m--) {
+            int a, b, c; cin >> a >> b >> c;
+            res = max(res, (x[a] + x[b]) / (double) c);
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
