754d78994c9d77f32ef212cc61b3cb9b31fb898a
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-07 18:23:00 +0800

    Add solution for problem `B - DZY Loves Chemistry` Link: https://codeforces.com/contest/445/problem/B Tags: dfs and similar, dsu, greedy, *1400 Ref: https://codeforces.com/contest/445/submission/8601082

---
+++ codeforces/445/B.cpp
@@ -0,0 +1,73 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+const int MN = 1011;
+struct DSU {
+    int lab[MN];
+    void init() {
+        memset(lab, -1, sizeof lab);
+    }
+
+    int getRoot(int u) {
+        if (lab[u] < 0) return u;
+        return lab[u] = getRoot(lab[u]);
+    }
+
+    bool merge(int u, int v) {
+        u = getRoot(u); v = getRoot(v);
+        if (u == v) return false;
+        if (lab[u] > lab[v]) swap(u, v);
+        lab[u] += lab[v];
+        lab[v] = u;
+        return true;
+    }
+};
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n, m;
+    while (cin >> n >> m) {
+        DSU dsu; dsu.init();
+        while (m--) {
+            int u, v; cin >> u >> v;
+            dsu.merge(u, v);
+        }
+        int cnt = n;
+        FOR(i,1,n) if (dsu.lab[i] < 0) --cnt;
+
+        cout << (1LL<<cnt) << endl;
+    }
+    return 0;
+}
