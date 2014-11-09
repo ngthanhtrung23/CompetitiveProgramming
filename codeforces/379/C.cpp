93abb13b712f75d5c35b3436e38da5cbcfc7093f
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-09 18:38:00 +0800

    Add solution for problem `C - New Year Ratings Change` Link: https://codeforces.com/contest/379/problem/C Tags: greedy, sortings, *1400 Ref: https://codeforces.com/contest/379/submission/8622143

---
+++ codeforces/379/C.cpp
@@ -0,0 +1,55 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+pair<int,int> a[300111];
+int n, res[300111];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    while (cin >> n) {
+        FOR(i,1,n) {
+            cin >> a[i].first;
+            a[i].second = i;
+        }
+        sort(a+1, a+n+1);
+
+        FOR(i,1,n) {
+            res[a[i].second] = a[i].first;
+            if (i > 1 && res[a[i].second] <= res[a[i-1].second]) res[a[i].second] = res[a[i-1].second] + 1;
+        }
+        FOR(i,1,n) cout << res[i] << ' '; cout << endl;
+    }
+    return 0;
+}
