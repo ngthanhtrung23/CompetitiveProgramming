082d6d9456b78fe1bc9c21a3fa1c8c789350b8a0
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-10 02:27:00 +0800

    Add solution for problem `C - Sereja and Brackets` Link: https://codeforces.com/contest/380/problem/C Tags: data structures, schedules, *2000 Ref: https://codeforces.com/contest/380/submission/8625481

--- codeforces/380/C.cpp
+++ codeforces/380/C.cpp
@@ -1,99 +1,88 @@
-#include <bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
-#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
 
-#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
-#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
-#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
-#define ll long long
-#define __builtin_popcount __builtin_popcountll
-#define SZ(x) ((int) (x).size())
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
 using namespace std;
 
-double safe_sqrt(double x) {
-    return sqrt(max((double)0.0,x));
-}
-int GI(ll& x) {
-    return scanf("%lld", &x);
-}
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-struct Node {
-    int optimal;
-    int open;
-    int close;
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
 
-    Node() {}
-    Node(int opt, int o, int c) { // Khởi tạo struct Node
-        optimal = opt;
-        open = o;
-        close = c;
-    }
-};
-
-const int MAXN = 1000111;
-Node st[MAXN * 4];
 string s;
+int n;
 
-Node operator + (const Node& left, const Node& right) {
-    Node res;
-    // min(số dấu "(" thừa ra ở cây con trái, và số dấu ")" thừa ra ở cây con phải)
-    int tmp = min(left.open, right.close);
+const int MN = 1000111;
+struct Node {
+    int len, open, close;
+    Node(char c) : len(0), open(c == '('), close(c == ')') {}
+    Node(int len = 0, int open = 0, int close = 0) : len(len), open(open), close(close) {}
 
-    // Để xây dựng kết quả tối ưu ở nút id, ta nối dãy ngoặc tối ưu ở 2 con, rồi thêm
-    // min(số "(" thừa ra ở con trái, số ")" thừa ra ở con phải).
-    res.optimal = left.optimal + right.optimal + tmp;
+    Node operator + (Node a) {
+        int can = min(open, a.close);
+        return Node(len + a.len + can,
+                open - can + a.open,
+                close + a.close - can);
+    }
+} it[MN * 8];
 
-    res.open = left.open + right.open - tmp;
-    res.close = left.close + right.close - tmp;
-
-    return res;
-}
-
-void build(int id, int l, int r) {
+#define CT(X) ((X) << 1)
+#define CP(X) (CT(X) + 1)
+#define mid ((l + r) >> 1)
+void build(int i, int l, int r) {
     if (l == r) {
-        // Đoạn [l, r] chỉ có 1 phần tử.
-        if (s[l] == '(') st[id] = Node(0, 1, 0);
-        else st[id] = Node(0, 0, 1);
+        it[i] = Node(s[l]);
         return ;
     }
-    int mid = (l + r) / 2;
-    build(id * 2, l, mid);
-    build(id * 2 + 1, mid+1, r);
+    build(CT(i), l, mid);
+    build(CP(i), mid+1, r);
 
-    st[id] = st[id * 2] + st[id * 2 + 1];
-}
-Node query(int id, int l, int r, int u, int v) {
-    if (v < l || r < u) {
-        // Trường hợp không giao nhau
-        return Node(0, 0, 0);
-    }
-    if (u <= l && r <= v) {
-        // Trường hợp [l, r] nằm hoàn toàn trong [u, v]
-        return st[id];
-    }
-
-    int mid = (l + r) / 2;
-    return query(id * 2, l, mid, u, v) + query(id * 2 + 1, mid+1, r, u, v);
+    it[i] = it[CT(i)] + it[CP(i)];
 }
 
-char tmp[MAXN];
+Node get(int i, int l, int r, int u, int v) {
+    if (v < l || r < u) return Node();
+    if (u <= l && r <= v) return it[i];
+    return get(CT(i), l, mid, u, v) + get(CP(i), mid+1, r, u, v);
+}
 
 int main() {
-    ios :: sync_with_stdio(0); cin.tie(0);
-    cout << (fixed) << setprecision(9);
-    scanf("%s\n", &tmp[0]);
-    int n = strlen(tmp);
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    while (cin >> s) {
+        n = s.length();
+        s = " " + s;
+        build(1, 1, n);
 
-    s = " " + string(tmp) + " ";
-    build(1, 1, n);
-
-    int q; scanf("%d", &q);
-    while (q--) {
-        int l, r; scanf("%d%d", &l, &r);
-        auto res = query(1, 1, n, l, r);
-        printf("%d\n", res.optimal * 2);
+        int q; cin >> q;
+        while (q--) {
+            int u, v; cin >> u >> v;
+            Node cur = get(1, 1, n, u, v);
+            cout << 2 * cur.len << "\n";
+        }
     }
+    return 0;
 }
