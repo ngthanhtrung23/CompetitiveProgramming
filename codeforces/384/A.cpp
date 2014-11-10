954c4988bac5effdfd19ce939067cc7401928f05
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-10 18:39:00 +0800

    Add solution for problem `A - Coder` Link: https://codeforces.com/contest/384/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/384/submission/8630152

---
+++ codeforces/384/A.cpp
@@ -0,0 +1,52 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n;
+    while (cin >> n) {
+        if (n % 2 == 0) {
+            cout << n * n / 2 << endl;
+        }
+        else {
+            cout << n * n / 2 + 1 << endl;
+        }
+        FOR(i,1,n) {
+            FOR(j,1,n) if ((i + j) % 2 == 0) cout << 'C'; else cout << '.';
+            cout << endl;
+        }
+    }
+    return 0;
+}
