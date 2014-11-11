4878cd32a1daf865147c70de0671a8563e0fa272
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-11 17:48:00 +0800

    Add solution for problem `E - New Year Tree Decorations` Link: https://codeforces.com/contest/379/problem/E Tags: geometry, schedules, sortings, *2500 Ref: https://codeforces.com/contest/379/submission/8647014

---
+++ codeforces/379/E.cpp
@@ -0,0 +1,167 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+#define EPS 1e-6
+inline int cmp(double a, double b) {
+    return (a < b - EPS) ? -1 : ((a > b + EPS) ? 1 : 0);
+}
+
+struct Point {
+    double x, y;
+    Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+
+    Point operator + (Point a) { return Point(x+a.x, y+a.y); }
+    Point operator - (Point a) { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) { return Point(x*k, y*k); }
+    Point operator / (double k) { return Point(x/k, y/k); }
+
+    double operator * (Point a) { return x*a.x + y*a.y; } // dot product
+    double operator % (Point a) { return x*a.y - y*a.x; } // cross product
+
+    int cmp(Point q) const { if (int t = ::cmp(x,q.x)) return t; return ::cmp(y,q.y); }
+
+    #define Comp(x) bool operator x (Point q) const { return cmp(q) x 0; }
+    Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
+    #undef Comp
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+};
+
+int ccw(Point a, Point b, Point c) {
+    return cmp((b-a)%(c-a),0);
+}
+
+struct Line {
+    double a, b, c;
+    Point A, B; // Added for polygon intersect line. Do not rely on assumption that these are valid
+
+    Line(double a, double b, double c) : a(a), b(b), c(c) {} 
+
+    Line(Point A, Point B) : A(A), B(B) {
+        a = B.y - A.y;
+        b = A.x - B.x;
+        c = - (a * A.x + b * A.y);
+    }
+    Line(Point P, double m) {
+        a = -m; b = 1;
+        c = -((a * P.x) + (b * P.y));
+    }
+    double f(Point A) {
+        return a*A.x + b*A.y + c;
+    }
+};
+typedef vector< Point > Polygon;
+bool areParallel(Line l1, Line l2) {
+    return cmp(l1.a*l2.b, l1.b*l2.a) == 0;
+}
+bool areIntersect(Line l1, Line l2, Point &p) {
+    if (areParallel(l1, l2)) return false;
+    double dx = l1.b*l2.c - l2.b*l1.c;
+    double dy = l1.c*l2.a - l2.c*l1.a;
+    double d  = l1.a*l2.b - l2.a*l1.b;
+    p = Point(dx/d, dy/d);
+    return true;
+}
+Polygon polygon_cut(Polygon P, Line l) {
+    Polygon Q;
+    for(int i = 0; i < P.size(); ++i) {
+        Point A = P[i], B = (i == P.size()-1) ? P[0] : P[i+1];
+        if (ccw(l.A, l.B, A) != -1) Q.push_back(A);
+        if (ccw(l.A, l.B, A)*ccw(l.A, l.B, B) < 0) {
+            Point p; areIntersect(Line(A, B), l, p);
+            Q.push_back(p);
+        }
+    }
+    return Q;
+}
+
+double signed_area(Polygon p) {
+    double area = 0;
+    for(int i = 0; i < p.size(); i++) {
+        int j = (i+1) % p.size();
+        area += p[i].x*p[j].y - p[j].x*p[i].y;
+    }
+    return area / 2.0;
+}
+double area(const Polygon &p) {
+    return fabs(signed_area(p));
+}
+
+int n, k;
+Point a[311][311];
+double res[311];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(9);
+    while (cin >> n >> k) {
+        FOR(i,1,n) FOR(j,0,k) {
+            cin >> a[i][j].y;
+            a[i][j].x = j;
+        }
+
+        memset(res, 0, sizeof res);
+        FOR(x,0,k-1) {
+            Polygon p;
+            p.push_back(Point(x, 0));
+            p.push_back(Point(x+1, 0));
+            p.push_back(Point(x+1, 1000));
+            p.push_back(Point(x, 1000));
+
+            double last = 0.0;
+            FOR(i,1,n) {
+                Line l(a[i][x], a[i][x+1]);
+                p = polygon_cut(p, l);
+
+                double cur = 1000 - area(p);
+                res[i] += cur - last;
+                last = cur;
+            }
+        }
+
+        FOR(i,1,n) cout << res[i] << endl;
+        cout << endl;
+    }
+    return 0;
+}
