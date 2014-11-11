04ef8a5915fb96deb4872ae5ee0e6ec278d0d735
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-11 10:44:00 +0800

    Add solution for problem `A - Ksenia and Pan Scales` Link: https://codeforces.com/contest/382/problem/A Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/382/submission/8644906

---
+++ codeforces/382/A.cpp
@@ -0,0 +1,54 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    string s;
+    while (cin >> s) {
+        string a, b;
+        a = s.substr(0, s.find('|'));
+        b = s.substr(s.find('|') + 1);
+
+        string add; cin >> add;
+        REP(i,add.length()) {
+            if (a.length() < b.length()) a += add[i];
+            else b += add[i];
+        }
+        if (a.length() != b.length()) cout << "Impossible";
+        else cout << a << '|' << b;
+        cout << endl;
+    }
+    return 0;
+}
