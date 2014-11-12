2a85595a1bacbd772486443eafe7052525b4ce0b
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-12 11:47:00 +0800

    Add solution for problem `A - Bear and Raspberry` Link: https://codeforces.com/contest/385/problem/A Tags: brute force, greedy, implementation, *1000 Ref: https://codeforces.com/contest/385/submission/8667040

---
+++ codeforces/385/A.cpp
@@ -0,0 +1,47 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+int a[111], n;
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int c;
+    while (cin >> n >> c) {
+        FOR(i,1,n) cin >> a[i];
+        int res = 0;
+        FOR(i,1,n-1) res = max(res, a[i] - a[i+1] - c);
+        cout << res << endl;
+    }
+    return 0;
+}
