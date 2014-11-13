323702ce5f4353b6a06ee124919cc0496c5ca117
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-14 02:58:00 +0800

    Add solution for problem `A - Winner` Link: https://codeforces.com/contest/2/problem/A Tags: hashing, implementation, *1500 Ref: https://codeforces.com/contest/2/submission/8687902

---
+++ codeforces/2/A.cpp
@@ -0,0 +1,91 @@
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
+#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
+#define SET(a,v) memset(a,v,sizeof(a))
+#define sqr(x) ((x)*(x))
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const long double PI = acos((long double) -1.0);
+
+map<string,int> score, cur;
+int n;
+string a[1011];
+int c[1011];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> n) {
+        score.clear();
+        FOR(i,1,n) {
+            cin >> a[i] >> c[i];
+            score[a[i]] += c[i];
+        }
+        int res = 0;
+        for(map<string,int> :: iterator it = score.begin(); it != score.end(); ++it)
+            res = max(res, it->second);
+
+        cur.clear();
+        string winner;
+        FOR(i,1,n) {
+            cur[a[i]] += c[i];
+            if (cur[a[i]] >= res && score[a[i]] == res) {
+                winner = a[i];
+                break;
+            }
+        }
+        puts(winner.c_str());
+    }
+    return 0;
+}
