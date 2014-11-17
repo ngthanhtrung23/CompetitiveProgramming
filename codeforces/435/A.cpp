2e244cc430ded8e5f76af2a5c9f511811643c47c
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-17 13:33:00 +0800

    Add solution for problem `A - Queue on Bus Stop` Link: https://codeforces.com/contest/435/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/435/submission/8715860

---
+++ codeforces/435/A.cpp
@@ -0,0 +1,53 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n, m, a;
+    while (cin >> n >> m) {
+        int cur = 0, res = 1;
+        FOR(i,1,n) {
+            cin >> a;
+            cur += a;
+            if (cur > m) {
+                ++res;
+                cur = a;
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
