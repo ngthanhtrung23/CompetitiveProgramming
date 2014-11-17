d350aea0f41ff0bf5ce8765f5670b99f9944b64c
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-17 23:39:00 +0800

    Add solution for problem `A - SwapSort` Link: https://codeforces.com/contest/489/problem/A Tags: greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/489/submission/8719359

---
+++ codeforces/489/A.cpp
@@ -0,0 +1,57 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int n, a[3011];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    while (cin >> n) {
+        REP(i,n) cin >> a[i];
+        vector< pair<int,int> > res;
+        REP(i,n) {
+            int j = min_element(a+i, a+n) - a;
+            if (i != j) {
+                res.push_back(make_pair(i, j));
+                swap(a[i], a[j]);
+            }
+            // PR0(a, n);
+        }
+        cout << res.size() << endl;
+        REP(i,res.size()) cout << res[i].first << ' ' << res[i].second << endl;
+    }
+    return 0;
+}
+
