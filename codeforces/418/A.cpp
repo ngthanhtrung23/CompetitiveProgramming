d8bf0a60db2a5714d2fb8da818887ac28dbe7782
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-17 23:12:00 +0800

    Add solution for problem `A - Football` Link: https://codeforces.com/contest/418/problem/A Tags: constructive algorithms, graphs, implementation, *1400 Ref: https://codeforces.com/contest/418/submission/8719193

---
+++ codeforces/418/A.cpp
@@ -0,0 +1,61 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n, k;
+    while (cin >> n >> k) {
+        int played = n * k;
+        if (played > n * (n-1) / 2) {
+            cout << -1 << endl;
+            continue;
+        }
+        set< pair<int,int> > s;
+        cout << played << endl;
+        FOR(i,1,n) {
+            int cnt = 0;
+            FOR(j,1,n) if (i != j && s.find(make_pair(i, j)) == s.end()) {
+                cout << i << ' ' << j << "\n";
+                s.insert(make_pair(i, j));
+                s.insert(make_pair(j, i));
+                ++cnt;
+                if (cnt == k) break;
+            }
+        }
+    }
+    return 0;
+}
+
