0764e810dd245e8e28f754460eb3acb50806cdca
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-23 18:09:00 +0800

    Add solution for problem `A - Team Olympiad` Link: https://codeforces.com/contest/490/problem/A Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/490/submission/8815267

---
+++ codeforces/490/A.cpp
@@ -0,0 +1,56 @@
+#include <set>
+#include <map>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+#include <complex>
+#include <iostream>
+#include <algorithm>
+
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <cctype>
+#include <utility>
+#include <cassert>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int n;
+vector<int> ls[5];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    while (cin >> n) {
+        FOR(t,1,3) ls[t].clear();
+        FOR(i,1,n) {
+            int a; cin >> a;
+            ls[a].push_back(i);
+        }
+
+        int res = min(ls[1].size(), min(ls[2].size(), ls[3].size()));
+        cout << res << endl;
+        REP(i,res) {
+            cout << ls[1][i] << ' ' << ls[2][i] << ' ' << ls[3][i] << endl;
+        }
+    }
+    return 0;
+}
