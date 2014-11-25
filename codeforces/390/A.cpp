32bf2d27502d1ce57f8f70a6e2ac543d17e74d76
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-11-25 15:42:00 +0800

    Add solution for problem `A - Inna and Alarm Clock` Link: https://codeforces.com/contest/390/problem/A Tags: implementation Ref: https://codeforces.com/contest/390/submission/8843179

---
+++ codeforces/390/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int n, x[100111], y[100111];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> x[i] >> y[i];
+        sort(x+1, x+n+1);
+        sort(y+1, y+n+1);
+        cout << min( unique(x+1, x+n+1) - x - 1, unique(y+1, y+n+1) - y - 1) << endl;
+    }
+    return 0;
+}
