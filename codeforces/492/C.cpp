9917fd37f62e68c746fa139d16baa85ed19706b1
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-12-02 11:44:00 +0800

    Add solution for problem `C - Vanya and Exams` Link: https://codeforces.com/contest/492/problem/C Tags: greedy, sortings, *1400 Ref: https://codeforces.com/contest/492/submission/8927057

---
+++ codeforces/492/C.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+pair<long long, long long> a[100111];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int n;
+    long long maxPt, need;
+    while (cin >> n >> maxPt >> need) {
+        need *= n;
+        FOR(i,1,n) cin >> a[i].second >> a[i].first;
+        sort(a+1, a+n+1);
+
+        long long has = 0;
+        FOR(i,1,n) has += a[i].second;
+
+        long long res = 0;
+        FOR(i,1,n) {
+            long long add = max(0LL, need - has);
+            add = min(add, maxPt - a[i].second);
+
+            res += a[i].first * add;
+            has += add;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
