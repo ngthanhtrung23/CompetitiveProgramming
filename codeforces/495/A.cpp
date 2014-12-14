f8e7ba503d6e880d313d014cf7a6d0cbb40189bc
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-12-14 22:13:00 +0800

    Add solution for problem `A - Digital Counter` Link: https://codeforces.com/contest/495/problem/A Tags: implementation, *1100 Ref: https://codeforces.com/contest/495/submission/9131203

---
+++ codeforces/495/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int f[10] = {2, 7, 2, 3, 3, 4, 2, 5, 1, 2};
+    char a, b;
+    while (cin >> a >> b) {
+        cout << f[a-'0'] * f[b-'0'] << endl;
+    }
+    return 0;
+}
