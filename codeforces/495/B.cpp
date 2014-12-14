f75463b4adb1491ef0805d004eafe3038018070a
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-12-14 22:26:00 +0800

    Add solution for problem `B - Modular Equations` Link: https://codeforces.com/contest/495/problem/B Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/495/submission/9131373

---
+++ codeforces/495/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    int a, b;
+    while (cin >> a >> b) {
+        if (a == b) cout << "infinity" << endl;
+        else if (a < b) cout << 0 << endl;
+        else {
+            int t = a - b, res = 0;
+            for(int i = 1; i*i <= t; ++i)
+                if (t % i == 0) {
+                    if (a % i == b) ++res;
+                    if (i*i != t && a % (t/i) == b) ++res;
+                }
+            cout << res << endl;
+        }
+    }
+    return 0;
+}
