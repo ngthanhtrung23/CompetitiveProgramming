4dfedcd0dbe1916497cca75680580cec40881239
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-12-15 20:42:00 +0800

    Add solution for problem `B - Farmer` Link: https://codeforces.com/gym/100482/problem/B Tags: Ref: https://codeforces.com/gym/100482/submission/9140114

---
+++ codeforces/100482/B.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define EPS 1e-6
+
+inline int cmp(double a, double b) {
+    return (a < b - EPS) ? -1 : ((a > b + EPS) ? 1 : 0);
+}
+
+struct Point {
+    double x, y;
+    Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+
+    Point operator + (Point a) { return Point(x+a.x, y+a.y); }
+    Point operator - (Point a) { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) { return Point(x*k, y*k); }
+    Point operator / (double k) { return Point(x/k, y/k); }
+
+    double operator * (Point a) { return x*a.x + y*a.y; } // dot product
+    double operator % (Point a) { return x*a.y - y*a.x; } // cross product
+
+    int cmp(Point q) const { if (int t = ::cmp(x,q.x)) return t; return ::cmp(y,q.y); }
+
+    #define Comp(x) bool operator x (Point q) const { return cmp(q) x 0; }
+    Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
+    #undef Comp
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+};
+
+typedef vector<Point> Polygon;
+
+// Area, perimeter, centroid
+double signed_area(Polygon p) {
+    double area = 0;
+    for(int i = 0; i < p.size(); i++) {
+        int j = (i+1) % p.size();
+        area += p[i].x*p[j].y - p[j].x*p[i].y;
+    }
+    return area / 2.0;
+}
+double area(const Polygon &p) {
+    return fabs(signed_area(p));
+}
+Point centroid(Polygon p) {
+    Point c(0,0);
+    double scale = 6.0 * signed_area(p);
+    for (int i = 0; i < p.size(); i++){
+        int j = (i+1) % p.size();
+        c = c + (p[i]+p[j])*(p[i].x*p[j].y - p[j].x*p[i].y);
+    }
+    return c / scale;
+}
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(2);
+    int ntest; cin >> ntest;
+    FOR(test,1,ntest) {
+        Point P;
+        Polygon a;
+        REP(t,4) {
+            cin >> P.x >> P.y;
+            a.push_back(P);
+        }
+        double t; cin >> t;
+        double res = t / area(a);
+        cout << "Case #" << test << ": " << res << endl;
+    }
+    return 0;
+}
