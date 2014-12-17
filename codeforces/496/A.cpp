7207eb0334e94561cdac0165895f63568d877fd7
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-12-18 03:20:00 +0800

    Add solution for problem `A - Minimum Difficulty` Link: https://codeforces.com/contest/496/problem/A Tags: brute force, implementation, math, *900 Ref: https://codeforces.com/contest/496/submission/9177840

---
+++ codeforces/496/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+#define SZ(S) ((int) ((S).size()))
+
+#define DEBUG(x) { cout << #x << " = " << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+int n, a[111], b[111];
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    cout << (fixed) << setprecision(6);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        int res = 1000111000;
+        FOR(skip,2,n-1) {
+            int cur = 0;
+            FOR(i,1,n) if (i != skip) b[++cur] = a[i];
+
+            int diff = 0;
+            FOR(i,1,n-2) diff = max(diff, b[i+1] - b[i]);
+            res = min(res, diff);
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
