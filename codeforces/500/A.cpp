3fd28903e5709e81bb3689dcd9658e837a313210
Trung Nguyen <ngthanhtrung23@gmail.com>
2014-12-31 03:05:00 +0800

    Add solution for problem `A - New Year Transportation` Link: https://codeforces.com/contest/500/problem/A Tags: dfs and similar, graphs, implementation, *1000 Ref: https://codeforces.com/contest/500/submission/9325727

---
+++ codeforces/500/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int n, t, a[300111];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    cout << (fixed) << setprecision(9);
+    while (cin >> n >> t) {
+        FOR(i,1,n-1) cin >> a[i];
+        int pos = 1;
+        while (pos < t) pos += a[pos];
+        if (pos == t) cout << "YES"; else cout << "NO";
+        cout << endl;
+    }
+    return 0;
+}
