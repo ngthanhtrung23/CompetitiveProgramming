1c79f6c63f0362ded56b867cfe762f177fef87a1
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-17 13:57:00 +0800

    Add solution for problem `B - Good Sequences` Link: https://codeforces.com/contest/264/problem/B Tags: dp, number theory, *1500 Ref: https://codeforces.com/contest/264/submission/9878841

--- codeforces/264/B.cpp
+++ codeforces/264/B.cpp
@@ -1,100 +1,117 @@
-#include <sstream>
-#include <iomanip>
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
+// Author: dolphinigle
+
 #include <vector>
-#include <set>
+#include <list>
 #include <map>
-#include <stack>
+#include <set>
+
 #include <queue>
-#include <string>
 #include <deque>
-#include <complex>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <assert.h>
 
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
+#define FORN(X,Y) for (int (X) = 0;(X) < (Y);++(X))
+#define REP(X,Y,Z) for (int (X) = (Y);(X) < (Z);++(X))
+
+#define SZ(Z) ((int)(Z).size())
+#define ALL(W) (W).begin(), (W).end()
 #define PB push_back
-#define MP make_pair
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define MP make_pair
+#define A first
+#define B second
+
+#define INF 1023123123
+#define EPS 1e-11
+
+#define MX(Z,Y) Z = max((Z),(Y))
+#define MN(X,Y) X = min((X),(Y))
+
+#define FORIT(X,Y) for(typeof((Y).begin()) X = (Y).begin();X!=(Y).end();X++)
+
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
+typedef long long ll;
+typedef double db;
+typedef vector<int> vint;
 
-const long double PI = acos((long double) -1.0);
+vector<int> Sieve(int abc) {
+	vector<int> sieveans;
+	abc++;
+	sieveans.reserve(abc);
+	FORN(i,abc) sieveans.push_back(1);
+	for (int i = 2;i * i < abc;i = (i == 2)?3:(i + 2)) {
+		for (int j = i + i;j < abc;j += i) {
+			sieveans[j] = 0;
+			}
+		}
+	sieveans[0] = sieveans[1] = 0;
+	return sieveans;
+	}
 
-const int MN = 100111;
-int sieve[MN], f[MN], a[MN], n;
+//generate all prime number lower than or equal to abc;
+vector<int> GeneratePrime(int abc) {
+	vector<int> stable = Sieve(abc);
+	vector<int> returnvalue;
+	for (int i = 0;i < (int)stable.size();i++) {
+		if (stable[i]) returnvalue.push_back(i);
+		}
+	return returnvalue;
+	}
 
-void init() {
-    FOR(i,2,1000) if (!sieve[i]) {
-        int j = i*i;
-        while (j <= 100000) {
-            sieve[j] = i;
-            j += i;
-        }
-    }
-}
+int dp[105000];
+int factors[105000];
+int primes[105000];
 
 int main() {
-    while (scanf("%d", &n) == 1) {
-        memset(f, 0, sizeof f);
-        FOR(i,1,n) scanf("%d", &a[i]);
 
-        int res = 0;
-        FOR(i,1,n) {
-            f[a[i]] = 1;
-            for(int x = 2; x*x <= a[i]; ++x)
-                if (a[i] % x == 0) {
-                    f[a[i]] = max(f[a[i]], f[x] + 1);
-                    f[a[i]] = max(f[a[i]], f[a[i]/x] + 1);
-                }
-
-            for(int x = 2; x*x <= a[i]; ++x)
-                if (a[i] % x == 0) {
-                    f[x] = max(f[x], f[a[i]]);
-                    f[a[i]/x] = max(f[a[i]/x], f[a[i]]);
-                }
-            res = max(res, f[a[i]]);
-        }
-        printf("%d\n", res);
+  int n;
+  cin >> n;
+  vector<int> rawprimes = GeneratePrime(102000);
+  int np = SZ(rawprimes);
+  FORN(i, np) primes[i] = rawprimes[i];
+  FORN(i, n) {
+    int x;
+    scanf("%d", &x);
+    int nf = 0;
+    int mx = x;
+    int j = 0;
+    while (primes[j] * primes[j] <= mx) {
+      if (x % primes[j]) ++j;
+      else {
+        factors[nf] = primes[j];
+        ++nf;
+        x /= primes[j];
+      }
     }
-    return 0;
+    if (x > 1) {
+      factors[nf] = x;
+      ++nf;
+    }
+    int best = 0;
+    FORN(i, nf) {
+      MX(best, dp[factors[i]]);
+    }
+    FORN(i, nf) {
+      MX(dp[factors[i]], best+1);
+    }
+  }
+  int best = 1;
+  FORN(i, 105000) {
+    MX(best, dp[i]);
+  }
+  cout << best << endl;
 }
