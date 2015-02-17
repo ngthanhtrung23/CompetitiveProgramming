f2a96a13a2f614cc7517230f75802f0b4d665240
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-17 17:16:00 +0800

    Add solution for problem `B - Suspects` Link: https://codeforces.com/contest/156/problem/B Tags: constructive algorithms, data structures, implementation, *1600 Ref: https://codeforces.com/contest/156/submission/9880076

--- codeforces/156/B.cpp
+++ codeforces/156/B.cpp
@@ -1,77 +1,55 @@
-#include <sstream>
-#include <iomanip>
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
 #include <algorithm>
+#include <cstdio>
 #include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
+#include <cstring>
 #include <string>
-#include <deque>
-#include <complex>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#include <cmath>
+#include <utility>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <sstream>
+#define fr(a,b,c) for (int a=b;a<=c;a++)
+#define frr(a,b,c) for (int a=b;a>=c;a--)
+#define rep(a,b) for (int a=0;a<b;a++)
+#define repp(a,b) for (int a=b-1;a>=0;a--)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(a) int(a.size())
+#define all(a) a.begin(),a.end()
+#define pii pair<int,int>
+#define oo 1000111222
+#define maxN 1
 using namespace std;
 
-const double PI = acos(-1.0);
-const int MN = 100111;
+int cnt,n,m,a[100100],pos[100100],neg[100100],P,N,maybe[100100];
 
-int n, m;
-pair<char,int> x[MN];
-int Plus[MN], Minus[MN], nMinus, nCan;
-bool can[MN];
-
-int main() {
-    while (scanf("%d%d\n", &n, &m) == 2) {
-        nMinus = 0; nCan = 0;
-        memset(Plus, 0, sizeof Plus);
-        memset(Minus, 0, sizeof Minus);
-        memset(can, false, sizeof can);
-
-        FOR(i,1,n) {
-            scanf("%c%d\n", &x[i].F, &x[i].S);
-            if (x[i].F == '+') Plus[x[i].S]++;
-            else {
-                Minus[x[i].S]++;
-                ++nMinus;
-            }
-        }
-
-        FOR(i,1,n) {
-            if (Plus[i] + nMinus - Minus[i] == m) {
-                can[i] = true;
-                nCan++;
-            }
-        }
-
-        FOR(i,1,n) {
-            if (x[i].F == '+') {
-                if (!can[x[i].S]) puts("Lie");
-                else if (nCan == 1) puts("Truth");
-                else puts("Not defined");
-            }
-            else {
-                if (!can[x[i].S]) puts("Truth");
-                else if (nCan == 1) puts("Lie");
-                else puts("Not defined");
-            }
-        }
-    }
-    return 0;
+int main()
+{
+	cin >> n >> m;
+	fr(i,1,n) 
+	{
+		cin >> a[i];
+		if (a[i]>0) pos[a[i]]++, P++;
+		else neg[-a[i]]++, N++;
+	}
+	
+	fr(i,1,n)
+		if (pos[i]+N-neg[i]==m) cnt++, maybe[i]=1;
+	
+	fr(i,1,n)
+		if (a[i]>0)
+		{
+			if (maybe[a[i]]) puts(cnt==1?"Truth":"Not defined");
+			else puts("Lie");
+		}
+		else
+		{
+			if (maybe[-a[i]]) puts(cnt==1?"Lie":"Not defined");
+			else puts("Truth");
+		}
 }
