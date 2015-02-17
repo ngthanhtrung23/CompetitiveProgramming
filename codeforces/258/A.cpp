2e052a36b3c099ce7f80018370a6286407cedfba
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-17 20:26:00 +0800

    Add solution for problem `A - Little Elephant and Bits` Link: https://codeforces.com/contest/258/problem/A Tags: greedy, math, *1100 Ref: https://codeforces.com/contest/258/submission/9881519

--- codeforces/258/A.cpp
+++ codeforces/258/A.cpp
@@ -1,81 +1,40 @@
-#include <sstream>
-#include <iomanip>
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
+#include <sstream>
+#include <string>
 #include <vector>
+#include <deque>
+#include <queue>
 #include <set>
 #include <map>
-#include <stack>
-#include <queue>
-#include <string>
-#include <deque>
-#include <complex>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
 
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
+#define REP(i,n) for((i)=0;(i)<(int)(n);(i)++)
+#define foreach(c,itr) for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
 
-const long double PI = acos((long double) -1.0);
+char buf[100010];
+string s,t;
 
-char s[100111];
-
-int main() {
-    while (gets(s)) {
-        int l = strlen(s);
-        bool ok = false;
-        REP(i,l-1) {
-            if (s[i] == '0' && !ok) {
-                ok = true;
-                continue;
-            }
-            putchar(s[i]);
-        }
-
-        if (ok) putchar(s[l-1]);
-        puts("");
+int main(void){
+    int i;
+    
+    scanf("%s", buf);
+    s = buf;
+    int N = s.length();
+    int x = 0;
+    REP(i,N) if(s[i] == '0'){
+        x = i;
+        break;
     }
+    REP(i,N) if(i != x) t += s[i];
+    printf("%s\n", t.c_str());
+    
     return 0;
 }
