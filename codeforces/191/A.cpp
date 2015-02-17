4c6e88b978c2a2861d72b004a6672f83a7b92a1c
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-17 16:04:00 +0800

    Add solution for problem `A - Dynasty Puzzles` Link: https://codeforces.com/contest/191/problem/A Tags: dp, *1500 Ref: https://codeforces.com/contest/191/submission/9879620

--- codeforces/191/A.cpp
+++ codeforces/191/A.cpp
@@ -1,60 +1,41 @@
-//#pragma comment(linker, "/STACK:66777216")
-#include <iomanip>
-#include <sstream>
 #include <iostream>
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
-#include <cmath>
 #include <algorithm>
+#include <string>
 #include <vector>
-#include <set>
-#include <map>
 #include <stack>
 #include <queue>
-#include <string>
-#include <deque>
-#include <complex>
-#include <bitset>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; ++i)
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define DEBUG(x) cout << #x << " = " << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
-#define PR0(a,n) cout << #a << " = "; REP(i,n) cout << a[i] << ' '; puts("");
+#include <set>
+#include <map>
+//#include <conio.h>
 using namespace std;
+#define FOR(i, a, b) for(int i=a; i<=b; i++)
+#define DOW(i, a, b) for(int i=a; i>=b; i--)
+#define FOREACH(it, c) for(typeof(c.begin()) it=c.begin(); it!=c.end(); it++)
+#define RESET(c, val) memset(c, val, sizeof(c))
 
-const double PI = acos(-1.0);
-
-int n;
-char a[1011];
+int n, le;
+string s;
+char fir, las;
 int f[300][300];
 
 int main() {
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
-    while (scanf("%d\n", &n) == 1) {
-        FOR(x,'a','z') FOR(y,'a','z') f[x][y] = -1000111000;
-        FOR(i,1,n) {
-            gets(a);
-            char y = a[0];
-            char z = a[strlen(a)-1];
-            FOR(x,'a','z') {
-                f[x][z] = max(f[x][z], f[x][y] + (int) strlen(a));
-            }
-            f[y][z] = max(f[y][z], (int) strlen(a));
-        }
-        int res = 0;
-        FOR(x,'a','z') {
-            res = max(res, f[x][x]);
-        }
-        cout << res << endl;
+    //freopen("test.inp", "r", stdin);
+    //freopen(".out", "w", stdout);
+    scanf("%d\n", &n);
+    int res=0;
+    for(char c1='a'; c1<='z'; c1++) for(char c2='a'; c2<='z'; c2++) f[c1][c2]=-1000000000;
+    FOR(i, 1, n) {
+        getline(cin, s); le=s.length();
+        fir=s[0]; las=s[le-1];
+        for(char c='a'; c<='z'; c++) 
+            if (f[c][fir]>=0) f[c][las]=max(f[c][las], f[c][fir]+le);
+        f[fir][las]=max(f[fir][las], le);
     }
+    for(char c='a'; c<='z'; c++) res=max(res, f[c][c]);
+    cout << res;
+    //getch();
     return 0;
 }
