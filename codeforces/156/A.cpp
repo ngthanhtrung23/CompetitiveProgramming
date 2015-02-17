7428fd47ed95567bee4ded28661f1f95bc6c7a77
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-17 17:24:00 +0800

    Add solution for problem `A - Message` Link: https://codeforces.com/contest/156/problem/A Tags: brute force, *1700 Ref: https://codeforces.com/contest/156/submission/9880132

--- codeforces/156/A.cpp
+++ codeforces/156/A.cpp
@@ -1,50 +1,35 @@
-#include <sstream>
-#include <iomanip>
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <string>
-#include <deque>
-#include <complex>
+
+#include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define DEBUG(x) { cout << #x << " = '"; cout << (x) << "'" << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
 using namespace std;
 
-const double PI = acos(-1.0);
-
-char a[4011], b[4011];
-
 int main() {
-    while (gets(a) && gets(b)) {
-        int n = strlen(a), m = strlen(b);
+    ios :: sync_with_stdio(false);
+    cout << (fixed) << setprecision(9);
+    string s, t;
+    while (cin >> s >> t) {
+        int ls = s.length(), lt = t.length();
+        REP(i,lt) s = ' ' + s;
+        REP(i,lt) s += ' ';
 
-        int res = m;
-        FOR(i,-m,n) {
+        int res = lt;
+        for(int start = 0, last = lt; last <= s.length(); ++start, ++last) {
             int cur = 0;
-            REP(j,m)
-                if (i+j < 0 || i+j >= n || b[j] != a[i+j]) ++cur;
+            FOR(i,start,last-1)
+                if (s[i] != t[i-start]) ++cur;
             res = min(res, cur);
         }
-        printf("%d\n", res);
+        cout << res << endl;
     }
     return 0;
 }
