fa896c1b90556f0037bd9ccdff511197f5ba176a
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-17 19:05:00 +0800

    Add solution for problem `A - Lucky Permutation` Link: https://codeforces.com/contest/286/problem/A Tags: constructive algorithms, math, *1400 Ref: https://codeforces.com/contest/286/submission/9880941

--- codeforces/286/A.cpp
+++ codeforces/286/A.cpp
@@ -1,89 +1,37 @@
-#define _GLIBCXX_DEBUG
-#include <sstream>
-#include <iomanip>
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
 #include <algorithm>
+#include <cstdio>
 #include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
+#include <cstring>
 #include <string>
-#include <deque>
-#include <complex>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#include <cmath>
+#include <utility>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <sstream>
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
-const long double PI = acos((long double) -1.0);
-
-void solve(int l, int r, int n) {
-    if (n == 0) return ;
-
-    if (n == 1) printf("%d ", l);
-    else {
-        printf("%d ", l+1);
-        printf("%d ", r);
-        solve(l+2, r-2, n - 4);
-        printf("%d ", l);
-        printf("%d ", r-1);
-    }
-}
-
-int main() {
-    int n;
-    while (cin >> n) {
-        if (n % 4 == 2 || n % 4 == 3) {
-            puts("-1");
-        }
-        else {
-            solve(1, n, n);
-            puts("");
-        }
-    }
-    return 0;
+int main()
+{
+	int n, p[100100];
+	cin >> n;
+	
+	if ((n / 2) % 2) 
+	{
+		puts("-1");
+		return 0;
+	}
+	
+	for (int i = 1; i < n / 2; i += 2)
+	{
+		p[i] = i + 1;
+		p[i + 1] = n - i + 1;
+		p[n - i + 1] = n - i;
+		p[n - i] = i;
+	}
+	if (n % 2) p[n / 2 + 1] = n / 2 + 1;
+	
+	for (int i = 1; i <= n; i++) cout << p[i] << (i == n?'\n':' ');
 }
