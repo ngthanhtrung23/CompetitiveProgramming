254b6b8f429e68fbb6636c3e7ee5bf081933ad88
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-18 17:36:00 +0800

    Add solution for problem `A - Replacement` Link: https://codeforces.com/contest/135/problem/A Tags: greedy, implementation, sortings, *1300 Ref: https://codeforces.com/contest/135/submission/9912470

--- codeforces/135/A.cpp
+++ codeforces/135/A.cpp
@@ -1,47 +1,29 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <string>
-#include <deque>
-#include <complex>
+
+#include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
 using namespace std;
 
-const double PI = acos(-1.0);
-
-int a[100111];
+int n, a[100111];
 
 int main() {
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
-    int ln = 0;
-    int n; scanf("%d", &n);
-    FOR(i,1,n) {
-        scanf("%d", &a[i]);
-        ln = max(ln, a[i]);
+    ios :: sync_with_stdio(false);
+    cout << (fixed) << setprecision(9);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        sort(a+1, a+n+1);
+        if (a[n] == 1) a[n] = 2; else a[n] = 1;
+        sort(a+1, a+n+1);
+        FOR(i,1,n) cout << a[i] << ' '; cout << endl;
     }
-    FOR(i,1,n) if (a[i] == ln) {
-        if (a[i] != 1) a[i] = 1;
-        else a[i] = 2;
-        break;
-    }
-    sort(a + 1, a + n + 1);
-    FOR(i,1,n) printf("%d ", a[i]);
     return 0;
 }
