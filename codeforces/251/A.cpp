5b69d82f88b3b1145ceca9a059dc0a3aeba4f03c
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-19 00:06:00 +0800

    Add solution for problem `A - Points on Line` Link: https://codeforces.com/contest/251/problem/A Tags: binary search, combinatorics, two pointers, *1300 Ref: https://codeforces.com/contest/251/submission/9916604

---
+++ codeforces/251/A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <utility>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <sstream>
+#define fr(a,b,c) for (int a=b;a<=c;a++)
+#define frr(a,b,c) for (int a=b;a>=c;a--)
+#define rep(a,b) for (int a=0;a<b;a++)
+#define repp(a,b) for (int a=b-1;a>=0;a--)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(a) int(a.size())
+#define all(a) a.begin(),a.end()
+#define reset(a,b) memset(a,b,sizeof(a))
+#define pii pair<int,int>
+#define oo 1000111222
+#define maxN 1
+using namespace std;
+
+int main()
+{
+	int n,d,a[100100];
+	long long ans=0;
+	cin >> n >> d;
+	rep(i,n) cin >> a[i];
+	for (int i=0,j=0;i<n;i++)
+	{
+		while (a[i]-a[j]>d) j++;
+		ans+=max(0LL,1LL*(i-j-1)*(i-j)/2);
+	}
+	cout << ans << endl;
+}
