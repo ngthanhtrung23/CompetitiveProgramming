620db449109a91f55f5cc167318132b88e425413
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-19 03:41:00 +0800

    Add solution for problem `B - Bubble Sort Graph` Link: https://codeforces.com/contest/341/problem/B Tags: binary search, data structures, dp, *1500 Ref: https://codeforces.com/contest/341/submission/9919068

--- codeforces/341/B.cpp
+++ codeforces/341/B.cpp
@@ -1,34 +1,68 @@
-
 #include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
-#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
-#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
 using namespace std;
 
-int a[100111];
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+const int MN = 100111;
+
+int n, a[MN], f[MN], bit[MN];
+
+#define _(u) ((u) & (-(u)))
+
+int get(int u) {
+    int res = 0;
+    for(int x = u; x > 0; x -= _(x))
+        res = max(res, bit[x]);
+    return res;
+}
+
+void update(int u, int k) {
+    for(int x = u; x <= n; x += _(x))
+        bit[x] = max(bit[x], k);
+}
 
 int main() {
     ios :: sync_with_stdio(false);
-    cout << (fixed) << setprecision(9);
-    int n;
     while (cin >> n) {
         FOR(i,1,n) cin >> a[i];
-        vector<int> d(n+2, 1000111000);
-        d[0] = 0;
+        memset(bit, 0, sizeof bit);
 
         int res = 0;
         FOR(i,1,n) {
-            auto t = lower_bound(d.begin(), d.end(), a[i]);
-            res = max(res, (int) (t - d.begin()));
-            *t = a[i];
+            f[i] = get(a[i]) + 1;
+            update(a[i], f[i]);
+
+            res = max(res, f[i]);
         }
         cout << res << endl;
     }
