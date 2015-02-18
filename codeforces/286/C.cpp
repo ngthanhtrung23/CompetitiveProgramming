9a4025520037ab943cfcd978d27be8c24e33558a
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-18 21:33:00 +0800

    Add solution for problem `C - Main Sequence` Link: https://codeforces.com/contest/286/problem/C Tags: greedy, implementation, *2100 Ref: https://codeforces.com/contest/286/submission/9914827

--- codeforces/286/C.cpp
+++ codeforces/286/C.cpp
@@ -1,96 +1,55 @@
-#define _GLIBCXX_DEBUG
-#include <sstream>
-#include <iomanip>
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <string>
-#include <deque>
-#include <complex>
+#include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
-const long double PI = acos((long double) -1.0);
 const int MN = 1000111;
+int n, a[MN];
+bool bad[MN];
 
-int n, a[MN], st[MN], top;
+const int BUFF_SIZE = 1<<20;
+char inp_buffer[BUFF_SIZE + 10];
+char out_buffer[BUFF_SIZE + 10];
 
 int main() {
-    while (scanf("%d", &n) == 1) {
-        FOR(i,1,n) scanf("%d", &a[i]);
-        int q; scanf("%d", &q);
-        while (q--) {
-            int u; scanf("%d", &u);
-            a[u] = -a[u];
-        }
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cin.rdbuf()->pubsetbuf(inp_buffer, BUFF_SIZE);
+    cout.rdbuf()->pubsetbuf(out_buffer, BUFF_SIZE);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        memset(bad, false, sizeof bad);
+        int q; cin >> q;
+        while (q--) { int u; cin >> u; bad[u] = true; }
+
+        stack<int> st;
         FORD(i,n,1) {
-            if (a[i] < 0) {
-                st[++top] = i;
+            if (bad[i]) a[i] = -a[i];
+            else if (!st.empty() && st.top() == a[i]) {
+                a[i] = +a[i];
             }
             else {
-                if (top && a[st[top]] == -a[i]) {
-                    --top;
-                }
-                else {
-                    st[++top] = i;
-                    a[i] = -a[i];
-                }
+                a[i] = -a[i];
             }
+
+            if (a[i] > 0) st.pop();
+            else st.push(-a[i]);
         }
-        if (top) puts("NO");
-        else {
+        if (st.empty()) {
             puts("YES");
-            FOR(i,1,n) printf("%d ", a[i]); puts("");
+            FOR(i,1,n) printf("%d ", a[i]);
+            puts("");
         }
+        else puts("NO");
+        break;
     }
     return 0;
 }
