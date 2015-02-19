6cf028c899e9b776cc161e7a629eb7486552adda
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-19 12:32:00 +0800

    Add solution for problem `B - Dima and To-do List` Link: https://codeforces.com/contest/366/problem/B Tags: brute force, implementation, *1200 Ref: https://codeforces.com/contest/366/submission/9921801

---
+++ codeforces/366/B.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+const int MN = 100111;
+
+int n, k, a[MN], sum[MN];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    cout << (fixed) << setprecision(9);
+    while (cin >> n >> k) {
+        FOR(i,1,n) {
+            cin >> a[i];
+            sum[i % k] += a[i];
+        }
+        sum[k] = sum[0];
+        int x = min_element(sum+1, sum+k+1) - sum;
+        cout << x << endl;
+    }
+    return 0;
+}
