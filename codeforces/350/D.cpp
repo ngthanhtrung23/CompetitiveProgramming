5a2096ddd2d7bc1af72aa681346df72f92322eb2
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-20 00:40:00 +0800

    Add solution for problem `D - Looking for Owls` Link: https://codeforces.com/contest/350/problem/D Tags: binary search, data structures, geometry, hashing, sortings, *2400 Ref: https://codeforces.com/contest/350/submission/9928590

--- codeforces/350/D.cpp
+++ codeforces/350/D.cpp
@@ -1,151 +1,142 @@
-
 #include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) { cerr << #x << " = "; cerr << (x) << endl; }
-#define PR(a,n) { cerr << #a << " = "; FOR(_,1,n) cerr << a[_] << ' '; cerr << endl; }
-#define PR0(a,n) { cerr << #a << " = "; REP(_,n) cerr << a[_] << ' '; cerr << endl; }
-
-#define sqr(x) ((x) * (x))
-#define ll long long
+#define DEBUG(x) { cout << #x << " = "; cout << x << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
 using namespace std;
 
-struct Point {
-    ll x, y;
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
 
-    Point(ll x = 0, ll y = 0) : x(x), y(y) {}
+struct Line {
+    int a, b, c;
 
-    Point operator * (ll k) const { return Point(x*k, y*k); }
-    Point operator / (ll k) const { return Point(x/k, y/k); }
-    Point operator + (const Point& a) const { return Point(x+a.x, y+a.y); }
-    Point operator - (const Point& a) const { return Point(x-a.x, y-a.y); }
+    Line() {}
 
-    void normalize() {
-        if (x < 0) {
-            x = -x; y = -y;
+    Line(int x1, int y1, int x2, int y2, bool rot = false) {
+        a = y2 - y1;
+        b = x1 - x2;
+        if (rot) {
+            swap(a, b);
+            a = -a;
         }
-        if (x == 0) y = 1;
-        else if (y == 0) x = 1;
-        else {
-            ll g = __gcd(x, y);
-            x /= g; y /= g;
-        }
+        int g = __gcd(a, b);
+        a /= g; b /= g;
+        if (a < 0) a = -a, b = -b;
+
+        c = - (a * x1 + b * y1);
     }
+} a[300111];
 
-    ll len() { return x*x + y*y; }
-    void read() { scanf("%I64d%I64d", &x, &y); }
-};
-
-bool operator < (const Point& a, const Point& b) {
-    if (a.x != b.x) return a.x < b.x;
-    return a.y < b.y;
-}
-bool operator == (const Point& a, const Point& b) {
-    return a.x == b.x && a.y == b.y;
-}
-
-struct Circle {
-    Point O;
-    ll r;
-
-    void read() { O.read(); scanf("%I64d", &r); }
-} circles[1511];
-
-bool intersect(const Circle& a, const Circle& b) {
-    ll dist = (a.O - b.O).len();
-    if (dist > 4LL*a.r*a.r) return false;
-    else return true;
+bool operator < (const Line &x, const Line &y) {
+    if (x.a != y.a) return x.a < y.a;
+    if (x.b != y.b) return x.b < y.b;
+    return x.c < y.c;
 }
 
 struct Event {
-    Point dir, x;
-    int typ;
-    ll offset;
-} events[3000111];
+    int a, b, c;
+    int x, y;
+    int type;
 
-bool operator < (const Event& a, const Event& b) {
-    if (a.dir == b.dir) {
-        if (a.offset != b.offset) return a.offset < b.offset;
-        else if (a.x == b.x) return a.typ < b.typ;
-        else return a.x < b.x;
-    }
-    else return a.dir < b.dir;
+    Event(int a, int b, int c, int x, int y, int type) 
+        : a(a), b(b), c(c), x(x), y(y), type(type) {}
+
+    Event() {}
+};
+
+bool operator < (const Event &a, const Event &b) {
+    if (a.a != b.a) return a.a < b.a;
+    if (a.b != b.b) return a.b < b.b;
+    if (a.c != b.c) return a.c < b.c;
+    if (a.x != b.x) return a.x < b.x;
+    if (a.y != b.y) return a.y < b.y;
+    return a.type < b.type;
 }
+
+Event event[3000111];
+
+struct Circle {
+    int x, y, r;
+} b[1511];
+
+int n, m;
+
+#define sqr(x) ((x)*(x))
 
 int main() {
-    int nLine, nCircle, nEvent;
-    while (scanf("%d%d", &nLine, &nCircle) == 2) {
-        nEvent = 0;
-        FOR(i,1,nLine) {
-            Point A, B; A.read(); B.read();
-            A = A * 2;
-            B = B * 2;
+    GN(n); GN(m);
+    int x1, y1, x2, y2;
+    int nEvent = 0;
+    FOR(i,1,n) {
+        GN(x1); GN(y1); GN(x2); GN(y2);
+        x1 *= 2; y1 *= 2; x2 *= 2; y2 *= 2;
 
-            Point dir = B - A;
-            swap(dir.x, dir.y);
-            dir.y = -dir.y;
+        a[i] = Line(x1, y1, x2, y2);
+        // cout << a[i].a << ' ' << a[i].b << ' ' << a[i].c << endl;
 
-            dir.normalize();
-            if (B < A) swap(A, B);
-
-            ll offset = - (dir.x*A.x + dir.y*A.y);
-
-            ++nEvent;
-            events[nEvent].dir = dir; events[nEvent].x = A; events[nEvent].typ = 1;
-            events[nEvent].offset = offset;
-
-            ++nEvent;
-            events[nEvent].dir = dir; events[nEvent].x = B; events[nEvent].typ = 3;
-            events[nEvent].offset = offset;
+        if (x2 < x1 || (x2 == x1 && y2 < y1)) {
+            swap(x1, x2);
+            swap(y1, y2);
         }
-//        cout << "DONE LINE" << endl;
-        FOR(i,1,nCircle) {
-            circles[i].read();
-            circles[i].O = circles[i].O * 2;
-            circles[i].r *= 2;
-        }
-
-        FOR(i,1,nCircle) FOR(j,i+1,nCircle)
-        if (!intersect(circles[i], circles[j]) && circles[i].r == circles[j].r) {
-            Point dir = circles[i].O - circles[j].O;
-            dir.normalize();
-
-            Point mid = (circles[i].O + circles[j].O) / 2;
-
-            ++nEvent;
-            events[nEvent].dir = dir;
-            events[nEvent].x = mid;
-            events[nEvent].typ = 2;
-            events[nEvent].offset = - (dir.x*mid.x + dir.y*mid.y);
-        }
-//        cout << "DONE CIRCLE" << endl;
-
-        sort(events+1, events+nEvent+1);
-        ll res = 0;
-        int cur = 0;
-        events[0].dir = events[1].dir * 2;
-        FOR(i,1,nEvent) {
-            if (!(events[i].dir == events[i-1].dir) || events[i].offset != events[i-1].offset)
-                cur = 0;
-
-            switch (events[i].typ) {
-                case 1:
-                    ++cur;
-                    break;
-                case 2:
-                    res += cur;
-                    break;
-                case 3:
-                    --cur;
-                    break;
-            }
-        }
-        printf("%I64d\n", res);
+        event[++nEvent] = Event(a[i].a, a[i].b, a[i].c, x1, y1, 1);
+        event[++nEvent] = Event(a[i].a, a[i].b, a[i].c, x2, y2, 3);
     }
+
+    FOR(i,1,m) {
+        GN(b[i].x); GN(b[i].y); GN(b[i].r);
+        b[i].x *= 2; b[i].y *= 2;
+        b[i].r *= 2;
+    }
+
+    FOR(i,1,m) FOR(j,i+1,m)
+    if (b[i].r == b[j].r && 4*b[i].r*b[i].r < sqr(b[i].x-b[j].x) + sqr(b[i].y-b[j].y)) {
+        Line l(b[i].x, b[i].y, b[j].x, b[j].y, true);
+
+        int x = (b[i].x + b[j].x) / 2;
+        int y = (b[i].y + b[j].y) / 2;
+        l.c = - (l.a * x + l.b * y);
+        // cout << l.a << ' ' << l.b << ' ' << l.c << endl;
+
+        event[++nEvent] = Event(l.a, l.b, l.c, x, y, 2);
+    }
+
+    int res = 0;
+    sort(event+1, event+nEvent+1);
+    int now = 0;
+    FOR(i,1,nEvent) {
+        if (i == 1 || event[i].a != event[i-1].a || event[i].b != event[i-1].b || event[i].c != event[i-1].c)
+            now = 0;
+        if (event[i].type == 1) ++now;
+        else if (event[i].type == 3) --now;
+        else res += now;
+    }
+    cout << res << endl;
+
     return 0;
 }
-
