40924e8d23fcdde9ae1011af6e125436d38737bc
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-20 12:14:00 +0800

    Add solution for problem `D - World of Darkraft` Link: https://codeforces.com/contest/138/problem/D Tags: dp, games, *2500 Ref: https://codeforces.com/contest/138/submission/9933598

--- codeforces/138/D.cpp
+++ codeforces/138/D.cpp
@@ -16,58 +16,54 @@ using namespace std;
 int m, n;
 char a[22][22];
 int f[2][44][44][44][44];
-int mark[5000111];
+
+int get(int t, int minSum, int maxSum, int minDiff, int maxDiff) {
+    if (minSum > maxSum || minDiff > maxDiff) return 0;
+    if (f[t][minSum][maxSum][minDiff][maxDiff] >= 0)
+        return f[t][minSum][maxSum][minDiff][maxDiff];
+
+    unordered_set<int> s;
+    FOR(i,1,m) FOR(j,1,n)
+        if ((i+j) % 2 == t) {
+            int sum = i+j, diff = i-j+n;
+            if (minSum <= sum && sum <= maxSum
+                    && minDiff <= diff && diff <= maxDiff) {
+                if (a[i][j] == 'R') {
+                    int x = get(t, minSum, maxSum, minDiff, diff-1);
+                    int y = get(t, minSum, maxSum, diff+1, maxDiff);
+                    s.insert(x ^ y);
+                }
+                if (a[i][j] == 'L') {
+                    int x = get(t, minSum, sum-1, minDiff, maxDiff);
+                    int y = get(t, sum+1, maxSum, minDiff, maxDiff);
+                    s.insert(x ^ y);
+                }
+                if (a[i][j] == 'X') {
+                    int x = get(t, minSum, sum-1, minDiff, diff-1);
+                    int y = get(t, minSum, sum-1, diff+1, maxDiff);
+                    int z = get(t, sum+1, maxSum, minDiff, diff-1);
+                    int w = get(t, sum+1, maxSum, diff+1, maxDiff);
+
+                    s.insert(x ^ y ^ z ^ w);
+                }
+            }
+        }
+
+    int res = 0;
+    while (s.count(res)) ++res;
+    return f[t][minSum][maxSum][minDiff][maxDiff] = res;
+}
 
 int main() {
     while (cin >> m >> n) {
         FOR(i,1,m) FOR(j,1,n) cin >> a[i][j];
 
-        memset(f, 0, sizeof f);
-        memset(mark, 0, sizeof mark);
-        int id = 0;
-        REP(t,2) {
-            FORD(minSum,m+n,2) FOR(maxSum,minSum,m+n)
-                FORD(minDiff,m+n-1,1) FOR(maxDiff,minDiff,m+n-1) {
-                    id++;
-
-                    FOR(i,1,maxSum-1)
-                    FOR(j,max(1,max(n + i - maxDiff, minSum - i)),min(n, min(n+i-minDiff,maxSum-i))) {
-                        if ((i+j) % 2 == t) {
-                            if (a[i][j] == 'R') {
-                                int diff = i - j + n;
-                                int x = f[t][minSum][maxSum][minDiff][diff-1];
-                                int y = f[t][minSum][maxSum][diff+1][maxDiff];
-                                mark[x^y] = id;
-                            }
-                            if (a[i][j] == 'L') {
-                                int sum = i + j;
-                                int x = f[t][minSum][sum-1][minDiff][maxDiff];
-                                int y = f[t][sum+1][maxSum][minDiff][maxDiff];
-                                mark[x^y] = id;
-                            }
-                            if (a[i][j] == 'X') {
-                                int sum = i+j, diff = i-j+n;
-                                int x = f[t][minSum][sum-1][minDiff][diff-1];
-                                int y = f[t][minSum][sum-1][diff+1][maxDiff];
-                                int z = f[t][sum+1][maxSum][minDiff][diff-1];
-                                int w = f[t][sum+1][maxSum][diff+1][maxDiff];
-
-                                mark[x^y^z^w] = id;
-                            }
-                        }
-                    }
-
-                    int res = 0;
-                    while (mark[res] == id) ++res;
-                    f[t][minSum][maxSum][minDiff][maxDiff] = res;
-                }
-        }
-//        DEBUG(f[0][2][m+n][1][m+n-1]);
-//        DEBUG(f[1][2][m+n][1][m+n-1]);
-        int res = f[0][2][m+n][1][m+n-1]
-                ^ f[1][2][m+n][1][m+n-1];
+        memset(f, -1, sizeof f);
+        int res = get(0, 1, m+n, 1, m+n)
+                ^ get(1, 1, m+n, 1, m+n);
         if (res) cout << "WIN"; else cout << "LOSE";
         cout << endl;
     }
     return 0;
 }
+
