df479a6bb410796001b2501b9d1ea0e14d10c170
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-20 14:08:00 +0800

    Add solution for problem `B - Xenia and Spies` Link: https://codeforces.com/contest/342/problem/B Tags: brute force, greedy, implementation, *1500 Ref: https://codeforces.com/contest/342/submission/9934145

--- codeforces/342/B.cpp
+++ codeforces/342/B.cpp
@@ -1,74 +1,49 @@
+
 #include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-const int BUFSIZE = (1<<12) + 17;
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp && !REACHEOF) { \
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
 const int MN = 200111;
-int n, m, s, f;
-pair<int,int> a[MN];
+int l[MN], r[MN];
 
-bool bad(int t, int s) {
-    if (s >= a[t].first && s <= a[t].second) return true;
-    return false;
-}
+#define MOVE { moved = true; if (cur < f) { putchar('R'); ++cur; } else { putchar('L'); --cur; } }
 
 int main() {
-    ios :: sync_with_stdio(false);
-    while (cin >> n >> m >> s >> f) {
-        memset(a, 0, sizeof a);
+    int n, q, s, f;
+    while (scanf("%d%d%d%d", &n, &q, &s, &f) == 4) {
+        memset(l, -1, sizeof l);
+        memset(r, -1, sizeof r);
 
-        FOR(i,1,m) {
-            int l, r, t; cin >> t >> l >> r;
-            if (t < 200111) {
-                a[t] = make_pair(l, r);
-            }
+        while (q--) {
+            int t, u, v; scanf("%d%d%d", &t, &u, &v);
+            if (t < MN) l[t] = u, r[t] = v;
         }
-        FOR(t,1,200111) {
-            if (s == f) break;
-            if (s < f) {
-                if (bad(t, s) || bad(t, s+1)) putchar('X');
-                else {
-                    ++s;
-                    putchar('R');
-                }
-            }
+
+        int cur = s, time = 0;
+        while (cur != f) {
+            bool moved = false;
+            ++time;
+            if (l[time] < 0) MOVE
             else {
-                if (bad(t, s) || bad(t, s-1)) putchar('X');
-                else {
-                    --s;
-                    putchar('L');
-                }
+                int next = cur;
+                if (next < f) ++next;
+                else --next;
+
+                if (cur < l[time] || r[time] < cur)
+                    if (next < l[time] || r[time] < next)
+                        MOVE;
             }
+            if (!moved) putchar('X');
         }
         puts("");
     }
