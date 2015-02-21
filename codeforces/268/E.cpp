fc8e08078f3b7ef5b04928224999f230e4aacb81
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-22 00:10:00 +0800

    Add solution for problem `E - Playlist` Link: https://codeforces.com/contest/268/problem/E Tags: math, probabilities, sortings, *2100 Ref: https://codeforces.com/contest/268/submission/9950121

--- codeforces/268/E.cpp
+++ codeforces/268/E.cpp
@@ -1,87 +1,46 @@
-#include <sstream>
-#include <iomanip>
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
 #include <algorithm>
+#include <cstdio>
 #include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
+#include <cstring>
 #include <string>
-#include <deque>
-#include <complex>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#include <cmath>
+#include <utility>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <sstream>
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
-const long double PI = acos((long double) -1.0);
-
+pair <double,double> a[50500];
 int n;
-pair<int,int> a[50111];
 
-bool cmp(const pair<int,int> &a, const pair<int,int> &b) {
-    return a.F * a.S * (100 - b.S) > b.F * b.S * (100 - a.S);
+bool cmp(pair <double,double> u, pair <double,double> v)
+{
+	return u.first * v.second > u.second * v.first;
 }
 
-int main() {
-    while (scanf("%d", &n) == 1) {
-        FOR(i,1,n) scanf("%d%d", &a[i].F, &a[i].S);
-        sort(a+1, a+n+1, cmp);
-
-        long double exp = 0.0, res = 0.0;
-
-        FOR(i,1,n) {
-            res += (100 - a[i].S) * exp / 100.0;
-            exp += (a[i].S * a[i].F) / (long double) 100.0;
-        }
-
-        FOR(i,1,n) res += a[i].F;
-
-        cout << (fixed) << setprecision(10) << res << endl;
-    }
-    return 0;
+int main()
+{
+	double ans = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) 
+	{
+		scanf("%lf%lf", &a[i].first, &a[i].second);
+		ans += a[i].first;
+		a[i].second /= 100;
+		a[i].first = a[i].first * a[i].second;
+		a[i].second = 1 - a[i].second;
+	}
+	sort(a, a + n, cmp);
+	
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		ans += a[i].second * sum;
+		sum += a[i].first;
+	}
+	
+	printf("%.12lf\n", ans);
 }
