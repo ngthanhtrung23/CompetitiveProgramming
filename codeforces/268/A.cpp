910875c4e9d9cda5b68c8cc096948465c2f4a941
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-22 00:11:00 +0800

    Add solution for problem `A - Games` Link: https://codeforces.com/contest/268/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/268/submission/9950133

--- codeforces/268/A.cpp
+++ codeforces/268/A.cpp
@@ -1,76 +1,25 @@
-#include <sstream>
-#include <iomanip>
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
 #include <algorithm>
+#include <cstdio>
 #include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
+#include <cstring>
 #include <string>
-#include <deque>
-#include <complex>
-
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
-#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#include <cmath>
+#include <utility>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <sstream>
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
-const long double PI = acos((long double) -1.0);
-
-int n, a[111], h[111];
-
-int main() {
-    while (scanf("%d", &n) == 1) {
-        FOR(i,1,n) scanf("%d%d", &h[i], &a[i]);
-        int res = 0;
-        FOR(i,1,n)
-            FOR(j,1,n)
-            if (j != i && h[i] == a[j]) ++res;
-
-        cout << res << endl;
-    }
-    return 0;
+int main()
+{
+	int n, h[33], a[33], ans = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> h[i] >> a[i];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			ans += h[i] == a[j];
+	cout << ans << endl;
 }
