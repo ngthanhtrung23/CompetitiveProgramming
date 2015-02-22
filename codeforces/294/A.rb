a7546d4224b3f16a21e2641b2e13df2f54ea8e0d
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 03:16:00 +0800

    Add solution for problem `A - Shaass and Oskols` Link: https://codeforces.com/contest/294/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/294/submission/9962658

--- codeforces/294/A.rb
+++ codeforces/294/A.rb
@@ -2,8 +2,8 @@ n=gets.to_i
 a=gets.split.map(&:to_i)
 gets.to_i.times {
   x,y=gets.split.map(&:to_i)
-  a[x-2]+=y-1 if x>1
-  a[x]+=a[x-1]-y if x<n
-  a[x-1]=0
+  a[x-2] += y-1 if x > 1
+  a[x] += a[x-1] - y if x < n
+  a[x-1] = 0
 }
 puts a
