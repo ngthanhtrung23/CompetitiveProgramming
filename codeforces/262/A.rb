2c00e8e7fb864c39424f255735108c3ca0d349fc
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 03:41:00 +0800

    Add solution for problem `A - Roma and Lucky Numbers` Link: https://codeforces.com/contest/262/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/262/submission/9962833

--- codeforces/262/A.rb
+++ codeforces/262/A.rb
@@ -1,2 +1,4 @@
-n,k=gets.split.map(&:to_i)
-p gets.split.map{|x| x.scan(/4|7/).size<=k ? 1 : 0}.reduce(:+)
+n, k = gets.split.map(&:to_i)
+p gets.split.map { |x|
+  x.count('4') + x.count('7') <= k ? 1 : 0
+}.reduce(:+)
