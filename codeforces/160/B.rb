f4093b0ffc8c486f5fbef0855578dffad70a23e1
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 03:50:00 +0800

    Add solution for problem `B - Unlucky Ticket` Link: https://codeforces.com/contest/160/problem/B Tags: greedy, sortings, *1100 Ref: https://codeforces.com/contest/160/submission/9962911

--- codeforces/160/B.rb
+++ codeforces/160/B.rb
@@ -1,3 +1,5 @@
-n=gets.to_i
-s=gets.split''
-puts ((0...n).map{|i| s[0...n].sort[i]<=>s[n...n+n].sort[i] }.reduce(:+).abs==n) ? :YES : :NO
+n = gets.to_i
+s = gets.split('')
+l = s[0...n].sort!
+r = s[n...n+n].sort!
+puts ((0...n).map{ |i| l[i] <=> r[i] }.reduce(:+).abs == n) ? "YES" : "NO"
