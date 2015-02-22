1db98f9ff193b96277ed2ee0ed1843c5598689a4
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 01:29:00 +0800

    Add solution for problem `A - Lunch Rush` Link: https://codeforces.com/contest/276/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/276/submission/9961709

--- codeforces/276/A.rb
+++ codeforces/276/A.rb
@@ -1,8 +1,11 @@
 i=[*$<]
-n,k=i[0].split.map(&:to_i)
-res=-10**9
+n, k = i[0].split.map(&:to_i)
+def get(f, t, k)
+  (t>k)?f-(t-k):f
+end
+res = -10**9
 (1..n).each{|j|
-  f,t=i[j].split.map(&:to_i)
-  res=[(t>k)?f-(t-k):f,res].max
+  f, t = i[j].split.map(&:to_i)
+  res = [get(f,t,k),res].max
 }
 p res
