63692066c301c1fb8e2242ea2794180ea8a9bdb8
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-22 20:02:00 +0800

    Add solution for problem `A - IQ test` Link: https://codeforces.com/contest/25/problem/A Tags: brute force, *1300 Ref: https://codeforces.com/contest/25/submission/9959056

--- codeforces/25/A.py
+++ codeforces/25/A.py
@@ -1,4 +1,7 @@
 n = input()
 a = map(int, raw_input().split())
-e = sum([x % 2 == 0 for x in a])
-print a.index([x for x in a if x % 2 == (e > 1)][0]) + 1
+n_even = sum([x % 2 == 0 for x in a])
+if n_even == 1:
+    print a.index([x for x in a if x % 2 == 0][0]) + 1
+else:
+    print a.index([x for x in a if x % 2 == 1][0]) + 1
