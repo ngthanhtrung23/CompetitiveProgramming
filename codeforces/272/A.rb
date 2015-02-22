b2319499c74f6b72dc71789d0cfce23e183890c1
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 03:06:00 +0800

    Add solution for problem `A - Dima and Friends` Link: https://codeforces.com/contest/272/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/272/submission/9962569

--- codeforces/272/A.rb
+++ codeforces/272/A.rb
@@ -1,3 +1,3 @@
-n=gets.to_i
-sum=gets.split.map(&:to_i).reduce(:+)
-p (1..5).map{|i|((i+sum)% (n+1)==1)?0:1}.reduce(:+)
+n = gets.to_i
+sum = gets.split.map(&:to_i).reduce(:+)
+p (1..5).map{|i| ((i + sum) % (n+1) == 1) ? 0 : 1}.reduce(:+)
