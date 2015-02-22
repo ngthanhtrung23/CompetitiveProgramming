234fd93dadb87d2f2402e581ab66a1ce2e530622
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 02:34:00 +0800

    Add solution for problem `A - Lucky Ticket` Link: https://codeforces.com/contest/146/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/146/submission/9962319

--- codeforces/146/A.rb
+++ codeforces/146/A.rb
@@ -1,3 +1,4 @@
 n = gets.to_i
-s = gets.chomp.split('')
-puts (s-['4','7']==[]&&s[0...n/2].sort==s[n/2...n].sort)?'YES':'NO'
+t = gets.chomp
+s = t.split('').map(&:to_i)
+puts (t=~/^(4|7)+$/ && s[0...n/2].sort == s[n/2...n].sort)?'YES':'NO'
