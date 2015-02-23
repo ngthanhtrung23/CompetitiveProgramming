c35ec2938a16bfdbb699bc31fbee29da922e12b0
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-23 10:49:00 +0800

    Add solution for problem `B - Squares` Link: https://codeforces.com/contest/263/problem/B Tags: Ref: https://codeforces.com/contest/263/submission/9964929

--- codeforces/263/B.rb
+++ codeforces/263/B.rb
@@ -1,2 +1,7 @@
-n,k=gets.split.map(&:to_i)
-puts k>n ? -1 : "#{gets.split.map(&:to_i).sort[-k]} 0"
+n, k = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i).sort
+if k > n
+  p -1
+else
+  print a[n-k],' ',a[n-k]
+end
