7ccd5d20ed547745d50c26c3dfd1c9ba7361a469
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-02-26 00:09:00 +0800

    Add solution for problem `A - Levko and Table` Link: https://codeforces.com/contest/361/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/361/submission/10022263

--- codeforces/361/A.cpp
+++ codeforces/361/A.cpp
@@ -1,25 +1,28 @@
 
 #include <bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
-#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
 
-#define DEBUG(X) { cout << #X << " = " << X << endl; }
-#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
-#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
 
 #define sqr(x) ((x) * (x))
-#define ll long long
-#define SZ(x) ((int) (x).size())
 using namespace std;
 
 int main() {
-    ios :: sync_with_stdio(0); cin.tie(0);
     int n, k;
     while (cin >> n >> k) {
         FOR(i,1,n) {
-            FOR(j,1,n) if (i == j) cout << k << ' '; else cout << 0 << ' ';
-            cout << '\n';
+            FOR(j,1,n) {
+                if (i == j) cout << k - (n-1) << ' ';
+                else cout << 1 << ' ';
+            }
+            cout << endl;
         }
     }
+    return 0;
 }
