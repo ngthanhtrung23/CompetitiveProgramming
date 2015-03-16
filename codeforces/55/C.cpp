256de296a42a0701722029654bc761cd9486ae68
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-03-17 03:03:00 +0800

    Add solution for problem `C - Pie or die` Link: https://codeforces.com/contest/55/problem/C Tags: games, *1900 Ref: https://codeforces.com/contest/55/submission/10304412

---
+++ codeforces/55/C.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int main() {
+    int m, n, k;
+    while (cin >> m >> n >> k) {
+        bool ok = false;
+        const int d = 5;
+        while (k--) {
+            int u, v; cin >> u >> v;
+            if (u <= d || v <= d || u >= m - d + 1 || v >= n - d + 1)
+                ok = true;
+        }
+        cout << (ok ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
