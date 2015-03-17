7417b18fcf4657db76795e16b0f2ce9e33aee064
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-03-18 04:10:00 +0800

    Add solution for problem `A - Playing with Paper` Link: https://codeforces.com/contest/527/problem/A Tags: implementation, math, *1100 Ref: https://codeforces.com/contest/527/submission/10329242

---
+++ codeforces/527/A.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+long long get(long long a, long long b) {
+    if (a == 0 || b == 0) return 0;
+    if (b > a) return get(b, a);
+    return a / b + get(b, a % b);
+}
+
+int main() {
+    long long a, b;
+    while (cin >> a >> b) cout << get(a, b) << endl;
+    return 0;
+}
