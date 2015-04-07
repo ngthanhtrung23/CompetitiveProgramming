223ed993d490c95fbcf7a0250b6f97a1b4745ce5
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-04-08 02:52:00 +0800

    Add solution for problem `C - Ladder` Link: https://codeforces.com/contest/279/problem/C Tags: dp, implementation, two pointers, *1700 Ref: https://codeforces.com/contest/279/submission/10624488

--- codeforces/279/C.cpp
+++ codeforces/279/C.cpp
@@ -1,100 +1,43 @@
-#define _GLIBCXX_DEBUG
-#include <sstream>
-#include <iomanip>
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <string>
-#include <deque>
-#include <complex>
+
+#include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
-#define FORN(i,a,b) for(int i=(a),_b=(b);i<_b;i++)
-#define DOWN(i,a,b) for(int i=a,_b=(b);i>=_b;i--)
-#define SET(a,v) memset(a,v,sizeof(a))
-#define sqr(x) ((x)*(x))
-#define ll long long
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define dec __dec
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp) { \
-        if (REACHEOF) return 0;\
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
-const long double PI = acos((long double) -1.0);
 const int MN = 100111;
-
-int n, q, a[MN], inc[MN], giam[MN];
+int n, q, a[MN], inc[MN], dec[MN];
 
 int main() {
-    while (scanf("%d%d", &n, &q) == 2) {
-        FOR(i,1,n) scanf("%d", &a[i]);
-        int i = 1;
-        while (i <= n) {
-            int j = i;
-            while (j < n && a[j+1] >= a[j]) ++j;
-            FOR(x,i,j) inc[x] = j;
-            i = j + 1;
-        }
-
-        i = 1;
-        while (i <= n) {
-            int j = i;
-            while (j < n && a[j+1] <= a[j]) ++j;
-            FOR(x,i,j) giam[x] = j;
-            i = j + 1;
-        }
-
-        // PR(a, n);
-        // PR(inc, n);
-        // PR(giam, n);
-
-        while (q--) {
-            int u, v; scanf("%d%d", &u, &v);
-            if (inc[u] >= v) puts("Yes");
+    ios :: sync_with_stdio(false);
+    while (cin >> n >> q) {
+        FOR(i,1,n) cin >> a[i];
+        FORD(i,n,1) {
+            if (i == n) inc[i] = dec[i] = n;
             else {
-                int x = inc[u];
-                if (giam[x] >= v) puts("Yes");
-                else puts("No");
+                if (a[i] <= a[i+1]) inc[i] = inc[i+1];
+                else inc[i] = i;
+
+                if (a[i] >= a[i+1]) dec[i] = dec[i+1];
+                else dec[i] = i;
             }
+        }
+        while (q--) {
+            int l, r; cin >> l >> r;
+            int u = inc[l];
+            if (u >= r) cout << "Yes";
+            else if (dec[u] >= r) cout << "Yes";
+            else cout << "No";
+            cout << '\n';
         }
     }
     return 0;
