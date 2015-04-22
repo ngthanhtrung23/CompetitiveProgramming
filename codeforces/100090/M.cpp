e3cf26b980e2340a22eb719ba589db3e1fecfba3
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-04-22 20:13:00 +0800

    Add solution for problem `M - Jumping along the Hummocks` Link: https://codeforces.com/gym/100090/problem/M Tags: Ref: https://codeforces.com/gym/100090/submission/10822184

---
+++ codeforces/100090/M.cpp
@@ -0,0 +1,42 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+const int MN = 200111;
+
+int a[MN], f[MN], last[MN];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+
+        memset(last, 0, sizeof last);
+        FOR(i,1,n) {
+            if (i == 1) {
+                f[i] = 0;
+                last[a[i]] = i;
+                continue;
+            }
+            f[i] = f[i-1] + 1;
+            if (last[a[i]]) {
+                f[i] = min(f[i], f[ last[a[i]] ] + 1);
+            }
+            last[a[i]] = i;
+        }
+        cout << f[n] << endl;
+    }
+    return 0;
+}
