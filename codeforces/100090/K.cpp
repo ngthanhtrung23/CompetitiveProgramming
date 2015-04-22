d1357d1909290e541f060a4d2a9896017a120e2c
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-04-22 20:05:00 +0800

    Add solution for problem `K - Bracket Sequence` Link: https://codeforces.com/gym/100090/problem/K Tags: Ref: https://codeforces.com/gym/100090/submission/10822055

---
+++ codeforces/100090/K.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+string s;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    while (cin >> s) {
+        int sum = 0;
+        int can = true;
+
+        for(char c : s) {
+            if (c == '(') ++sum;
+            else {
+                --sum;
+                if (sum < 0) can = false;
+            }
+        }
+
+        if (!can) cout << "IMPOSSIBLE" << endl;
+        else {
+            cout << s;
+            REP(turn,sum) cout << ')';
+            cout << endl;
+        }
+    }
+    return 0;
+}
