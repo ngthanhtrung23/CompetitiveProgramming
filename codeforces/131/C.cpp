0eb2b0bc5c7a3aee87ac3f10fc32ca36eb12a29a
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-05-13 18:57:00 +0800

    Add solution for problem `C - The World is a Theatre` Link: https://codeforces.com/contest/131/problem/C Tags: combinatorics, math, *1400 Ref: https://codeforces.com/contest/131/submission/11087496

---
+++ codeforces/131/C.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+long long c[111][111];
+
+int main() {
+    ios :: sync_with_stdio(false);
+    FOR(i,0,100) {
+        c[i][0] = 1;
+        FOR(j,1,i) c[i][j] = c[i-1][j-1] + c[i-1][j];
+    }
+    int a, b, x;
+    while (cin >> a >> b >> x) {
+        long long res = 0;
+        FOR(u,4,a) FOR(v,1,b) if (u + v == x)
+            res += c[a][u] * c[b][v];
+        cout << res << endl;
+    }
+    return 0;
+}
+
