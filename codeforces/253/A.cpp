3db7c76d13f576eb8adbf90db3c0aa9d9400c71f
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-05-13 18:38:00 +0800

    Add solution for problem `A - Boys and Girls` Link: https://codeforces.com/contest/253/problem/A Tags: greedy, *1100 Ref: https://codeforces.com/contest/253/submission/11087355

---
+++ codeforces/253/A.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+    ios :: sync_with_stdio(false);
+    int g, b;
+    while (cin >> b >> g) {
+        char start = 'G';
+        if (b > g) start = 'B';
+
+        while (b || g) {
+            if (start == 'G' && g) putchar(start), --g;
+            if (start == 'B' && b) putchar(start), --b;
+
+            start = 'G' + 'B' - start;
+        }
+        puts("");
+    }
+    return 0;
+}
