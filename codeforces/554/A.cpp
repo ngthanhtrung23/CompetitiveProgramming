8cf65fe4c542160e2391ab23e97864869ebfe9fd
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-06-25 03:22:00 +0800

    Add solution for problem `A - Kyoya and Photobooks` Link: https://codeforces.com/contest/554/problem/A Tags: brute force, math, strings, *900 Ref: https://codeforces.com/contest/554/submission/11750845

---
+++ codeforces/554/A.cpp
@@ -0,0 +1,31 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    string s;
+    while (cin >> s) {
+        set<string> all;
+        FOR(i,0,s.length()) {
+            FOR(c,'a','z') {
+                string t = s;
+                string tmp = ""; tmp += c;
+                t.insert(i, tmp);
+                all.insert(t);
+            }
+        }
+        cout << all.size() << endl;
+    }
+}
