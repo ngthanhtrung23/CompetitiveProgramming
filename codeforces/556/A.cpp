c9ddf2cc0e875435f57bdd3d0dbc2c2e477383c7
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-07-02 00:39:00 +0800

    Add solution for problem `A - Case of the Zeros and Ones` Link: https://codeforces.com/contest/556/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/556/submission/11885159

---
+++ codeforces/556/A.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    string s;
+    int n;
+    while (cin >> n >> s) {
+        stack<char> st;
+        for(char c : s) {
+            if (!st.empty() && c != st.top()) st.pop();
+            else st.push(c);
+        }
+        cout << st.size() << endl;
+    }
+}
