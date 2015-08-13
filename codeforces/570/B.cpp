d57ff5bd1d19d7d42cf34a33df6b3b1d077693ea
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-08-14 00:35:00 +0800

    Add solution for problem `B - Simple Game` Link: https://codeforces.com/contest/570/problem/B Tags: constructive algorithms, games, greedy, implementation, math, *1300 Ref: https://codeforces.com/contest/570/submission/12494966

---
+++ codeforces/570/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n, m;
+    while (cin >> n >> m) {
+        if (n == 1) cout << 1 << endl;
+        else {
+            int left = m - 1, right = n - m;
+            if (m > 1 && left >= right) cout << m-1 << endl;
+            else cout << m + 1 << endl;
+        }
+    }
+}
