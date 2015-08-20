0e8bf4ee7216c08443bcb41a08959ba815f5743f
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-08-21 02:39:00 +0800

    Add solution for problem `D - Little Jumper` Link: https://codeforces.com/gym/100197 Tags: Ref: https://codeforces.com/gym/100197/submission/12619657

--- codeforces/100197/D.cpp
+++ codeforces/100197/D.cpp
@@ -17,35 +17,25 @@ using namespace std;
 double b1, t1, b2, t2, l, ds, df, g, vxvy, vxvy2;
 
 double get_vmax_vy_D(double vy2, double D) {
-    return sqrt(vy2 + vxvy2 / vy2);
+    return vy2 + vxvy2 / vy2;
 }
 
 double get_vmax(double ds, double low, double high, double D) {
-    double c = ds * (1 - ds / D);
-    low /= c;
-    high /= c;
-    if (low > 1) {
-        return sqrt(g * D / 2 * (low + 1 / low));
-    }
-    else if (high < 1) {
-        return sqrt(g * D / 2 * (high + 1 / high));
-    }
-    else return sqrt(g * D);
-//    double t1 = ds / D;
-//    double t3 = 2.0 / g * (t1 - sqr(t1));
-//
-//    vxvy = 0.5 * g * D;
-//    vxvy2 = vxvy * vxvy;
-//
-//    double L = low / t3;
-//    double R = high / t3;
-//
-//    double vmax = get_vmax_vy_D(L, D);
-//    vmax = min(vmax, get_vmax_vy_D(R, D));
-//
-//    double vy2 = vxvy;
-//    if (L < vy2 && vy2 < R) vmax = min(vmax, get_vmax_vy_D(vy2, D));
-//    return vmax;
+    double t1 = ds / D;
+    double t3 = 2.0 / g * (t1 - sqr(t1));
+
+    vxvy = 0.5 * g * D;
+    vxvy2 = vxvy * vxvy;
+
+    double L = low / t3;
+    double R = high / t3;
+
+    double vmax = get_vmax_vy_D(L, D);
+    vmax = min(vmax, get_vmax_vy_D(R, D));
+
+    double vy2 = vxvy;
+    if (L < vy2 && vy2 < R) vmax = min(vmax, get_vmax_vy_D(vy2, D));
+    return vmax;
 }
 
 double get_vmax(double D) {
@@ -54,18 +44,18 @@ double get_vmax(double D) {
 
 double ternary(double l, double r) {
     double res = 1e100;
-    REP(turn,100) {
+    REP(turn,30) {
         double x1 = (l + l + r) / 3.0;
         double x2 = (l + r + r) / 3.0;
         double y1 = get_vmax(x1);
-        double y2 = get_vmax(x2);
-        
+        double y2 = get_vmax(y2);
+
         res = min(res, min(y1, y2));
 
         if (y1 < y2) r = x2;
         else l = x1;
     }
-    return get_vmax(l);
+    return res;
 }
 
 int main() {
@@ -75,8 +65,38 @@ int main() {
     while (scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &b1, &t1, &b2, &t2, &l, &ds, &df, &g) == 8) {
         double L = ds, R = ds + l;
         double res = 1e100;
-        res = ternary(L, R);
+        REP(outer,20) {
+            double best = 1e100;
+            double worst = -1e100;
+            double savemid;
+            int K = 100;
+            double range = (R - L) / K;
+
+            REP(turn,K) {
+                double l = L + range * turn;
+                double r = l + range;
+                double mid = (l + r) / 2.0;
+
+                double cur = get_vmax(mid);
+                worst = max(worst, cur);
+                if (cur < best) {
+                    best = cur;
+                    savemid = mid;
+                }
+
+                l += range;
+                r += range;
+            }
+            if (fabs(best - worst) < 1e-6) break;
+            range *= 10;
+            L = savemid - range;
+            R = savemid + range;
+            if (L < ds) L = ds;
+            if (R > ds + l) R = ds + l;
+
+            res = min(res, best);
+        }
+        res = sqrt(res);
         printf("%.6f\n", res);
     }
 }
-
