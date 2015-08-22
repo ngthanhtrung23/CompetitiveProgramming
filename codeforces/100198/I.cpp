e072f947def0216f29377d2a5b7782cc962fd186
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-08-22 22:14:00 +0800

    Add solution for problem `I - Two Cylinders` Link: https://codeforces.com/gym/100198 Tags: Ref: https://codeforces.com/gym/100198/submission/12643556

---
+++ codeforces/100198/I.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double PI = acos(-1.0);
+const int INTERVAL = int(1e6);
+
+double r1, r2;
+
+double f(double x)
+{
+  return cos(x) * cos(x) * sqrt(r1 * r1 - r2 * r2 + r2 * r2 * cos(x) * cos(x));
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  freopen("twocyl.in", "r", stdin);
+  freopen("twocyl.out", "w", stdout);
+  cin >> r1 >> r2;
+  if (r2 > r1) swap(r1, r2);
+
+  double ans = 0;
+  for (int i = 0; i < INTERVAL; i++)
+  {
+    double a = 1. * i / INTERVAL * PI / 2;
+    double b = 1. * (i + 1) / INTERVAL * PI / 2;
+    ans += (b - a) / 2 * (f(a) + f(b));
+  }
+
+  cout << fixed << setprecision(6) << ans * 8 * r2 * r2 << endl;
+}
