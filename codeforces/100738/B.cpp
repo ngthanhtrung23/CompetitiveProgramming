fcb8cb383c978511af0e177d5fa2a71fc9754ea9
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-08-30 18:06:00 +0800

    Add solution for problem `B - Board with lights and switches` Link: https://codeforces.com/gym/100738 Tags: Ref: https://codeforces.com/gym/100738/submission/12774115

---
+++ codeforces/100738/B.cpp
@@ -0,0 +1,36 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    ll n;
+    while (cin >> n) {
+        bool ok = false;
+        FOR(turn,1,100) {
+            ll left = n / 2;
+            ll right = n - left;
+
+            n = left * right;
+            if (n >= 1e9) {
+                cout << turn << endl;
+                ok = true;
+                break;
+            }
+        }
+        if (!ok) cout << -1 << endl;
+    }
+}
+
