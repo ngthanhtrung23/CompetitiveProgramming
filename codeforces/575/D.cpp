2c58f7a34e899806e09d121457f82be7c0d433a7
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-09-06 22:55:00 +0800

    Add solution for problem `D - Tablecity` Link: https://codeforces.com/contest/575/problem/D Tags: constructive algorithms, implementation, *1700 Ref: https://codeforces.com/contest/575/submission/12873979

---
+++ codeforces/575/D.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    cout << 2000 << endl;
+    FOR(i,1,1000) cout << i << ' ' << 1 << ' ' << i << ' ' << 2 << endl;
+    FORD(i,1000,1) cout << i << ' ' << 1 << ' ' << i << ' ' << 2 << endl;
+}
