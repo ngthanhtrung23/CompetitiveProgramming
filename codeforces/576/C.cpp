e9b0112679beb2082154d1e07b46cbc164ba7ba7
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-09-11 04:32:00 +0800

    Add solution for problem `C - Points on Plane` Link: https://codeforces.com/contest/576/problem/C Tags: constructive algorithms, divide and conquer, geometry, greedy, sortings, *2100 Ref: https://codeforces.com/contest/576/submission/12947891

--- codeforces/576/C.cpp
+++ codeforces/576/C.cpp
@@ -1,38 +1,44 @@
-#include<bits/stdc++.h>
-#define MAX   1000100
-#define FOR(i,a,b) for (int i=(a),_b=(b);i<=_b;i=i+1)
-#define REP(i,n) for (int i=0,_n=(n);i<_n;i=i+1)
-#define FORE(i,v) for (__typeof((v).begin()) i=(v).begin();i!=(v).end();i++)
-#define ALL(v) (v).begin(),(v).end()
-#define fi   first
-#define se   second
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
 using namespace std;
-const int d=1000;
-pair<int,int> a[MAX];
-vector<int> block[MAX];
+
 int n;
-bool compLeft(const int &x,const int &y) {
-    return (a[x].se<a[y].se);
+struct Hieu {
+    int x, y;
+    int id;
+
+    void read() { scanf("%d%d", &x, &y); }
+} a[1000111];
+
+const int C = 1011;
+
+bool operator < (const Hieu& a, const Hieu& b) {
+    if (a.x / C != b.x / C) return a.x / C < b.x / C;
+    else return a.y < b.y;
 }
-bool compRight(const int &x,const int &y) {
-    return (a[x].se>a[y].se);
-}
-void init(void) {
-    scanf("%d",&n);
-    FOR(i,1,n) scanf("%d%d",&a[i].fi,&a[i].se);
-    FOR(i,1,n) block[a[i].fi/d].push_back(i);
-}
-void process(void) {
-    bool mode=true;
-    REP(i,MAX) if (!block[i].empty()) {
-        sort(ALL(block[i]),mode?compLeft:compRight);
-        mode^=1;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (scanf("%d", &n) == 1) {
+        FOR(i,1,n) {
+            a[i].read();
+            a[i].id = i;
+        }
+
+        sort(a+1, a+n+1);
+        FOR(i,1,n) printf("%d ", a[i].id); puts("");
     }
-    REP(i,MAX) if (!block[i].empty()) FORE(it,block[i]) printf("%d ",*it);
-    printf("\n");
-}
-int main(void) {
-    init();
-    process();
-    return 0;
 }
