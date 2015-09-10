48d745c9d1a1a8f1706eae92b2e654264e4c1ecc
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-09-10 16:38:00 +0800

    Add solution for problem `B - Depression` Link: https://codeforces.com/contest/80/problem/B Tags: geometry, math, *1200 Ref: https://codeforces.com/contest/80/submission/12920142

---
+++ codeforces/80/B.cpp
@@ -0,0 +1,26 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int h, m;
+    while (scanf("%d:%d", &h, &m) == 2) {
+        int min = m * 6;
+        int hour = (h * 60 + m) % 720;
+
+        cout << hour / 2.0 << ' ' << min << '\n';
+    }
+}
