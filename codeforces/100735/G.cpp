c9968a5adf0190fc7ee610fc4914e37e4fd733fb
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-09-15 19:13:00 +0800

    Add solution for problem `G - LCS Revised` Link: https://codeforces.com/gym/100735 Tags: Ref: https://codeforces.com/gym/100735/submission/13006158

---
+++ codeforces/100735/G.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    string s;
+    while (cin >> s) {
+        int c0 = 0, c1 = 0;
+        for(char c : s) {
+            if (c == '0') ++c0;
+            else ++c1;
+        }
+        cout << min(c0, c1) << endl;
+    }
+}
