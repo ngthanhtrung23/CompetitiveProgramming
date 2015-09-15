4c1a54f605c27b0d64f0e5b9003eab5c6732ad99
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-09-15 21:42:00 +0800

    Add solution for problem `H - Words from cubes` Link: https://codeforces.com/gym/100735 Tags: Ref: https://codeforces.com/gym/100735/submission/13008763

---
+++ codeforces/100735/H.cpp
@@ -0,0 +1,96 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+using namespace std;
+
+const int inf = 1000111;
+struct Matching {
+    int n;
+    vector<int> matchL, matchR, dist;
+    vector<bool> seen;
+    vector< vector<int> > ke;
+
+    Matching(int n) : n(n), matchL(n+1), matchR(n+1), dist(n+1), seen(n+1, false), ke(n+1) {}
+
+    void addEdge(int u, int v) {
+        ke[u].push_back(v);
+    }
+
+    bool bfs() {
+        queue<int> qu;
+        for(int u = 1; u <= n; ++u)
+            if (!matchL[u]) {
+                dist[u] = 0;
+                qu.push(u);
+            } else dist[u] = inf;
+        dist[0] = inf;
+
+        while (!qu.empty()) {
+            int u = qu.front(); qu.pop();
+            for(__typeof(ke[u].begin()) v = ke[u].begin(); v != ke[u].end(); ++v) {
+                if (dist[matchR[*v]] == inf) {
+                    dist[matchR[*v]] = dist[u] + 1;
+                    qu.push(matchR[*v]);
+                }
+            }
+        }
+        return dist[0] != inf;
+    }
+
+    bool dfs(int u) {
+        if (u) {
+            for(__typeof(ke[u].begin()) v = ke[u].begin(); v != ke[u].end(); ++v)
+                if (dist[matchR[*v]] == dist[u] + 1 && dfs(matchR[*v])) {
+                    matchL[u] = *v;
+                    matchR[*v] = u;
+                    return true;
+                }
+            dist[u] = inf;
+            return false;
+        }
+        return true;
+    }
+
+    int match() {
+        int res = 0;
+        while (bfs()) {
+            for(int u = 1; u <= n; ++u)
+                if (!matchL[u])
+                    if (dfs(u)) ++res;
+        }
+        return res;
+    }
+};
+
+string s;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (cin >> s >> n) {
+        int k = max(n, (int) s.length());
+        s = " " + s;
+
+        Matching mat(k);
+        FOR(i,1,n) {
+            REP(t,6) {
+                char c; cin >> c;
+                FOR(j,1,s.length()-1)
+                    if (c == s[j])
+                        mat.addEdge(i, j);
+            }
+        }
+        cout << (mat.match() == s.length()-1 ? "YES" : "NO") << endl;
+    }
+}
