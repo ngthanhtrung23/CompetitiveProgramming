c17d1b7e39a0cd555fbbada1f9c395ea1b2b79ad
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-09-19 22:59:00 +0800

    Add solution for problem `A - Nonoptimal Assignments` Link: https://codeforces.com/gym/100204 Tags: Ref: https://codeforces.com/gym/100204/submission/13103584

--- codeforces/100204/A.cpp
+++ codeforces/100204/A.cpp
@@ -12,7 +12,7 @@ int main()
   for (int i = 0; i < n; ++i)
   {
     for (int j = 0; j < n; j++)
-      if (i <= j) cout << 1 << ' ';
+      if (i <= j) cout << i << ' ';
       else cout << 0 << ' ';
     cout << endl;
   }
