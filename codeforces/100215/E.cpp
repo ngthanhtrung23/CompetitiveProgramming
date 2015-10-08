0729aea9311f3c7caa957b42088c25ecf4899834
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-10-09 00:10:00 +0800

    Add solution for problem `E - Fool's Game` Link: https://codeforces.com/gym/100215 Tags: Ref: https://codeforces.com/gym/100215/submission/13496952

--- codeforces/100215/E.cpp
+++ codeforces/100215/E.cpp
@@ -1,41 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Matching {
-    int n;
-    vector< vector<int> > ke;
-    vector< bool > seen;
-    vector< int > matchL, matchR;
+struct HopcroftKarp
+{
+  vector <int> leftMatch, rightMatch, dist, cur;
+  vector < vector <int> > a;
+  int m, n;
 
-    Matching(int n) : n(n), ke(n), seen(n, false), matchL(n, -1), matchR(n, -1) {
+  HopcroftKarp() {}
+  HopcroftKarp(int m, int n)
+  {
+    this -> m = m;
+    this -> n = n;
+    a = vector < vector <int> >(m);
+    leftMatch = vector <int>(n, -1);
+    rightMatch = vector <int>(m, -1);
+    dist = vector <int>(m, -1);
+    cur = vector <int>(m, -1);
+  }
+
+  void addEdge(int x, int y)
+  {
+    a[x].push_back(y);
+  }
+
+  int bfs()
+  {
+    int found = 0;
+    queue <int> q;
+    for (int i = 0; i < m; i++)
+      if (rightMatch[i] < 0) dist[i] = 0, q.push(i);
+      else dist[i] = -1;
+
+    while (!q.empty())
+    {
+      int x = q.front(); q.pop();
+      for (int i = 0; i < int(a[x].size()); i++)
+      {
+        int y = a[x][i];
+        if (leftMatch[y] < 0) found = 1;
+        else
+          if (dist[leftMatch[y]] < 0)
+            dist[leftMatch[y]] = dist[x] + 1, q.push(leftMatch[y]);
+      }
     }
 
-    void addEdge(int u, int v) {
-        ke[u].push_back(v);
-    }
+    return found;
+  }
 
-    bool bpm(int u) {
-        for(__typeof(ke[u].begin()) v = ke[u].begin(); v != ke[u].end(); ++v) {
-            if (seen[*v]) continue;
-            seen[*v] = true;
-
-            if (matchR[*v] < 0 || bpm(matchR[*v])) {
-                matchL[u] = *v;
-                matchR[*v] = u;
-                return true;
-            }
-        }
-        return false;
+  int dfs(int x)
+  {
+    for (; cur[x] < int(a[x].size()); cur[x]++)
+    {
+      int y = a[x][cur[x]];
+      if (leftMatch[y] < 0 || (dist[leftMatch[y]] == dist[x] + 1 && dfs(leftMatch[y])))
+      {
+        leftMatch[y] = x; rightMatch[x] = y;
+        return 1;
+      }
     }
+    return 0;
+  }
 
-    int match() {
-        int res = 0;
-        for(int i = 0; i < n; ++i) {
-            for(int i = 0; i < n; ++i) seen[i] = false;
-            if (bpm(i)) ++res;
-        }
-        return res;
+  int maxMatching()
+  {
+    int match = 0;
+    while (bfs())
+    {
+      for (int i = 0; i < m; i++) cur[i] = 0;
+      for (int i = 0; i < m; i++)
+        if (rightMatch[i] < 0) match += dfs(i);
     }
+    return match;
+  }
 };
 
 int n, onTable;
@@ -65,12 +102,12 @@ int ok(int mask)
     for (int j = 0; j < n; j++)
       if (mask >> a[i][j].first & 1)
         b[i].push_back(a[i][j]);
-  Matching hk(max(b[0].size(), b[1].size()));
+  HopcroftKarp hk(b[0].size(), b[1].size());
   for (int i = 0; i < b[0].size(); i++)
     for (int j = 0; j < b[1].size(); j++)
       if (canCover(b[0][i], b[1][j]))
         hk.addEdge(i, j);
-  return hk.match() == b[0].size();
+  return hk.maxMatching() == b[0].size();
 }
 
 int main()
