7df5ba38dce0236897b3acf34c22d56bd3d6bf62
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-11-03 04:02:00 +0800

    Add solution for problem `E - Blood Cousins` Link: https://codeforces.com/contest/208/problem/E Tags: binary search, data structures, dfs and similar, trees, *2100 Ref: https://codeforces.com/contest/208/submission/14033363

--- codeforces/208/E.cpp
+++ codeforces/208/E.cpp
@@ -1,138 +1,100 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <map>
-#include <cstring>
-#include <string>
-#include <queue>
 
-#define FOR(i,a,b) for(int i=(a),_b=(b); i <= _b; ++i)
-#define FORD(i,a,b) for(int i=(a), _b=(b); i >= _b; --i)
-#define REP(i,a) for(int i = 0, _a=(a); i < _a; ++i)
-#define MP make_pair
-#define PB push_back
-#define F first
-#define S second
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(X) ((int) ((X).size()))
 using namespace std;
 
-const int MN = 400111;
-
-int n, father[22][MN];
+const int MN = 100111;
+int n, q, cur;
 vector<int> ke[MN];
-vector<int> list[MN];
-int first[MN], last[MN], a[MN], now, h[MN];
-int cnt[MN];
-int res[MN], m;
+int father[20][MN], h[MN], getIn[MN], getOut[MN];
+vector<int> ls[MN];
 
-struct Query {
-    int u;
-    int ind;
-
-    Query(int u, int ind) : u(u), ind(ind) {}
-};
-vector<Query> query[MN];
-
-#define _(u) ((u) & (-(u)))
-
-int get(int x) {
-    int res = 0;
-    while (x > 0) {
-        res += cnt[x];
-        x -= _(x);
-    }
-    return res;
-}
-
-int update(int x, int k) {
-    while (x <= 400000) {
-        cnt[x] += k;
-        x += _(x);
-    }
-}
-
-void inp() {
-    FOR(i,1,n) {
-        int u; scanf("%d", &u);
-        ke[u].PB(i);
-    }
+void init() {
+    FOR(i,1,n) ke[i].clear();
+    memset(father, 0, sizeof father);
 }
 
 void dfs(int u) {
-    list[h[u]].PB(u);
-    first[u] = ++now;
-    REP(i,ke[u].size()) {
-        int v = ke[u][i];
-        father[0][v] = u;
+    ls[h[u]].push_back(u);
+    getIn[u] = ++cur;
+    for(int v : ke[u]) {
         h[v] = h[u] + 1;
         dfs(v);
     }
-    last[u] = ++now;
-}
-
-void initLCA() {
-    FOR(i,1,19) FOR(u,1,n) {
-        if (father[i-1][u] == 0) father[i][u] = 0;
-        else father[i][u] = father[i-1][father[i-1][u]];
-    }
-}
-
-#define TWO(x) (1<<(x))
-#define CONTAIN(S,x) (S & TWO(x))
-
-;
-int jump(int u, int k) {
-    if (k == 0) return u;
-    int res = u;
-    REP(i,20) if (CONTAIN(k,i)) {
-        res = father[i][res];
-    }
-    return res;
-}
-
-void solve() {
-    scanf("%d", &m);
-    FOR(i,1,m) {
-        int v, p; scanf("%d%d", &v, &p);
-        int fp = jump(v, p);
-        if (fp == 0) {
-            res[i] = 0;
-        }
-        else {
-            int fp1 = jump(v, p-1);
-            query[h[v]].PB(Query(fp, i));
-            query[h[v]].PB(Query(fp1, -i));
-        }
-    }
-    FOR(h,1,n) {
-        REP(i,list[h].size()) {
-            int u = list[h][i];
-            update(first[u], 1);
-        }
-        REP(i,query[h].size()) {
-            int u = query[h][i].u;
-            int ind = query[h][i].ind;
-            int v = get(last[u]) - get(first[u]-1);
-            if (ind > 0) res[ind] += v - 1;
-            
-        }
-        REP(i,list[h].size()) {
-            int u = list[h][i];
-            update(first[u], -1);
-        }
-    }
-    FOR(i,1,m) printf("%d ", res[i]); puts("");
+    getOut[u] = ++cur;
 }
 
 int main() {
-    scanf("%d", &n);
-    inp();
-    dfs(0);
-    initLCA();
-    solve();
-    return 0;
+    ios :: sync_with_stdio(false);
+    while (scanf("%d", &n) == 1) {
+        init();
+        FOR(i,1,n) {
+            scanf("%d", &father[0][i]);
+            ke[father[0][i]].push_back(i);
+        }
+        cur = 0;
+        FOR(i,1,n) if (!father[0][i]) {
+            h[i] = 1;
+            dfs(i);
+        }
+        FOR(t,1,19) {
+            FOR(u,1,n) {
+                int v = father[t-1][u];
+                if (v) father[t][u] = father[t-1][v];
+            }
+        }
+        int q; scanf("%d", &q);
+        while (q--) {
+            int u, p; scanf("%d%d", &u, &p);
+            int f = u;
+            FORD(i,19,0)
+                if ((p >> i) & 1) {
+                    f = father[i][f];
+                }
+
+            if (f == 0) {
+                printf("%d ", 0);
+            }
+            else {
+                int x = h[f] + p;
+
+                int l = 0, r = SZ(ls[x])-1; int res = r;
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (getIn[ ls[x][mid] ] >= getIn[f]) {
+                        res = mid;
+                        r = mid - 1;
+                    }
+                    else l = mid + 1;
+                }
+                int firstId = res;
+
+                l = 0, r = SZ(ls[x]) - 1, res = 0;
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (getOut[ ls[x][mid] ] <= getOut[f]) {
+                        res = mid;
+                        l = mid + 1;
+                    }
+                    else r = mid - 1;
+                }
+                int lastId = res;
+
+                printf("%d ", lastId - firstId);
+            }
+        }
+    }
 }
+
