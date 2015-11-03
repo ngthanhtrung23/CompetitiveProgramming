180864e8e847bd0d321219ac45b1118436e1cea5
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-11-03 15:32:00 +0800

    Add solution for problem `A - Sereja and Bottles` Link: https://codeforces.com/contest/315/problem/A Tags: brute force, *1400 Ref: https://codeforces.com/contest/315/submission/14038287

--- codeforces/315/A.cpp
+++ codeforces/315/A.cpp
@@ -1,55 +1,32 @@
+
 #include <bits/stdc++.h>
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
 #define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
 
-#define DEBUG(x) cout << #x << " = "; cout << x << endl;
-#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
-#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(X) ((int) ((X).size()))
 using namespace std;
 
-//Buffer reading
-int INP,AM,REACHEOF;
-const int BUFSIZE = (1<<12) + 17;
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp && !REACHEOF) { \
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
-//End of buffer reading
-
 int n, a[111], b[111];
-bool good[111];
 
 int main() {
+    ios :: sync_with_stdio(false);
     while (cin >> n) {
-        memset(good, false, sizeof good);
         FOR(i,1,n) cin >> a[i] >> b[i];
-
-        FOR(i,1,n)
-            FOR(j,1,n) if (j != i && a[j] == b[i]) {
-                good[j] = true;
-            }
-
         int res = 0;
-        FOR(i,1,n)
-            if (!good[i]) ++res;
+        FOR(i,1,n) {
+            bool can = false;
+            FOR(j,1,n) if (j != i && b[j] == a[i]) can = true;
+            if (!can) ++res;
+        }
         cout << res << endl;
     }
-    return 0;
 }
