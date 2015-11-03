17d4d84c2a94bef08ab0b6362535b09f938f5146
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-11-03 15:18:00 +0800

    Add solution for problem `A - Snow Footprints` Link: https://codeforces.com/contest/298/problem/A Tags: greedy, implementation, *1300 Ref: https://codeforces.com/contest/298/submission/14038134

--- codeforces/298/A.cpp
+++ codeforces/298/A.cpp
@@ -1,37 +1,52 @@
-#include <iostream>
-#include <cstdio>
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(X) ((int) ((X).size()))
 using namespace std;
 
-char a[1011];
-
 int main() {
-	int n;
-	while (scanf("%d\n", &n) == 1) {
-		gets(a);
-		bool allL = true, allR = true;
-		int firstL = -1, lastL = -1, firstR = -1, lastR = -1;
-
-		for(int i = 0; i < n; ++i) {
-			if (a[i] == 'L') {
-				allR = false;
-				if (firstL < 0) firstL = i+1;
-				lastL = i+1;
-			}
-			if (a[i] == 'R') {
-				allL = false;
-				if (firstR < 0) firstR = i+1;
-				lastR = i+1;
-			}
-		}
-
-		if (allL) {
-			cout << lastL << ' ' << firstL-1 << endl;
-		}
-		else if (allR) {
-			cout << firstR << ' ' << lastR + 1 << endl;
-		}
-		else {
-			cout << firstR << ' ' << firstL - 1 << endl;
-		}
-	}
+    ios :: sync_with_stdio(false);
+    int n;
+    string s;
+    while (cin >> n >> s) {
+        int firstR = -1, lastR = -1;
+        int firstL = -1, lastL = -1;
+        REP(i,n) {
+            if (s[i] == 'R') {
+                if (firstR < 0) firstR = i;
+                lastR = i;
+            }
+            if (s[i] == 'L') {
+                if (firstL < 0) firstL = i;
+                lastL = i;
+            }
+        }
+        int s, t;
+        if (firstR < 0) {
+            // all L
+            s = lastL;
+            t = firstL - 1;
+        }
+        else if (firstL < 0) {
+            // all R
+            s = firstR;
+            t = lastR + 1;
+        }
+        else {
+            s = firstR;
+            t = lastR;
+        }
+        cout << s+1 << ' ' << t+1 << endl;
+    }
 }
