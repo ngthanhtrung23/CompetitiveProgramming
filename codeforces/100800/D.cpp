1292f9cd5c794a7c1c7e0aaf55361a760ea8f789
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-11-06 01:50:00 +0800

    Add solution for problem `D - Slant Drilling` Link: https://codeforces.com/gym/100800 Tags: Ref: https://codeforces.com/gym/100800/submission/14093847

---
+++ codeforces/100800/D.cpp
@@ -0,0 +1,173 @@
+
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(X) ((int) ((X).size()))
+#define double long double
+using namespace std;
+
+#define EPS 1e-6
+
+double PI = acos(-1.0);
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+inline int cmp(double a, double b) {
+    return (a < b - EPS) ? -1 : ((a > b + EPS) ? 1 : 0);
+}
+
+struct Point {
+    double x, y;
+    Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+
+    Point operator + (Point a) { return Point(x+a.x, y+a.y); }
+    Point operator - (Point a) { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) { return Point(x*k, y*k); }
+    Point operator / (double k) { return Point(x/k, y/k); }
+
+    double operator * (Point a) { return x*a.x + y*a.y; } // dot product
+    double operator % (Point a) { return x*a.y - y*a.x; } // cross product
+
+    int cmp(Point q) const { if (int t = ::cmp(x,q.x)) return t; return ::cmp(y,q.y); }
+
+    #define Comp(x) bool operator x (Point q) const { return cmp(q) x 0; }
+    Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
+    #undef Comp
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+
+    void read() {
+        int t;
+        scanf("%d", &t); x = t;
+        scanf("%d", &t); y = t;
+    }
+};
+
+int ccw(Point a, Point b, Point c) {
+    return cmp((b-a)%(c-a),0);
+}
+// Distance from p to Line ab (closest Point --> c)
+double distToLine(Point p, Point a, Point b, Point &c) {
+    Point ap = p - a, ab = b - a;
+    double u = (ap * ab) / ab.norm();
+    c = a + (ab * u);
+    return (p-c).len();
+}
+
+// Distance from p to segment ab (closest Point --> c)
+double distToLineSegment(Point p, Point a, Point b, Point &c) {
+    Point ap = p - a, ab = b - a;
+    double u = (ap * ab) / ab.norm();
+    if (u < 0.0) {
+        c = Point(a.x, a.y);
+        return (p - a).len();
+    }
+    if (u > 1.0) {
+        c = Point(b.x, b.y);
+        return (p - b).len();
+    }
+    return distToLine(p, a, b, c);
+}
+
+typedef vector<Point> Polygon;
+double angle(Point a, Point o, Point b) { // min of directed angle AOB & BOA
+    a = a - o; b = b - o;
+    return acos((a * b) / sqrt(a.norm() * b.norm()));
+}
+
+bool in_polygon(const Polygon &P, Point pt) {
+    if ((int)P.size() == 0) return false;
+    double sum = 0;
+    for (int i = 0; i < (int)P.size(); i++) {
+        Point Pj = P[(i+1) % P.size()];
+        // If allow on edge --> uncomment the following line
+        // if (ccw(P[i], Pj, pt) == 0 && min(P[i], Pj) <= pt && pt <= max(P[i], Pj)) return true;
+        if (ccw(pt, P[i], Pj) > 0)
+            sum += angle(P[i], pt, Pj);
+        else sum -= angle(P[i], pt, Pj);
+    }
+    return fabs(fabs(sum) - 2*PI) < EPS;
+}
+double signed_area(Polygon p) {
+    double area = 0;
+    for(int i = 0; i < p.size(); i++) {
+        int j = (i+1) % p.size();
+        area += p[i].x*p[j].y - p[j].x*p[i].y;
+    }
+    return area / 2.0;
+}
+double area(const Polygon &p) {
+    return fabs(signed_area(p));
+}
+
+
+int main() {
+    ios :: sync_with_stdio(false);
+    int n;
+    while (scanf("%d", &n) == 1) {
+        double res = 1e100;
+        Point O(0, 0);
+        bool hasContained = false;
+
+        double maxArea = -1, minArea = 1e100;
+        int outerHeight = 0, innerHeight = 0;
+
+        FOR(i,1,n) {
+            int h1, h2; scanf("%d%d", &h1, &h2);
+
+            int h = min(h1, h2);
+            int k; scanf("%d", &k);
+            Polygon P; P.resize(k);
+            REP(i,k) P[i].read();
+
+            REP(i,k) {
+                int j = (i + 1) % k;
+                Point tmp;
+
+                double len = distToLineSegment(O, P[i], P[j], tmp);
+                res = min(res, sqrt(len*len + h*(double) h));
+            }
+
+            double S = area(P);
+
+            if (in_polygon(P, O)) {
+                hasContained = true;
+                if (minArea > S) {
+                    minArea = S;
+                    innerHeight = h2;
+                }
+            }
+
+            if (S > maxArea) {
+                maxArea = S;
+                outerHeight = h1;
+            }
+        }
+        if (!hasContained) {
+            res = min(res, (double) outerHeight);
+        }
+        else res = min(res, (double) innerHeight);
+        cout << (fixed) << setprecision(9) << res << endl;
+    }
+}
