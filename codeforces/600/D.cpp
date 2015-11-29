69793045a9316117013e6864f93fd791b61ca14d
Trung Nguyen <ngthanhtrung23@gmail.com>
2015-11-30 04:19:00 +0800

    Add solution for problem `D - Area of Two Circles' Intersection` Link: https://codeforces.com/contest/600/problem/D Tags: geometry, *2000 Ref: https://codeforces.com/contest/600/submission/14562903

--- codeforces/600/D.cpp
+++ codeforces/600/D.cpp
@@ -15,7 +15,6 @@
 using namespace std;
 
 const double PI = acos((double) -1.0);
-const double EPS = 1e-10;
 
 struct Point {
     double x, y;
@@ -33,29 +32,49 @@ struct Circle : Point {
     double r;
 };
 
-double cir_area_solve(double a, double b, double c) {
+double solve(double a, double b, double c) {
     return acos((a*a + b*b - c*c) / 2 / a / b);
 }
-double cir_area_cut(double a, double r) {
+
+double cut(double a, double r) {
     double s1 = a * r * r / 2;
     double s2 = sin(a) * r * r / 2;
     return s1 - s2;
 }
 
 double commonCircleArea(Circle c1, Circle c2) { //return the common area of two circle
-    if (c1.r < c2.r) swap(c1, c2);
+    if (c1.r < c2.r) {
+        swap(c1, c2);
+    }
     double d = (c1 - c2).len();
-	if (d + c2.r <= c1.r + EPS) return c2.r*c2.r*acos(-1.0);
-	if (d >= c1.r + c2.r - EPS) return 0.0;
-    double a1 = cir_area_solve(d, c1.r, c2.r);
-    double a2 = cir_area_solve(d, c2.r, c1.r);
-    return cir_area_cut(a1*2, c1.r) + cir_area_cut(a2*2, c2.r);
+    double a1 = solve(d, c1.r, c2.r);
+    double a2 = solve(d, c2.r, c1.r);
+    return cut(a1*2, c1.r) + cut(a2*2, c2.r);
 }
 
 int main() {
     cout << (fixed) << setprecision(12);
     ll x1, y1, r1, x2, y2, r2;
     while (cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2) {
+        // do not intersect
+        ll dist2 = sqr(x1 - x2) + sqr(y1 - y2);
+        if (dist2 >= sqr(r1 + r2)) {
+            cout << 0.0 << endl;
+            continue;
+        }
+        
+        // C1 in C2
+        double dist = sqrt(dist2);
+        if (dist + r1 <= r2) {
+            cout << PI * r1 * r1 << endl;
+            continue;
+        }
+        // C2 in C1
+        if (dist + r2 <= r1) {
+            cout << PI * r2 * r2 << endl;
+            continue;
+        }
+
         Circle c1; c1.x = x1; c1.y = y1; c1.r = r1;
         Circle c2; c2.x = x2; c2.y = y2; c2.r = r2;
         cout << commonCircleArea(c1, c2) << endl;
