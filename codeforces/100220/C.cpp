23e69aaa8157d27a37fed3dc2b3798914bc69fba
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-01-01 00:00:00 +0800

    Add solution for problem `C - Domino in Casino` Link: https://codeforces.com/gym/100220 Tags: Ref: https://codeforces.com/gym/100220/submission/15138885

---
+++ codeforces/100220/C.cpp
@@ -0,0 +1,161 @@
+
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << X << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+template<class Flow=int, class Cost=int>
+struct MinCostFlow {
+    const Flow INF_FLOW = 1000111000;
+    const Cost INF_COST = 1000111000111000LL;
+
+    int n, t, S, T;
+    Flow totalFlow;
+    Cost totalCost;
+    vector<int> last, visited;
+    vector<Cost> dis;
+    struct Edge {
+        int to;
+        Flow cap;
+        Cost cost;
+        int next;
+        Edge(int to, Flow cap, Cost cost, int next) :
+                to(to), cap(cap), cost(cost), next(next) {}
+    };
+    vector<Edge> edges;
+
+    MinCostFlow(int n) : n(n), t(0), totalFlow(0), totalCost(0), last(n, -1), visited(n, 0), dis(n, 0) {
+        edges.clear();
+    }
+
+    int addEdge(int from, int to, Flow cap, Cost cost) {
+        edges.push_back(Edge(to, cap, cost, last[from]));
+        last[from] = t++;
+        edges.push_back(Edge(from, 0, -cost, last[to]));
+        last[to] = t++;
+        return t - 2;
+    }
+
+    pair<Flow, Cost> minCostFlow(int _S, int _T) {
+        S = _S; T = _T;
+        SPFA();
+        while (1) {
+            while (1) {
+                REP(i,n) visited[i] = 0;
+                if (!findFlow(S, INF_FLOW)) break;
+            }
+            if (!modifyLabel()) break;
+        }
+        return make_pair(totalFlow, totalCost);
+    }
+
+private:
+    void SPFA() {
+        REP(i,n) dis[i] = INF_COST;
+        priority_queue< pair<Cost,int> > Q;
+        Q.push(make_pair(dis[S]=0, S));
+        while (!Q.empty()) {
+            int x = Q.top().second;
+            Cost d = -Q.top().first;
+            Q.pop();
+            // For double: dis[x] > d + EPS
+            if (dis[x] != d) continue;
+            for(int it = last[x]; it >= 0; it = edges[it].next)
+                if (edges[it].cap > 0 && dis[edges[it].to] > d + edges[it].cost)
+                    Q.push(make_pair(-(dis[edges[it].to] = d + edges[it].cost), edges[it].to));
+        }
+        Cost disT = dis[T];
+        REP(i,n) dis[i] = disT - dis[i];
+    }
+
+    Flow findFlow(int x, Flow flow) {
+        if (x == T) {
+            totalCost += dis[S] * flow;
+            totalFlow += flow;
+            return flow;
+        }
+        visited[x] = 1;
+        Flow now = flow;
+        for(int it = last[x]; it >= 0; it = edges[it].next)
+            // For double: fabs(dis[edges[it].to] + edges[it].cost - dis[x]) < EPS
+            if (edges[it].cap && !visited[edges[it].to] && dis[edges[it].to] + edges[it].cost == dis[x]) {
+                Flow tmp = findFlow(edges[it].to, min(now, edges[it].cap));
+                edges[it].cap -= tmp;
+                edges[it ^ 1].cap += tmp;
+                now -= tmp;
+                if (!now) break;
+            }
+        return flow - now;
+    }
+
+    bool modifyLabel() {
+        Cost d = INF_COST;
+        REP(i,n) if (visited[i])
+            for(int it = last[i]; it >= 0; it = edges[it].next)
+                if (edges[it].cap && !visited[edges[it].to])
+                    d = min(d, dis[edges[it].to] + edges[it].cost - dis[i]);
+
+        // For double: if (d > INF_COST / 10)     INF_COST = 1e20
+        if (d == INF_COST) return false;
+        REP(i,n) if (visited[i])
+            dis[i] += d;
+        return true;
+    }
+};
+
+
+#define TWO(X) (1<<(X))
+#define CONTAIN(S,X) (S & TWO(X))
+
+const int MN = 211;
+int a[MN][MN], id[MN][MN];
+
+const int di[4] = {-1,1,0,0};
+const int dj[4] = {0,0,-1,1};
+
+int main() {
+    ios :: sync_with_stdio(0); cin.tie(0);
+    freopen("domino.in", "r", stdin);
+    freopen("domino.out", "w", stdout);
+
+    int m, n, k;
+    while (cin >> m >> n >> k) {
+        FOR(i,1,m) FOR(j,1,n) cin >> a[i][j];
+
+        int x = 0, y = 1, cur = 2;
+        int z = cur++;
+        FOR(i,1,m) FOR(j,1,n) {
+            id[i][j] = cur++;
+        }
+
+        MinCostFlow<int,ll> flow(cur);
+        flow.addEdge(x, y, k, 0);
+        FOR(i,1,m) FOR(j,1,n) {
+            if ((i + j) % 2 == 0) {
+                flow.addEdge(y, id[i][j], 1, 0);
+
+                REP(dir,4) {
+                    int ii = i + di[dir], jj = j + dj[dir];
+                    if (ii < 1 || ii > m || jj < 1 || jj > n) continue;
+
+                    flow.addEdge(id[i][j], id[ii][jj],
+                            1, -a[i][j] * a[ii][jj]);
+                }
+            }
+            else {
+                flow.addEdge(id[i][j], z, 1, 0);
+            }
+        }
+        auto p = flow.minCostFlow(x, z);
+        cout << -p.second << endl;
+    }
+}
