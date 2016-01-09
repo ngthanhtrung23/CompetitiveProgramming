b0ef29db8c3bf7dcd6ae8ee5ebf6cd79f5e96421
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-01-09 16:17:00 +0800

    Add solution for problem `L - Crossed ladders` Link: https://codeforces.com/gym/100227 Tags: Ref: https://codeforces.com/gym/100227/submission/15266068

---
+++ codeforces/100227/L.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+int main() {
+    double x, y, c;
+    ios :: sync_with_stdio(false);
+    while (cin >> x >> y >> c) {
+        double l = 1e-9, r = min(x, y), res = l;
+        REP(turn,1000) {
+            double mid = (l + r) / 2.0;
+
+            double cx = sqrt(x*x - mid*mid);
+            double cy = sqrt(y*y - mid*mid);
+
+            double cc = cx * cy / (cx + cy);
+
+            if (cc > c) {
+                l = mid;
+            }
+            else {
+                r = res = mid;
+            }
+        }
+        cout << (fixed) << setprecision(3) << res << endl;
+    }
+    return 0;
+}
