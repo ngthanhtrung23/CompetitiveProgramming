0b3b3508108ed3b90581d5d331354650696a6ac2
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-01-11 01:17:00 +0800

    Add solution for problem `A - Theatre Square` Link: https://codeforces.com/gym/100001 Tags: Ref: https://codeforces.com/gym/100001/submission/15282597

---
+++ codeforces/100001/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(false);
+    long long m, n, a;
+    while (cin >> m >> n >> a) {
+        cout << ((m+a-1) / a) * ((n+a-1) / a) << endl;
+    }
+    return 0;
+}
