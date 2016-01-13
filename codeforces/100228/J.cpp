5992620c408d5bdac8278d2b367d9e604ecc4742
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-01-14 02:29:00 +0800

    Add solution for problem `J - Common Permutation` Link: https://codeforces.com/gym/100228 Tags: Ref: https://codeforces.com/gym/100228/submission/15335525

---
+++ codeforces/100228/J.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+
+#define DEBUG(x) cout << #x << " = "; cout << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl;
+#define PR0(a,n) cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl;
+using namespace std;
+
+//Buffer reading
+int INP,AM,REACHEOF;
+const int BUFSIZE = (1<<12) + 17;
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+//End of buffer reading
+
+char a[100111], b[100111];
+int cnt[2][333];
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    while (gets(a)) {
+        gets(b);
+        memset(cnt, 0, sizeof cnt);
+        REP(i,strlen(a)) cnt[0][a[i]]++;
+        REP(i,strlen(b)) cnt[1][b[i]]++;
+
+        FOR(c,'a','z') {
+            int t = min(cnt[0][c], cnt[1][c]);
+            while (t--) putchar(c);
+        }
+        puts("");
+    }
+    return 0;
+}
