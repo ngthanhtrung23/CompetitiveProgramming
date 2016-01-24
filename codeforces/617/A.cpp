7344f6d234dcca8a392f6ba8e1cf98c00d72821b
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-01-24 08:55:00 +0800

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/15544623

---
+++ codeforces/617/A.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << X << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+int main() {
+    ios :: sync_with_stdio(0); cin.tie(0);
+    int n;
+    while (cin >> n) {
+        cout << (n + 4) / 5 << endl;
+    }
+}
