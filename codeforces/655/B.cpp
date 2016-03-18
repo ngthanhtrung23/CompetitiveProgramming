3580d37579d5db8920d83baa0abed2da034360dd
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-03-19 00:45:00 +0800

    Add solution for problem `B - Mischievous Mess Makers` Link: https://codeforces.com/contest/655/problem/B Tags: greedy, math, *1200 Ref: https://codeforces.com/contest/655/submission/16786188

---
+++ codeforces/655/B.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+const int MN = 100111;
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    int n, k;
+    while (cin >> n >> k) {
+        int res = 0;
+        int ln = n / 2;
+        if (k > ln) k = ln;
+
+        FORD(i,n-1,n-2*k) res += i;
+        cout << res << endl;
+    }
+}
