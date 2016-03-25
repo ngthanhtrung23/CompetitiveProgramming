3f87f9be22b6895245e5b304100cd8a27d9fbfd8
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-03-26 02:23:00 +0800

    Add solution for problem `D - Delivery Bears` Link: https://codeforces.com/contest/653/problem/D Tags: binary search, flows, graphs, *2200 Ref: https://codeforces.com/contest/653/submission/16936873

---
+++ codeforces/653/D.cpp
@@ -0,0 +1,125 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+const int MN = 55;
+const int INF = 1000000000;
+ 
+struct Edge {
+    int a, b, cap, flow;
+};
+
+struct MaxFlow {
+    int n, s, t;
+    vector<int> d, ptr, q;
+    vector< Edge > e;
+    vector< vector<int> > g;
+
+    MaxFlow(int n) : n(n), d(n), ptr(n), q(n), g(n) {
+        e.clear();
+        REP(i,n) {
+            g[i].clear();
+            ptr[i] = 0;
+        }
+    }
+
+    void addEdge(int a, int b, int cap) {
+        Edge e1 = { a, b, cap, 0 };
+        Edge e2 = { b, a, 0, 0 };
+        g[a].push_back( (int) e.size() );
+        e.push_back(e1);
+        g[b].push_back( (int) e.size() );
+        e.push_back(e2);
+    }
+    int getMaxFlow(int _s, int _t) {
+        s = _s; t = _t;
+        int flow = 0;
+        for (;;) {
+            if (!bfs()) break;
+            REP(i,n) ptr[i] = 0;
+            while (int pushed = dfs(s, INF))
+                flow += pushed;
+        }
+        return flow;
+    }
+
+private:
+    bool bfs() {
+        int qh = 0, qt = 0;
+        q[qt++] = s;
+        REP(i,n) d[i] = -1;
+        d[s] = 0;
+
+        while (qh < qt && d[t] == -1) {
+            int v = q[qh++];
+            REP(i,g[v].size()) {
+                int id = g[v][i], to = e[id].b;
+                if (d[to] == -1 && e[id].flow < e[id].cap) {
+                    q[qt++] = to;
+                    d[to] = d[v] + 1;
+                }
+            }
+        }
+        return d[t] != -1;
+    }
+
+    int dfs (int v, int flow) {
+        if (!flow) return 0;
+        if (v == t) return flow;
+        for (; ptr[v] < (int)g[v].size(); ++ptr[v]) {
+            int id = g[v][ptr[v]],
+                to = e[id].b;
+            if (d[to] != d[v] + 1) continue;
+            int pushed = dfs(to, min(flow, e[id].cap - e[id].flow));
+            if (pushed) {
+                e[id].flow += pushed;
+                e[id^1].flow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+};
+int eu[555], ev[555], ec[555];
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    int n, m, x;
+    while (cin >> n >> m >> x) {
+        FOR(i,1,m) cin >> eu[i] >> ev[i] >> ec[i];
+        double l = 0, r = 1e9, res = 0;
+        REP(turn,200) {
+            double mid = (l + r) / 2.0;
+            MaxFlow flow(n+1);
+            FOR(i,1,m) {
+                double t = ec[i] / mid;
+                int can;
+                if (t > 1e9) can = 1e6;
+                else can = (int) t;
+
+                flow.addEdge(eu[i], ev[i], can);
+            }
+            if (flow.getMaxFlow(1, n) >= x) {
+                res = mid;
+                l = mid;
+            }
+            else r = mid;
+        }
+        cout << (fixed) << setprecision(9) << res * x << endl;
+    }
+}
