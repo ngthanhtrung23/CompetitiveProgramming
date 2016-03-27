8a774f9c9a37ff12ba08096a166cffc31a4e1f08
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-03-27 23:35:00 +0800

    Add solution for problem `B - z-sort` Link: https://codeforces.com/contest/652/problem/B Tags: sortings, *1000 Ref: https://codeforces.com/contest/652/submission/16975215

---
+++ codeforces/652/B.cpp
@@ -0,0 +1,35 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+int a[1011], n, res[1011];
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    while (cin >> n) {
+        FOR(i,1,n) cin >> a[i];
+        sort(a+1, a+n+1);
+
+        int t = 0;
+        for(int i = 1; i <= n; i += 2) res[i] = a[++t];
+        for(int i = 2; i <= n; i += 2) res[i] = a[++t];
+
+        FOR(i,1,n) cout << res[i] << ' '; cout << endl;
+    }
+}
