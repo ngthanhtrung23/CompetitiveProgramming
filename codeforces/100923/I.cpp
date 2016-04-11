f648a939349528b7791aedbbe33bfa0bccb0353a
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-11 22:02:00 +0800

    Add solution for problem `I - Por Costel and the Pairs` Link: https://codeforces.com/gym/100923 Tags: Ref: https://codeforces.com/gym/100923/submission/17278888

---
+++ codeforces/100923/I.cpp
@@ -0,0 +1,62 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+long long sumdiv(long long n) {
+    long long s=0;
+    for (long long i=1; i<=n; ) {
+        long long q=n/i, r=n/q;
+        s+=(long long)(r-i+1)*q;
+        i=r+1;
+    }
+    return s;
+}
+
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    freopen("perechi3.in", "r", stdin);
+    freopen("perechi3.out", "w", stdout);
+    int ntest; GN(ntest);
+    while (ntest--) {
+        int n; GN(n);
+        cout << sumdiv(n) << '\n';
+    }
+}
