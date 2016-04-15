8aead5539e0913e67ea327f5bc262d762716afd1
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-15 22:57:00 +0800

    Add solution for problem `F - The Jedi Killer` Link: https://codeforces.com/gym/100960 Tags: Ref: https://codeforces.com/gym/100960/submission/17328660

---
+++ codeforces/100960/F.cpp
@@ -0,0 +1,217 @@
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+#define double long double
+using namespace std;
+
+int GI(int& x) {
+    return scanf("%lld", &x);
+}
+
+#define EPS 1e-6
+const double PI = acos(-1.0);
+
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+inline int cmp(double a, double b) {
+    return (a < b - EPS) ? -1 : ((a > b + EPS) ? 1 : 0);
+}
+
+struct Point {
+    double x, y;
+    Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+
+    Point operator + (const Point& a) const { return Point(x+a.x, y+a.y); }
+    Point operator - (const Point& a) const { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) const { return Point(x*k, y*k); }
+    Point operator / (double k) const { return Point(x/k, y/k); }
+
+    double operator * (const Point& a) const { return x*a.x + y*a.y; } // dot product
+    double operator % (const Point& a) const { return x*a.y - y*a.x; } // cross product
+
+    int cmp(Point q) const { if (int t = ::cmp(x,q.x)) return t; return ::cmp(y,q.y); }
+
+    #define Comp(x) bool operator x (Point q) const { return cmp(q) x 0; }
+    Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
+    #undef Comp
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+};
+
+int ccw(Point a, Point b, Point c) {
+    return cmp((b-a)%(c-a),0);
+}
+int RE_TRAI = ccw(Point(0, 0), Point(0, 1), Point(-1, 1));
+int RE_PHAI = ccw(Point(0, 0), Point(0, 1), Point(1, 1));
+istream& operator >> (istream& cin, Point& p) {
+    cin >> p.x >> p.y;
+    return cin;
+}
+ostream& operator << (ostream& cout, Point& p) {
+    cout << p.x << ' ' << p.y;
+    return cout;
+}
+
+double angle(Point a, Point o, Point b) { // min of directed angle AOB & BOA
+    a = a - o; b = b - o;
+    return acos((a * b) / sqrt(a.norm()) / sqrt(b.norm()));
+}
+
+double directed_angle(Point a, Point o, Point b) { // angle AOB, in range [0, 2*PI)
+    double t = -atan2(a.y - o.y, a.x - o.x)
+            + atan2(b.y - o.y, b.x - o.x);
+    while (t < 0) t += 2*PI;
+    return t;
+}
+
+// Distance from p to Line ab (closest Point --> c)
+double distToLine(Point p, Point a, Point b, Point &c) {
+    Point ap = p - a, ab = b - a;
+    double u = (ap * ab) / ab.norm();
+    c = a + (ab * u);
+    return (p-c).len();
+}
+
+// Distance from p to segment ab (closest Point --> c)
+double distToLineSegment(Point p, Point a, Point b, Point &c) {
+    Point ap = p - a, ab = b - a;
+    double u = (ap * ab) / ab.norm();
+    if (u < 0.0) {
+        c = Point(a.x, a.y);
+        return (p - a).len();
+    }
+    if (u > 1.0) {
+        c = Point(b.x, b.y);
+        return (p - b).len();
+    }
+    return distToLine(p, a, b, c);
+}
+
+// NOTE: WILL NOT WORK WHEN a = b = 0.
+struct Line {
+    double a, b, c;
+    Point A, B; // Added for polygon intersect line. Do not rely on assumption that these are valid
+
+    Line(double a, double b, double c) : a(a), b(b), c(c) {} 
+
+    Line(Point A, Point B) : A(A), B(B) {
+        a = B.y - A.y;
+        b = A.x - B.x;
+        c = - (a * A.x + b * A.y);
+    }
+    Line(Point P, double m) {
+        a = -m; b = 1;
+        c = -((a * P.x) + (b * P.y));
+    }
+    double f(Point A) {
+        return a*A.x + b*A.y + c;
+    }
+};
+
+bool areParallel(Line l1, Line l2) {
+    return cmp(l1.a*l2.b, l1.b*l2.a) == 0;
+}
+
+bool areSame(Line l1, Line l2) {
+    return areParallel(l1 ,l2) && cmp(l1.c*l2.a, l2.c*l1.a) == 0
+                && cmp(l1.c*l2.b, l1.b*l2.c) == 0;
+}
+
+bool areIntersect(Line l1, Line l2, Point &p) {
+    if (areParallel(l1, l2)) return false;
+    double dx = l1.b*l2.c - l2.b*l1.c;
+    double dy = l1.c*l2.a - l2.c*l1.a;
+    double d  = l1.a*l2.b - l2.a*l1.b;
+    p = Point(dx/d, dy/d);
+    return true;
+}
+
+void closestPoint(Line l, Point p, Point &ans) {
+    if (fabs(l.b) < EPS) {
+        ans.x = -(l.c) / l.a; ans.y = p.y;
+        return;
+    }
+    if (fabs(l.a) < EPS) {
+        ans.x = p.x; ans.y = -(l.c) / l.b;
+        return;
+    }
+    Line perp(l.b, -l.a, - (l.b*p.x - l.a*p.y));
+    areIntersect(l, perp, ans);
+}
+
+void reflectionPoint(Line l, Point p, Point &ans) {
+    Point b;
+    closestPoint(l, p, b);
+    ans = p + (b - p) * 2;
+}
+
+double max3(double a, double b, double c) {
+    return max(max(a, b), c);
+}
+bool order(Point a, Point b, Point c) {
+    return cmp(min(a.x, c.x), b.x) <= 0 && cmp(b.x, max(a.x, c.x)) <= 0
+        && cmp(min(a.y, c.y), b.y) <= 0 && cmp(b.y, max(a.y, c.y)) <= 0;
+}
+
+double Lg, Lm;
+Point A, B, C;
+
+bool check(Point A, Point B, Point C) {
+    Line l(A, B);
+    Point X; closestPoint(l, C, X);
+    if (cmp((C - X).len(), Lm) <= 0
+            && cmp((A - X).len(), Lg) <= 0
+            && cmp((B - X).len(), Lg) <= 0) return true;
+
+    if (cmp((C - X).len(), Lg) <= 0
+            && cmp((A - X).len(), Lm) <= 0
+            && cmp((B - X).len(), Lm) <= 0
+            && (order(X, A, B) || order(X, B, A))) return true;
+
+    return false;
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    int ntest; cin >> ntest;
+    while (ntest--) {
+        cin >> Lm >> Lg;
+        cin >> A >> B >> C;
+        if (ccw(A, B, C) == 0) {
+            double len = max3((C - A).len(), (C - B).len(), (A - B).len());
+            cout << (len <= max(Lm, 2*Lg) ? "YES" : "NO") << '\n';
+        }
+        else {
+            if (check(A, B, C) || check(A, C, B) || check(B, C, A)) {
+                cout << "YES" << '\n';
+            }
+            else cout << "NO" << '\n';
+        }
+    }
+}
