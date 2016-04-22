72eaed873a31d9da1f66bd0866221ce109d01f2d
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-22 21:18:00 +0800

    Add solution for problem `E - Mirror Rice Cake` Link: https://codeforces.com/gym/100959 Tags: Ref: https://codeforces.com/gym/100959/submission/17435123

--- codeforces/100959/E.cpp
+++ codeforces/100959/E.cpp
@@ -1,42 +1,22 @@
-
 #include <bits/stdc++.h>
-#define int long long
-#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
-#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
-#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
-
-#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
-#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
-#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
-
-#define sqr(x) ((x) * (x))
-#define ll long long
-#define __builtin_popcount __builtin_popcountll
-#define SZ(x) ((int) (x).size())
 using namespace std;
 
-int GI(int& x) {
-    return scanf("%lld", &x);
-}
+int main()
+{
+  int n, a[1111];
+  cin >> n;
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
 
-const int MN = 1011;
-int a[MN];
-
-#undef int
-int main() {
-#define int long long
-    ios :: sync_with_stdio(0); cin.tie(0);
-    cout << (fixed) << setprecision(9);
-    int n;
-    while (GI(n) == 1) {
-        FOR(i,1,n) GI(a[i]);
-        sort(a+1, a+n+1);
-
-        int sum = 0, res = 0;
-        FOR(i,1,n) if (a[i] > sum) {
-            ++res;
-            sum += a[i];
-        }
-        cout << res << endl;
+  sort(a, a + n);
+  int ans = 0;
+  long long sum = 0; 
+  for (int i = 0; i < n; i++)
+    if (a[i] > sum)
+    {
+      ans++;
+      sum += a[i];
     }
+
+  cout << ans << endl;
 }
