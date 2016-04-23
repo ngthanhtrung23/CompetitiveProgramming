b0f5126c67e6386ff7a0b5ff389967799480f130
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-23 18:20:00 +0800

    Add solution for problem `G - City Square` Link: https://codeforces.com/gym/100247 Tags: Ref: https://codeforces.com/gym/100247/submission/17468118

---
+++ codeforces/100247/G.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+int GI(int& x) {
+    return scanf("%lld", &x);
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+
+    set<int> sq;
+    FOR(i,0,100000) sq.insert(i*i);
+
+    int n;
+    while (GI(n) == 1) {
+        int can = 0;
+        FOR(i,1,100000) {
+            int x = n - i*i;
+            if (x < 0) continue;
+
+            if (sq.count(x)) {
+                can = 1;
+                break;
+            }
+        }
+        cout << (can ? "Yes" : "No") << endl;
+    }
+}
