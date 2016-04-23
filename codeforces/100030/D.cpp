9d6f708ddcd9a4437e91d3a8f36262b228355186
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-23 16:20:00 +0800

    Add solution for problem `D - Broadcasting` Link: https://codeforces.com/gym/100030 Tags: Ref: https://codeforces.com/gym/100030/submission/17466631

---
+++ codeforces/100030/D.cpp
@@ -0,0 +1,47 @@
+//#pragma comment(linker, "/STACK:66777216")
+#include <iomanip>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <deque>
+#include <complex>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define ll long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PR(a,n) cout << #a << " = "; FOR(i,1,n) cout << a[i] << ' '; puts("");
+using namespace std;
+
+const double PI = acos(-1.0);
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n, k;
+    while (scanf("%d%d", &n, &k) == 2) {
+        ll cur = 1;
+        int res = 0;
+        while (cur < n) {
+            cur += cur * k;
+            res++;
+        }
+        printf("%d\n", res);
+    }
+    return 0;
+}
