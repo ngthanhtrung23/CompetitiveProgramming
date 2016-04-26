768cec9f5d210ad0c93aff57904c8b6a4fe6c147
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-27 00:54:00 +0800

    Add solution for problem `B - Dictionary` Link: https://codeforces.com/gym/100958 Tags: Ref: https://codeforces.com/gym/100958/submission/17525113

--- codeforces/100958/B.cpp
+++ codeforces/100958/B.cpp
@@ -1,5 +1,5 @@
+
 #include <bits/stdc++.h>
-#define int long long
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
 #define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
@@ -14,70 +14,12 @@
 #define SZ(x) ((int) (x).size())
 using namespace std;
 
-double safe_sqrt(double x) {
-    return sqrt(max(0.0,x));
-}
-int GI(int& x) {
-    return scanf("%lld", &x);
-}
-
 const int MN = 55;
 const int MOD = 1e9 + 7;
 int f[55][55][22][30], len[55], n;
 string s[55];
 
-int dp(int i1, int i2, int j, char x) {
-    int ix = x - 'a';
-    if (f[i1][i2][j][ix] >= 0) return f[i1][i2][j][ix];
-
-    int& res = f[i1][i2][j][ix];
-
-    // only 1 string left
-    if (i1 == i2) {
-        if (len[i1] == j) return res = 1;
-
-        if (s[i1][j+1] == '?') {
-            return res = ('z' - x + 1) * dp(i1, i2, j+1, 'a') % MOD;
-        }
-        else {
-            if (s[i1][j+1] >= x)
-                return res = dp(i1, i2, j+1, 'a') % MOD;
-            else return res = 0;
-        }
-    }
-
-    // some string is too short
-    FOR(i,i1+1,i2) if (len[i] <= j) return res = 0;
-
-    // first string is too short
-    if (len[i1] == j) {
-        return res = dp(i1+1, i2, j, x);
-    }
-
-    FOR(i,i1,i2) if (s[i][j+1] != '?' && s[i][j+1] < x) return res = 0;
-    
-    // all is good
-    res = 0;
-    FOR(mid,i1,i2) {
-        if (s[mid][j+1] != '?' && s[mid][j+1] != x) break;
-
-        if (mid == i2) {
-            res = (res + dp(i1, i2, j+1, 'a')) % MOD;
-        }
-        else {
-            if (x == 'z') continue;
-            res = (res + dp(i1, mid, j+1, 'a') * dp(mid+1, i2, j, x + 1)) % MOD;
-        }
-    }
-    if (x < 'z') {
-        res = (res + dp(i1, i2, j, x+1)) % MOD;
-    }
-    return res;
-}
-
-#undef int
 int main() {
-#define int long long
     ios :: sync_with_stdio(0); cin.tie(0);
     cout << (fixed) << setprecision(9);
     while (cin >> n) {
@@ -88,7 +30,61 @@ int main() {
             s[i] = " " + s[i] + " ";
         }
 
-        memset(f, -1, sizeof f);
-        cout << dp(1, n, 0, 'a') << endl;
+        memset(f, 0, sizeof f);
+        FORD(i1,n,1) FOR(i2,i1,n) FORD(j,20,0) FORD(x,'z','a') {
+            int ix = x - 'a';
+            int& res = f[i1][i2][j][ix];
+            res = 0;
+
+            if (i1 == i2) {
+                if (len[i1] == j) {
+                    res = 1;
+                    continue;
+                }
+
+                if (s[i1][j+1] == '?') {
+                    res = ('z' - x + 1LL) * f[i1][i2][j+1][0] % MOD;
+                    continue;
+                }
+                else {
+                    if (s[i1][j+1] >= x) {
+                        res = f[i1][i2][j+1][0];
+                        continue;
+                    }
+                }
+            }
+
+            // some string is too short
+            int bad = 0;
+            FOR(i,i1+1,i2) if (len[i] <= j) bad = 1;
+            if (bad) continue;
+
+            // first string too short
+            if (len[i1] == j) {
+                res = f[i1+1][i2][j][ix];
+                continue;
+            }
+
+            FOR(i,i1,i2) if (s[i][j+1] != '?' && s[i][j+1] < x) bad = 1;
+            if (bad) continue;
+            
+            // normal case
+            res = 0;
+            FOR(cur,x,'z') {
+                FOR(mid,i1,i2) {
+                    if (s[mid][j+1] != '?' && s[mid][j+1] != cur) break;
+
+                    if (mid == i2) {
+                        res = res + f[i1][i2][j+1][0];
+                        if (res >= MOD) res -= MOD;
+                    }
+                    else {
+                        if (cur == 'z') continue;
+                        res = (res + f[i1][mid][j+1][0] * (ll) f[mid+1][i2][j][cur + 1 - 'a']) % MOD;
+                    }
+                }
+            }
+        }
+        cout << f[1][n][0][0] << endl;
     }
 }
