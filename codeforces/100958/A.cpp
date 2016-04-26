34807344bd3979da53c7898dbf66a7e12fb78541
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-04-26 21:28:00 +0800

    Add solution for problem `A - Manhattan` Link: https://codeforces.com/gym/100958 Tags: Ref: https://codeforces.com/gym/100958/submission/17522189

---
+++ codeforces/100958/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define inf 1000000005
+#define all(a) (a).begin(), (a).end()
+#define ms(a,x) memset(a, x, sizeof(a))
+#define mod 1000000007
+#define sz(a) ((int)(a).size())
+
+template<class T> int getbit(T s, int i) { return (s >> i) & 1; }
+template<class T> T onbit(T s, int i) { return s | (T(1) << i); }
+template<class T> T offbit(T s, int i) { return s & (~(T(1) << i)); }
+template<class T> int cntbit(T s) { return __builtin_popcount(s);}
+#define Rep(i,n) for(int i = 0; i < (n); ++i)
+#define Repd(i,n) for(int i = (n)-1; i >= 0; --i)
+#define For(i,a,b) for(int i = (a); i <= (b); ++i)
+#define Ford(i,a,b) for(int i = (a); i >= (b); --i)
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef double ld;
+#define eps 1e-12
+typedef pair<int, int> II;
+typedef pair<ll, ll> LL;
+template<class T> T gcd(T a, T b){ T r; while (b != 0) { r = a % b; a = b; b = r; } return a;}
+template<class T> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+#define PI (2 * acos(0))
+#define linf (1ll << 60)
+#define y1 y32432
+#define y2 y435346
+
+#define maxn 200005
+
+ld x;
+
+int main(){
+//	freopen("in.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> x;
+    ld res = x * sqrt(2);
+
+    for(int i = 1; i <= x + eps; i++){
+    	ld T = x * x - i * i;
+    	if(T < 1){
+    		if(i == 1){
+    			ld ret = 2;
+    			res = max(res, ret);
+    		}else{
+    			ld ret = i + 1;
+    			res = max(res, ret);
+    		}
+    	}
+    }
+
+    cout << fixed << setprecision(10);
+    cout << res << endl;
+
+    return 0;
+}
