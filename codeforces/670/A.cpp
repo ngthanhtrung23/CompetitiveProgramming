1309009b5b621228fef74505ddb5e792601e8383
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-05-06 23:16:00 +0800

    Add solution for problem `A - Holidays` Link: https://codeforces.com/contest/670/problem/A Tags: brute force, constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/670/submission/17764123

---
+++ codeforces/670/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max(0.0,x));
+}
+int GI(int& x) {
+    return scanf("%lld", &x);
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+	int n;
+	while (cin >> n) {
+		int week = n / 7;
+		int rem = n % 7;
+
+		cout << (week * 2) + max(0LL, rem - 5) << ' ' << (week * 2) + min(rem, 2LL) << endl;
+	}
+}
