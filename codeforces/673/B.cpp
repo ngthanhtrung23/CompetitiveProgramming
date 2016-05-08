7246d5a48255098c96554db9c24d893f94b8f19e
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-05-08 14:55:00 +0800

    Add solution for problem `B - Problems for Round` Link: https://codeforces.com/contest/673/problem/B Tags: greedy, implementation, *1300 Ref: https://codeforces.com/contest/673/submission/17803675

---
+++ codeforces/673/B.cpp
@@ -0,0 +1,44 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max((double)0.0,x));
+}
+int GI(ll& x) {
+    return scanf("%lld", &x);
+}
+
+int n, q;
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    while (GI(n) == 1 && GI(q) == 1) {
+        int nn = 1, ln = n-1;
+        FOR(i,1,q) {
+            int u, v; GI(u); GI(v);
+            if (u > v) swap(u, v);
+
+            nn = max(nn, u);
+            ln = min(ln, v - 1);
+        }
+        if (nn <= ln) cout << ln - nn + 1 << endl;
+        else cout << 0 << endl;
+    }
+}
