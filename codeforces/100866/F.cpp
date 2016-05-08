f4de570a41c0f74b8f215ca29ead54314f13dcdb
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-05-08 15:38:00 +0800

    Add solution for problem `F - Fractions` Link: https://codeforces.com/gym/100866 Tags: Ref: https://codeforces.com/gym/100866/submission/17804214

---
+++ codeforces/100866/F.cpp
@@ -0,0 +1,45 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max((double)0.0,x));
+}
+int GI(ll& x) {
+    return scanf("%lld", &x);
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    int x;
+    while (scanf("0.%lld\n", &x) == 1) {
+        int y = 10;
+        while (x >= y) y *= 10LL;
+
+        FOR(q,1,1000111000) {
+            int p = x*q / y;
+            while (x*q > y*p) ++p;
+
+            if (p > 0 && p < q && y*p < (x+1LL)*q) {
+                cout << p << ' ' << q << endl;
+                break;
+            }
+        }
+    }
+}
