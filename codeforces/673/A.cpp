f5ebc1d3e318fe10e8a6dd1731a404d7fbba59d2
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-05-08 14:45:00 +0800

    Add solution for problem `A - Bear and Game` Link: https://codeforces.com/contest/673/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/673/submission/17803518

---
+++ codeforces/673/A.cpp
@@ -0,0 +1,41 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max((double)0.0,x));
+}
+int GI(ll& x) {
+    return scanf("%lld", &x);
+}
+
+int n, a[99];
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    while (GI(n) == 1) {
+        FOR(i,1,n) GI(a[i]);
+        a[n+1] = 1000;
+
+        FOR(i,0,n) if (a[i] + 15 < a[i+1]) {
+            cout << min(a[i] + 15, 90LL) << endl;
+            break;
+        }
+    }
+}
