f11486fde0f4b15071a45174d6ef956b79bec721
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-05-11 22:08:00 +0800

    Add solution for problem `I - Farm` Link: https://codeforces.com/gym/100935 Tags: Ref: https://codeforces.com/gym/100935/submission/17844407

---
+++ codeforces/100935/I.cpp
@@ -0,0 +1,170 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max((double)0.0,x));
+}
+int GI(ll& x) {
+    return scanf("%lld", &x);
+}
+#define EPS 1e-6
+const double PI = acos(-1.0);
+
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+inline int cmp(double a, double b) {
+    return (a < b - EPS) ? -1 : ((a > b + EPS) ? 1 : 0);
+}
+
+struct Point {
+    double x, y;
+    Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+
+    Point operator + (const Point& a) const { return Point(x+a.x, y+a.y); }
+    Point operator - (const Point& a) const { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) const { return Point(x*k, y*k); }
+    Point operator / (double k) const { return Point(x/k, y/k); }
+
+    double operator * (const Point& a) const { return x*a.x + y*a.y; } // dot product
+    double operator % (const Point& a) const { return x*a.y - y*a.x; } // cross product
+
+    int cmp(Point q) const { if (int t = ::cmp(x,q.x)) return t; return ::cmp(y,q.y); }
+
+    #define Comp(x) bool operator x (Point q) const { return cmp(q) x 0; }
+    Comp(>) Comp(<) Comp(==) Comp(>=) Comp(<=) Comp(!=)
+    #undef Comp
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+};
+istream& operator >> (istream& cin, Point& p) {
+    cin >> p.x >> p.y;
+    return cin;
+}
+ostream& operator << (ostream& cout, Point& p) {
+    cout << p.x << ' ' << p.y;
+    return cout;
+}
+struct Line {
+    double a, b, c;
+    Point A, B; // Added for polygon intersect line. Do not rely on assumption that these are valid
+
+    Line(double a, double b, double c) : a(a), b(b), c(c) {} 
+
+    Line(Point A, Point B) : A(A), B(B) {
+        a = B.y - A.y;
+        b = A.x - B.x;
+        c = - (a * A.x + b * A.y);
+    }
+    Line(Point P, double m) {
+        a = -m; b = 1;
+        c = -((a * P.x) + (b * P.y));
+    }
+    double f(Point A) {
+        return a*A.x + b*A.y + c;
+    }
+};
+struct Circle : Point {
+    double r;
+    Circle(double x = 0, double y = 0, double r = 0) : Point(x, y), r(r) {}
+    Circle(Point p, double r) : Point(p), r(r) {}
+    
+    bool contains(Point p) { return (*this - p).len() <= r + EPS; }
+};
+vector<Point> intersection(Line l, Circle cir) {
+    double r = cir.r, a = l.a, b = l.b, c = l.c + l.a*cir.x + l.b*cir.y;
+    vector<Point> res;
+
+    double x0 = -a*c/(a*a+b*b),  y0 = -b*c/(a*a+b*b);
+    if (c*c > r*r*(a*a+b*b)+EPS) return res;
+    else if (fabs(c*c - r*r*(a*a+b*b)) < EPS) {
+        res.push_back(Point(x0, y0) + Point(cir.x, cir.y));
+        return res;
+    }
+    else {
+        double d = r*r - c*c/(a*a+b*b);
+        double mult = sqrt (d / (a*a+b*b));
+        double ax,ay,bx,by;
+        ax = x0 + b * mult;
+        bx = x0 - b * mult;
+        ay = y0 - a * mult;
+        by = y0 + a * mult;
+
+        res.push_back(Point(ax, ay) + Point(cir.x, cir.y));
+        res.push_back(Point(bx, by) + Point(cir.x, cir.y));
+        return res;
+    }
+}
+
+bool order(Point a, Point b, Point c) {
+    return cmp(min(a.x, c.x), b.x) <= 0 && cmp(max(a.x, c.x), b.x) >= 0
+        && cmp(min(a.y, c.y), b.y) <= 0 && cmp(max(a.y, c.y), b.y) >= 0;
+}
+
+double angle(Point a, Point o, Point b) { // min of directed angle AOB & BOA
+    a = a - o; b = b - o;
+    return acos((a * b) / sqrt(a.norm()) / sqrt(b.norm()));
+}
+double solve(Circle O, Point B, Point D) {
+    Point A(B.x, D.y);
+    Point C(D.x, B.y);
+
+    assert(O.contains(A));
+
+    Line ab(A, B);
+    Line ad(A, D);
+
+    auto pp = intersection(ab, O);
+    Point p;
+    for(auto x : pp) if (order(A, x, B)) p = x;
+
+    auto qq = intersection(ad, O);
+    Point q;
+    for(auto x : qq) if (order(A, x, D)) q = x;
+
+    double res = fabs((p - A) % (q - A)) / 2.0;
+
+    res += angle(p, O, q) / 2 * O.r * O.r;
+    res -= fabs((p - O) % (q - O)) / 2.0;
+
+    return res;
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(5);
+    int ntest; cin >> ntest;
+    FOR(test,1,ntest) {
+        Circle O; cin >> O.x >> O.y >> O.r;
+        Point B, D;
+        cin >> B >> D;
+        cout << "Case " << test << ": " << solve(O, B, D) << '\n';
+    }
+}
