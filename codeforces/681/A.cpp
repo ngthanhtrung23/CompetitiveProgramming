d6ad240c8e45a079015c41139f902ee23a95b022
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-06-15 03:23:00 +0800

    Add solution for problem `A - A Good Contest` Link: https://codeforces.com/contest/681/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/681/submission/18477483

---
+++ codeforces/681/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max((double)0.0,x));
+}
+int GI(ll& x) {
+    return scanf("%lld", &x);
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    int n;
+    while (cin >> n) {
+        bool good = false;
+        FOR(i,1,n) {
+            string t; int x, y;
+            cin >> t >> x >> y;
+            if (x >= 2400 && y > x) good = true;
+        }
+        cout << (good ? "YES" : "NO") << endl;
+    }
+}
