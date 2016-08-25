8278277a66d659b395b972056a6d461860fc48fd
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-08-25 18:16:00 +0800

    Add solution for problem `A - Letters Cyclic Shift` Link: https://codeforces.com/contest/708/problem/A Tags: constructive algorithms, greedy, implementation, strings, *1200 Ref: https://codeforces.com/contest/708/submission/20150954

---
+++ codeforces/708/A.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; ++i)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; --i)
+#define REP(i,a) for(int i=0,_a=(a); i < _a; ++i)
+
+#define DEBUG(X) { cout << #X << " = " << (X) << endl; }
+#define PR(A,n)  { cout << #A << " = "; FOR(_,1,n) cout << A[_] << ' '; cout << endl; }
+#define PR0(A,n) { cout << #A << " = "; REP(_,n) cout << A[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int) (x).size())
+using namespace std;
+
+double safe_sqrt(double x) {
+    return sqrt(max((double)0.0,x));
+}
+int GI(ll& x) {
+    return scanf("%lld", &x);
+}
+
+bool alla(string s) {
+    for(char c : s) if (c != 'a') return false;
+    return true;
+}
+
+#undef int
+int main() {
+#define int long long
+    ios :: sync_with_stdio(0); cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    string s;
+    while (cin >> s) {
+        if (alla(s)) {
+            s.back() = 'z';
+        }
+        else {
+            int l = 0; while (s[l] == 'a') ++l;
+            int r = l; while (r < SZ(s) - 1 && s[r+1] != 'a') ++r;
+            FOR(i,l,r) --s[i];
+        }
+        cout << s << endl;
+    }
+}
