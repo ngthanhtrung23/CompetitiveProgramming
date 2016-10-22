51def19aa82f3c041f715bd8f1ff7e2a2d1b9b46
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-10-22 23:10:00 +0800

    Add solution for problem `A - Jumping Ball` Link: https://codeforces.com/contest/725/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/725/submission/21671041

---
+++ codeforces/725/A.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; ++i)
+
+#define DEBUG(X) \
+  { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n)                      \
+  {                                   \
+    cerr << #A << " = ";              \
+    FOR(_, 1, n) cerr << A[_] << ' '; \
+    cerr << endl;                     \
+  }
+#define PR0(A, n)                  \
+  {                                \
+    cerr << #A << " = ";           \
+    REP(_, n) cerr << A[_] << ' '; \
+    cerr << endl;                  \
+  }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+using namespace std;
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(ll& x) { return scanf("%lld", &x); }
+
+#undef int
+int main() {
+#define int long long
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout << (fixed) << setprecision(9);
+  int n;
+  string s;
+  while (cin >> n >> s) {
+    int res = 0;
+    int i = 0;
+    while (i < n && s[i] == '<') ++i, ++res;
+
+    i = n - 1;
+    while (i >= 0 && s[i] == '>') --i, ++res;
+
+    cout << res << endl;
+  }
+}
