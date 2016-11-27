11bdad2fc8def2ada91199e059bd198fcb689a68
Trung Nguyen <ngthanhtrung23@gmail.com>
2016-11-28 00:43:00 +0800

    Add solution for problem `A - Tennis Championship` Link: https://codeforces.com/contest/736/problem/A Tags: combinatorics, constructive algorithms, dfs and similar, dp, math, *1600 Ref: https://codeforces.com/contest/736/submission/22532058

---
+++ codeforces/736/A.cpp
@@ -0,0 +1,52 @@
+
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; ++i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+using namespace std;
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(ll& x) { return scanf("%lld", &x); }
+
+int f[111];
+
+#undef int
+int main() {
+#define int long long
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    f[1] = 2;
+    f[2] = 3;
+    FOR(i,3,100) f[i] = f[i-1] + f[i-2];
+
+    int n;
+    while (cin >> n) {
+        int res = 1;
+        while (n >= f[res+1]) ++res;
+        cout << res << endl;
+    }
+}
