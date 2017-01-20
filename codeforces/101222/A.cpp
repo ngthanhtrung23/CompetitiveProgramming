e2f0b2af858bc337540f1753983d8681fdd917fb
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-01-21 04:49:00 +0800

    Add solution for problem `A - Progress Pie` Link: https://codeforces.com/gym/101222 Tags: Ref: https://codeforces.com/gym/101222/submission/23998481

---
+++ codeforces/101222/A.cpp
@@ -0,0 +1,96 @@
+
+#include <sstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; ++i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+using namespace std;
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(ll& x) { return scanf("%lld", &x); }
+
+const double PI = acos(-1.0);
+
+double DEG_to_RAD(double d) { return d * PI / 180.0; }
+double RAD_to_DEG(double r) { return r * 180.0 / PI; }
+
+struct Point {
+    double x, y;
+    Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+
+    Point operator + (const Point& a) const { return Point(x+a.x, y+a.y); }
+    Point operator - (const Point& a) const { return Point(x-a.x, y-a.y); }
+    Point operator * (double k) const { return Point(x*k, y*k); }
+    Point operator / (double k) const { return Point(x/k, y/k); }
+
+    double operator * (const Point& a) const { return x*a.x + y*a.y; } // dot product
+    double operator % (const Point& a) const { return x*a.y - y*a.x; } // cross product
+
+    Point conj() { return Point(x, -y); }
+    double norm() { return x*x + y*y; }
+
+    // Note: There are 2 ways for implementing len():
+    // 1. sqrt(norm()) --> fast, but inaccurate (produce some values that are of order X^2)
+    // 2. hypot(x, y) --> slow, but much more accurate
+    double len() { return sqrt(norm()); }
+
+    Point rotate(double alpha) {
+        double cosa = cos(alpha), sina = sin(alpha);
+        return Point(x * cosa - y * sina, x * sina + y * cosa);
+    }
+};
+
+double directed_angle(Point a, Point o, Point b) { // angle AOB, in range [0, 2*PI)
+    double t = -atan2(a.y - o.y, a.x - o.x)
+            + atan2(b.y - o.y, b.x - o.x);
+    while (t < 0) t += 2*PI;
+    return t;
+}
+
+#undef int
+int main() {
+#define int long long
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    int ntest; cin >> ntest;
+    FOR(test,1,ntest) {
+        double p; cin >> p;
+        Point A(50, 100);
+        Point O(50, 50);
+        Point B; cin >> B.x >> B.y;
+
+        double alpha = RAD_to_DEG(directed_angle(B, O, A));
+
+        cout << "Case #" << test << ": ";
+        if (alpha / 360.0 * 100 <= p && (B - O).len() <= 50) {
+            cout << "black";
+        }
+        else cout << "white";
+        cout << endl;
+    }
+}
