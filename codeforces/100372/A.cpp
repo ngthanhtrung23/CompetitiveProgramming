4a350514c644135e025a8a71818715390dab1f09
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-01-28 15:57:00 +0800

    Add solution for problem `A - Sergey and reduction (Junior)` Link: https://codeforces.com/gym/100372 Tags: Ref: https://codeforces.com/gym/100372/submission/24216693

---
+++ codeforces/100372/A.cpp
@@ -0,0 +1,83 @@
+
+#include <sstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; ++i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+using namespace std;
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int INP,AM,REACHEOF;
+#define BUFSIZE (1<<12)
+char BUF[BUFSIZE+1], *inp=BUF;
+#define GETCHAR(INP) { \
+    if(!*inp && !REACHEOF) { \
+        memset(BUF,0,sizeof BUF);\
+        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
+        if (inpzzz != BUFSIZE) REACHEOF = true;\
+        inp=BUF; \
+    } \
+    INP=*inp++; \
+}
+#define DIG(a) (((a)>='0')&&((a)<='9'))
+#define GN(j) { \
+    AM=0;\
+    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
+    if (INP=='-') {AM=1;GETCHAR(INP);} \
+    j=INP-'0'; GETCHAR(INP); \
+    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
+    if (AM) j=-j;\
+}
+
+int a[4011];
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    int n, q;
+    GN(n); GN(q);
+    FOR(i,1,n) GN(a[i]);
+
+    while (q--) {
+        int typ; GN(typ);
+        int l; GN(l);
+        int r; GN(r);
+
+        if (typ == 0) {
+            int val; GN(val);
+            FOR(i,l,r) {
+                a[i] -= val;
+                if (a[i] < 0) a[i] = -1;
+            }
+        }
+        else {
+            int res = 0;
+            FOR(i,l,r) if (a[i] <= 0) ++res;
+            printf("%d\n", res);
+        }
+    }
+}
