028d35651a664d4a99bb7b1a7453ce1c6f0fcf8a
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-01-28 16:13:00 +0800

    Add solution for problem `F - Sergey’s columns` Link: https://codeforces.com/gym/100372 Tags: Ref: https://codeforces.com/gym/100372/submission/24216880

---
+++ codeforces/100372/F.cpp
@@ -0,0 +1,62 @@
+
+#include <sstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; ++i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+using namespace std;
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(int& x) { return scanf("%lld", &x); }
+
+int n, k, a[100111];
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    while (GI(n) == 1) {
+        GI(k);
+        FOR(i,1,n) GI(a[i]);
+
+        int l = a[1], r = a[n];
+        if (l > r) swap(l, r);
+
+        int can = min(r - l, k);
+        l += can;
+        k -= can;
+
+        if (n == 1) cout << 0 << endl;
+        else if (n == 2) {
+            if (k % 2 == 1) ++r;
+            cout << r - l << endl;
+        }
+        else {
+            cout << r - l << endl;
+        }
+    }
+}
