0d3bd824039435fd023c7e337b093902b21e56ad
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-02-23 22:47:00 +0800

    Add solution for problem `A - A Serial Killer` Link: https://codeforces.com/contest/776/problem/A Tags: brute force, implementation, strings, *900 Ref: https://codeforces.com/contest/776/submission/24917431

---
+++ codeforces/776/A.cpp
@@ -0,0 +1,64 @@
+
+#include <sstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; ++i)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; --i)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define double long double
+typedef pair<int, int> II;
+#define PI (2 * acos((double)0))
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+#define MS(a,x) memset(a, x, sizeof(a))
+#define next ackjalscjaowjico
+#define prev ajcsoua0wucckjsl
+#define y1 alkscj9u20cjeijc
+#define y0 u9cqu3jioajc
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(int& x) { return scanf("%lld", &x); }
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9);
+    string a, b;
+    while (cin >> a >> b) {
+        int n; cin >> n;
+        cout << a << ' ' << b << '\n';
+        FOR(i,1,n) {
+            string x, y;
+            cin >> x >> y;
+
+            if (a == x) a = y;
+            else b = y;
+            cout << a << ' ' << b << '\n';
+        }
+    }
+}
