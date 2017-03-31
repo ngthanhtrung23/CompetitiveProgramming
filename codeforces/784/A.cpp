a5c7ee65fe586f6820a7d0c35114fda05b80ff0f
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-04-01 03:44:00 +0800

    Add solution for problem `A - Numbers Joke` Link: https://codeforces.com/contest/784/problem/A Tags: *special problem, *1700 Ref: https://codeforces.com/contest/784/submission/26034331

---
+++ codeforces/784/A.cpp
@@ -0,0 +1,62 @@
+
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define double long double
+typedef pair<int, int> II;
+#define PI (2 * acos((double)0))
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+#define MS(a,x) memset(a, x, sizeof(a))
+#define next ackjalscjaowjico
+#define prev ajcsoua0wucckjsl
+#define y1 alkscj9u20cjeijc
+#define left lajcljascjljl
+#define right aucouasocjolkjl
+#define y0 u9cqu3jioajc
+
+#define TWO(X) (1LL<<(X))
+#define CONTAIN(S,X) ((S) & TWO(X))
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(int& x) { return scanf("%lld", &x); }
+
+int a[] = {0,4, 22, 27, 58, 85, 94, 121, 166, 202, 265, 274, 319, 346, 355, 378, 382, 391, 438, 454, 483, 517, 526, 535, 562, 576, 588, 627, 634, 636, 645, 648, 654, 663, 666, 690, 706, 728, 729, 762, 778, 825, 852, 861, 895, 913, 915, 922, 958, 985, 1086, 1111, 1165};
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9) << boolalpha;
+    int n;
+    while (cin >> n) cout << a[n] << endl;
+    return 0;
+}
