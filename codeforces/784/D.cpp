2a9e65355f0cbe3578c84f530d3a1e6155ab9a7f
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-04-01 03:45:00 +0800

    Add solution for problem `D - Touchy-Feely Palindromes` Link: https://codeforces.com/contest/784/problem/D Tags: *special problem, implementation, *1900 Ref: https://codeforces.com/contest/784/submission/26034347

---
+++ codeforces/784/D.cpp
@@ -0,0 +1,82 @@
+
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define double long double
+typedef pair<int, int> II;
+#define PI (2 * acos((double)0))
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+#define MS(a,x) memset(a, x, sizeof(a))
+#define next ackjalscjaowjico
+#define prev ajcsoua0wucckjsl
+#define y1 alkscj9u20cjeijc
+#define left lajcljascjljl
+#define right aucouasocjolkjl
+#define y0 u9cqu3jioajc
+
+#define TWO(X) (1LL<<(X))
+#define CONTAIN(S,X) ((S) & TWO(X))
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(int& x) { return scanf("%lld", &x); }
+
+bool check(string s) {
+    if (SZ(s) % 2 == 1) {
+        char mid = s[SZ(s) / 2];
+        if (mid != '3' && mid != '7') return false;
+    }
+    map<char,char> r;
+
+    r['3'] = '3';
+    r['4'] = '6';
+    r['5'] = '9';
+    r['7'] = '7';
+    r['8'] = '0';
+
+    for(int i = 0, j = SZ(s) - 1; i < j; i++, j--) {
+        if (r[s[i]] == s[j] || r[s[j]] == s[i]) continue;
+        return false;
+    }
+    return true;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9) << boolalpha;
+    string s;
+    while (cin >> s) {
+        cout << (check(s) ? "Yes" : "No") << endl;
+    }
+    return 0;
+}
