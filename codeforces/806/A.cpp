c494f493744ac1e4b6b45215bdbff97de775406c
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-05-08 00:12:00 +0800

    Add solution for problem `A - Success Rate` Link: https://codeforces.com/contest/806/problem/A Tags: binary search, math, *1700 Ref: https://codeforces.com/contest/806/submission/26930667

---
+++ codeforces/806/A.cpp
@@ -0,0 +1,88 @@
+
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define double long double
+typedef pair<int, int> II;
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+#define MS(a,x) memset(a, x, sizeof(a))
+#define next ackjalscjaowjico
+#define prev ajcsoua0wucckjsl
+#define y1 alkscj9u20cjeijc
+#define left lajcljascjljl
+#define right aucouasocjolkjl
+#define y0 u9cqu3jioajc
+
+#define TWO(X) (1LL<<(X))
+#define CONTAIN(S,X) ((S) & TWO(X))
+
+int rand16() {
+    return rand() & (TWO(16) - 1);
+}
+int my_rand() {
+    return rand16() << 15 | rand16();
+}
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(int& x) { return scanf("%lld", &x); }
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9) << boolalpha;
+    int x, y, p, q;
+    int ntest; cin >> ntest;
+    while (ntest--) {
+        cin >> x >> y >> p >> q;
+        if (p == 0) {
+            if (x == 0) cout << 0 << endl;
+            else cout << -1 << endl;
+            continue;
+        }
+        if (p == q) {
+            if (x == y) cout << 0 << endl;
+            else cout << -1 << endl;
+            continue;
+        }
+
+        int minb = max(0LL, (q*x - y*p + p - 1) / p);
+
+        minb = max(minb, (p*y - q*x + q - p - 1) / (q-p));
+
+        int k = (y + minb + q - 1) / q;
+
+        int b = (k * q) - y;
+        cout << b << endl;
+    }
+    return 0;
+}
