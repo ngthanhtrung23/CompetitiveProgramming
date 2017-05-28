73d082c34f9f1dfb525c3fa67602f7e3834f7512
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-05-28 18:12:00 +0800

    Add solution for problem `N - April Fools' Problem (medium)` Link: https://codeforces.com/contest/802/problem/N Tags: binary search, flows, graphs, *2400 Ref: https://codeforces.com/contest/802/submission/27408702

---
+++ codeforces/802/N.cpp
@@ -0,0 +1,192 @@
+
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { cerr << #X << " = " << (X) << endl; }
+#define PR(A, n) { cerr << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+#define double long double
+typedef pair<int, int> II;
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+#define MS(a,x) memset(a, x, sizeof(a))
+#define next ackjalscjaowjico
+#define prev ajcsoua0wucckjsl
+#define y1 alkscj9u20cjeijc
+#define left lajcljascjljl
+#define right aucouasocjolkjl
+#define y0 u9cqu3jioajc
+
+#define TWO(X) (1LL<<(X))
+#define CONTAIN(S,X) ((S) & TWO(X))
+
+int rand16() {
+    return rand() & (TWO(16) - 1);
+}
+int my_rand() {
+    return rand16() << 15 | rand16();
+}
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(int& x) { return scanf("%lld", &x); }
+
+const int MN = 5011;
+int n, k;
+int a[MN], b[MN];
+
+template<class Flow=int, class Cost=int>
+struct MinCostFlow {
+    const Flow INF_FLOW = 1000111000;
+    const Cost INF_COST = 1000111000111000LL;
+
+    int n, t, S, T;
+    Flow totalFlow;
+    Cost totalCost;
+    vector<int> last, visited;
+    vector<Cost> dis;
+    struct Edge {
+        int to;
+        Flow cap;
+        Cost cost;
+        int next;
+        Edge(int to, Flow cap, Cost cost, int next) :
+                to(to), cap(cap), cost(cost), next(next) {}
+    };
+    vector<Edge> edges;
+
+    MinCostFlow(int n) : n(n), t(0), totalFlow(0), totalCost(0), last(n, -1), visited(n, 0), dis(n, 0) {
+        edges.clear();
+    }
+
+    int addEdge(int from, int to, Flow cap, Cost cost) {
+        edges.push_back(Edge(to, cap, cost, last[from]));
+        last[from] = t++;
+        edges.push_back(Edge(from, 0, -cost, last[to]));
+        last[to] = t++;
+        return t - 2;
+    }
+
+    pair<Flow, Cost> minCostFlow(int _S, int _T) {
+        S = _S; T = _T;
+        SPFA();
+        while (1) {
+            while (1) {
+                REP(i,n) visited[i] = 0;
+                if (!findFlow(S, INF_FLOW)) break;
+            }
+            if (!modifyLabel()) break;
+        }
+        return make_pair(totalFlow, totalCost);
+    }
+
+private:
+    void SPFA() {
+        REP(i,n) dis[i] = INF_COST;
+        priority_queue< pair<Cost,int> > Q;
+        Q.push(make_pair(dis[S]=0, S));
+        while (!Q.empty()) {
+            int x = Q.top().second;
+            Cost d = -Q.top().first;
+            Q.pop();
+            // For double: dis[x] > d + EPS
+            if (dis[x] != d) continue;
+            for(int it = last[x]; it >= 0; it = edges[it].next)
+                if (edges[it].cap > 0 && dis[edges[it].to] > d + edges[it].cost)
+                    Q.push(make_pair(-(dis[edges[it].to] = d + edges[it].cost), edges[it].to));
+        }
+        Cost disT = dis[T]; REP(i,n) dis[i] = disT - dis[i];
+    }
+
+    Flow findFlow(int x, Flow flow) {
+        if (x == T) {
+            totalCost += dis[S] * flow;
+            totalFlow += flow;
+            return flow;
+        }
+        visited[x] = 1;
+        Flow now = flow;
+        for(int it = last[x]; it >= 0; it = edges[it].next)
+            // For double: fabs(dis[edges[it].to] + edges[it].cost - dis[x]) < EPS
+            if (edges[it].cap && !visited[edges[it].to] && dis[edges[it].to] + edges[it].cost == dis[x]) {
+                Flow tmp = findFlow(edges[it].to, min(now, edges[it].cap));
+                edges[it].cap -= tmp;
+                edges[it ^ 1].cap += tmp;
+                now -= tmp;
+                if (!now) break;
+            }
+        return flow - now;
+    }
+
+    bool modifyLabel() {
+        Cost d = INF_COST;
+        REP(i,n) if (visited[i])
+            for(int it = last[i]; it >= 0; it = edges[it].next)
+                if (edges[it].cap && !visited[edges[it].to])
+                    d = min(d, dis[edges[it].to] + edges[it].cost - dis[i]);
+
+        // For double: if (d > INF_COST / 10)     INF_COST = 1e20
+        if (d == INF_COST) return false;
+        REP(i,n) if (visited[i])
+            dis[i] += d;
+        return true;
+    }
+};
+
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9) << boolalpha;
+    while (cin >> n >> k) {
+        FOR(i,1,n) cin >> a[i];
+        FOR(i,1,n) cin >> b[i];
+
+        MinCostFlow<int,int> flow(n*2 + 5);
+
+        int sink = n+n+1;
+        int source = n+n+2;
+        flow.addEdge(source, 0, k, 0);
+        FOR(i,1,n) {
+            flow.addEdge(0, i, 1, a[i]);
+        }
+
+        FOR(i,1,n) flow.addEdge(i, i+n, 1, 0);
+
+        FOR(i,n+2,n+n) {
+            flow.addEdge(i-1, i, n, 0);
+        }
+
+        FOR(i,1,n) flow.addEdge(i+n, sink, 1, b[i]);
+
+        auto t = flow.minCostFlow(source, sink);
+        assert(t.first == k);
+        cout << t.second << endl;
+    }
+    return 0;
+}
