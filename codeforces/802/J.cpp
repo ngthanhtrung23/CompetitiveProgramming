72b2c5f5b95ea0995a2b01edd2374672ea8843b8
Trung Nguyen <ngthanhtrung23@gmail.com>
2017-05-28 16:30:00 +0800

    Add solution for problem `J - Send the Fool Further! (easy)` Link: https://codeforces.com/contest/802/problem/J Tags: dfs and similar, graphs, trees, *1400 Ref: https://codeforces.com/contest/802/submission/27404506

---
+++ codeforces/802/J.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ms(s, n) memset(s, n, sizeof(s))
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORd(i, a, b) for (int i = (a) - 1; i >= (b); i--)
+#define FORall(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define sz(a) int((a).size())
+#define present(t, x) (t.find(x) != t.end())
+#define all(a) (a).begin(), (a).end()
+#define uni(a) (a).erase(unique(all(a)), (a).end())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fi first
+#define se second
+#define prec(n) fixed<<setprecision(n)
+#define bit(n, i) (((n) >> (i)) & 1)
+#define bitcount(n) __builtin_popcountll(n)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vii;
+const int MOD = (int) 1e9 + 7;
+const int INF = (int) 1e9;
+const ll LINF = (ll) 1e18;
+const ld PI = acos((ld) -1);
+const ld EPS = 1e-9;
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+inline ll fpow(ll n, ll k, int p = MOD) {ll r = 1; for (; k; k >>= 1) {if (k & 1) r = r * n % p; n = n * n % p;} return r;}
+template<class T> inline int chkmin(T& a, const T& val) {return val < a ? a = val, 1 : 0;}
+template<class T> inline int chkmax(T& a, const T& val) {return a < val ? a = val, 1 : 0;}
+inline ll isqrt(ll k) {ll r = sqrt(k) + 1; while (r * r > k) r--; return r;}
+inline ll icbrt(ll k) {ll r = cbrt(k) + 1; while (r * r * r > k) r--; return r;}
+inline void addmod(int& a, int val, int p = MOD) {if ((a = (a + val)) >= p) a -= p;}
+inline void submod(int& a, int val, int p = MOD) {if ((a = (a - val)) < 0) a += p;}
+inline int mult(int a, int b, int p = MOD) {return (ll) a * b % p;}
+inline int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
+inline int sign(ld x) {return x < -EPS ? -1 : x > +EPS;}
+inline int sign(ld x, ld y) {return sign(x - y);}
+#define db(x) cerr << #x << " = " << (x) << ", ";
+#define endln cerr << "\n";
+#define chkpt cerr << "-----\n";
+
+const int maxn = 100 + 5;
+int n;
+vii adj[maxn];
+
+pi best;
+void dfs(int u, int p, int d) {
+    chkmax(best, mp(d, u));
+    FOR(i, 0, sz(adj[u])) {
+        int v = adj[u][i].fi;
+        int w = adj[u][i].se;
+        if (v != p) {
+            dfs(v, u, d + w);
+        }
+    }
+}
+
+void solve() {
+    cin >> n;
+    FOR(i, 0, n - 1) {
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].pb(mp(v, w));
+        adj[v].pb(mp(u, w));
+    }
+    best = mp(0, 0);
+    dfs(0, -1, 0);
+    cout << best.fi << "\n";
+}
+
+int main() {
+    int JUDGE_ONLINE = 1;
+    if (fopen("in.txt", "r")) {
+        JUDGE_ONLINE = 0;
+        assert(freopen("in.txt", "r", stdin));
+        //assert(freopen("out.txt", "w", stdout));
+    }
+    else {
+        ios_base::sync_with_stdio(0), cin.tie(0);
+    }
+    solve();
+    if (!JUDGE_ONLINE) {
+        //cout << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    }
+    return 0;
+}
