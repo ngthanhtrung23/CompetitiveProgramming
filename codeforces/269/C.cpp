9caf32626a4a19ad713b3f487f30bc4c46b365a8
Trung Nguyen <ngthanhtrung23@gmail.com>
2018-10-05 13:52:00 +0800

    Add solution for problem `C - Flawed Flow` Link: https://codeforces.com/contest/269/problem/C Tags: constructive algorithms, flows, graphs, greedy, *2100 Ref: https://codeforces.com/contest/269/submission/43816790

--- codeforces/269/C.cpp
+++ codeforces/269/C.cpp
@@ -41,89 +41,129 @@ long long my_rand() {
 double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
 int GI(long long& x) { return scanf("%lld", &x); }
 
-template<typename flow_t = long long>
-struct PushRelabel {
-    struct Edge {
-        int to, rev;
-        flow_t f, c;
+template<typename flow_t = int, typename excess_t = long long>
+struct Flow{
+    struct Edge{
+        int to, next; flow_t f;
+        void set(int _to, flow_t _f, int _n){
+            tie(to, next, f)=make_tuple(_to, _n, _f);
+        }
     };
-    vector<vector<Edge> > g;
-    PushRelabel(int n) : g(n), ec(n), cur(n), hs(2*n), H(n) {}
-
-    int addEdge(int s, int t, flow_t cap, flow_t rcap=0) {
-        if (s == t) return -1;
-        Edge a = {t, (int)g[t].size(), 0, cap};
-        Edge b = {s, (int)g[s].size(), 0, rcap};
-        g[s].push_back(a);
-        g[t].push_back(b);
-
-        // Return ID of forward edge.
-        return b.rev;
+    int n, ledge, s, t, *fe, *h, *hp, *cnt, htop;
+    Edge* ed;
+    excess_t *ex, flow;
+    bool *isq;
+    int add_edge(int a, int b, flow_t cap, flow_t rcap = flow_t(0)){
+        assert(a>0&&b>0&&a<n&&b<n);
+        if(a==b) return -1;
+        ed[ledge].set(a, rcap, fe[b]);
+        fe[b] = ledge++;
+        ed[ledge].set(b, cap, fe[a]);
+        fe[a] = ledge++;
+        
+        return ledge-1;
     }
-
-    flow_t maxFlow(int s, int t) {
-        int v = g.size();
-        H[s] = v;
-        ec[t] = 1;
-        vector<int> co(2*v);
-        co[0] = v-1;
-        for (int i = 0; i < v; ++i) {
-            cur[i] = g[i].data();
-        }
-        for (auto &e : g[s]) {
-            add_flow(e, e.c);
-        }
-        if (hs[0].size()) {
-            for (int hi = 0; hi >= 0;) {
-                int u = hs[hi].back();
-                hs[hi].pop_back();
-                while (ec[u] > 0) { // discharge u
-                    if (cur[u] == g[u].data() + g[u].size()) {
-                        H[u] = 1e9;
-                        for (auto &e : g[u]) {
-                            if (e.c && H[u] > H[e.to]+1) {
-                                H[u] = H[e.to]+1;
-                                cur[u] = &e;
-                            }
-                        }
-                        if (++co[H[u]], !--co[hi] && hi < v) {
-                            for (int i = 0; i < v; ++i) {
-                                if (hi < H[i] && H[i] < v){
-                                    --co[H[i]];
-                                    H[i] = v + 1;
-                                }
-                            }
-                        }
-                        hi = H[u];
-                    } else if (cur[u]->c && H[u] == H[cur[u]->to] + 1) {
-                        add_flow(*cur[u], min(ec[u], cur[u]->c));
-                    }
-                    else {
-                        ++cur[u];
-                    }
+    void push(int u, int v, int p) {
+        flow_t d = ex[u] < ed[p].f ? ex[u] : ed[p].f;
+        ed[p].f -= d, ed[p^1].f += d;
+        ex[u] -= d, ex[v] += d;
+    }
+    inline void relabel(int u) {
+        if(--cnt[h[u]]){
+            int min_h = 2*n;
+            for (int p = fe[u]; p; p = ed[p].next)
+                if (ed[p].f > 0 && min_h > h[ed[p].to])
+                    min_h = h[ed[p].to];
+            ++cnt[h[u] = min_h + 1];
+        } else {//gap
+            for(int i=1;i<n;++i){
+                if(i!=s && h[i]>h[u]){
+                    --cnt[h[i]];
+                    ++cnt[h[i]=h[s]+1];
                 }
-                while (hi>=0 && hs[hi].empty()) --hi;
+            }
+            ++cnt[h[u]=h[s]+1];
+        }
+    }
+    void hpush(int u) {
+        int i;
+        for (i = ++htop; h[hp[i >> 1]] < h[u]; i >>= 1)
+            hp[i] = hp[i >> 1];
+        hp[i] = u;
+    }
+    void hpop() {
+        int last = hp[htop--], i, ch;
+        for (i = 1; (i << 1) <= htop; i = ch) {
+            ch = i << 1;
+            if (ch != htop && h[hp[ch]] < h[hp[ch + 1]]) ++ch;
+            if (h[last] >= h[hp[ch]]) break;
+            else  hp[i] = hp[ch];
+        }
+        hp[i] = last;
+    }
+    inline void init() {
+        fill(h, h + n, n+2);
+        fill(cnt, cnt + 2*n + 2, 0);
+        h[hp[0]=0] = ~0u >> 1;
+        int f = 1, b = 1;
+        cnt[h[s] = n+1] = cnt[h[t] = 0] = 1;
+        hp[b++] = t;
+        while (f < b) {
+            int u = hp[f++];
+            for (int p = fe[u]; p; p = ed[p].next) {
+                int v = ed[p].to;
+                if (h[v] == n+2 && ed[p^1].f > 0) {
+                    ++cnt[h[v] = h[u] + 1];
+                    hp[b++] = v;
+                }
             }
         }
-        return -ec[s];
-    }
-
-private:
-    vector<flow_t> ec;
-    vector<Edge*> cur;
-    vector<vector<int> > hs;
-    vector<int> H;
-
-    void add_flow(Edge& e, flow_t f) {
-        Edge &back = g[e.to][e.rev];
-        if (!ec[e.to] && f) {
-            hs[H[e.to]].push_back(e.to);
+        flow = htop = 0;
+        fill(ex, ex + n, 0);
+        fill(isq, isq + n, 0);
+        ex[s] = ~0llU >> 1;
+        for (int p = fe[s]; p != 0; p = ed[p].next) {
+            if (ed[p].f > 0) {
+                int v = ed[p].to;
+                push(s, v, p);
+                if(!isq[v]) {
+                    hpush(v);
+                    isq[v] = true;
+                }
+            }
         }
-        e.f += f; e.c -= f;
-        ec[e.to] += f;
-        back.f -= f; back.c += f;
-        ec[back.to] -= f;
     }
+    excess_t max_flow() {
+        init();
+        while (htop) {
+            int u = hp[1];
+            if (u == t) {
+                hpop();
+            } else {
+                for (int p = fe[u]; p; p = ed[p].next) {
+                    int v = ed[p].to;
+                    if (ed[p].f > 0 && h[u] == h[v] + 1) {
+                        push(u, v, p);
+                        if (isq[v] == false) {
+                            if (v != s && v != t)
+                                hpush(v);
+                            isq[v] = true;
+                        }
+                        if (ex[u] == 0) {
+                            hpop();
+                            isq[u] = false;
+                            break;
+                        }
+                    }
+                }
+                if (ex[u]) relabel(u);
+            }
+        }
+        return ex[t];
+    }
+    Flow(int N, int M, int S, int T):n(N+1), ledge(2), s(S), t(T), fe(new int[n]), h(new int[n]), hp(new int[n+2]), cnt(new int[2*n+2]), htop(0), ed(new Edge[2*M+2]), ex(new excess_t[n]), isq(new bool[n]){fill(fe, fe+n, 0);}
+    Flow(int N, int M):Flow(N+2, M, N+1, N+2){}
+    ~Flow(){delete[] fe, delete[]h, delete[]hp, delete[]cnt, delete[]ed, delete[]ex; delete[]isq;}
 };
 
 int INP,AM,REACHEOF;
@@ -150,8 +190,8 @@ char BUF[BUFSIZE+1], *inp=BUF;
 }
 
 struct E {
-    int from;
     int eid;
+    int cap;
 };
 
 E forwardEdges[200111];
@@ -162,23 +202,22 @@ int32_t main() {
     cout << (fixed) << setprecision(9) << boolalpha;
     int n, m;
     GN(n); GN(m);
-    PushRelabel<int> flow(n);
+    Flow<int,int> flow(n, m, 1, n);
     REP(i,m) {
         int u, v, f;
         GN(u); GN(v); GN(f);
 
-        forwardEdges[i].from = u - 1;
-        forwardEdges[i].eid = flow.addEdge(u-1, v-1, f, f);
+        forwardEdges[i].eid = flow.add_edge(u, v, f, f);
+        forwardEdges[i].cap = f;
     }
 
-    int res = flow.maxFlow(0, n-1);
-    // DEBUG(res);
+    int res = flow.max_flow();
+    DEBUG(res);
 
     REP(i,m) {
-        int u = forwardEdges[i].from;
         int eid = forwardEdges[i].eid;
 
-        if (flow.g[u][eid].f >= 0) puts("0");
+        if (flow.ed[eid].f <= forwardEdges[i].cap) puts("0");
         else puts("1");
     }
 
