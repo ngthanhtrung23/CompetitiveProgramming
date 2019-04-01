8c494ef7fc4fd958187c1fb8c3a922f5e09d3d3a
Trung Nguyen <ngthanhtrung23@gmail.com>
2019-04-02 01:39:00 +0800

    Add solution for problem `F - Neat Words` Link: https://codeforces.com/contest/1145/problem/F Tags: Ref: https://codeforces.com/contest/1145/submission/52206677

---
+++ codeforces/1145/F.cpp
@@ -0,0 +1,81 @@
+
+/*
+#pragma GCC optimize("O3")
+#ifdef ONLINE_JUDGE
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#endif
+#pragma GCC optimize("unroll-loops")
+*/
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { auto _X = (X); cerr << "L" << __LINE__ << ": " << #X << " = " << (_X) << endl; }
+#define PR(A, n) { cerr << "L" << __LINE__ << ": " << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << "L" << __LINE__ << ": " << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define sqr(x) ((x) * (x))
+#define ll long long
+// On CF, GNU C++ seems to have some precision issues with long double?
+// #define double long double
+typedef pair<int, int> II;
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+#define MS(a,x) memset(a, x, sizeof(a))
+#define stat akjcjalsjcjalscj
+#define hash ajkscjlsjclajsc
+#define next ackjalscjaowjico
+#define prev ajcsoua0wucckjsl
+#define y1 alkscj9u20cjeijc
+#define left lajcljascjljl
+#define right aucouasocjolkjl
+#define y0 u9cqu3jioajc
+
+#define TWO(X) (1LL<<(X))
+#define CONTAIN(S,X) (((S) >> (X)) & 1)
+
+long long rand16() {
+    return rand() & (TWO(16) - 1);
+}
+long long my_rand() {
+    return rand16() << 32 | rand16() << 16 | rand16();
+}
+
+double safe_sqrt(double x) { return sqrt(max((double)0.0, x)); }
+int GI(long long& x) { return scanf("%lld", &x); }
+
+const string straight = "AEFHIKLMNTVWXYZ";
+
+bool isCurve(const string& s) {
+    for (char c : s) {
+        if (straight.find(c) != string::npos) return false;
+    }
+    return true;
+}
+bool isStraight(const string& s) {
+    for (char c : s) {
+        if (straight.find(c) == string::npos) return false;
+    }
+    return true;
+}
+
+bool check(string s) {
+    return isCurve(s) || isStraight(s);
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9) << boolalpha;
+    string s;
+    while (cin >> s) {
+        cout << (check(s) ? "YES" : "NO") << endl;
+    }
+    return 0;
+}
