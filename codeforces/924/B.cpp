006ed8638a2df13ece6ea1d5cb3688cd5c4368d4
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-02-28 23:39:00 +0800

    Add solution for problem `B - Three-level Laser` Link: https://codeforces.com/contest/924/problem/B Tags: binary search, greedy, two pointers, *1600 Ref: https://codeforces.com/contest/924/submission/72046588

---
+++ codeforces/924/B.cpp
@@ -0,0 +1,44 @@
+
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define REPD(i,n) for(int i = (n)-1; i >= 0; --i)
+
+#define DEBUG(X) { auto _X = (X); cerr << "L" << __LINE__ << ": " << #X << " = " << (_X) << endl; }
+#define PR(A, n) { cerr << "L" << __LINE__ << ": " << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << "L" << __LINE__ << ": " << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+
+int a[100111];
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << (fixed) << setprecision(9) << boolalpha;
+    int n, u;
+    while (cin >> n >> u) {
+        FOR(i,1,n) cin >> a[i];
+        double res = -1.0;
+
+        int k = 1;
+        FOR(i,1,n-2) {
+            while (k < n && a[k+1] - a[i] <= u) ++k;
+
+            if (k >= i+2) {
+                double tu = a[k] - a[i+1];
+                double mau = a[k] - a[i];
+                res = max(res, tu / mau);
+            }
+        }
+
+        cout << res << '\n';
+    }
+    return 0;
+}
