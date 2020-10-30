210b00ca97cf090a35aa0933ffcac72c71b8ebb2
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-10-30 17:07:00 +0800

    Add solution for problem `A - Marketing Scheme` Link: https://codeforces.com/contest/1437/problem/A Tags: brute force, constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1437/submission/97153503

---
+++ codeforces/1437/A.cpp
@@ -0,0 +1,29 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+using namespace std;
+
+#define int long long
+#define FOR(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
+#define FORD(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+ 
+#define DEBUG(X) { auto _X = (X); cerr << "L" << __LINE__ << ": " << #X << " = " << (_X) << endl; }
+#define PR(A, n) { cerr << "L" << __LINE__ << ": " << #A << " = "; FOR(_, 1, n) cerr << A[_] << ' '; cerr << endl; }
+#define PR0(A, n) { cerr << "L" << __LINE__ << ": " << #A << " = "; REP(_, n) cerr << A[_] << ' '; cerr << endl; }
+ 
+#define __builtin_popcount __builtin_popcountll
+#define SZ(x) ((int)(x).size())
+#define ALL(a) (a).begin(), (a).end()
+inline int gcd(int a, int b) {int r; while (b) {r = a % b; a = b; b = r;} return a;}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    int ntest; cin >> ntest;
+    while (ntest--) {
+        int l, r; cin >> l >> r;
+        cout << (2*l > r ? "YES" : "NO") << '\n';
+    }
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
