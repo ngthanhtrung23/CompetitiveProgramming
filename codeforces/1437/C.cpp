9c8b43f862df7f7b2c692d47c67b2f52632941f1
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-10-30 17:27:00 +0800

    Add solution for problem `C - Chef Monocarp` Link: https://codeforces.com/contest/1437/problem/C Tags: dp, flows, graph matchings, greedy, math, sortings, *1800 Ref: https://codeforces.com/contest/1437/submission/97154787

--- codeforces/1437/C.cpp
+++ codeforces/1437/C.cpp
@@ -17,125 +17,118 @@ using namespace std;
 #define ALL(a) (a).begin(), (a).end()
 inline int gcd(int a, int b) {int r; while (b) {r = a % b; a = b; b = r;} return a;}
 
-const int INF = 1000111000;
-class PerfectMatchingMinCost {
-private:
-    typedef vector<int> VI;
-    typedef vector<VI> VII;
+const int MN = 444;
+const int inf = 1000111000;
+#define arg __arg
+long long c[MN][MN];
+long long fx[MN], fy[MN];
+int mx[MN], my[MN];
+int trace[MN], qu[MN], arg[MN];
+long long d[MN];
+int front, rear, start, finish;
+int N;
 
-    int N;
-    VII cost, adj;
-    VI d, fx, fy, mx, my, arg, trace;
-
-    queue<int> q;
-
-    int getCost(int x, int y) {
-        return cost[x][y] - fx[x] - fy[y];
+void init() {
+    FOR(i,1,N) {
+        fy[i] = mx[i] = my[i] = 0;
+        FOR(j,1,N) c[i][j] = inf;
     }
+}
 
-    void initBFS(int start) {
-        for (int i = 0; i < N; i++)
-            trace[i] = -1;
-        while (!q.empty()) q.pop();
-        q.push(start);
-        for (int i = 0; i < N; i++)
-            d[i] = getCost(start, i), arg[i] = start;
+void addEdge(int i, int j, long long cost) {
+    c[i][j] = min(c[i][j], cost);
+}
+
+inline long long getC(int i, int j) {
+    return c[i][j] - fx[i] - fy[j];
+}
+
+void initBFS() {
+    front = rear = 1;
+    qu[1] = start;
+    memset(trace, 0, sizeof trace);
+    FOR(j,1,N) {
+        d[j] = getC(start, j);
+        arg[j] = start;
     }
+    finish = 0;
+}
 
-    int findPath() {
-        while (!q.empty()) {
-            int x = q.front(); q.pop();
-            for (int i = 0; i < (int) adj[x].size(); i++) {
-                int y = adj[x][i];
-                if (trace[y] == -1) {
-                    int w = getCost(x, y);
-                    if (w == 0) {
-                        trace[y] = x;
-                        if (my[y] == -1) 
-                            return y;
-                        q.push(my[y]);
-                    }
-                    if (d[y] > w) {
-                        d[y] = w;
-                        arg[y] = x;
-                    }
+void findAugPath() {
+    while (front <= rear) {
+        int i = qu[front++];
+        FOR(j,1,N) if (!trace[j]) {
+            long long w = getC(i, j);
+            if (!w) {
+                trace[j] = i;
+                if (!my[j]) {
+                    finish = j;
+                    return ;
                 }
+                qu[++rear] = my[j];
+            }
+            if (d[j] > w) {
+                d[j] = w;
+                arg[j] = i;
             }
         }
-        return -1;
     }
+}
 
-    int update(int start) {
-        int delta = INF;
-        for (int y = 0; y < N; y++)
-            if (trace[y] == -1)
-                delta = min(delta, d[y]);   
-        fx[start] += delta;
-        for (int y = 0; y < N; y++)
-            if (trace[y] != -1) {
-                int x = my[y];
-                fx[x] += delta;
-                fy[y] -= delta;
-            } 
-            else d[y] -= delta;
-        for (int y = 0; y < N; y++)
-            if (trace[y] == -1 && d[y] == 0) {
-                trace[y] = arg[y];
-                if (my[y] == -1) 
-                    return y;
-                q.push(my[y]);
-            }
-        return -1;
-    }
+void subx_addy() {
+    long long delta = inf;
+    FOR(j,1,N)
+        if (trace[j] == 0 && d[j] < delta) delta = d[j];
 
-    void enlarge(int finish) {
-        for (int y = finish; y != -1; ) {
-            int x = trace[y];
-            int yy = mx[x];
-            mx[x] = y;
-            my[y] = x;
-            y = yy;
+    // xoay
+    fx[start] += delta;
+    FOR(j,1,N)
+        if (trace[j]) {
+            int i = my[j];
+            fy[j] -= delta;
+            fx[i] += delta;
+        }
+        else d[j] -= delta;
+
+    FOR(j,1,N)
+        if (!trace[j] && !d[j]) {
+            trace[j] = arg[j];
+            if (!my[j]) { finish = j; return ; }
+            qu[++rear] = my[j];
+        }
+}
+
+void enlarge() {
+    do {
+        int i = trace[finish];
+        int next = mx[i];
+        mx[i] = finish;
+        my[finish] = i;
+        finish = next;
+    } while (finish);
+}
+
+int mincost() {
+    FOR(i,1,N) fx[i] = *min_element(c[i]+1, c[i]+N+1);
+    FOR(j,1,N) {
+        fy[j] = c[1][j] - fx[1];
+        FOR(i,1,N) {
+            fy[j] = min(fy[j], c[i][j] - fx[i]);
         }
     }
-
-public:
-    PerfectMatchingMinCost(int n = 0) {
-        N = n;
-        cost = VII(n, VI(n, INF));
-        adj = VII(n);
-
-        trace = VI(n);
-        arg = VI(n);
-        fx = VI(n, -INF);
-        fy = VI(n);
-        d = VI(n);
-        mx = VI(n, -1);
-        my = VI(n, -1);
-    }
-
-    void AddEdge(int x, int y, int c) {
-        if (cost[x][y] == INF) adj[x].push_back(y);
-        if (cost[x][y] > c) cost[x][y] = c;
-    }
-
-    int GetMinCost() {
-        for (int x = 0; x < N; x++) {
-            initBFS(x);
-            int finish = -1;
-            do {                
-                finish = findPath();
-                if (finish != -1) break;
-                finish = update(x);
-            } while (finish == -1);
-            enlarge(finish);
+    FOR(i,1,N) {
+        start = i;
+        initBFS();
+        while (finish == 0) {
+            findAugPath();
+            if (!finish) subx_addy();
         }
-        int ret = 0;
-        for (int x = 0; x < N; x++) 
-            ret += cost[x][mx[x]];
-        return ret;
+        enlarge();
     }
-};
-
+    int res = 0;
+    FOR(i,1,N) res += c[i][mx[i]];
+    return res;
+}
 
 
 int32_t main() {
@@ -147,12 +140,13 @@ int32_t main() {
         for (auto& val : a) cin >> val;
         sort(ALL(a));
 
-        PerfectMatchingMinCost matching(2*n);
-        REP(i,n) FOR(j,1,2*n) {
-            matching.AddEdge(i, j-1, llabs(a[i] - j));
+        N = 2*n;
+        init();
+        REP(i,n) FOR(j,1,N) {
+            addEdge(i+1, j, llabs(a[i] - j));
         }
-
-        cout << matching.GetMinCost() - n * INF << endl;
+        int t = mincost();
+        cout << t - n * inf << endl;
     }
     cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
     return 0;
